95b6ac434e32284e6145cfdbebf5f5cbbee9bae9
jraragon <jra@mega.co.nz>
2014-01-19 00:18:49 +0100

    File list view Completed GUI file listView (included ImageButton) To-do: implement onClick(ImageButton) and onClickItem (ListView)

--- MegaCOM/src/com/mega/MegaBrowserAdapter.java
+++ MegaCOM/src/com/mega/MegaBrowserAdapter.java
@@ -3,12 +3,16 @@ package com.mega;
 import java.util.List;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MegaBrowserAdapter extends BaseAdapter {
 	
@@ -23,11 +27,16 @@ public class MegaBrowserAdapter extends BaseAdapter {
 	/*private view holder class*/
     private class ViewHolder {
         ImageView imageView;
-        TextView textView;
+        TextView textViewFileName;
+        TextView textViewFileSize;
+        TextView textViewUpdated;
+        ImageButton imageButtonThreeDots;
     }
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+	
+		final int _position = position;
 		
 		ViewHolder holder = null;
 		
@@ -36,7 +45,10 @@ public class MegaBrowserAdapter extends BaseAdapter {
 			convertView = inflater.inflate(R.layout.item_file_list, parent, false);
 			holder = new ViewHolder();
 			holder.imageView = (ImageView) convertView.findViewById(R.id.file_list_thumbnail);
-			holder.textView = (TextView) convertView.findViewById(R.id.file_list_text);
+			holder.textViewFileName = (TextView) convertView.findViewById(R.id.file_list_filename);
+			holder.textViewFileSize = (TextView) convertView.findViewById(R.id.file_list_filesize);
+			holder.textViewUpdated = (TextView) convertView.findViewById(R.id.file_list_updated);
+			holder.imageButtonThreeDots = (ImageButton) convertView.findViewById(R.id.file_list_three_dots);
 			convertView.setTag(holder);
 		}
 		else{
@@ -45,8 +57,20 @@ public class MegaBrowserAdapter extends BaseAdapter {
 		
 		ItemFileListBrowser rowItem = (ItemFileListBrowser) getItem(position);
 		
-		holder.textView.setText(rowItem.getName());
+		holder.textViewFileName.setText(rowItem.getName());
+		holder.textViewFileSize.setText("100 KB");
+		holder.textViewUpdated.setText("Updated: 1 week ago");
 		holder.imageView.setImageResource(rowItem.getImageId());
+		
+		holder.imageButtonThreeDots.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							String cadena = "ImageButton " + (_position + 1) + " clicked";
+	                        Toast toast = Toast.makeText( context, cadena,Toast.LENGTH_LONG);
+	                        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+	                        toast.show();
+						}
+					});
 		
 		return convertView;
 	}
@@ -64,5 +88,5 @@ public class MegaBrowserAdapter extends BaseAdapter {
     @Override
     public long getItemId(int position) {
         return rowItems.indexOf(getItem(position));
-    }
+    }    
 }
