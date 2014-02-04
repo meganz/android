c9ad0dea251f09ac7f1a3ab0f8ae9d544016996f
jraragon <jra@mega.co.nz>
2014-02-04 16:17:00 +0100

    GridView except three-dots button action Grid View of the File list completed. Next step now is to add the action of the three-dots button (in the same way as in the File list ListView)

--- MegaCOM/src/com/mega/android/MegaBrowserGridAdapter.java
+++ MegaCOM/src/com/mega/android/MegaBrowserGridAdapter.java
@@ -3,24 +3,14 @@ package com.mega.android;
 import java.util.List;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.opengl.Visibility;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.TypedValue;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,18 +29,22 @@ public class MegaBrowserGridAdapter extends BaseAdapter {
 	
 	/*private view holder class*/
     private class ViewHolder {
-        ImageView imageView;
-        TextView textViewFileName;
-        TextView textViewFileSize;
-        TextView textViewUpdated;
-        ImageButton imageButtonThreeDots;
-        RelativeLayout itemLayout;
-        ImageView arrowSelection;
-        RelativeLayout optionsLayout;
-        ImageButton optionOpen;
-        ImageButton optionProperties;
-        ImageButton optionDownload;
-        ImageButton optionDelete;
+        ImageView imageView1;
+        TextView textViewFileName1;
+        RelativeLayout itemLayout1;
+        ImageView imageView2;
+        TextView textViewFileName2;
+        RelativeLayout itemLayout2;
+        TextView textViewFileSize1;
+        TextView textViewFileSize2;
+        ImageButton imageButtonThreeDots1;
+        ImageButton imageButtonThreeDots2;
+//        ImageView arrowSelection;
+//        RelativeLayout optionsLayout;
+//        ImageButton optionOpen;
+//        ImageButton optionProperties;
+//        ImageButton optionDownload;
+//        ImageButton optionDelete;
     }
 
 	@Override
@@ -61,95 +55,126 @@ public class MegaBrowserGridAdapter extends BaseAdapter {
 		ViewHolder holder = null;
 		
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.item_file_list, parent, false);
-			holder = new ViewHolder();
-			holder.itemLayout = (RelativeLayout) convertView.findViewById(R.id.file_list_item_layout);
-			holder.imageView = (ImageView) convertView.findViewById(R.id.file_list_thumbnail);
-			holder.textViewFileName = (TextView) convertView.findViewById(R.id.file_list_filename);
-			holder.textViewFileSize = (TextView) convertView.findViewById(R.id.file_list_filesize);
-			holder.textViewUpdated = (TextView) convertView.findViewById(R.id.file_list_updated);
-			holder.imageButtonThreeDots = (ImageButton) convertView.findViewById(R.id.file_list_three_dots);
-			holder.optionsLayout = (RelativeLayout) convertView.findViewById(R.id.file_list_options);
-			holder.optionOpen = (ImageButton) convertView.findViewById(R.id.file_list_option_open);
-			holder.optionProperties = (ImageButton) convertView.findViewById(R.id.file_list_option_properties);
-			holder.optionDownload = (ImageButton) convertView.findViewById(R.id.file_list_option_download);
-			holder.optionDelete = (ImageButton) convertView.findViewById(R.id.file_list_option_delete);
-			holder.arrowSelection = (ImageView) convertView.findViewById(R.id.file_list_arrow_selection);
-			holder.arrowSelection.setVisibility(View.GONE);
-			convertView.setTag(holder);
-		}
-		else{
-			holder = (ViewHolder) convertView.getTag();
-		}
-		
-		ItemFileBrowser rowItem = (ItemFileBrowser) getItem(position);
-		
-		holder.textViewFileName.setText(rowItem.getName());
-		holder.textViewFileSize.setText("100 KB");
-		holder.textViewUpdated.setText("Updated: 1 week ago");
-		holder.imageView.setImageResource(rowItem.getImageId());
-		
-		holder.imageButtonThreeDots.setTag(holder);
-		holder.imageButtonThreeDots.setOnClickListener(
-					new OnClickListener() {
-						public void onClick(View v) {
-							if (positionClicked == -1){
-								positionClicked = _position;
-								notifyDataSetChanged();
-							}
-							else{
-								if (positionClicked == _position){
-									positionClicked = -1;
-									notifyDataSetChanged();
-								}
-								else{
-									positionClicked = _position;
-									notifyDataSetChanged();
-								}
-							}
-						}
-					});
-		
-		if (positionClicked != -1){
-			if (positionClicked == position){
-				holder.arrowSelection.setVisibility(View.VISIBLE);
-				LayoutParams params = holder.optionsLayout.getLayoutParams();
-				params.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, context.getResources().getDisplayMetrics());
-				holder.itemLayout.setBackgroundColor(context.getResources().getColor(R.color.file_list_selected_row));
-				holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_grey);
-				ListView list = (ListView) parent;
-				list.smoothScrollToPosition(_position);
+		if ((position % 2) == 0){
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.item_file_grid, parent, false);
+				holder = new ViewHolder();
+				holder.itemLayout1 = (RelativeLayout) convertView.findViewById(R.id.file_grid_item_layout1);
+				holder.itemLayout2 = (RelativeLayout) convertView.findViewById(R.id.file_grid_item_layout2);
+				
+				holder.imageView1 = (ImageView) convertView.findViewById(R.id.file_grid_thumbnail1);
+	            holder.imageView2 = (ImageView) convertView.findViewById(R.id.file_grid_thumbnail2);
+	            
+				holder.textViewFileName1 = (TextView) convertView.findViewById(R.id.file_grid_filename1);
+				holder.textViewFileName2 = (TextView) convertView.findViewById(R.id.file_grid_filename2);
+				
+				holder.textViewFileSize1 = (TextView) convertView.findViewById(R.id.file_grid_filesize1);
+				holder.textViewFileSize2 = (TextView) convertView.findViewById(R.id.file_grid_filesize2);
+				
+				holder.imageButtonThreeDots1 = (ImageButton) convertView.findViewById(R.id.file_list_three_dots1);
+				holder.imageButtonThreeDots2 = (ImageButton) convertView.findViewById(R.id.file_list_three_dots2);
+				
+				convertView.setTag(holder);
 			}
 			else{
-				holder.arrowSelection.setVisibility(View.GONE);
-				LayoutParams params = holder.optionsLayout.getLayoutParams();
-				params.height = 0;
-				holder.itemLayout.setBackgroundColor(Color.WHITE);
-				holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_white);
+				holder = (ViewHolder) convertView.getTag();
 			}
+			
+			ItemFileBrowser rowItem1 = (ItemFileBrowser) getItem(position);
+			holder.imageView1.setImageResource(rowItem1.getImageId());
+			holder.textViewFileName1.setText(rowItem1.getName());
+			holder.textViewFileSize1.setText("100 KB");
+			
+			ItemFileBrowser rowItem2;
+			if (position < (getCount()-1)){
+				rowItem2 = (ItemFileBrowser) getItem(position+1);
+				holder.imageView2.setImageResource(rowItem2.getImageId());
+				holder.textViewFileName2.setText(rowItem2.getName());	
+				holder.itemLayout2.setVisibility(View.VISIBLE);
+				holder.textViewFileSize2.setText("100 KB");
+			}
+			else{
+				holder.itemLayout2.setVisibility(View.GONE);
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	
+	//		holder.imageButtonThreeDots.setTag(holder);
+	//		holder.imageButtonThreeDots.setOnClickListener(
+	//					new OnClickListener() {
+	//						public void onClick(View v) {
+	//							if (positionClicked == -1){
+	//								positionClicked = _position;
+	//								notifyDataSetChanged();
+	//							}
+	//							else{
+	//								if (positionClicked == _position){
+	//									positionClicked = -1;
+	//									notifyDataSetChanged();
+	//								}
+	//								else{
+	//									positionClicked = _position;
+	//									notifyDataSetChanged();
+	//								}
+	//							}
+	//						}
+	//					});
+	//		
+	//		if (positionClicked != -1){
+	//			if (positionClicked == position){
+	//				holder.arrowSelection.setVisibility(View.VISIBLE);
+	//				LayoutParams params = holder.optionsLayout.getLayoutParams();
+	//				params.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, context.getResources().getDisplayMetrics());
+	//				holder.itemLayout.setBackgroundColor(context.getResources().getColor(R.color.file_list_selected_row));
+	//				holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_grey);
+	//				ListView list = (ListView) parent;
+	//				list.smoothScrollToPosition(_position);
+	//			}
+	//			else{
+	//				holder.arrowSelection.setVisibility(View.GONE);
+	//				LayoutParams params = holder.optionsLayout.getLayoutParams();
+	//				params.height = 0;
+	//				holder.itemLayout.setBackgroundColor(Color.WHITE);
+	//				holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_white);
+	//			}
+	//		}
+	//		else{
+	//			holder.arrowSelection.setVisibility(View.GONE);
+	//			LayoutParams params = holder.optionsLayout.getLayoutParams();
+	//			params.height = 0;
+	//			holder.itemLayout.setBackgroundColor(Color.WHITE);
+	//			holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_white);
+	//		}
+	//		
+	//		holder.optionProperties.setTag(holder);
+	//		holder.optionProperties.setOnClickListener(
+	//					new OnClickListener() {
+	//						public void onClick(View v) {
+	//							Intent i = new Intent(context, FilePropertiesActivity.class);
+	//							i.putExtra("imageId", rowItems.get(_position).getImageId());
+	//							i.putExtra("name", rowItems.get(_position).getName());
+	//							context.startActivity(i);							
+	//							positionClicked = -1;
+	//							notifyDataSetChanged();
+	//						}
+	//					});
+	//		
 		}
 		else{
-			holder.arrowSelection.setVisibility(View.GONE);
-			LayoutParams params = holder.optionsLayout.getLayoutParams();
-			params.height = 0;
-			holder.itemLayout.setBackgroundColor(Color.WHITE);
-			holder.imageButtonThreeDots.setImageResource(R.drawable.three_dots_background_white);
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.item_file_empty_grid, parent, false);
+			}
 		}
-		
-		holder.optionProperties.setTag(holder);
-		holder.optionProperties.setOnClickListener(
-					new OnClickListener() {
-						public void onClick(View v) {
-							Intent i = new Intent(context, FilePropertiesActivity.class);
-							i.putExtra("imageId", rowItems.get(_position).getImageId());
-							i.putExtra("name", rowItems.get(_position).getName());
-							context.startActivity(i);							
-							positionClicked = -1;
-							notifyDataSetChanged();
-						}
-					});
-		
+
 		return convertView;
 	}
 
