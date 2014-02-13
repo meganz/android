d0dbb85171e061a9537f338dcc8825cad9e3fd26
jraragon <jra@mega.co.nz>
2014-02-13 13:45:18 +0100

    Image Gallery Solved bug when slidding from a zoomed image. Now, when back, the image has zoom = 1 again; The ViewPager.onPageChangeListener has been implemented and the functionality is added into onPageScrollStateChanged(int state). In the custom PagerAdapter (MegaFullScreenImageAdapter), now we maintain the TouchImageViews which are created so we can modify their zoom value from the main activity.

--- MegaCOM/src/com/mega/android/FullScreenImageViewer.java
+++ MegaCOM/src/com/mega/android/FullScreenImageViewer.java
@@ -3,6 +3,7 @@ package com.mega.android;
 import java.util.ArrayList;
 
 import com.mega.components.ExtendedViewPager;
+import com.mega.components.TouchImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +11,15 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 
-public class FullScreenImageViewer extends ActionBarActivity{
+public class FullScreenImageViewer extends ActionBarActivity implements OnPageChangeListener{
 	
 	private ExtendedViewPager viewPager;
 	private MegaFullScreenImageAdapter adapter;
-	ActionBar aB;
+	private ActionBar aB;
+	private int positionG;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,8 @@ public class FullScreenImageViewer extends ActionBarActivity{
 		viewPager.setPageMargin(40);
 		
 		Intent i = getIntent();
-		int position = i.getIntExtra("position", 0);
+		positionG = i.getIntExtra("position", 0);
+		
 		ArrayList<String> names = i.getStringArrayListExtra("names");
 		ArrayList<Integer> imageIds = i.getIntegerArrayListExtra("imageIds");
 //		Toast.makeText(this, position + "_" + name + "_" + imageIds.size(), Toast.LENGTH_SHORT).show();
@@ -39,9 +44,35 @@ public class FullScreenImageViewer extends ActionBarActivity{
 		
 		viewPager.setAdapter(adapter);
 		
-		viewPager.setCurrentItem(position);
-
-
+		viewPager.setCurrentItem(positionG);
+		
+		viewPager.setOnPageChangeListener(this);
 	}
 
+	@Override
+	public void onPageSelected(int position) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onPageScrollStateChanged(int state) {
+
+		if (state == ViewPager.SCROLL_STATE_IDLE){
+			if (viewPager.getCurrentItem() != positionG){
+				int oldPosition = positionG;
+				int newPosition = viewPager.getCurrentItem();
+				positionG = newPosition;
+				
+				TouchImageView tIV = adapter.getVisibleImage(oldPosition);
+				tIV.setZoom(1);
+			}
+		}
+	}
 }
