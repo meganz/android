8957389338a1b71a63decbfc14bf4c5aecf3ee70
jraragon <jra@mega.co.nz>
2014-03-13 10:53:13 +0100

    Image Gallery with previews complete (setPreview, setThumbnail not working until Android SDK change)

--- src/com/mega/android/PreviewUtils.java
+++ src/com/mega/android/PreviewUtils.java
@@ -4,10 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.PowerManager;
+import android.os.StatFs;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -22,6 +28,8 @@ import com.mega.sdk.MegaError;
 import com.mega.sdk.MegaNode;
 import com.mega.sdk.MegaRequest;
 import com.mega.sdk.MegaRequestListenerInterface;
+import com.mega.sdk.MegaTransfer;
+import com.mega.sdk.MegaTransferListenerInterface;
 import com.mega.sdk.MegaUtils;
 
 public class PreviewUtils {
@@ -29,8 +37,62 @@ public class PreviewUtils {
 	public static File previewDir;
 	public static PreviewCache previewCache = new PreviewCache();
 	public static ArrayList<Long> pendingPreviews = new ArrayList<Long>();
+	public static ArrayList<Long> pendingFullImages = new ArrayList<Long>();
 	
 	static HashMap<Long, PreviewDownloadListener> listeners = new HashMap<Long, PreviewDownloadListener>();
+	static HashMap<Long, FullImageDownloadListener> fullImageListeners = new HashMap<Long, FullImageDownloadListener>();
+
+	static class FullImageDownloadListener implements MegaTransferListenerInterface{
+		Context context;
+		ViewHolderFullImage holder;
+		MegaFullScreenImageAdapter adapter;
+		MegaApiAndroid megaApi;
+		
+		public FullImageDownloadListener(Context context, ViewHolderFullImage holder, MegaFullScreenImageAdapter adapter, MegaApiAndroid megaApi) {
+			this.context = context;
+			this.holder = holder;
+			this.adapter = adapter;
+			this.megaApi = megaApi;
+		}
+		
+		@Override
+		public void onTransferStart(MegaApiJava api, MegaTransfer transfer) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void onTransferFinish(MegaApiJava api, MegaTransfer transfer,
+				MegaError e) {
+			log("Downloading image finished");
+			final long handle = transfer.getNodeHandle();
+			MegaNode node = api.getNodeByHandle(handle);
+			
+			pendingFullImages.remove(handle);
+			
+			if (e.getErrorCode() == MegaError.API_OK){
+				log("Downloading image OK: " + handle);
+				
+				String localPath = transfer.getPath();
+				ResizerParams params = new ResizerParams();
+				params.document = node;
+				params.file = new File(localPath);
+				new AttachPreviewTask(context, megaApi, holder, adapter).execute(params);
+			}
+			
+		}
+		@Override
+		public void onTransferUpdate(MegaApiJava api, MegaTransfer transfer) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void onTransferTemporaryError(MegaApiJava api,
+				MegaTransfer transfer, MegaError e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 	static class PreviewDownloadListener implements MegaRequestListenerInterface{
 		Context context;
@@ -127,7 +189,7 @@ public class PreviewUtils {
 	
 	public static Bitmap getPreviewFromMega(MegaNode document, Context context, ViewHolderFullImage holder, MegaApiAndroid megaApi, MegaFullScreenImageAdapter adapter){
 		
-		if (pendingPreviews.contains(document.getHandle()) || !document.hasPreview()){
+		if (pendingPreviews.contains(document.getHandle())){
 			log("the preview is already downloaded or added to the list");
 			return previewCache.get(document.getHandle());
 		}
@@ -163,6 +225,10 @@ public class PreviewUtils {
 			return;
 		}
 		
+		if (!Util.isOnline(context)){
+			return;
+		}
+		
 		String localPath = Util.getLocalFile(context, document.getName(), document.getSize(), null); //if file already exists returns != null
 		if(localPath != null) //Si la tengo en el sistema de ficheros
 		{
@@ -173,7 +239,70 @@ public class PreviewUtils {
 			new AttachPreviewTask(context, megaApi, holder, adapter).execute(params);
 		}
 		else{	//Si no, me toca descargármela
+			File cacheDir = context.getCacheDir();
+			File dir = new File (cacheDir, document.getBase64Handle());
+			dir.mkdir();
+			dir.setReadable(true, false);
+			dir.setExecutable(true, false);
+			dir.setLastModified(System.currentTimeMillis());
+			File destination = new File(dir, document.getName());
 			
+			if (destination.exists()){
+				if (destination.length() == document.getSize()){
+					ResizerParams params = new ResizerParams();
+					params.document = document;
+					params.file = destination;
+					new AttachPreviewTask(context, megaApi, holder, adapter).execute(params);
+					destination.deleteOnExit();
+					return;
+				}
+				else{
+					destination.delete();
+				}
+			}
+			
+			try{
+				StatFs stat = new StatFs(destination.getParentFile().getAbsolutePath());
+				double availableFreeSpace = (double)stat.getAvailableBlocks() * (double)stat.getBlockSize();
+				
+				if (availableFreeSpace < document.getSize()){
+					Util.showErrorAlertDialog(context.getString(R.string.error_not_enough_free_space), false, (Activity)context);
+					return;
+				}
+			}
+			catch (Exception e) {}
+			
+			if(!Util.isOnline(context)){
+				Util.showErrorAlertDialog(context.getString(R.string.error_server_connection_problem),	false, (Activity)context);
+				return;
+			}
+			else{
+//				int wifiLockMode = WifiManager.WIFI_MODE_FULL;
+//		        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+//		            wifiLockMode = WifiManager.WIFI_MODE_FULL_HIGH_PERF;
+//		        }
+//		        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+//		        WifiLock lock = wifiManager.createWifiLock(wifiLockMode, "MegaUploadServiceWifiLock");
+//				PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+//		    	PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MegaUploadServicePowerLock");
+//		    	
+//		    	lock.acquire();
+//		    	wl.acquire();
+				
+				if (pendingFullImages.contains(document.getHandle())){
+					log("the image is already downloaded or added to the list");
+					return;
+				}
+				
+				pendingFullImages.add(document.getHandle());
+		    	
+				log("Dir: " + dir);
+				log("document.name: " +  document.getName() + "_handle: " + document.getHandle());
+				log("destination.getabsolutepath: " + destination.getAbsolutePath());
+				FullImageDownloadListener listener = new FullImageDownloadListener(context, holder, adapter, megaApi);
+				fullImageListeners.put(document.getHandle(), listener);
+				megaApi.startDownload(document, dir.getAbsolutePath() + "/", listener);
+			}
 		}
 	}
 	
