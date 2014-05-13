ee792f6398968fc779b6fbbd1cf7583a78103ef4
jraragon <jra@mega.co.nz>
2014-05-13 11:07:34 +0200

    Share images (the content, not the link) using WhatsApp, Gmail, etc implemented from FullScreenImageViewer

--- src/com/mega/android/FullScreenImageViewer.java
+++ src/com/mega/android/FullScreenImageViewer.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
 import com.mega.components.ExtendedViewPager;
 import com.mega.components.TouchImageView;
 import com.mega.sdk.MegaApiAndroid;
+import com.mega.sdk.MegaApiJava;
+import com.mega.sdk.MegaError;
 import com.mega.sdk.MegaNode;
+import com.mega.sdk.MegaRequest;
+import com.mega.sdk.MegaRequestListenerInterface;
 import com.mega.sdk.NodeList;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.view.ViewPager;
@@ -26,7 +33,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class FullScreenImageViewer extends Activity implements OnPageChangeListener, OnClickListener{
+public class FullScreenImageViewer extends Activity implements OnPageChangeListener, OnClickListener, MegaRequestListenerInterface{
 	
 	private Display display;
 	private DisplayMetrics outMetrics;
@@ -36,13 +43,17 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 	
 	private boolean aBshown = true;
 	
-
+	ProgressDialog statusDialog;
+	
 	private MegaFullScreenImageAdapter adapterMega;
 	private MegaOfflineFullScreenImageAdapter adapterOffline;
 	private int positionG;
 	private ArrayList<Long> imageHandles;
 	
 	private ImageView actionBarIcon;
+	private ImageView overflowIcon;
+	private ImageView shareIcon;
+	
 	private RelativeLayout bottomLayout;
     private RelativeLayout topLayout;
 	private ExtendedViewPager viewPager;	
@@ -60,6 +71,8 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 		fullScreenImageViewer = this;
 		MegaApplication app = (MegaApplication)getApplication();
 		megaApi = app.getMegaApi();
+		
+		fullScreenImageViewer = this;
 		
 		setContentView(R.layout.activity_full_screen_image_viewer);
 		
@@ -143,6 +156,12 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 			actionBarIcon = (ImageView) findViewById(R.id.full_image_viewer_icon);
 			actionBarIcon.setOnClickListener(this);
 			
+			overflowIcon = (ImageView) findViewById(R.id.full_image_viewer_overflow);
+			overflowIcon.setVisibility(View.GONE);
+			
+			shareIcon = (ImageView) findViewById(R.id.full_image_viewer_share);
+			shareIcon.setVisibility(View.GONE);
+			
 			bottomLayout = (RelativeLayout) findViewById(R.id.image_viewer_layout_bottom);
 		    topLayout = (RelativeLayout) findViewById(R.id.image_viewer_layout_top);
 			
@@ -188,7 +207,7 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 					imageNumber++;
 				}
 			}
-			Toast.makeText(this, ""+parentNode.getName() + "_" + imageHandles.size(), Toast.LENGTH_LONG).show();
+//			Toast.makeText(this, ""+parentNode.getName() + "_" + imageHandles.size(), Toast.LENGTH_LONG).show();
 				
 			adapterMega = new MegaFullScreenImageAdapter(fullScreenImageViewer,imageHandles, megaApi);
 			
@@ -200,6 +219,12 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 			
 			actionBarIcon = (ImageView) findViewById(R.id.full_image_viewer_icon);
 			actionBarIcon.setOnClickListener(this);
+			
+			overflowIcon = (ImageView) findViewById(R.id.full_image_viewer_overflow);
+			overflowIcon.setOnClickListener(this);
+			
+			shareIcon = (ImageView) findViewById(R.id.full_image_viewer_share);
+			shareIcon.setOnClickListener(this);
 			
 			bottomLayout = (RelativeLayout) findViewById(R.id.image_viewer_layout_bottom);
 		    topLayout = (RelativeLayout) findViewById(R.id.image_viewer_layout_top);
@@ -259,6 +284,28 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 				finish();
 				break;
 			}
+			case R.id.full_image_viewer_overflow:{
+				Toast.makeText(this, "OVERFLOW CLICKED", Toast.LENGTH_LONG).show();
+				break;
+			}
+			case R.id.full_image_viewer_share:{
+				MegaNode n = megaApi.getNodeByHandle(imageHandles.get(positionG));
+				
+				File previewFolder = PreviewUtils.getPreviewFolder(this);
+				File previewFile = new File(previewFolder, n.getBase64Handle() + ".jpg");
+				
+				if (previewFile.exists()){
+					Intent share = new Intent(android.content.Intent.ACTION_SEND);
+					share.setType("image/*");
+					share.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + previewFile));
+					startActivity(Intent.createChooser(share, getString(R.string.context_share_image)));
+				}
+				else{
+					Toast.makeText(this, getString(R.string.full_image_viewer_not_preview), Toast.LENGTH_LONG).show();
+				}
+				
+				break;
+			}
 		}
 	}
 	
@@ -300,5 +347,74 @@ public class FullScreenImageViewer extends Activity implements OnPageChangeListe
 			animTop.setFillAfter( true );
 			topLayout.setAnimation(animTop);
 		}
+	}
+	
+//	public void getPublicLinkAndShareIt(){
+//		
+//		if (!Util.isOnline(this)){
+//			Util.showErrorAlertDialog(getString(R.string.error_server_connection_problem), false, this);
+//			return;
+//		}
+//		
+//		if(isFinishing()){
+//			return;	
+//		}
+//		
+//		ProgressDialog temp = null;
+//		try{
+//			temp = new ProgressDialog(this);
+//			temp.setMessage(getString(R.string.context_creating_link));
+//			temp.show();
+//		}
+//		catch(Exception e){
+//			return;
+//		}
+//		statusDialog = temp;
+//		
+//		megaApi.exportNode(node, this);
+//	}
+
+	@Override
+	public void onRequestStart(MegaApiJava api, MegaRequest request) {
+		log("onRequestStart: " + request.getRequestString());
+	}
+
+	@Override
+	public void onRequestFinish(MegaApiJava api, MegaRequest request,
+			MegaError e) {
+		
+//		node = megaApi.getNodeByHandle(request.getNodeHandle());
+//		
+//		log("onRequestFinish");
+//		if (request.getType() == MegaRequest.TYPE_EXPORT){
+//			try { 
+//				statusDialog.dismiss();	
+//			} 
+//			catch (Exception ex) {}
+//			
+//			if (e.getErrorCode() == MegaError.API_OK){
+//				String link = request.getLink();
+//				if (fullScreenImageViewer != null){
+//					Intent intent = new Intent(Intent.ACTION_SEND);
+//					intent.setType("text/plain");
+//					intent.putExtra(Intent.EXTRA_TEXT, link);
+//					startActivity(Intent.createChooser(intent, getString(R.string.context_share_image)));
+//				}
+//			}
+//			else{
+//				Toast.makeText(this, "Impossible to get the link", Toast.LENGTH_LONG).show();
+//			}
+//			log("export request finished");
+//		}
+	}
+
+	@Override
+	public void onRequestTemporaryError(MegaApiJava api, MegaRequest request,
+			MegaError e) {
+		log("onRequestTemporaryError: " + request.getRequestString());	
+	}
+	
+	public static void log(String message) {
+		Util.log("FilePropertiesActivity", message);
 	}
 }
