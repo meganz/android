05244ef8376598a23eb21984187e4a782a71b987
jraragon <jra@mega.co.nz>
2014-10-11 11:58:49 +0200

    Task #927 - Upload service optimized: no more duplicates in MEGA

--- src/com/mega/android/UploadService.java
+++ src/com/mega/android/UploadService.java
@@ -2,6 +2,7 @@ package com.mega.android;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.mega.sdk.MegaApiAndroid;
 import com.mega.sdk.MegaApiJava;
@@ -23,6 +24,7 @@ import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.WifiLock;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
@@ -30,6 +32,7 @@ import android.support.v4.app.NotificationCompat;
 import android.text.format.Formatter;
 import android.util.SparseArray;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /*
  * Service to Upload files
@@ -43,6 +46,12 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 	public static String EXTRA_NAME = "MEGA_FILE_NAME";
 	public static String EXTRA_SIZE = "MEGA_SIZE";
 	public static String EXTRA_PARENT_HASH = "MEGA_PARENT_HASH";
+	
+	
+	public static final int CHECK_FILE_TO_UPLOAD_UPLOAD = 1000;
+	public static final int CHECK_FILE_TO_UPLOAD_COPY = 1001;
+	public static final int CHECK_FILE_TO_UPLOAD_OVERWRITE = 1002;
+	public static final int CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER = 1003;
 	
 	
 	private int successCount = 0;
@@ -76,6 +85,8 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 	private SparseArray<MegaTransfer> transfersError;
 	private SparseArray<Long> transfersUploadedSize;
 	
+	private HashMap<String, String> transfersCopy;
+	
 	int lastTag = -1;
 	int totalToUpload;
 	int totalUploaded;
@@ -106,6 +117,7 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 		transfersOK = new SparseArray<MegaTransfer>();
 		transfersError = new SparseArray<MegaTransfer>();
 		transfersUploadedSize = new SparseArray<Long>();
+		transfersCopy = new HashMap<String, String>();
 		
 		isForeground = false;
 		canceled = false;
@@ -168,7 +180,7 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 	protected void onHandleIntent(final Intent intent) {
 		log("onHandleIntent");
 
-		updateProgressNotification(totalSizeUploaded);
+//		updateProgressNotification(totalSizeUploaded);
 
 		final File file = new File(intent.getStringExtra(EXTRA_FILEPATH));
 		
@@ -179,19 +191,92 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 			uploadFolderTask.start();
 		} 
 		else {
-			if(!wl.isHeld()){ 
-				wl.acquire();
+			switch(checkFileToUpload(file, parentHandle)){
+				case CHECK_FILE_TO_UPLOAD_UPLOAD:{
+					log("CHECK_FILE_TO_UPLOAD_UPLOAD");
+					
+					if(!wl.isHeld()){ 
+						wl.acquire();
+					}
+					if(!lock.isHeld()){
+						lock.acquire();
+					}
+					
+					String nameInMEGA = intent.getStringExtra(EXTRA_NAME);
+					if (nameInMEGA != null){
+						megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), nameInMEGA, this);
+					}
+					else{
+						megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), this);
+					}
+					break;
+				}
+				case CHECK_FILE_TO_UPLOAD_COPY:{
+					log("CHECK_FILE_TO_UPLOAD_COPY");
+					break;
+				}
+				case CHECK_FILE_TO_UPLOAD_OVERWRITE:{
+					log("CHECK_FILE_TO_UPLOAD_OVERWRITE");
+					MegaNode nodeExistsInFolder = megaApi.getNodeByPath(file.getName(), megaApi.getNodeByHandle(parentHandle));
+					megaApi.remove(nodeExistsInFolder);
+					
+					if(!wl.isHeld()){ 
+						wl.acquire();
+					}
+					if(!lock.isHeld()){
+						lock.acquire();
+					}
+					
+					String nameInMEGA = intent.getStringExtra(EXTRA_NAME);
+					if (nameInMEGA != null){
+						megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), nameInMEGA, this);
+					}
+					else{
+						megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), this);
+					}
+					break;
+				}
+				case CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER:{
+					log("CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER");
+					Toast.makeText(getApplicationContext(), file.getName() + " already uploaded in folder " + megaApi.getNodeByHandle(parentHandle).getName(), Toast.LENGTH_SHORT).show();
+					
+					if ((currentTransfers.size() == 0) && (transfersCopy.size() == 0)){
+						successCount = transfersOK.size();
+						errorCount = transfersError.size();
+						onQueueComplete();
+					}
+
+					return;					
+				}
 			}
-			if(!lock.isHeld()){
-				lock.acquire();
+		}
+	}
+	
+	int checkFileToUpload(File file, long parentHandle){
+		
+		MegaNode nodeExistsInFolder = megaApi.getNodeByPath(file.getName(), megaApi.getNodeByHandle(parentHandle));
+		if (nodeExistsInFolder == null){
+			String localFingerPrint = megaApi.getFingerprint(file.getAbsolutePath());
+			MegaNode nodeExists = megaApi.getNodeByFingerprint(localFingerPrint);
+			if (nodeExists == null){
+				return CHECK_FILE_TO_UPLOAD_UPLOAD;				
 			}
-			String nameInMEGA = intent.getStringExtra(EXTRA_NAME);
-			if (nameInMEGA != null){
-				megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), nameInMEGA, this);
+			else{				
+				transfersCopy.put(localFingerPrint, file.getName());
+				totalToUpload++;
+				totalSizeToUpload += file.length();
+				megaApi.copyNode(nodeExists, megaApi.getNodeByHandle(parentHandle), this);
+				return CHECK_FILE_TO_UPLOAD_COPY;				
+			}	
+		}
+		else{
+			if (file.length() == nodeExistsInFolder.getSize()){
+				return CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER;
 			}
 			else{
-				megaApi.startUpload(file.getAbsolutePath(), megaApi.getNodeByHandle(parentHandle), this);
+				return CHECK_FILE_TO_UPLOAD_OVERWRITE;	
 			}
+						
 		}
 	}
 	
@@ -318,6 +403,29 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 		long firstFolderHandle = -1;
 		UploadService uploadService;
 		
+		private class FolderCreation{
+			public String localPath;
+			public String folderName;
+			public String parentFolderPath;
+			public long parentFolderHandle;
+			
+			FolderCreation(String localPath, String folderName, String parentFolderPath){
+				this.localPath = localPath;
+				this.folderName = folderName;
+				this.parentFolderPath = parentFolderPath;
+				this.parentFolderHandle = -1;
+			}
+			
+			FolderCreation(String localPath, String folderName, String parentFolderPath, long parentFolderHandle){
+				this.localPath = localPath;
+				this.folderName = folderName;
+				this.parentFolderPath = parentFolderPath;
+				this.parentFolderHandle = parentFolderHandle;
+			}
+		}
+		
+		ArrayList<FolderCreation> foldersCreation = new ArrayList<FolderCreation>();
+		
 		UploadFolderTask(File folder, long parentHandle, UploadService uploadService){
 			this.folder = folder;
 			this.parentHandle = parentHandle;
@@ -326,9 +434,115 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 
 		@Override
 		public void run(){
-			foldersPath.add(folder.getAbsolutePath());
-			createFoldersPathArray(folder);
-			createFolder(foldersPath.get(0));
+			FolderCreation fC = new FolderCreation(folder.getAbsolutePath(), folder.getName(), megaApi.getNodePath(megaApi.getNodeByHandle(parentHandle)), parentHandle);
+			foldersCreation.add(fC);
+			log("FC: " + fC.parentFolderPath + "/" + fC.folderName);
+			createFoldersCreationArray(folder, fC.parentFolderPath + "/" + fC.folderName);
+			for (int i=0;i<foldersCreation.size();i++){
+				log("[" + i + "] PARENTPATH: " + foldersCreation.get(i).parentFolderPath +  "___ FOLDER NAME: " + foldersCreation.get(i).folderName + "____ PARENTFOLDERHANDLE: " + foldersCreation.get(i).parentFolderHandle);
+			}
+			createFolderCreation(foldersCreation.get(0));
+		}
+		
+		private void createFolderCreation(FolderCreation fC){
+			if (foldersCreation.size() > 0){
+				String nodePath = fC.parentFolderPath + "/" + fC.folderName;
+				MegaNode parentNode = megaApi.getNodeByHandle(fC.parentFolderHandle);
+				log("CURRENT NODEPATH:" + nodePath);
+				if (parentNode != null){
+					NodeList nL = megaApi.search (parentNode, fC.folderName, false);
+					if (nL.size() == 0){
+						megaApi.createFolder(fC.folderName, parentNode, this);
+					}
+					else{
+						MegaNode currentNode = nL.get(0).copy();
+						long currentNodeHandle = currentNode.getHandle();
+						File localFolder = new File(fC.localPath);
+						if (localFolder.isDirectory()){
+							File[] files = localFolder.listFiles();
+							for (int i=0;i<files.length;i++){
+								File f = files[i];
+								if (f.isFile()){
+									switch(checkFileToUpload(f, currentNodeHandle)){
+										case CHECK_FILE_TO_UPLOAD_UPLOAD:{
+											log(f.getName() + "__" + currentNode.getName() + "___CHECK_FILE_TO_UPLOAD_UPLOAD");
+											
+											if(!wl.isHeld()){ 
+												wl.acquire();
+											}
+											if(!lock.isHeld()){
+												lock.acquire();
+											}
+											
+											megaApi.startUpload(f.getAbsolutePath(), currentNode, uploadService);
+											break;
+										}
+										case CHECK_FILE_TO_UPLOAD_COPY:{
+											log(f.getName() + "__" + currentNode.getName() + "___CHECK_FILE_TO_UPLOAD_COPY");
+											break;
+										}
+										case CHECK_FILE_TO_UPLOAD_OVERWRITE:{
+											log(f.getName() + "__" + currentNode.getName() + "___CHECK_FILE_TO_UPLOAD_OVERWRITE");
+											MegaNode nodeExistsInFolder = megaApi.getNodeByPath(f.getName(), currentNode);
+											megaApi.remove(nodeExistsInFolder);
+											
+											if(!wl.isHeld()){ 
+												wl.acquire();
+											}
+											if(!lock.isHeld()){
+												lock.acquire();
+											}
+											
+											megaApi.startUpload(f.getAbsolutePath(), currentNode, uploadService);
+
+											break;
+										}
+										case CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER:{
+											log(f.getName() + "__" + currentNode.getName() + "___CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER");
+//											Toast.makeText(getApplicationContext(), f.getName() + " already uploaded in folder " + currentNode.getName(), Toast.LENGTH_SHORT).show();
+											
+											if ((currentTransfers.size() == 0) && (transfersCopy.size() == 0) && (foldersCreation.size() == 0)){
+												successCount = transfersOK.size();
+												errorCount = transfersError.size();
+												
+												onQueueComplete();												
+											}
+	
+											break;					
+										}
+									}
+								}
+							}
+						}
+						
+						foldersCreation.remove(0);
+						if (!foldersCreation.isEmpty()){
+							for (int i=0; i<foldersCreation.size(); i++){
+								if (nodePath.compareTo(foldersCreation.get(i).parentFolderPath) == 0){
+									foldersCreation.get(i).parentFolderHandle = currentNodeHandle;
+									log("FOLDER EXISTS: NAME" + foldersCreation.get(i).folderName + "____ PARENTPATH: " + foldersCreation.get(i).parentFolderPath + "____ HANDLE: " + foldersCreation.get(i).parentFolderHandle);
+								}
+							}
+							
+							createFolderCreation(foldersCreation.get(0));
+						}
+					}
+				}
+			}
+		}
+		
+		private void createFoldersCreationArray(File currentFolder, String parentFolderPath){
+			if (currentFolder.isDirectory()){
+				File[] files = currentFolder.listFiles();
+				for (int i=0; i<files.length; i++){
+					File f = files[i];
+					if (f.isDirectory()){
+						FolderCreation fC = new FolderCreation(f.getAbsolutePath(), f.getName(), parentFolderPath);
+						foldersCreation.add(fC);
+						createFoldersCreationArray(f, parentFolderPath + "/" + f.getName());
+					}
+				}
+			}
 		}
 		
 		private void createFoldersPathArray(File currentFolder){
@@ -347,22 +561,85 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 		private void createFolder(String path){
 			if (foldersPath.size() > 0){
 				if (path.compareTo(folder.getAbsolutePath()) == 0){
-//					NodeList nL = megaApi.getChildren(megaApi.getNodeByHandle(parentHandle));
-//					boolean folderExists = false;
-//					for (int i=0;i<nL.size();i++){
-//						if ((folder.getName().compareTo(nL.get(i).getName()) == 0) && (nL.get(i).isFolder())){
-//							folderExists = true;
-//						}
-//					}
-//					if (!folderExists){
+					NodeList nL = megaApi.getChildren(megaApi.getNodeByHandle(parentHandle));
+					MegaNode nFolder = null;
+					boolean folderExists = false;
+					for (int i=0;i<nL.size();i++){
+						if ((folder.getName().compareTo(nL.get(i).getName()) == 0) && (nL.get(i).isFolder())){
+							folderExists = true;
+							nFolder = nL.get(i);
+							break;
+						}
+					}
+					if (!folderExists){
 						megaApi.createFolder(new File(path).getName(), megaApi.getNodeByHandle(parentHandle), this);
-//					}
-//					else{
-//						foldersPath.remove(0);
-//						if (!foldersPath.isEmpty()){
-//							createFolder(foldersPath.get(0));
-//						}
-//					}
+					}
+					else{
+						if (!firstFolder){
+							firstFolder = true;				
+							firstFolderHandle = nFolder.getHandle();
+						}
+							
+						File currentFolder = new File(foldersPath.get(0));
+						File[] files = currentFolder.listFiles();
+						for (int i=0;i<files.length;i++){
+							File f = files[i];
+							if (f.isFile()){
+								switch(checkFileToUpload(f, nFolder.getHandle())){
+									case CHECK_FILE_TO_UPLOAD_UPLOAD:{
+										log("CHECK_FILE_TO_UPLOAD_UPLOAD");
+										
+										if(!wl.isHeld()){ 
+											wl.acquire();
+										}
+										if(!lock.isHeld()){
+											lock.acquire();
+										}
+										
+										megaApi.startUpload(f.getAbsolutePath(), nFolder, uploadService);
+										
+										break;
+									}
+									case CHECK_FILE_TO_UPLOAD_COPY:{
+										log("CHECK_FILE_TO_UPLOAD_COPY");
+										break;
+									}
+									case CHECK_FILE_TO_UPLOAD_OVERWRITE:{
+										log("CHECK_FILE_TO_UPLOAD_OVERWRITE");
+										MegaNode nodeExistsInFolder = megaApi.getNodeByPath(f.getName(), nFolder);
+										megaApi.remove(nodeExistsInFolder);
+										
+										if(!wl.isHeld()){ 
+											wl.acquire();
+										}
+										if(!lock.isHeld()){
+											lock.acquire();
+										}
+										
+										megaApi.startUpload(f.getAbsolutePath(), nFolder, uploadService);
+										
+										break;
+									}
+									case CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER:{
+										log("CHECK_FILE_TO_UPLOAD_SAME_FILE_IN_FOLDER");
+	
+										break;					
+									}
+								}
+							}
+						}
+						
+						foldersPath.remove(0);
+						if (!foldersPath.isEmpty()){
+							createFolder(foldersPath.get(0));
+						}
+						
+						if ((currentTransfers.size() == 0) && (transfersCopy.size() == 0) && (foldersPath.isEmpty())){
+							successCount = transfersOK.size();
+							errorCount = transfersError.size();
+							onQueueComplete();
+						}
+					}
 				}
 				else{
 					long uploadParentHandle = getUploadParentHandle(path);
@@ -400,34 +677,44 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 
 		@Override
 		public void onRequestStart(MegaApiJava api, MegaRequest request) {
-			log("onRequestStart: " + request.getType());
+			log("onRequestStart: " + request.getRequestString());
 		}
 
 		@Override
 		public void onRequestFinish(MegaApiJava api, MegaRequest request,
 				MegaError e) {
 			if (request.getType() == MegaRequest.TYPE_MKDIR){
-				log("onRequestFinish: " + request.getType() + "_" + foldersPath.get(0));
-				if (e.getErrorCode() == MegaError.API_OK){
-					MegaNode n = megaApi.getNodeByHandle(request.getNodeHandle()).copy();
-					if (!firstFolder){
-						firstFolder = true;				
-						firstFolderHandle = n.getHandle();
-					}
-						
-					File currentFolder = new File(foldersPath.get(0));
-					File[] files = currentFolder.listFiles();
-					for (int i=0;i<files.length;i++){
-						File f = files[i];
-						if (f.isFile()){
-							megaApi.startUpload(f.getAbsolutePath(), n, uploadService);
+				log("onRequestFinish: " + request.getRequestString() + "_" + foldersCreation.get(0).folderName);
+				if (e.getErrorCode() == MegaError.API_OK){					
+					long currentNodeHandle = request.getNodeHandle();
+					MegaNode currentNode = megaApi.getNodeByHandle(currentNodeHandle);
+					
+					FolderCreation fC = foldersCreation.get(0);
+					
+					File localFolder = new File(fC.localPath);
+					if (localFolder.isDirectory()){
+						File[] files = localFolder.listFiles();
+						for (int i=0;i<files.length;i++){
+							File f = files[i];
+							if (f.isFile()){
+								megaApi.startUpload(f.getAbsolutePath(), currentNode, uploadService);
+							}
 						}
 					}
 					
-					foldersPath.remove(0);
-					if (!foldersPath.isEmpty()){
-						createFolder(foldersPath.get(0));
-					}
+					foldersCreation.remove(0);
+					if (!foldersCreation.isEmpty()){
+						String newFolderPath = megaApi.getNodePath(currentNode);
+						
+						for (int i=0; i<foldersCreation.size(); i++){
+							if (newFolderPath.compareTo(foldersCreation.get(i).parentFolderPath) == 0){
+								foldersCreation.get(i).parentFolderHandle = currentNodeHandle;
+								log("NEW FOLDERSCREATION: NAME___" + foldersCreation.get(i).folderName + "____ PARENTPATH: " + foldersCreation.get(i).parentFolderPath + "____ HANDLE: " + foldersCreation.get(i).parentFolderHandle);
+							}
+						}
+						
+						createFolderCreation(foldersCreation.get(0));
+					}					
 				}
 			}
 		}
@@ -520,6 +807,8 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 	public void onTransferStart(MegaApiJava api, MegaTransfer transfer) {
 		log("Upload start: " + transfer.getFileName() + "_" + megaApi.getTotalUploads());
 		
+		updateProgressNotification(totalSizeUploaded);
+		
 		currentTransfers.put(transfer.getTag(), transfer);
 		totalToUpload++;
 		totalSizeToUpload += transfer.getTotalBytes();
@@ -586,7 +875,7 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 			}
 			
 			log("CURRENTTRANSFERS: " + currentTransfers.size() + "___ OK: " + transfersOK.size() + "___ ERROR: " + transfersError.size());
-			if (currentTransfers.size() == 0){
+			if ((currentTransfers.size() == 0) && (transfersCopy.size() == 0)){
 				successCount = transfersOK.size();
 				errorCount = transfersError.size();
 				onQueueComplete();
@@ -631,6 +920,9 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 	@Override
 	public void onRequestStart(MegaApiJava api, MegaRequest request) {
 		log("onRequestStart: " + request.getName());
+		if (request.getType() == MegaRequest.TYPE_COPY){
+			updateProgressNotification(totalSizeUploaded);
+		}
 	}
 
 	@Override
@@ -648,6 +940,29 @@ public class UploadService extends Service implements MegaTransferListenerInterf
 			log("pause_transfer false received");
 			if (e.getErrorCode() == MegaError.API_OK){
 				cancel();
+			}
+		}
+		else if (request.getType() == MegaRequest.TYPE_COPY){
+			MegaNode n = megaApi.getNodeByHandle(request.getNodeHandle());
+			String currentNodeName = n.getName();
+			String megaFingerPrint = megaApi.getFingerprint(n);
+			log("copy node");
+			if (e.getErrorCode() == MegaError.API_OK){
+				String nameInMega = transfersCopy.get(megaFingerPrint);
+				if (nameInMega != null){
+					if (nameInMega.compareTo(currentNodeName) != 0){
+						megaApi.renameNode(n, nameInMega);
+					}
+				}
+			}
+			totalSizeUploaded += n.getSize();
+			totalUploaded++;
+			transfersCopy.remove(megaFingerPrint);
+			
+			if ((currentTransfers.size() == 0) && (transfersCopy.size() == 0)){
+				successCount = transfersOK.size();
+				errorCount = transfersError.size();
+				onQueueComplete();
 			}
 		}
 	}
