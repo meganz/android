e7e29fe9e7615a3eb3725fc36dc0e9644c7b9ad9
jraragon <jra@mega.co.nz>
2014-10-27 12:47:15 +0100

    Retry pending connections in PinActivity (onResume)

--- src/com/mega/android/PinActivity.java
+++ src/com/mega/android/PinActivity.java
@@ -1,20 +1,30 @@
 package com.mega.android;
 
 import com.mega.android.utils.Util;
+import com.mega.sdk.MegaApiAndroid;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class PinActivity extends ActionBarActivity{
 	
+	private MegaApiAndroid megaApi;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		if (megaApi == null){
+			megaApi = ((MegaApplication)getApplication()).getMegaApi();
+		}
 	}
 
 	@Override
 	protected void onPause() {
 		log("onPause");
+		if (megaApi == null){
+			megaApi = ((MegaApplication)getApplication()).getMegaApi();
+		}
 		PinUtil.pause(this);
 		super.onPause();
 	}
@@ -23,6 +33,13 @@ public class PinActivity extends ActionBarActivity{
 	protected void onResume() {
 		log("onResume");
 		super.onResume();
+		if (megaApi == null){
+			megaApi = ((MegaApplication)getApplication()).getMegaApi();
+		}
+		
+		log("retryPendingConnections()");
+		megaApi.retryPendingConnections();
+		
 		PinUtil.resume(this);
 	}
 	
