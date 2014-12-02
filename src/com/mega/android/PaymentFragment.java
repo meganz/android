e317ff9433cd2640ee2d26c1d253d9bf3640f17e
Carolina Zato <cz@mega.co.nz>
2014-12-02 20:46:26 +0100

    Feature #1328 - Payment Workflow (1 of 2 steps)
    DONE: all fragments
    TODO: onback feature, updatePricing in PaymentFragment

--- src/com/mega/android/PaymentFragment.java
+++ src/com/mega/android/PaymentFragment.java
@@ -1,19 +1,22 @@
 package com.mega.android;
 
-import org.json.JSONArray;
+import java.text.DecimalFormat;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.DisplayMetrics;
 import android.view.Display;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.mega.android.utils.Util;
 import com.mega.sdk.MegaApiAndroid;
 import com.mega.sdk.MegaApiJava;
@@ -21,11 +24,8 @@ import com.mega.sdk.MegaError;
 import com.mega.sdk.MegaRequest;
 import com.mega.sdk.MegaRequestListenerInterface;
 
-public class UpgradePaymentActivity extends ActionBarActivity implements MegaRequestListenerInterface{
+public class PaymentFragment extends Fragment implements MegaRequestListenerInterface{
 	
-	/*
-	 * Account type enum
-	 */
 	public enum AccountType {
 		
 		FREE(0, R.drawable.ic_free),
@@ -59,18 +59,14 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 		public int getImageResource() {
 			return resource;
 		}
-		
+	
 		public int getNameResource() {
 			return nameResource;
 		}
 	}
 	
 	private ActionBar aB;
-	
-	public static String ACCOUNT_TYPE_EXTRA = "account_type";
-
 	private AccountType accountType;
-
 	private ImageView packageIcon;
 	private TextView packageName;
 	private TextView storage;
@@ -79,53 +75,66 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 	private TextView perYear;
 	private TextView pricingFrom;
 //	private TextView perMonthTitle;
-	int typeAccount;
-	
-	private double monthlyPrice, yearlyPrice;
-	private String monthlyHash, yearlyHash;
-	
+	int parameterType;	
 	MegaApiAndroid megaApi;
+	Context context;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
+	public void onCreate (Bundle savedInstanceState){
+		if (megaApi == null){
+			megaApi = ((MegaApplication) ((Activity)context).getApplication()).getMegaApi();
+		}
+
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_upgrade_payment);
-
-
-		aB = getSupportActionBar();
-		aB.setHomeButtonEnabled(true);
-		aB.setLogo(R.drawable.ic_action_navigation_accept);
+		log("onCreate");
+	}
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
-		packageIcon = (ImageView) findViewById(R.id.pro_image);
+		parameterType = this.getArguments().getInt("type");
+
+		if (megaApi == null){
+			megaApi = ((MegaApplication) ((Activity)context).getApplication()).getMegaApi();
+		}
+
+		if (aB == null){
+			aB = ((ActionBarActivity)context).getSupportActionBar();
+		}
 		
-		Display display = getWindowManager().getDefaultDisplay();
+		aB.setTitle(R.string.section_account);
+		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
 		DisplayMetrics outMetrics = new DisplayMetrics();
 		display.getMetrics(outMetrics);
-		float density = getResources().getDisplayMetrics().density;
-		
+		float density = ((Activity) context).getResources().getDisplayMetrics().density;
+
 		float scaleW = Util.getScaleW(outMetrics, density);
 		float scaleH = Util.getScaleH(outMetrics, density);
+
+		View v = null;
+		v = inflater.inflate(R.layout.activity_upgrade_payment, container, false);
+		
+		packageIcon = (ImageView) v.findViewById(R.id.pro_image);
 		
 		packageIcon.getLayoutParams().width = Util.px2dp((100*scaleW), outMetrics);
 		packageIcon.getLayoutParams().height = Util.px2dp((100*scaleW), outMetrics);
 		
-		packageName = (TextView) findViewById(R.id.pro_title);
-		storage = (TextView) findViewById(R.id.pro_storage);
-		bandwidth = (TextView) findViewById(R.id.pro_bandwidth);
-		pricingFrom = (TextView) findViewById(R.id.pricing_from);
+		packageName = (TextView) v.findViewById(R.id.pro_title);
+		storage = (TextView) v.findViewById(R.id.pro_storage);
+		bandwidth = (TextView) v.findViewById(R.id.pro_bandwidth);
+		pricingFrom = (TextView) v.findViewById(R.id.pricing_from);
 		
 //		perMonthTitle = (TextView) findViewById(R.id.per_month);
-		perMonth = (TextView) findViewById(R.id.per_month_price);
-		perYear = (TextView) findViewById(R.id.per_year_price);
-
-		accountType = AccountType.getById(getIntent().getIntExtra(ACCOUNT_TYPE_EXTRA, 0));
-		typeAccount = getIntent().getIntExtra(ACCOUNT_TYPE_EXTRA, 0);
+		perMonth = (TextView) v.findViewById(R.id.per_month_price);
+		perYear = (TextView) v.findViewById(R.id.per_year_price);
+		
+		accountType = AccountType.getById(parameterType);
 
 		packageIcon.setImageResource(accountType.getImageResource());
 		packageName.setText(accountType.getNameResource());
 		
-		switch(typeAccount){
+		switch(parameterType){
 		
 			case 1:{
 				storage.setText("500 GB");
@@ -158,37 +167,13 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 			
 		}
 		
-		megaApi = ((MegaApplication)getApplication()).getMegaApi();
-//		updatePricing();
-//		megaApi.getPaymentUrl(p.getHandle(1), this);
-	}
+		return v;
+	}	
 	
-	/*
-	 * Get pricing information
-	 */
-	private void updatePricing() {
-//		final ProgressDialog updateProgress = Util.createProgress(this, R.string.general_updating);
-//		updateProgress.show();
-//		OLD_MegaApi.getPricing(megaApi, new GenericObjectListener() {
-//
-//			@Override
-//			public void onResult(Object responseObject, OLD_MegaError error) {
-//				updateProgress.dismiss();
-//				log(responseObject + " of obj " + error + " of Error");
-//				if (error != null) {
-//					Util.showErrorAlertDialogFinish(error,
-//							UpgradePaymentActivity.this);
-//				} else {
-//					parseJson((JSONArray) responseObject);
-//				}
-//			}
-//		});
-	}
-	
-	public void onYearlyClick(View view) {
+	public void payYear() {
 		log("yearly");
 		
-		switch(typeAccount){
+		switch(parameterType){
 		
 			case 1:{
 				String handle = "7472683699866478542";
@@ -209,10 +194,10 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 		}
 	}
 	
-	public void onMonthlyClick(View view) {
+	public void payMonth() {
 		log("monthly");
 
-		switch(typeAccount){
+		switch(parameterType){
 		
 			case 1:{
 				String handle = "1560943707714440503";
@@ -233,48 +218,6 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 		}
 	}
 	
-	/*
-	 * Request URL for payment
-	 */
-	private void requestUrl(String hash, double price) {
-//		if (hash == null) {
-//			return;
-//		}
-//		final ProgressDialog updateProgress = Util.createProgress(this,
-//				R.string.general_updating);
-//		updateProgress.show();
-//		OLD_MegaApi.getPaymentUrl(megaApi, hash, price, new GenericObjectListener() {
-//			
-//			@Override
-//			public void onResult(Object responseObject, OLD_MegaError error) {
-//				updateProgress.dismiss();
-//				if (error != null) {
-//					Util.showErrorAlertDialogFinish(error, UpgradePaymentActivity.this);
-//				} else {
-//					String url = (String)responseObject;
-//					log(url + " OF URL!");
-//					// Start browser with payment URL
-//					Util.openUrl(UpgradePaymentActivity.this, url);
-//				}
-//			}
-//		});
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    // Respond to the action bar's Up/Home button
-		    case android.R.id.home:{
-		    	finish();
-		    	return true;
-		    }
-		}	    
-	    return super.onOptionsItemSelected(item);
-	}
-
-	public static void log(String message) {
-		Util.log("UpgradePaymentActivity", message);
-	}
 
 	@Override
 	public void onRequestStart(MegaApiJava api, MegaRequest request) {
@@ -289,15 +232,30 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 	}
 
 	@Override
-	public void onRequestFinish(MegaApiJava api, MegaRequest request,
-			MegaError e) {
+	public void onRequestFinish(MegaApiJava api, MegaRequest request,MegaError e) {
+		 
 		if (request.getType() == MegaRequest.TYPE_GET_PAYMENT_URL){
-            log("PAYMENT URL: " + request.getLink());
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(request.getLink()));
-            startActivity(browserIntent);
-            
-        }
-		
+	            log("PAYMENT URL: " + request.getLink());
+	            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(request.getLink()));
+	            startActivity(browserIntent);
+	            
+	        }
+	}
+	
+	public String sizeTranslation(long size, int type) {
+		switch(type){
+			case 0:{
+				//From GB to TB
+				if(size!=1024){
+					size=size/1024;
+				}
+								
+				String value = new DecimalFormat("#").format(size) + "TB";			
+				return value;
+			}
+		}
+		return null;
+	      
 	}
 
 	@Override
@@ -307,5 +265,14 @@ public class UpgradePaymentActivity extends ActionBarActivity implements MegaReq
 		
 	}
 	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		context = activity;
+		aB = ((ActionBarActivity)activity).getSupportActionBar();
+	}
 	
+	public static void log(String message) {
+		Util.log("UpgradePaymentActivity", message);
+	}
 }
