2de04e2639f8dc3961afc6a2c4746b9fd19cda01
Jesus Aragon <jra@mega.co.nz>
2015-01-16 13:54:33 +0100

    Update SDK and disable Sodium

--- jni/mega/Android.mk
+++ jni/mega/Android.mk
@@ -14,7 +14,7 @@ LOCAL_MODULE    := megasdk
 LOCAL_CFLAGS := -fexceptions -frtti -fvisibility=hidden -fvisibility-inlines-hidden -fdata-sections -ffunction-sections -DUSE_PTHREAD -DDEBUG
 LOCAL_SRC_FILES := $(CPP_SOURCES) $(C_SOURCES) $(C_WRAPPER_SOURCES)
 LOCAL_C_INCLUDES += $(local_c_includes)
-LOCAL_STATIC_LIBRARIES := curl cryptopp sodium
+LOCAL_STATIC_LIBRARIES := curl cryptopp
 include $(BUILD_STATIC_LIBRARY)
 
 
