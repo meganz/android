332bf0ac8d6be386ab61abeb081104dc4dfde69d
Carolina Zato <cz@mega.co.nz>
2015-02-05 22:16:15 +0100

    Task #1798 - Shadow/Hide current PRO plan while upgrading the account

--- src/nz/mega/android/UpgradeAccountFragment.java
+++ src/nz/mega/android/UpgradeAccountFragment.java
@@ -4,6 +4,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import nz.mega.android.utils.Util;
+import nz.mega.sdk.MegaAccountDetails;
 import nz.mega.sdk.MegaApiAndroid;
 import nz.mega.sdk.MegaApiJava;
 import nz.mega.sdk.MegaError;
@@ -15,6 +16,7 @@ import nz.mega.sdk.MegaUser;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -27,7 +29,9 @@ import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -63,6 +67,10 @@ public class UpgradeAccountFragment extends Fragment implements MegaRequestListe
 	private ImageView pro1;
 	private ImageView pro2;
 	private ImageView pro3;
+	private RelativeLayout pro1Layout;
+	private RelativeLayout pro1LayoutTrans;
+	private RelativeLayout pro3Layout;
+	private RelativeLayout pro2Layout;
 	private Fragment selectMembership;
 	Context context;
 	MegaUser myUser;
@@ -115,6 +123,9 @@ public class UpgradeAccountFragment extends Fragment implements MegaRequestListe
 		pro2 = (ImageView) v.findViewById(R.id.pro2_image);
 		pro3 = (ImageView) v.findViewById(R.id.pro3_image);
 		
+		pro1Layout = (RelativeLayout) v.findViewById(R.id.pro1_layout);
+		pro2Layout = (RelativeLayout) v.findViewById(R.id.pro2_layout);
+		pro3Layout = (RelativeLayout) v.findViewById(R.id.pro3_layout);	
 	
 		pro1.getLayoutParams().width = Util.px2dp((100*scaleW), outMetrics);
 		pro1.getLayoutParams().height = Util.px2dp((100*scaleH), outMetrics);
@@ -171,7 +182,7 @@ public class UpgradeAccountFragment extends Fragment implements MegaRequestListe
 		pricingPerMonth2.setTextSize(TypedValue.COMPLEX_UNIT_SP, (18*scaleText));
 		pricingPerMonth3.setTextSize(TypedValue.COMPLEX_UNIT_SP, (18*scaleText));
 		
-//		megaApi.getAccountDetails(this);
+		megaApi.getAccountDetails(this);
 
 		megaApi.getPricing(this);
 		
@@ -204,49 +215,112 @@ public class UpgradeAccountFragment extends Fragment implements MegaRequestListe
 
 	@Override
 	public void onRequestFinish(MegaApiJava api, MegaRequest request,MegaError e) {
-		 
-		DecimalFormat df = new DecimalFormat("#.##");
-		
-		if (request.getType() == MegaRequest.TYPE_GET_PRICING){
-	            MegaPricing p = request.getPricing();
-	            
-	            for (int i=0;i<p.getNumProducts();i++){
-	                log("p["+ i +"] = " + p.getHandle(i) + "__" + p.getAmount(i) + "___" + p.getGBStorage(i) + "___" + p.getMonths(i) + "___" + p.getProLevel(i) + "___" + p.getGBTransfer(i));
 
-	                Product account = new Product (p.getHandle(i), p.getProLevel(i), p.getMonths(i), p.getGBStorage(i), p.getAmount(i), p.getGBTransfer(i));
-	                
-	                if(account.getLevel()==1&&account.getMonths()==12){
-	                	storage1.setText(account.getStorage()+"GB");
-	                	bandwidth1.setText(sizeTranslation(account.getTransfer(),0));
-	                	double saving1 = account.getAmount()/12.00/100.00;	    	            
-	    	            String saving1String =df.format(saving1);
-	    	            pricingPerMonth1.setText("from " + saving1String +" € per month");
-	                }
-	                else if(account.getLevel()==2&&account.getMonths()==12){
-	                	 storage2.setText(sizeTranslation(account.getStorage(),0));
-	                	 bandwidth2.setText(sizeTranslation(account.getTransfer(),0));
-	                	 double saving2 = account.getAmount()/12.00/100.00;
-	                	 String saving2String =df.format(saving2);
-	                	 pricingPerMonth2.setText("from " + saving2String +" € per month");
-	                }
-	                else if(account.getLevel()==3&&account.getMonths()==12){	                	 
-	    	            storage3.setText(sizeTranslation(account.getStorage(),0));         
-	    	            bandwidth3.setText(sizeTranslation(account.getTransfer(),0));
-	    	            double saving3 = account.getAmount()/12.00/100.00;
-	    	            String saving3String =df.format(saving3);
-	    	            pricingPerMonth3.setText("from " + saving3String +" € per month");
-	                }
-	                
-	                accounts.add(account);
-	            }    
-	            /*RESULTS
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		if (request.getType() == MegaRequest.TYPE_GET_PRICING){
+			MegaPricing p = request.getPricing();
+
+			for (int i=0;i<p.getNumProducts();i++){
+				log("p["+ i +"] = " + p.getHandle(i) + "__" + p.getAmount(i) + "___" + p.getGBStorage(i) + "___" + p.getMonths(i) + "___" + p.getProLevel(i) + "___" + p.getGBTransfer(i));
+
+				Product account = new Product (p.getHandle(i), p.getProLevel(i), p.getMonths(i), p.getGBStorage(i), p.getAmount(i), p.getGBTransfer(i));
+
+				if(account.getLevel()==1&&account.getMonths()==12){
+					storage1.setText(account.getStorage()+"GB");
+					bandwidth1.setText(sizeTranslation(account.getTransfer(),0));
+					double saving1 = account.getAmount()/12.00/100.00;	    	            
+					String saving1String =df.format(saving1);
+					pricingPerMonth1.setText("from " + saving1String +" € per month");
+				}
+				else if(account.getLevel()==2&&account.getMonths()==12){
+					storage2.setText(sizeTranslation(account.getStorage(),0));
+					bandwidth2.setText(sizeTranslation(account.getTransfer(),0));
+					double saving2 = account.getAmount()/12.00/100.00;
+					String saving2String =df.format(saving2);
+					pricingPerMonth2.setText("from " + saving2String +" € per month");
+				}
+				else if(account.getLevel()==3&&account.getMonths()==12){	                	 
+					storage3.setText(sizeTranslation(account.getStorage(),0));         
+					bandwidth3.setText(sizeTranslation(account.getTransfer(),0));
+					double saving3 = account.getAmount()/12.00/100.00;
+					String saving3String =df.format(saving3);
+					pricingPerMonth3.setText("from " + saving3String +" € per month");
+				}
+
+				accounts.add(account);
+			}    
+			/*RESULTS
 	            p[0] = 1560943707714440503__999___500___1___1___1024 - PRO 1 montly
         		p[1] = 7472683699866478542__9999___500___12___1___12288 - PRO 1 annually
         		p[2] = 7974113413762509455__1999___2048___1___2___4096  - PRO 2 montly
         		p[3] = 370834413380951543__19999___2048___12___2___49152 - PRO 2 annually
         		p[4] = -2499193043825823892__2999___4096___1___3___8192 - PRO 3 montly
         		p[5] = 7225413476571973499__29999___4096___12___3___98304 - PRO 3 annually*/
- 	        }
+		}
+		if (request.getType() == MegaRequest.TYPE_ACCOUNT_DETAILS){
+			log ("account_details request");
+			if (e.getErrorCode() == MegaError.API_OK){
+
+				MegaAccountDetails accountInfo = request.getMegaAccountDetails();
+
+				int accountType = accountInfo.getProLevel();
+				switch(accountType){				
+	
+					case 1:{
+						pro1Layout.setBackgroundColor(Color.parseColor("#80ffffff"));
+						pro1Layout.setClickable(false);
+						
+						AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F);
+						alpha.setDuration(0); 
+						alpha.setFillAfter(true); 
+						pro1.startAnimation(alpha);
+						
+						pro1Title.setTextColor(getResources().getColor(R.color.transparent_black));		
+						proStorage1.setTextColor(getResources().getColor(R.color.transparent_black));	
+						storage1.setTextColor(getResources().getColor(R.color.transparent_black));
+						proBandwith1.setTextColor(getResources().getColor(R.color.transparent_black));						
+						bandwidth1.setTextColor(getResources().getColor(R.color.transparent_black));						
+						pricingPerMonth1.setTextColor(getResources().getColor(R.color.transparent_mega));
+						break;
+					}	
+					case 2:{
+						pro2Layout.setBackgroundColor(Color.parseColor("#80ffffff"));
+						pro2Layout.setClickable(false);
+						
+						AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F);
+						alpha.setDuration(0); 
+						alpha.setFillAfter(true); 
+						pro2.startAnimation(alpha);
+						
+						pro2Title.setTextColor(getResources().getColor(R.color.transparent_black));		
+						proStorage2.setTextColor(getResources().getColor(R.color.transparent_black));	
+						storage2.setTextColor(getResources().getColor(R.color.transparent_black));
+						proBandwith2.setTextColor(getResources().getColor(R.color.transparent_black));						
+						bandwidth2.setTextColor(getResources().getColor(R.color.transparent_black));						
+						pricingPerMonth2.setTextColor(getResources().getColor(R.color.transparent_mega));
+						break;
+					}	
+					case 3:{
+						pro3Layout.setBackgroundColor(Color.parseColor("#80ffffff"));
+						pro3Layout.setClickable(false);
+						
+						AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F);
+						alpha.setDuration(0); 
+						alpha.setFillAfter(true); 
+						pro3.startAnimation(alpha);
+						
+						pro3Title.setTextColor(getResources().getColor(R.color.transparent_black));		
+						proStorage3.setTextColor(getResources().getColor(R.color.transparent_black));	
+						storage3.setTextColor(getResources().getColor(R.color.transparent_black));
+						proBandwith3.setTextColor(getResources().getColor(R.color.transparent_black));						
+						bandwidth3.setTextColor(getResources().getColor(R.color.transparent_black));						
+						pricingPerMonth3.setTextColor(getResources().getColor(R.color.transparent_mega));
+						break;
+					}
+				}
+			}
+		}
 	}
 	
 	public String sizeTranslation(long size, int type) {
