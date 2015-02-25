d76cef6a299d1073a516d90e110899980f67cbb7
Carolina Zato <cz@mega.co.nz>
2015-02-25 19:47:02 +0100

    Bug #1930 - NullPointerException when clicking physical Menu button in Login screen

--- src/nz/mega/android/LoginActivity.java
+++ src/nz/mega/android/LoginActivity.java
@@ -493,6 +493,15 @@ public class LoginActivity extends ActionBarActivity implements OnClickListener,
 		finish();
 	}
 	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if ( keyCode == KeyEvent.KEYCODE_MENU ) {
+	        // do nothing
+	        return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}  
+	
 	/*
 	 * Log in form submit
 	 */
