a43bda91a2ebc572027d57441be149c2ef5f0353
Carolina Zato <cz@mega.co.nz>
2015-05-07 21:48:55 +0200

    Inbox Icon

--- src/nz/mega/android/NavigationDrawerAdapter.java
+++ src/nz/mega/android/NavigationDrawerAdapter.java
@@ -72,17 +72,17 @@ public class NavigationDrawerAdapter extends BaseAdapter{
 			case 0:
 				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_cloud_drive,0,0,0);
 				holder.layout.setBackgroundColor(context.getResources().getColor(R.color.navigation_drawer_background_odd));
-				break;
+				break;			
 			case 1:
-				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_cloud_drive,0,0,0);
+				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_saved_for_offline,0,0,0);
 				holder.layout.setBackgroundColor(context.getResources().getColor(R.color.navigation_drawer_background));
 				break;
 			case 2:
-				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_saved_for_offline,0,0,0);
+				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_photo_sync,0,0,0);
 				holder.layout.setBackgroundColor(context.getResources().getColor(R.color.navigation_drawer_background_odd));
 				break;
 			case 3:
-				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_photo_sync,0,0,0);
+				holder.text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_inbox,0,0,0);
 				holder.layout.setBackgroundColor(context.getResources().getColor(R.color.navigation_drawer_background));
 				break;
 			case 4:
