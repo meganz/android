ee4c43802000acc1071bba5159ea067b08bf7d62
Carolina Zato <cz@mega.co.nz>
2015-05-18 20:23:25 +0200

    Feature #2264 - DROPBOX2MEGA: Download files

--- src/nz/mega/android/MegaApplication.java
+++ src/nz/mega/android/MegaApplication.java
@@ -6,6 +6,10 @@ package nz.mega.android;
 
 import java.util.ArrayList;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+
 import nz.mega.android.utils.Util;
 import nz.mega.sdk.MegaApiAndroid;
 import nz.mega.sdk.MegaApiJava;
@@ -26,12 +30,14 @@ import android.util.Log;
 public class MegaApplication extends Application implements MegaListenerInterface
 {
 	final String TAG = "MegaApplication";
-	static final String APP_KEY = "U5NE3TxD";
 	static final String USER_AGENT = "MEGAAndroid/2.3";
 	MegaApiAndroid megaApi;
 	MegaApiAndroid megaApiFolder;
 	String localIpAddress = "";
 	BackgroundRequestListener requestListener;
+	private DropboxAPI<AndroidAuthSession> mDBApi;
+	final static private String APP_KEY = "6tioyn8ka5l6hty";
+	final static private String APP_SECRET = "hfzgdtrma231qdm";
 	
 //	static final String GA_PROPERTY_ID = "UA-59254318-1";
 //	
@@ -162,6 +168,44 @@ public class MegaApplication extends Application implements MegaListenerInterfac
 		
 		return megaApi;
 	}
+	
+	public DropboxAPI<AndroidAuthSession> getDropboxApi()
+	{
+		if(mDBApi == null)
+		{
+//			log("MEGAAPI = null");
+//			PackageManager m = getPackageManager();
+//			String s = getPackageName();
+//			PackageInfo p;
+//			String path = null;
+//			try
+//			{
+//				p = m.getPackageInfo(s, 0);
+//				path = p.applicationInfo.dataDir + "/";
+//			}
+//			catch (NameNotFoundException e)
+//			{
+//				e.printStackTrace();
+//			}
+//			
+//			Log.d(TAG, "Database path: " + path);
+			AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+	    	AndroidAuthSession session = new AndroidAuthSession(appKeys);
+			
+			// Initialize DropboxAPI object
+			mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+			
+//			requestListener = new BackgroundRequestListener();
+//			log("ADD REQUESTLISTENER");
+//			megaApi.addRequestListener(requestListener);
+//			megaApi.addListener(this);
+		}
+		
+		return mDBApi;
+	}
+	
+	
+	
 	
 //	synchronized Tracker getTracker(TrackerName trackerId) {
 //		if (!mTrackers.containsKey(trackerId)) {
