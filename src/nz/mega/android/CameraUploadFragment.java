b2ecb404565e10a9c8e3b264b67c72156cc1c872
Jesus Aragon <jra@mega.co.nz>
2015-05-25 18:02:26 +0200

    Thumbs/Previews in CameraUploadFragment

--- src/nz/mega/android/CameraUploadFragment.java
+++ src/nz/mega/android/CameraUploadFragment.java
@@ -453,6 +453,13 @@ public class CameraUploadFragment extends Fragment implements OnClickListener, O
 			}
 			
 			if (megaApi.getNodeByHandle(photosyncHandle) != null){
+				
+				if (context != null){
+					Intent intent = new Intent(context, CreateThumbPreviewService.class);
+					intent.putExtra(CreateThumbPreviewService.EXTRA_PARENT_HASH, photosyncHandle);
+					context.startService(intent);
+				}
+				
 				nodes = megaApi.getChildren(megaApi.getNodeByHandle(photosyncHandle), MegaApiJava.ORDER_MODIFICATION_DESC);
 				int month = 0;
 				int year = 0;
@@ -648,6 +655,13 @@ public class CameraUploadFragment extends Fragment implements OnClickListener, O
 			}
 			
 			if (megaApi.getNodeByHandle(photosyncHandle) != null){
+				
+				if (context != null){
+					Intent intent = new Intent(context, CreateThumbPreviewService.class);
+					intent.putExtra(CreateThumbPreviewService.EXTRA_PARENT_HASH, photosyncHandle);
+					context.startService(intent);
+				}
+				
 				nodes = megaApi.getChildren(megaApi.getNodeByHandle(photosyncHandle), MegaApiJava.ORDER_MODIFICATION_DESC);
 				int month = 0;
 				int year = 0;
