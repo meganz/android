507427e84308df02fb90125a2c72f7be4ba1b417
Jesus Aragon <jra@mega.co.nz>
2015-11-04 13:54:54 +0100

    Task #3006 - Automatic patch for a compilation error in cryptopp (js)

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,10 @@
 APP_PLATFORM=android-9
-NDK_TOOLCHAIN_VERSION=4.6
+NDK_TOOLCHAIN_VERSION=4.8
 APP_STL := gnustl_static
 APP_ABI := armeabi x86
 APP_OPTIM := release
 APP_PIE := false
 
+# then enable c++11 extentions in source code
+APP_CPPFLAGS += -std=c++11
+# or use APP_CPPFLAGS := -std=gnu++11
