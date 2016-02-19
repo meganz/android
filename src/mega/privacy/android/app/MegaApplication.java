2b66003ff3e457a12aeb75f52b99ff023c34b35d
Jesus Aragon <jra@mega.co.nz>
2016-02-19 19:56:59 +0100

    v3.0.3.beta

--- src/mega/privacy/android/app/MegaApplication.java
+++ src/mega/privacy/android/app/MegaApplication.java
@@ -95,7 +95,7 @@ public class MegaApplication extends Application implements MegaListenerInterfac
 		super.onCreate();
 		
 		MegaApiAndroid.setLoggerObject(new AndroidLogger());
-		MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_MAX);
+		MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_FATAL);
 
 		
 //		initializeGA();
