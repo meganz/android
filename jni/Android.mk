f1b8f2b1783d6eff983a7a1a7f15ef0d9c6b4384
Javier Serrano <js@mega.co.nz>
2016-02-29 18:50:44 +0100

    Updated build scripts

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,6 @@ include $(TOP_PATH)/openssl/Android.mk
 NDK_PROJECT_PATH := $(TOP_PATH)
 include $(TOP_PATH)/curl/Android.mk
 include $(TOP_PATH)/cryptopp/Android.mk
-#include $(TOP_PATH)/sodium/Android.mk
+include $(TOP_PATH)/sqlite/Android.mk
 include $(TOP_PATH)/mega/Android.mk
 
