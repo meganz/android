dfa2f31f0beb15a5ca61bcb96915757261cd1abd
Javier Serrano <js@mega.co.nz>
2016-02-29 18:50:44 +0100

    Updated build scripts

--- jni/mega/Android.mk
+++ jni/mega/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 local_c_includes := \
         $(LOCAL_PATH) \
         $(LOCAL_PATH)/.. \
+        $(LOCAL_PATH)/sdk \
         $(LOCAL_PATH)/sdk/include \
         $(LOCAL_PATH)/sdk/include/mega/posix \
         $(LOCAL_PATH)/sdk/third_party/glob \
@@ -11,10 +12,10 @@ local_c_includes := \
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/Makefile.inc
 LOCAL_MODULE    := megasdk
-LOCAL_CFLAGS := -fexceptions -frtti -fvisibility=hidden -fvisibility-inlines-hidden -fdata-sections -ffunction-sections -DUSE_PTHREAD -DDEBUG
+LOCAL_CFLAGS := -fexceptions -frtti -fvisibility=hidden -fvisibility-inlines-hidden -fdata-sections -ffunction-sections -DDEBUG
 LOCAL_SRC_FILES := $(CPP_SOURCES) $(C_SOURCES) $(C_WRAPPER_SOURCES)
 LOCAL_C_INCLUDES += $(local_c_includes)
-LOCAL_STATIC_LIBRARIES := curl cryptopp
+LOCAL_STATIC_LIBRARIES := curl cryptopp sqlite
 include $(BUILD_STATIC_LIBRARY)
 
 
@@ -24,7 +25,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/Makefile.inc
 LOCAL_MODULE    := mega
-LOCAL_CFLAGS := -fexceptions -frtti -fdata-sections -ffunction-sections -DUSE_PTHREAD -DDEBUG
+LOCAL_CFLAGS := -fexceptions -frtti -fdata-sections -ffunction-sections -DDEBUG
 LOCAL_SRC_FILES := $(JAVA_WRAPS)
 LOCAL_C_INCLUDES += -fexceptions -frtti $(local_c_includes)
 LOCAL_LDLIBS := -lm -lz -llog
