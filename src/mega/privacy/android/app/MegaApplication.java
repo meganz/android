cbd33e166f466d6b7c398e53b35bec6ddb6fd359
Jesus Aragon <jra@mega.co.nz>
2016-03-20 00:45:46 +0100

    v3.0.5.beta

--- src/mega/privacy/android/app/MegaApplication.java
+++ src/mega/privacy/android/app/MegaApplication.java
@@ -27,7 +27,7 @@ import android.util.Log;
 public class MegaApplication extends Application implements MegaListenerInterface
 {
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.0.4.beta";
+	static final String USER_AGENT = "MEGAAndroid/3.0.5.beta";
 	MegaApiAndroid megaApi;
 	MegaApiAndroid megaApiFolder;
 	String localIpAddress = "";
@@ -95,7 +95,7 @@ public class MegaApplication extends Application implements MegaListenerInterfac
 		super.onCreate();
 		
 		MegaApiAndroid.setLoggerObject(new AndroidLogger());
-		MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_MAX);
+		MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_FATAL);
 
 		
 //		initializeGA();
