f4f90324519ab9b915b875e4bfbe26b4b9482aae
Jesus Aragon <jra@mega.co.nz>
2016-05-26 12:22:06 +0200

    Task #4624 - Set new log level when enabling/disabling logs

--- app/src/main/java/mega/privacy/android/app/SettingsActivity.java
+++ app/src/main/java/mega/privacy/android/app/SettingsActivity.java
@@ -631,12 +631,14 @@ public class SettingsActivity extends PinPreferenceActivity implements OnPrefere
 							dbH.setFileLogger(true);
 							Util.setFileLogger(true);
 							numberOfClicksSDK = 0;
+							MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_MAX);
 							Toast.makeText(this, getString(R.string.settings_enable_logs), Toast.LENGTH_LONG).show();
 						}
 						else{
 							dbH.setFileLogger(false);
 							Util.setFileLogger(false);
 							numberOfClicksSDK = 0;
+							MegaApiAndroid.setLogLevel(MegaApiAndroid.LOG_LEVEL_FATAL);
 							Toast.makeText(this, getString(R.string.settings_disable_logs), Toast.LENGTH_LONG).show();
 						}
 					}
