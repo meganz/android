92b5343d82d82880c1c4cd01065e6ef52e18866d
Jesus Aragon <jra@mega.co.nz>
2016-08-22 13:29:25 +0200

    Include libsodium (enable chat)

--- app/src/main/jni/sodium/Android.mk
+++ app/src/main/jni/sodium/Android.mk
@@ -3,4 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sodium
 LOCAL_SRC_FILES := $(LOCAL_PATH)/sodium/libsodium-android-$(TARGET_ARCH_ABI)/lib/libsodium.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/sodium/src/libsodium/include
+LOCAL_EXPORT_CFLAGS := -DENABLE_CHAT
 include $(PREBUILT_STATIC_LIBRARY)
