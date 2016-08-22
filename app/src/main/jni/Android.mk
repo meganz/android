83a66ffacfe2dd598c89c2bcd025da1f88eb734e
Jesus Aragon <jra@mega.co.nz>
2016-08-22 13:29:25 +0200

    Include libsodium (enable chat)

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,5 +6,6 @@ include $(TOP_PATH)/curl/Android.mk
 include $(TOP_PATH)/cryptopp/Android.mk
 include $(TOP_PATH)/sqlite/Android.mk
 include $(TOP_PATH)/libuv/Android.mk
+include $(TOP_PATH)/sodium/Android.mk
 include $(TOP_PATH)/mega/Android.mk
 
