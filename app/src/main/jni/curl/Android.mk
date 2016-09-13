fa527a7da897759f47b7ea39fd3a2c886b342988
Javier Serrano <js@mega.co.nz>
2016-09-13 21:17:26 +0200

    Use default config files for cURL and c-ares if there aren't others

--- app/src/main/jni/curl/Android.mk
+++ app/src/main/jni/curl/Android.mk
@@ -1,14 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
-ANDROID_ROOT := /mnt/data/android
 CFLAGS := -Wpointer-arith -Wwrite-strings -Wunused -Winline -Wnested-externs -Wmissing-declarations -Wmissing-prototypes -Wno-long-long -Wfloat-equal -Wno-multichar -Wsign-compare -Wno-format-nonliteral -Wendif-labels -Wstrict-prototypes -Wdeclaration-after-statement -Wno-system-headers -Wno-nested-externs -DHAVE_CONFIG_H
 
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/curl/lib/Makefile.inc
 LOCAL_MODULE := curl
 LOCAL_SRC_FILES := $(addprefix curl/lib/,$(CSOURCES))
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/curl/lib
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/curl/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/curl/lib $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/include 
 LOCAL_CFLAGS += $(CFLAGS) -DHAVE_CONFIG_H -fexceptions -frtti -fvisibility=hidden -fdata-sections -ffunction-sections
 LOCAL_STATIC_LIBRARIES := ares ssl crypto
 include $(BUILD_STATIC_LIBRARY)
@@ -17,8 +16,8 @@ include $(CLEAR_VARS)
 include $(LOCAL_PATH)/ares/Makefile.inc
 LOCAL_MODULE := ares
 LOCAL_SRC_FILES := $(addprefix ares/,$(CSOURCES))
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ares
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/ares
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ares $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ares $(LOCAL_PATH)/include 
 LOCAL_CFLAGS += $(CFLAGS) -DHAVE_CONFIG_H -fexceptions -frtti -fvisibility=hidden -fdata-sections -ffunction-sections
 LOCAL_STATIC_LIBRARIES := crypto ssl
 include $(BUILD_STATIC_LIBRARY)
