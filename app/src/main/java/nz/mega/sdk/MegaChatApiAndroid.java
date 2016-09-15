5b6ba902edacd0cbd30c8ace70883891bf8a9337
Javier Serrano <js@mega.co.nz>
2016-09-15 20:11:19 +0200

    Initial Java wrappers for MEGAchat

---
+++ app/src/main/java/nz/mega/sdk/MegaChatApiAndroid.java
@@ -0,0 +1,17 @@
+package nz.mega.sdk;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class MegaChatApiAndroid extends MegaChatApiJava {
+    static Handler handler = new Handler(Looper.getMainLooper());
+
+    public MegaChatApiAndroid(MegaApiAndroid megaApi) {
+        super(megaApi);
+    }
+
+    @Override
+    void runCallback(Runnable runnable) {
+        handler.post(runnable);
+    }
+}
