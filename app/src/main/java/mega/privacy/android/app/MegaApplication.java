7f0bd48481349c18aa58ca88a85685e936c370ad
Jesus Aragon <jra@mega.co.nz>
2016-11-28 14:28:49 +0100

    v3.0.12.2.2

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -29,7 +29,7 @@ import nz.mega.sdk.MegaUser;
 public class MegaApplication extends Application implements MegaListenerInterface
 {
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.0.12.2.1";
+	static final String USER_AGENT = "MEGAAndroid/3.0.12.2.2";
 	MegaApiAndroid megaApi;
 	MegaApiAndroid megaApiFolder;
 	String localIpAddress = "";
