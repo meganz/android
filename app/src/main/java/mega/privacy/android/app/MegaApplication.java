8ee0fc66aee5d1bebd03812b4ee4532b4ac3ecce
Jesus Aragon <jra@mega.co.nz>
2017-01-25 00:04:31 +0100

    v3.1.alfa.3

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -36,7 +36,7 @@ import nz.mega.sdk.MegaUser;
 
 public class MegaApplication extends Application implements MegaListenerInterface{
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.1.alfa.2";
+	static final String USER_AGENT = "MEGAAndroid/3.1.alfa.3";
 
 
 	MegaApiAndroid megaApi;
