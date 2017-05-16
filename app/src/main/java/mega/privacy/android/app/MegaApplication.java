0a77881cb459860535d167cdea4f3e8774e98649
Jesús Aragón <jra@mega.co.nz>
2017-05-16 11:42:33 +0200

    Bug #6754 - Without session, Downloading a folder link doesn't show the progress

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -29,7 +29,7 @@ import nz.mega.sdk.MegaUser;
 
 public class MegaApplication extends Application implements MegaListenerInterface{
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.1.3_132";
+	static final String USER_AGENT = "MEGAAndroid/3.1.4_133";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
