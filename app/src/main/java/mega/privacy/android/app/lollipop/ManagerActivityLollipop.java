3c6676d076cf35e598ec0bffb53ab88716c3379c
Jesús Aragón <jra@mega.co.nz>
2017-05-17 12:32:38 +0200

    Bug #6769 - Show transfers (rootNode == null) --> LoginActivity -> ManagerActivity

--- app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
@@ -1469,6 +1469,15 @@ public class ManagerActivityLollipop extends PinActivityLollipop implements Netw
 						finish();
 						return;
 					}
+					else if (getIntent().getAction().equals(Constants.ACTION_SHOW_TRANSFERS)){
+						Intent intent = new Intent(managerActivity, LoginActivityLollipop.class);
+						intent.putExtra("visibleFragment", Constants. LOGIN_FRAGMENT);
+						intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						intent.setAction(Constants.ACTION_SHOW_TRANSFERS);
+						startActivity(intent);
+						finish();
+						return;
+					}
 				}
 			}
 			Intent intent = new Intent(managerActivity, LoginActivityLollipop.class);
