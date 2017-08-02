9b00ffdc3b5f7d376daa7129cc3704ef6d5f62bd
raquelgc6 <usando6raPALOMITAS>
2017-08-02 09:18:37 +0200

    qareport#5340-Tour cards introducing Mega

--- app/src/main/java/mega/privacy/android/app/TourImageAdapter.java
+++ app/src/main/java/mega/privacy/android/app/TourImageAdapter.java
@@ -2,6 +2,20 @@ package mega.privacy.android.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ThumbnailUtils;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -45,9 +59,43 @@ public class TourImageAdapter extends PagerAdapter {
         View viewLayout = inflater.inflate(R.layout.tour_image_layout, container,false);
  
         imgDisplay = (ImageView) viewLayout.findViewById(R.id.imageTour);
-        
+
+
         imgDisplay.setImageResource(mImages[position]);
-        
+
+		/****************************/
+
+		Bitmap bitmap = ((BitmapDrawable)imgDisplay.getDrawable()).getBitmap();
+			int w = bitmap.getWidth();
+			int h = bitmap.getHeight();
+			int radius =20;
+			Bitmap output = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+			Canvas canvas = new Canvas(output);
+
+			final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+			final RectF rectF = new RectF(0, 0, w, h);
+
+			canvas.drawRoundRect(rectF, radius, radius, paint);
+
+			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+			canvas.drawBitmap(bitmap, null, rectF, paint);
+
+			/*left top and right top corners*/
+
+			final Rect clipRect = new Rect(0, radius, w, h);
+			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
+			canvas.drawRect(clipRect, paint);
+
+			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+			canvas.drawBitmap(bitmap, null, rectF, paint);
+
+			imgDisplay.setImageBitmap(output);
+
+
+
+		/******************************/
+
+
         ((ViewPager) container).addView(viewLayout);
  
         return viewLayout;
