ac992a439a4d8476a9cecbdda05605adb35c9c7c
Alvaro Fernandez <afv@mega.nz>
2017-09-14 18:29:43 +0200

    Bug #7555 - Show icon play on videos on MegaBrowserLollipopAdapter

--- app/src/main/java/mega/privacy/android/app/lollipop/adapters/MegaBrowserLollipopAdapter.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/adapters/MegaBrowserLollipopAdapter.java
@@ -133,7 +133,8 @@ public class MegaBrowserLollipopAdapter extends RecyclerView.Adapter<MegaBrowser
 		public RelativeLayout thumbLayout;
 		public View separator;
 		public ImageView imageViewVideoIcon;
-		
+		public TextView videoDuration;
+		public RelativeLayout videoInfo_layout;
 	}
 
 	public void toggleAllSelection(int pos) {
@@ -534,6 +535,8 @@ public class MegaBrowserLollipopAdapter extends RecyclerView.Adapter<MegaBrowser
 			holderGrid.separator = (View) v.findViewById(R.id.file_grid_separator);
 			holderGrid.publicLinkImage = (ImageView) v.findViewById(R.id.file_grid_public_link);
 			holderGrid.imageViewVideoIcon = (ImageView) v.findViewById(R.id.file_grid_video_icon);
+			holderGrid.videoDuration = (TextView) v.findViewById(R.id.file_grid_title_video_duration);
+			holderGrid.videoInfo_layout = (RelativeLayout) v.findViewById(R.id.item_file_videoinfo_layout);
 
 			if(holderGrid.textViewFileSize!=null){
 				holderGrid.textViewFileSize.setVisibility(View.VISIBLE);
@@ -592,7 +595,7 @@ public class MegaBrowserLollipopAdapter extends RecyclerView.Adapter<MegaBrowser
 		
 		holder.textViewFileName.setText(node.getName());
 		holder.textViewFileSize.setText("");
-		holder.imageViewVideoIcon.setVisibility(View.GONE);
+		holder.videoInfo_layout.setVisibility(View.GONE);
 
 		if (!multipleSelect) {
 			holder.itemLayout.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.background_item_grid));
@@ -709,7 +712,27 @@ public class MegaBrowserLollipopAdapter extends RecyclerView.Adapter<MegaBrowser
 			holder.thumbLayout.setBackgroundColor(Color.TRANSPARENT);
 
 			if(Util.isVideoFile(node.getName())){
-				holder.imageViewVideoIcon.setVisibility(View.VISIBLE);
+				holder.videoInfo_layout.setVisibility(View.VISIBLE);
+				holder.videoDuration.setVisibility(View.GONE);
+				log(node.getName()+" DURATION: "+node.getDuration());
+				int duration = node.getDuration();
+				if(duration>0) {
+					int hours = duration / 3600;
+					int minutes = (duration % 3600) / 60;
+					int seconds = duration % 60;
+
+					String timeString;
+					if (hours > 0) {
+						timeString = String.format("%d:%d:%02d", hours, minutes, seconds);
+					} else {
+						timeString = String.format("%d:%02d", minutes, seconds);
+					}
+
+					log("The duration is: " + hours + " " + minutes + " " + seconds);
+
+					holder.videoDuration.setText(timeString);
+					holder.videoDuration.setVisibility(View.VISIBLE);
+				}
 			}
 
 			if (node.hasThumbnail()) {
