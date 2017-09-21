452490bb41f6cf869d17e0580bf821d7765f022f
Alvaro Fernandez <afv@mega.nz>
2017-09-21 18:17:10 +0200

    Bug #7595 - FullScreenImageViewerLollipop doesn't sort correctly list of meganodes if it has fromShared parameter

--- app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
@@ -1025,11 +1025,11 @@ public class FullScreenImageViewerLollipop extends PinActivityLollipop implement
 			ArrayList<MegaNode> nodes = megaApi.getChildren(parentNode, orderGetChildren);
 			if (fromShared){
 				if(orderGetChildren == MegaApiJava.ORDER_DEFAULT_DESC){
-					nodes = sortByNameDescending(nodes);
+					nodes = sortByMailDescending(nodes);
 				}
-				else{
-					nodes = sortByNameAscending(nodes);
-				}
+//				else{
+//					nodes = sortByNameAscending(nodes);
+//				}
 			}
 			int imageNumber = 0;
 			for (int i=0;i<nodes.size();i++){
@@ -1270,6 +1270,29 @@ public class FullScreenImageViewerLollipop extends PinActivityLollipop implement
 		nodes.addAll(folderNodes);
 		nodes.addAll(fileNodes);
 		
+		return nodes;
+	}
+
+	public ArrayList<MegaNode> sortByMailDescending(ArrayList<MegaNode> nodes){
+		log("sortByNameDescending");
+		ArrayList<MegaNode> folderNodes = new ArrayList<MegaNode>();
+		ArrayList<MegaNode> fileNodes = new ArrayList<MegaNode>();
+
+		for (int i=0;i<nodes.size();i++){
+			if (nodes.get(i).isFolder()){
+				folderNodes.add(nodes.get(i));
+			}
+			else{
+				fileNodes.add(nodes.get(i));
+			}
+		}
+
+//		Collections.reverse(folderNodes);
+//		Collections.reverse(fileNodes);
+
+		nodes.clear();
+		nodes.addAll(folderNodes);
+		nodes.addAll(fileNodes);
 		return nodes;
 	}
 
