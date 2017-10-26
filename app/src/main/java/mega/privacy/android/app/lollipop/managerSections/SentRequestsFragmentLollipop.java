6a80a3158d01a77fa39e39c5f014537e27c15762
Carolina Zato <cz@mega.co.nz>
2017-10-26 16:56:50 +0200

    Task #7850: - Open the menu by clicking anywhere in the request list item (not just on the three vertical dots)

--- app/src/main/java/mega/privacy/android/app/lollipop/managerSections/SentRequestsFragmentLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/managerSections/SentRequestsFragmentLollipop.java
@@ -410,7 +410,9 @@ public class SentRequestsFragmentLollipop extends Fragment {
 			}
 		}
 		else{
-			log("nothing, not multiple select");
+			log("not multiple select - show menu");
+			MegaContactRequest c = contacts.get(position);
+			((ManagerActivityLollipop) context).showSentRequestOptionsPanel(c);
 		}
 	}
 }
