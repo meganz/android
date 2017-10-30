2c8e33a3e353b4888ffd39ea205bfda8f118a8f7
Carolina Zato <cz@mega.co.nz>
2017-10-30 20:47:06 +0100

    Add management of local video to calls

---
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/calls/LocalCameraCallFragment.java
@@ -0,0 +1,114 @@
+package mega.privacy.android.app.lollipop.megachat.calls;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.nio.ByteBuffer;
+import java.util.Stack;
+
+import mega.privacy.android.app.DatabaseHandler;
+import mega.privacy.android.app.MegaApplication;
+import mega.privacy.android.app.R;
+import mega.privacy.android.app.utils.Util;
+import nz.mega.sdk.MegaChatApiAndroid;
+import nz.mega.sdk.MegaChatApiJava;
+import nz.mega.sdk.MegaChatVideoListenerInterface;
+
+
+public class LocalCameraCallFragment extends Fragment implements MegaChatVideoListenerInterface {
+
+    int width = 0;
+    int height = 0;
+    Bitmap bitmap;
+    MegaChatApiAndroid megaChatApi;
+    Context context;
+
+    public SurfaceView localSurfaceView;
+    MegaSurfaceRenderer localRenderer;
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        log("onCreate");
+        if (megaChatApi == null){
+            megaChatApi = ((MegaApplication) ((Activity)context).getApplication()).getMegaChatApi();
+        }
+
+        super.onCreate(savedInstanceState);
+        log("after onCreate called super");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        log("onCreateView");
+
+        if (!isAdded()) {
+            return null;
+        }
+
+        View v = inflater.inflate(R.layout.fragment_local_camera_call, container, false);
+
+        localSurfaceView = (SurfaceView)v.findViewById(R.id.surface_local_video);
+        localSurfaceView.setZOrderOnTop(true);
+        localRenderer = new MegaSurfaceRenderer(localSurfaceView);
+
+        megaChatApi.addChatLocalVideoListener(this);
+
+        return v;
+    }
+
+
+        @Override
+    public void onChatVideoData(MegaChatApiJava api, long chatid, int width, int height, byte[] byteBuffer) {
+        if (this.width != width || this.height != height)
+        {
+            this.width = width;
+            this.height = height;
+            this.bitmap = localRenderer.CreateBitmap(width, height);
+        }
+
+        bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(byteBuffer));
+
+        localRenderer.DrawBitmap();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        context = activity;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onDestroy(){
+        log("onDestroy");
+        megaChatApi.removeChatVideoListener(this);
+        super.onDestroy();
+    }
+
+    public void setVideoFrame(boolean visible){
+        if(visible){
+            localSurfaceView.setVisibility(View.VISIBLE);
+        }
+        else{
+            localSurfaceView.setVisibility(View.GONE);
+        }
+    }
+
+    private static void log(String log) {
+        Util.log("LocalCameraCallFragment", log);
+    }
+}
