428906e6a918e553b77063f77e92dc22b4ae4bd2
Jesús Aragón <jra@mega.co.nz>
2017-11-21 23:09:03 +0100

    Features #8037 and #8038 with @Yenel

---
+++ app/src/main/java/com/github/barteksc/pdfviewer/DragPinchManager.java
@@ -0,0 +1,241 @@
+/**
+ * Copyright 2016 Bartosz Schiller
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.barteksc.pdfviewer;
+
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+import com.github.barteksc.pdfviewer.model.LinkTapEvent;
+import com.github.barteksc.pdfviewer.scroll.ScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
+import com.shockwave.pdfium.util.SizeF;
+
+import static com.github.barteksc.pdfviewer.util.Constants.Pinch.MAXIMUM_ZOOM;
+import static com.github.barteksc.pdfviewer.util.Constants.Pinch.MINIMUM_ZOOM;
+
+/**
+ * This Manager takes care of moving the PDFView,
+ * set its zoom track user actions.
+ */
+class DragPinchManager implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
+
+    private PDFView pdfView;
+    private AnimationManager animationManager;
+
+    private GestureDetector gestureDetector;
+    private ScaleGestureDetector scaleGestureDetector;
+
+    private boolean scrolling = false;
+    private boolean scaling = false;
+    private boolean enabled = false;
+
+    DragPinchManager(PDFView pdfView, AnimationManager animationManager) {
+        this.pdfView = pdfView;
+        this.animationManager = animationManager;
+        gestureDetector = new GestureDetector(pdfView.getContext(), this);
+        scaleGestureDetector = new ScaleGestureDetector(pdfView.getContext(), this);
+        pdfView.setOnTouchListener(this);
+    }
+
+    void enable() {
+        enabled = true;
+    }
+
+    void disable() {
+        enabled = false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        boolean onTapHandled = pdfView.callbacks.callOnTap(e);
+        boolean linkTapped = checkLinkTapped(e.getX(), e.getY());
+        if (!onTapHandled && !linkTapped) {
+            ScrollHandle ps = pdfView.getScrollHandle();
+            if (ps != null && !pdfView.documentFitsView()) {
+                if (!ps.shown()) {
+                    ps.show();
+                } else {
+                    ps.hide();
+                }
+            }
+        }
+        pdfView.performClick();
+        return true;
+    }
+
+    private boolean checkLinkTapped(float x, float y) {
+        PdfFile pdfFile = pdfView.pdfFile;
+        float mappedX = -pdfView.getCurrentXOffset() + x;
+        float mappedY = -pdfView.getCurrentYOffset() + y;
+        int page = pdfFile.getPageAtOffset(pdfView.isSwipeVertical() ? mappedY : mappedX, pdfView.getZoom());
+        SizeF pageSize = pdfFile.getScaledPageSize(page, pdfView.getZoom());
+        int pageX, pageY;
+        if (pdfView.isSwipeVertical()) {
+            pageX = (int) pdfFile.getSecondaryPageOffset(page, pdfView.getZoom());
+            pageY = (int) pdfFile.getPageOffset(page, pdfView.getZoom());
+        } else {
+            pageY = (int) pdfFile.getSecondaryPageOffset(page, pdfView.getZoom());
+            pageX = (int) pdfFile.getPageOffset(page, pdfView.getZoom());
+        }
+        for (PdfDocument.Link link : pdfFile.getPageLinks(page)) {
+            RectF mapped = pdfFile.mapRectToDevice(page, pageX, pageY, (int) pageSize.getWidth(),
+                    (int) pageSize.getHeight(), link.getBounds());
+            if (mapped.contains(mappedX, mappedY)) {
+                pdfView.callbacks.callLinkHandler(new LinkTapEvent(x, y, mappedX, mappedY, mapped, link));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        if (!pdfView.isDoubletapEnabled()) {
+            return false;
+        }
+
+        if (pdfView.getZoom() < pdfView.getMidZoom()) {
+            pdfView.zoomWithAnimation(e.getX(), e.getY(), pdfView.getMidZoom());
+        } else if (pdfView.getZoom() < pdfView.getMaxZoom()) {
+            pdfView.zoomWithAnimation(e.getX(), e.getY(), pdfView.getMaxZoom());
+        } else {
+            pdfView.resetZoomWithAnimation();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        animationManager.stopFling();
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        scrolling = true;
+        if (pdfView.isZooming() || pdfView.isSwipeEnabled()) {
+            pdfView.moveRelativeTo(-distanceX, -distanceY);
+        }
+        if (!scaling || pdfView.doRenderDuringScale()) {
+            pdfView.loadPageByOffset();
+        }
+        return true;
+    }
+
+    private void onScrollEnd(MotionEvent event) {
+        pdfView.loadPages();
+        hideHandle();
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (!pdfView.isSwipeEnabled()) {
+            return false;
+        }
+        int xOffset = (int) pdfView.getCurrentXOffset();
+        int yOffset = (int) pdfView.getCurrentYOffset();
+
+        float minX, minY;
+        PdfFile pdfFile = pdfView.pdfFile;
+        if (pdfView.isSwipeVertical()) {
+            minX = -(pdfView.toCurrentScale(pdfFile.getMaxPageWidth()) - pdfView.getWidth());
+            minY = -(pdfFile.getDocLen(pdfView.getZoom()) - pdfView.getHeight());
+        } else {
+            minX = -(pdfFile.getDocLen(pdfView.getZoom()) - pdfView.getWidth());
+            minY = -(pdfView.toCurrentScale(pdfFile.getMaxPageHeight()) - pdfView.getHeight());
+        }
+
+        animationManager.startFlingAnimation(xOffset, yOffset, (int) (velocityX), (int) (velocityY),
+                (int) minX, 0, (int) minY, 0);
+
+        return true;
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        float dr = detector.getScaleFactor();
+        float wantedZoom = pdfView.getZoom() * dr;
+        if (wantedZoom < MINIMUM_ZOOM) {
+            dr = MINIMUM_ZOOM / pdfView.getZoom();
+        } else if (wantedZoom > MAXIMUM_ZOOM) {
+            dr = MAXIMUM_ZOOM / pdfView.getZoom();
+        }
+        pdfView.zoomCenteredRelativeTo(dr, new PointF(detector.getFocusX(), detector.getFocusY()));
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        scaling = true;
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        pdfView.loadPages();
+        hideHandle();
+        scaling = false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (!enabled) {
+            return false;
+        }
+
+        boolean retVal = scaleGestureDetector.onTouchEvent(event);
+        retVal = gestureDetector.onTouchEvent(event) || retVal;
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (scrolling) {
+                scrolling = false;
+                onScrollEnd(event);
+            }
+        }
+        return retVal;
+    }
+
+    private void hideHandle() {
+        ScrollHandle scrollHandle = pdfView.getScrollHandle();
+        if (scrollHandle != null && scrollHandle.shown()) {
+            scrollHandle.hideDelayed();
+        }
+    }
+}
