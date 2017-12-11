54c162d2224b232a56dc52de03874aed71fc5003
Jesús Aragón <jra@mega.co.nz>
2017-12-11 03:41:19 +0100

    v3.3 (170) - setRTFFormat returns null

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -75,7 +75,7 @@ import nz.mega.sdk.MegaUser;
 
 public class MegaApplication extends Application implements MegaListenerInterface, MegaChatRequestListenerInterface {
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.3_169";
+	static final String USER_AGENT = "MEGAAndroid/3.3_170";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
