8aa4dbb54d5de3893b51937da13f5bee29b3da5d
Jesús Aragón <jra@mega.co.nz>
2018-01-12 22:30:56 +0100

    Crash in ChatController when trying to download a file from ChatFullScreenImageViewer (without permissions)

--- app/src/main/java/mega/privacy/android/app/lollipop/controllers/ChatController.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/controllers/ChatController.java
@@ -1360,9 +1360,16 @@ public class ChatController {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             boolean hasStoragePermission = (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
             if (!hasStoragePermission) {
-                ActivityCompat.requestPermissions(((ManagerActivityLollipop) context),
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        Constants.REQUEST_WRITE_STORAGE);
+                if (context instanceof ManagerActivityLollipop) {
+                    ActivityCompat.requestPermissions(((ManagerActivityLollipop) context),
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            Constants.REQUEST_WRITE_STORAGE);
+                }
+                else if (context instanceof ChatFullScreenImageViewer){
+                    ActivityCompat.requestPermissions(((ChatFullScreenImageViewer) context),
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            Constants.REQUEST_WRITE_STORAGE);
+                }
             }
         }
 
