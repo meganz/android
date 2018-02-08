7601edb76e27ad1c80b8630ba3a0278f2bc3a321
Yenel Rodriguez <yr@mega.nz>
2018-02-08 10:19:57 +0100

    Bug #8473 - The contact status icon is not well placed on Contact Info for contacts without name

--- app/src/main/java/mega/privacy/android/app/lollipop/ContactInfoActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ContactInfoActivityLollipop.java
@@ -385,6 +385,7 @@ public class ContactInfoActivityLollipop extends PinActivityLollipop implements 
 			removeContactChatLayout.setOnClickListener(this);
 
 			chatHandle = extras.getLong("handle",-1);
+			userEmailExtra = extras.getString("name");
 			if (chatHandle != -1) {
 
 				log("From chat!!");
@@ -401,8 +402,17 @@ public class ContactInfoActivityLollipop extends PinActivityLollipop implements 
 
 				chatPrefs = dbH.findChatPreferencesByHandle(String.valueOf(chatHandle));
 
-				nameContact.setText(chat.getTitle());
-				nameLength.setText(chat.getTitle());
+				if (chat.getTitle() != null && !chat.getTitle().isEmpty() && !chat.getTitle().equals("")){
+					nameContact.setText(chat.getTitle());
+					nameLength.setText(chat.getTitle());
+				}
+				else {
+					if (userEmailExtra != null) {
+						String[] splitEmail = userEmailExtra.split("[@._]");
+						nameContact.setText(splitEmail[0]);
+						nameLength.setText(splitEmail[0]);
+					}
+				}
 				String fullname = (String)nameContact.getText();
 				setDefaultAvatar(fullname);
 			}
@@ -410,7 +420,6 @@ public class ContactInfoActivityLollipop extends PinActivityLollipop implements 
 				log("From contacts!!");
 
 				fromContacts = true;
-				userEmailExtra = extras.getString("name");
 				user = megaApi.getContact(userEmailExtra);
 
 				String fullName = "";
@@ -435,8 +444,10 @@ public class ContactInfoActivityLollipop extends PinActivityLollipop implements 
 						if (fullName.trim().length() <= 0){
 							log("Put email as fullname");
 							String email = user.getEmail();
-							String[] splitEmail = email.split("[@._]");
-							fullName = splitEmail[0];
+							if (email != null && !email.isEmpty() && !email.equals("")) {
+								String[] splitEmail = email.split("[@._]");
+								fullName = splitEmail[0];
+							}
 						}
 
 						nameContact.setText(fullName);
