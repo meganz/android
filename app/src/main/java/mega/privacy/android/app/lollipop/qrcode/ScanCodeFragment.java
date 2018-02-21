aaef584d05e27e9730e6399a105fa3580eb9aec4
Yenel Rodriguez <yr@mega.nz>
2018-02-21 16:47:20 +0100

    Feature #8390 - QR code for contact requests

--- app/src/main/java/mega/privacy/android/app/lollipop/qrcode/ScanCodeFragment.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/qrcode/ScanCodeFragment.java
@@ -5,33 +5,67 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.zxing.Result;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Locale;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import mega.privacy.android.app.DatabaseHandler;
+import mega.privacy.android.app.MegaApplication;
+import mega.privacy.android.app.MegaContactDB;
 import mega.privacy.android.app.R;
+import mega.privacy.android.app.components.RoundedImageView;
+import mega.privacy.android.app.lollipop.ManagerActivityLollipop;
+import mega.privacy.android.app.lollipop.controllers.ContactController;
 import mega.privacy.android.app.utils.Util;
+import nz.mega.sdk.MegaApiAndroid;
+import nz.mega.sdk.MegaApiJava;
+import nz.mega.sdk.MegaContactRequest;
+import nz.mega.sdk.MegaError;
+import nz.mega.sdk.MegaRequest;
+import nz.mega.sdk.MegaRequestListenerInterface;
+import nz.mega.sdk.MegaUser;
+
+import static android.app.Activity.RESULT_OK;
+import static android.graphics.Color.WHITE;
 
 /**
  * Created by mega on 22/01/18.
  */
 
-public class ScanCodeFragment extends Fragment implements ZXingScannerView.ResultHandler{
+public class ScanCodeFragment extends Fragment implements ZXingScannerView.ResultHandler, View.OnClickListener, MegaRequestListenerInterface{
 
+    public static int DEFAULT_AVATAR_WIDTH_HEIGHT = 150;
+    public static int WIDTH = 500;
+    public static String EXTRA_CONTACTS = "extra_contacts";
+    public static String EXTRA_NODE_HANDLE = "node_handle";
+    public static String EXTRA_MEGA_CONTACTS = "mega_contacts";
 
     private ActionBar aB;
 
@@ -40,6 +74,27 @@ public class ScanCodeFragment extends Fragment implements ZXingScannerView.Resul
     public static ZXingScannerView scannerView;
 
     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1010;
+
+    private AlertDialog inviteAlertDialog;
+    private AlertDialog requestedAlertDialog;
+    private MegaRequest request;
+
+    private Button invite;
+    private RoundedImageView avatarImage;
+    private TextView initialLetter;
+    private TextView contactName;
+    private TextView contactMail;
+
+    private TextView dialogTitle;
+    private TextView dialogText;
+    private Button dialogButton;
+
+    MegaUser myUser;
+    String myEmail;
+    MegaApiAndroid megaApi;
+    DatabaseHandler dbH = null;
+    Handler handler;
+    long handle;
 
     public static ScanCodeFragment newInstance() {
         log("newInstance");
@@ -56,6 +111,13 @@ public class ScanCodeFragment extends Fragment implements ZXingScannerView.Resul
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
         }
+
+        if (megaApi == null){
+            megaApi = ((MegaApplication) ((Activity)context).getApplication()).getMegaApi();
+        }
+
+        dbH = DatabaseHandler.getDbHandler(context);
+        handler = new Handler();
     }
 
     @Override
@@ -126,44 +188,59 @@ public class ScanCodeFragment extends Fragment implements ZXingScannerView.Resul
     @Override
     public void handleResult(Result rawResult) {
         log("handleResult");
-//        Do something with the result here
-//        log(rawResult.getText()); // Prints scan results
-//        log(rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
-
-        // call the alert dialog
-
-//        Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                scannerView.resumeCameraPreview(ScanCodeFragment.this);
-//            }
-//        }, 2000);
 
         invite(rawResult);
+    }
 
+    public void showAlertDialog (boolean accept) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_invite, null);
+        builder.setView(v);
+
+        dialogTitle = (TextView) v.findViewById(R.id.dialog_invite_title);
+        dialogText = (TextView) v.findViewById(R.id.dialog_invite_text);
+        dialogButton = (Button) v.findViewById(R.id.dialog_invite_button);
+        dialogButton.setOnClickListener(this);
+
+        if (accept){
+            dialogTitle.setText(getResources().getString(R.string.invite_accepted));
+            dialogText.setText(getResources().getString(R.string.invite_accepted_text, myEmail));
+        }
+        else {
+            dialogTitle.setText(getResources().getString(R.string.invite_sent));
+            dialogText.setText(getResources().getString(R.string.invite_sent_text, myEmail));
+        }
+
+        requestedAlertDialog = builder.create();
+        requestedAlertDialog.show();
     }
 
     public void invite (Result rawResult){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         LayoutInflater inflater = getActivity().getLayoutInflater();
+        String contactLink = rawResult.getText();
+        String[] s = contactLink.split("C!");
+        handle = MegaApiAndroid.base64ToHandle(s[1].trim());
+        log("Contact link: "+contactLink+ " s[1]: "+s[1]+" handle: "+handle);
+        if (megaApi == null){
+            megaApi = ((MegaApplication) ((Activity)context).getApplication()).getMegaApi();
+        }
+        megaApi.contactLinkQuery(handle, this);
 
-        builder.setView(inflater.inflate(R.layout.dialog_accept_contact, null));
+        View v = inflater.inflate(R.layout.dialog_accept_contact, null);
+        builder.setView(v);
 
+        invite = (Button) v.findViewById(R.id.accept_contact_invite);
+        invite.setOnClickListener(this);
 
-        builder.setMessage("Result :"+rawResult.getText()+"\nType :"+rawResult.getBarcodeFormat().toString())
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        scannerView.resumeCameraPreview(ScanCodeFragment.this);
-                    }
-                })
-                .setNegativeButton("Scan Again", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        scannerView.resumeCameraPreview(ScanCodeFragment.this);
-                    }
-                });
+        avatarImage = (RoundedImageView) v.findViewById(R.id.accept_contact_avatar);
+        initialLetter = (TextView) v.findViewById(R.id.accept_contact_initial_letter);
+        contactName = (TextView) v.findViewById(R.id.accept_contact_name);
+        contactMail = (TextView) v.findViewById(R.id.accept_contact_mail);
 
-        builder.create().show();
+        inviteAlertDialog = builder.create();
+        inviteAlertDialog.show();
     }
 
     @Override
@@ -186,4 +263,232 @@ public class ScanCodeFragment extends Fragment implements ZXingScannerView.Resul
         Util.log("ScanCodeFragment", log);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.accept_contact_invite: {
+                sendInvitation();
+                if (inviteAlertDialog != null){
+                    inviteAlertDialog.dismiss();
+                }
+                break;
+            }
+            case R.id.dialog_invite_button: {
+                scannerView.stopCamera();
+                if (requestedAlertDialog != null){
+                    requestedAlertDialog.dismiss();
+                }
+                getActivity().finish();
+                break;
+            }
+        }
+    }
+
+    public void sendInvitation () {
+        megaApi.inviteContact(myEmail, null, MegaContactRequest.INVITE_ACTION_ADD, (QRCodeActivity) context);
+    }
+
+    public void updateAvatar(boolean retry){
+        log("updateAvatar");
+        File avatar = null;
+        if(context!=null){
+            log("context is not null");
+
+            if (context.getExternalCacheDir() != null){
+                avatar = new File(context.getExternalCacheDir().getAbsolutePath(), myEmail + ".jpg");
+            }
+            else{
+                avatar = new File(context.getCacheDir().getAbsolutePath(), myEmail + ".jpg");
+            }
+        }
+        else{
+            log("context is null!!!");
+            if(getActivity()!=null){
+                log("getActivity is not null");
+                if (getActivity().getExternalCacheDir() != null){
+                    avatar = new File(getActivity().getExternalCacheDir().getAbsolutePath(), myEmail + ".jpg");
+                }
+                else{
+                    avatar = new File(getActivity().getCacheDir().getAbsolutePath(), myEmail + ".jpg");
+                }
+            }
+            else{
+                log("getActivity is ALSOOO null");
+                return;
+            }
+        }
+
+        if(avatar!=null){
+            setProfileAvatar(avatar, retry);
+        }
+        else{
+            setDefaultAvatar();
+        }
+    }
+
+    public void setProfileAvatar(File avatar, boolean retry){
+        log("setProfileAvatar");
+
+        Bitmap imBitmap = null;
+        if (avatar.exists()){
+            log("avatar path: "+avatar.getAbsolutePath());
+            if (avatar.length() > 0){
+                log("my avatar exists!");
+                BitmapFactory.Options bOpts = new BitmapFactory.Options();
+                bOpts.inPurgeable = true;
+                bOpts.inInputShareable = true;
+                imBitmap = BitmapFactory.decodeFile(avatar.getAbsolutePath(), bOpts);
+                if (imBitmap == null) {
+                    avatar.delete();
+                    log("Call to getUserAvatar");
+                    if(retry){
+                        log("Retry!");
+                        if (context.getExternalCacheDir() != null){
+                            megaApi.getUserAvatar(myUser, context.getExternalCacheDir().getAbsolutePath() + "/" + myEmail);
+                        }
+                        else{
+                            megaApi.getUserAvatar(myUser, context.getCacheDir().getAbsolutePath() + "/" + myEmail);
+                        }
+                    }
+                    else{
+                        log("DO NOT Retry!");
+                        setDefaultAvatar();
+                    }
+                }
+                else{
+                    log("Show my avatar");
+                    avatarImage.setImageBitmap(imBitmap);
+                    initialLetter.setVisibility(View.GONE);
+                }
+            }
+        }else{
+            log("my avatar NOT exists!");
+            log("Call to getUserAvatar");
+            if(retry){
+                log("Retry!");
+                if (context.getExternalCacheDir() != null){
+                    megaApi.getUserAvatar(myUser, context.getExternalCacheDir().getAbsolutePath() + "/" + myEmail);
+                }
+                else{
+                    megaApi.getUserAvatar(myUser, context.getCacheDir().getAbsolutePath() + "/" + myEmail);
+                }
+            }
+            else{
+                log("DO NOT Retry!");
+                setDefaultAvatar();
+            }
+        }
+    }
+
+    public void setDefaultAvatar(){
+        log("setDefaultAvatar");
+        Bitmap defaultAvatar = Bitmap.createBitmap(DEFAULT_AVATAR_WIDTH_HEIGHT,DEFAULT_AVATAR_WIDTH_HEIGHT, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(defaultAvatar);
+        Paint p = new Paint();
+        p.setAntiAlias(true);
+
+        String color = megaApi.getUserAvatarColor(myUser);
+        if(color!=null){
+            log("The color to set the avatar is "+color);
+            p.setColor(Color.parseColor(color));
+        }
+        else{
+            log("Default color to the avatar");
+            p.setColor(context.getResources().getColor(R.color.lollipop_primary_color));
+        }
+
+        int radius;
+        if (defaultAvatar.getWidth() < defaultAvatar.getHeight())
+            radius = defaultAvatar.getWidth()/2;
+        else
+            radius = defaultAvatar.getHeight()/2;
+
+        c.drawCircle(defaultAvatar.getWidth()/2, defaultAvatar.getHeight()/2, radius, p);
+        avatarImage.setImageBitmap(defaultAvatar);
+
+        float density = ((Activity) context).getResources().getDisplayMetrics().density;
+        int avatarTextSize = getAvatarTextSize(density);
+        log("DENSITY: " + density + ":::: " + avatarTextSize);
+
+        if (dbH == null) {
+            dbH = DatabaseHandler.getDbHandler(context);
+        }
+        MegaContactDB contactDB = dbH.findContactByHandle(String.valueOf(myUser.getHandle()+""));
+        String fullName = "";
+        if(contactDB!=null){
+            ContactController cC = new ContactController(context);
+            fullName = cC.getFullName(contactDB.getName(), contactDB.getLastName(),myEmail);
+        }
+        else{
+            //No name, ask for it and later refresh!!
+            fullName = myEmail;
+        }
+        String firstLetter = fullName.charAt(0) + "";
+        firstLetter = firstLetter.toUpperCase(Locale.getDefault());
+
+        initialLetter.setText(firstLetter);
+        initialLetter.setTextSize(30);
+        initialLetter.setTextColor(WHITE);
+        initialLetter.setVisibility(View.VISIBLE);
+    }
+
+    private int getAvatarTextSize (float density){
+        float textSize = 0.0f;
+
+        if (density > 3.0){
+            textSize = density * (DisplayMetrics.DENSITY_XXXHIGH / 72.0f);
+        }
+        else if (density > 2.0){
+            textSize = density * (DisplayMetrics.DENSITY_XXHIGH / 72.0f);
+        }
+        else if (density > 1.5){
+            textSize = density * (DisplayMetrics.DENSITY_XHIGH / 72.0f);
+        }
+        else if (density > 1.0){
+            textSize = density * (72.0f / DisplayMetrics.DENSITY_HIGH / 72.0f);
+        }
+        else if (density > 0.75){
+            textSize = density * (72.0f / DisplayMetrics.DENSITY_MEDIUM / 72.0f);
+        }
+        else{
+            textSize = density * (72.0f / DisplayMetrics.DENSITY_LOW / 72.0f);
+        }
+
+        return (int)textSize;
+    }
+
+    @Override
+    public void onRequestStart(MegaApiJava api, MegaRequest request) {
+
+    }
+
+    @Override
+    public void onRequestUpdate(MegaApiJava api, MegaRequest request) {
+
+    }
+
+    @Override
+    public void onRequestFinish(MegaApiJava api, MegaRequest request, MegaError e) {
+
+
+//        megaApi.contactLinkQuery(request.getNodeHandle(), this);
+        if (request.getType() == MegaRequest.TYPE_CONTACT_LINK_QUERY){
+            if (e.getErrorCode() == MegaError.API_OK){
+                log("Contact link query " + request.getNodeHandle() + "_" + MegaApiAndroid.handleToBase64(request.getNodeHandle()) + "_" + request.getEmail() + "_" + request.getName() + "_" + request.getText());
+
+                this.request = request;
+                myEmail = request.getEmail();
+                myUser = megaApi.getContact(myEmail);
+                contactName.setText(request.getName());
+                contactMail.setText(request.getEmail());
+
+                updateAvatar(false);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestTemporaryError(MegaApiJava api, MegaRequest request, MegaError e) {
+
+    }
 }
