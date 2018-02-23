41a2cd6647f9ebcc6e5c6d227500ca92a5643104
Carolina Zato <cz@mega.co.nz>
2018-02-23 13:50:41 +0100

    Task #8806 - Add video quality upload to chat settings

--- app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatSettings.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatSettings.java
@@ -6,12 +6,22 @@ public class ChatSettings {
     String notificationsEnabled = "";
     String notificationsSound = "";
     String vibrationEnabled = "";
+    String sendOriginalAttachments = "";
 
-    public ChatSettings(String enabled, String notificationsEnabled, String notificationsSound, String vibrationEnabled) {
+    public ChatSettings(String enabled, String notificationsEnabled, String notificationsSound, String vibrationEnabled, String sendOriginalAttachments) {
         this.enabled = enabled;
         this.notificationsEnabled = notificationsEnabled;
         this.notificationsSound = notificationsSound;
         this.vibrationEnabled = vibrationEnabled;
+        this.sendOriginalAttachments = sendOriginalAttachments;
+    }
+
+    public ChatSettings() {
+        this.enabled = true+"";
+        this.notificationsEnabled = true+"";
+        this.notificationsSound = "";
+        this.vibrationEnabled = true+"";
+        this.sendOriginalAttachments = false+"";
     }
 
     public String getEnabled() {
@@ -44,5 +54,13 @@ public class ChatSettings {
 
     public void setVibrationEnabled(String vibrationEnabled) {
         this.vibrationEnabled = vibrationEnabled;
+    }
+
+    public String getSendOriginalAttachments() {
+        return sendOriginalAttachments;
+    }
+
+    public void setSendOriginalAttachments(String sendOriginalAttachments) {
+        this.sendOriginalAttachments = sendOriginalAttachments;
     }
 }
