c4272c1598e5004b600d8ef6974987feb4299f42
Jesús Aragón <jra@mega.co.nz>
2018-05-03 11:45:34 +0200

    v3.3.5 (192)

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -59,7 +59,7 @@ import nz.mega.sdk.MegaUser;
 
 public class MegaApplication extends Application implements MegaListenerInterface, MegaChatRequestListenerInterface, MegaChatNotificationListenerInterface, MegaChatCallListenerInterface {
 	final String TAG = "MegaApplication";
-	static final String USER_AGENT = "MEGAAndroid/3.3.4_191";
+	static final String USER_AGENT = "MEGAAndroid/3.3.5_192";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
