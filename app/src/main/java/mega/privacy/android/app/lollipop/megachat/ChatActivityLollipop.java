72f7b815b8bbb47c84c515603dfe41044f875d4f
raquelgc6 <rgc@mega.nz>
2018-06-06 09:29:08 +0200

    Bug #9706-The scroll is incorrect when returning from full screen viewer in a chat conversation Bug #9625-Modify borders of Unread Messages layout

--- app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatActivityLollipop.java
@@ -188,7 +188,6 @@ String mOutputFilePath;
 //    AndroidMegaChatMessage selectedMessage;
     int selectedPosition;
     public long selectedMessageId = -1;
-
     MegaChatRoom chatRoom;
 
     public long idChat;
