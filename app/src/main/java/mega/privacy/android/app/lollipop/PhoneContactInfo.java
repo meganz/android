d06b9910b2e6f0a463bd8d0f1d079856c2499ccf
Yenel Rodriguez <yr@mega.nz>
2018-06-08 11:13:22 +0200

    Save instance on AddContactsActivity

--- app/src/main/java/mega/privacy/android/app/lollipop/PhoneContactInfo.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/PhoneContactInfo.java
@@ -1,8 +1,10 @@
 package mega.privacy.android.app.lollipop;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
-public class PhoneContactInfo implements  Comparable<PhoneContactInfo>{
+public class PhoneContactInfo implements  Comparable<PhoneContactInfo>, Parcelable{
     long id;
     String name;
     String email;
@@ -39,4 +41,36 @@ public class PhoneContactInfo implements  Comparable<PhoneContactInfo>{
 
         return a.compareTo(b);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(name);
+        dest.writeString(email);
+        dest.writeString(phoneNumber);
+    }
+
+    protected PhoneContactInfo(Parcel in) {
+        id = in.readLong();
+        name = in.readString();
+        email = in.readString();
+        phoneNumber = in.readString();
+    }
+
+    public static final Creator<PhoneContactInfo> CREATOR = new Creator<PhoneContactInfo>() {
+        @Override
+        public PhoneContactInfo createFromParcel(Parcel in) {
+            return new PhoneContactInfo(in);
+        }
+
+        @Override
+        public PhoneContactInfo[] newArray(int size) {
+            return new PhoneContactInfo[size];
+        }
+    };
 }
