b411899c9f25ca39d88cb543c571fbbcf8585130
Carolina Zato <cz@mega.co.nz>
2018-06-15 11:54:17 +0200

    Task #9687: Unify notification icon on status bar

--- app/src/main/java/mega/privacy/android/app/lollipop/LoginFragmentLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/LoginFragmentLollipop.java
@@ -1930,6 +1930,7 @@ public class LoginFragmentLollipop extends Fragment implements View.OnClickListe
                     try {
                         serversBusyText.setVisibility(View.VISIBLE);
                         if(error.getErrorCode()==MegaError.API_EAGAIN){
+                            log("onRequestTemporaryError:onFinish:API_EAGAIN: :value: "+error.getValue());
                             if(error.getValue() == MegaApiJava.RETRY_CONNECTIVITY){
                                 serversBusyText.setText(getString(R.string.login_connectivity_issues));
                             }
