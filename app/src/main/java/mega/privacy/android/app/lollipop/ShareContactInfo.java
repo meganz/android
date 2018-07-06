8375156316d5e9a3368d7c4bc95a22d969e140fd
Yenel Rodriguez <yr@mega.nz>
2018-07-06 14:48:20 +0200

    Change AddContactActivity design

--- app/src/main/java/mega/privacy/android/app/lollipop/ShareContactInfo.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ShareContactInfo.java
@@ -14,86 +14,79 @@ import nz.mega.sdk.MegaUser;
 
 public class ShareContactInfo{
 
-    long id;
-    String name;
-    String email;
-    String phoneNumber;
+    PhoneContactInfo phoneContactInfo;
+    MegaContactAdapter megaContactAdapter;
+    String mail;
+    boolean isPhoneContact;
+    boolean isMegaContact;
+    boolean isHeader;
+    boolean isLastItem;
 
-    MegaContactDB megaContactDB;
-    MegaUser megaUser;
-    String fullName;
-
-    boolean phoneContactInfo;
-    boolean megaContactAdapter;
-
-    public ShareContactInfo(long id, String name, String email, String phoneNumber, MegaContactDB megaContactDB, MegaUser megaUser, String fullName, boolean phoneContactInfo, boolean megaContactAdapter) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-
-
-        this.megaContactDB = megaContactDB;
-        this.megaUser = megaUser;
-        this.fullName = fullName;
-
+    public ShareContactInfo(PhoneContactInfo phoneContactInfo, MegaContactAdapter megaContactAdapter, String mail) {
         this.phoneContactInfo = phoneContactInfo;
+        if (phoneContactInfo != null) {
+            isPhoneContact = true;
+        }
+        else {
+            isPhoneContact = false;
+        }
+
         this.megaContactAdapter = megaContactAdapter;
+        if (megaContactAdapter != null) {
+            isMegaContact = true;
+        }
+        else {
+            isMegaContact = false;
+        }
+
+        this.mail = mail;
+        isHeader = false;
+        isLastItem = false;
     }
 
-    public long getId(){
-        return id;
+    public ShareContactInfo(boolean isHeader, boolean isMegaContact, boolean isPhoneContact){
+        this.isHeader = isHeader;
+        this.isMegaContact = isMegaContact;
+        this.isPhoneContact = isPhoneContact;
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getPhoneNumber(){
-        return phoneNumber;
-    }
-
-    public MegaContactDB getMegaContactDB() {
-        return megaContactDB;
-    }
-
-    public void setMegaContactDB(MegaContactDB megaContactDB) {
-        this.megaContactDB = megaContactDB;
-    }
-
-    public MegaUser getMegaUser() {
-        return megaUser;
-    }
-
-    public void setMegaUser(MegaUser megaUser) {
-        this.megaUser = megaUser;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public boolean getPhoneContactInfo() {
+    public PhoneContactInfo getPhoneContactInfo() {
         return phoneContactInfo;
     }
 
-    public void setPhoneContactInfo(boolean phoneContactInfo) {
+    public void setPhoneContactInfo(PhoneContactInfo phoneContactInfo) {
         this.phoneContactInfo = phoneContactInfo;
     }
 
-    public boolean getMegaContactAdapter() {
+    public MegaContactAdapter getMegaContactAdapter() {
         return megaContactAdapter;
     }
 
-    public void setMegaContactAdapter(boolean megaContactAdapter) {
+    public void setMegaContactAdapter(MegaContactAdapter megaContactAdapter) {
         this.megaContactAdapter = megaContactAdapter;
+    }
+
+    public String getMail () {
+        return mail;
+    }
+
+    public boolean isHeader () {
+        return isHeader;
+    }
+
+    public void setLastItem (boolean isLastItem){
+        this.isLastItem = isLastItem;
+    }
+
+    public boolean isLastItem (){
+        return isLastItem;
+    }
+
+    public boolean isPhoneContact () {
+        return isPhoneContact;
+    }
+
+    public boolean isMegaContact () {
+        return isMegaContact;
     }
 }
