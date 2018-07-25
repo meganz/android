ce982fc121e54e1d3e7464b7de8d10a25c3353f5
ygaoy <yg@mega.co.nz>
2018-07-26 11:19:19 +1200

    Move console log to seperate thread to improve performance

--- app/src/main/java/mega/privacy/android/app/AndroidLogger.java
+++ app/src/main/java/mega/privacy/android/app/AndroidLogger.java
@@ -1,150 +1,109 @@
 package mega.privacy.android.app;
 
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import mega.privacy.android.app.utils.Util;
-import nz.mega.sdk.MegaLoggerInterface;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import mega.privacy.android.app.utils.Util;
+import nz.mega.sdk.MegaLoggerInterface;
+
 
 public class AndroidLogger implements MegaLoggerInterface {
+    public static final String LOG_FILE_NAME = "logSDK.txt";
+    protected static ConcurrentLinkedDeque<String> logQueue;
+    protected static String separator = "&&";
+    protected SimpleDateFormat simpleDateFormat;
+    protected File logFile;
+    protected String dir;
 
-	public void log(String time, int loglevel, String source, String message) {
+    public AndroidLogger(String fileName, boolean fileLogger) {
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        logFile = null;
+        dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Util.logDIR + "/";
+        logQueue = new ConcurrentLinkedDeque<>();
 
-		try {
-			if (Util.DEBUG) {
-				try {
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-					String currentDateandTime = sdf.format(new Date());
+        if (fileLogger) {
 
-					message = "(" + currentDateandTime + ") - " + message;
-				} catch (Exception e) {
-				}
+            //check if log file exist, create one if not
+            File dirFile = new File(dir);
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
+            }
 
-				String sourceMessage = "";
-				if (source != null) {
-					String[] s = source.split("jni/mega");
-					if (s != null) {
-						if (s.length > 1) {
-							sourceMessage = s[1] + "";
-						} else {
-							sourceMessage = source + "";
-						}
-					}
-				}
-
-				Log.d("AndroidLogger", sourceMessage + ": " + message);
-				//			addRecordToLog("AndroidLogger: " + source + ": " + message);
-			}
-
-			File logFile = null;
-			boolean fileLogger = Util.getFileLoggerSDK();
-			if (fileLogger) {
-				//Send the log to a file
-
-				try {
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-					String currentDateandTime = sdf.format(new Date());
-
-					message = "(" + currentDateandTime + ") - " + message;
-				} catch (Exception e) {
-				}
-
-				String sourceMessage = "";
-				if (source != null) {
-					String[] s = source.split("jni/mega");
-					if (s != null) {
-						if (s.length > 1) {
-							sourceMessage = s[1] + "";
-						} else {
-							sourceMessage = source + "";
-						}
-					}
-				}
-
-				String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Util.logDIR + "/";
-				//			String file = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+logDIR+"/log.txt";
-				File dirFile = new File(dir);
-				if (!dirFile.exists()) {
-					dirFile.mkdirs();
-					logFile = new File(dirFile, "logSDK.txt");
-					if (!logFile.exists()) {
-						try {
-							logFile.createNewFile();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				} else {
-					logFile = new File(dirFile, "logSDK.txt");
-					if (!logFile.exists()) {
-						try {
-							logFile.createNewFile();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-
-				if (logFile != null && logFile.exists()) {
-					//				String sourceMessage = "";
-					//				if (source != null) {
-					//					String[] s = source.split("jni/mega");
-					//					if (s != null) {
-					//						if (s.length > 1) {
-					//							sourceMessage = s[1] + "";
-					//						} else {
-					//							sourceMessage = source + "";
-					//						}
-					//					}
-					//				}
-
-					Util.appendStringToFile(sourceMessage + ": " + message + "\n", logFile);
-				}
-			}
-		}
-		catch (Exception exc) {
-			Log.d("AndroidLogger", source + ": " + message);
-		}
-
-	}
-	 
-	public static void addRecordToLog(String message) {
-		 
-		File logDir;
-		if (Environment.getExternalStorageDirectory() != null){
-			logDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Util.logDIR + "/");
-			logDir.mkdirs();
-			
-			File logFile = new File (logDir, "log.txt");
-			if (!logFile.exists()){
-				try  {
+            logFile = new File(dirFile, fileName);
+            if (!logFile.exists()) {
+                try {
                     logFile.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-			}
-			
-			try { 
-				BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
-				buf.write(message + "\r\n");
-				buf.newLine();
-                buf.flush();
-                buf.close();
-			}
-			catch (IOException e) {
-                e.printStackTrace();
             }
-			
-		}
-	}
+        }
+
+        startAsyncLogger();
+    }
+
+    @Override
+    public synchronized void log(String time, int logLevel, String source, String message) {
+        //display to console
+        if (Util.DEBUG) {
+            logQueue.add("AndroidLogger" + separator + createSourceMessage(message) + ": " + createMessage(message));
+        }
+
+        //save to log file
+        if (logFile != null && logFile.exists()) {
+            Util.appendStringToFile(createSourceMessage(message) + ": " + createMessage(message) + "\n", logFile);
+        }
+    }
+
+    protected String createMessage(String message) {
+        String currentDateAndTime = simpleDateFormat.format(new Date());
+        message = "(" + currentDateAndTime + ") - " + message;
+        return message;
+    }
+
+    protected String createSourceMessage(String source) {
+        String sourceMessage = "";
+        if (source != null) {
+            String[] s = source.split("jni/mega");
+            if (s != null) {
+                if (s.length > 1) {
+                    sourceMessage = s[1] + "";
+                } else {
+                    sourceMessage = source + "";
+                }
+            }
+        }
+
+        return sourceMessage;
+    }
+
+    protected synchronized static void startAsyncLogger() {
+        AsyncTask<Void, Void, Void> myTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                while (true) {
+                    String log = logQueue.pollFirst();
+                    if (log != null) {
+                        String[] combined = log.split(separator);
+                        Log.d(combined[0], combined[1]);
+                    } else {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        myTask.execute();
+    }
 }
