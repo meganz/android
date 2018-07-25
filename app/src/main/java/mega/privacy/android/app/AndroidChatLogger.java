47063df9160a9c5d522aa08d6f66266f9d08e497
ygaoy <yg@mega.co.nz>
2018-07-26 11:19:19 +1200

    Move console log to seperate thread to improve performance

--- app/src/main/java/mega/privacy/android/app/AndroidChatLogger.java
+++ app/src/main/java/mega/privacy/android/app/AndroidChatLogger.java
@@ -1,106 +1,25 @@
 package mega.privacy.android.app;
 
-
-import android.os.Environment;
-import android.util.Log;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import mega.privacy.android.app.utils.Util;
 import nz.mega.sdk.MegaChatLoggerInterface;
-import nz.mega.sdk.MegaLoggerInterface;
 
 
-public class AndroidChatLogger implements MegaChatLoggerInterface {
+public class AndroidChatLogger extends AndroidLogger implements MegaChatLoggerInterface {
+    public static final String LOG_FILE_NAME = "logKarere.txt";
+    public AndroidChatLogger(String fileName, boolean fileLogger) {
+        super(fileName, fileLogger);
+    }
 
-	public void log(int loglevel, String message) {
+    @Override
+    public synchronized void log(int logLevel, String message) {
+        //display to console
+        if (Util.DEBUG) {
+            logQueue.add("AndroidChatLogger" + separator + createMessage(message));
+        }
 
-		try {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			String currentDateandTime = sdf.format(new Date());
-
-			message = "(" + currentDateandTime + ") - " + message;
-
-
-			if (Util.DEBUG){
-				Log.d("AndroidChatLogger: ", message);
-	//			addRecordToLog("AndroidLogger: " + source + ": " + message);
-			}
-
-			File logFile=null;
-			boolean fileLogger = Util.getFileLoggerKarere();
-			if (fileLogger) {
-				//Send the log to a file
-
-				String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Util.logDIR + "/";
-				//			String file = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+logDIR+"/log.txt";
-				File dirFile = new File(dir);
-				if (!dirFile.exists()) {
-					dirFile.mkdirs();
-					logFile = new File(dirFile, "logKarere.txt");
-					if (!logFile.exists()) {
-						try {
-							logFile.createNewFile();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				} else {
-					logFile = new File(dirFile, "logKarere.txt");
-					if (!logFile.exists()) {
-						try {
-							logFile.createNewFile();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-
-				if (logFile != null && logFile.exists()) {
-					Util.appendStringToFile(message, logFile);
-				}
-			}
-		}
-		catch (Exception e){
-			Log.d("AndroidChatLogger: ", message);
-		}
-
-	}
-	 
-	public static void addRecordToLog(String message) {
-		 
-		File logDir;
-		if (Environment.getExternalStorageDirectory() != null){
-			logDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Util.logDIR + "/");
-			logDir.mkdirs();
-			
-			File logFile = new File (logDir, "log.txt");
-			if (!logFile.exists()){
-				try  {
-                    logFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-			}
-			
-			try { 
-				BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
-				buf.write(message + "\r\n");
-				buf.newLine();
-                buf.flush();
-                buf.close();
-			}
-			catch (IOException e) {
-                e.printStackTrace();
-            }
-			
-		}
-	}
+        //save to log file
+        if (logFile != null && logFile.exists()) {
+            Util.appendStringToFile(message, logFile);
+        }
+    }
 }
