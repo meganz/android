de8c66473b488e72d71af86397b453e71949331c
Yenel Rodriguez <yr@mega.nz>
2018-08-08 12:56:56 +0200

    Minimal changes on UI Force user at the end of the 2FA enable process to back up his Recovery Key if has not already done

--- app/src/main/java/mega/privacy/android/app/lollipop/LoginFragmentLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/LoginFragmentLollipop.java
@@ -6,6 +6,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Typeface;
@@ -24,6 +25,7 @@ import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -705,6 +707,89 @@ public class LoginFragmentLollipop extends Fragment implements View.OnClickListe
             }
         });
         ((LoginActivityLollipop) context).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        firstPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb1 = firstPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb1.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb1.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        firstPin.setLayoutParams(paramsb1);
+        LinearLayout.LayoutParams textParams = (LinearLayout.LayoutParams)firstPin.getLayoutParams();
+        textParams.setMargins(0, 0, Util.scaleWidthPx(8, outMetrics), 0);
+        firstPin.setLayoutParams(textParams);
+
+        secondPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb2 = secondPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb2.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb2.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        secondPin.setLayoutParams(paramsb2);
+        textParams = (LinearLayout.LayoutParams)secondPin.getLayoutParams();
+        textParams.setMargins(0, 0, Util.scaleWidthPx(8, outMetrics), 0);
+        secondPin.setLayoutParams(textParams);
+        secondPin.setEt(firstPin);
+
+        thirdPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb3 = thirdPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb3.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb3.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        thirdPin.setLayoutParams(paramsb3);
+        textParams = (LinearLayout.LayoutParams)thirdPin.getLayoutParams();
+        textParams.setMargins(0, 0, Util.scaleWidthPx(25, outMetrics), 0);
+        thirdPin.setLayoutParams(textParams);
+        thirdPin.setEt(secondPin);
+
+        fourthPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb4 = fourthPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb4.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb4.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        fourthPin.setLayoutParams(paramsb4);
+        textParams = (LinearLayout.LayoutParams)fourthPin.getLayoutParams();
+        textParams.setMargins(0, 0, Util.scaleWidthPx(8, outMetrics), 0);
+        fourthPin.setLayoutParams(textParams);
+        fourthPin.setEt(thirdPin);
+
+        fifthPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb5 = fifthPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb5.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb5.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        fifthPin.setLayoutParams(paramsb5);
+        textParams = (LinearLayout.LayoutParams)fifthPin.getLayoutParams();
+        textParams.setMargins(0, 0, Util.scaleWidthPx(8, outMetrics), 0);
+        fifthPin.setLayoutParams(textParams);
+        fifthPin.setEt(fourthPin);
+
+        sixthPin.setGravity(Gravity.CENTER_HORIZONTAL);
+        android.view.ViewGroup.LayoutParams paramsb6 = sixthPin.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            paramsb6.width = Util.scaleWidthPx(42, outMetrics);
+        }
+        else {
+            paramsb6.width = Util.scaleWidthPx(25, outMetrics);
+        }
+        sixthPin.setLayoutParams(paramsb6);
+        textParams = (LinearLayout.LayoutParams)sixthPin.getLayoutParams();
+        textParams.setMargins(0, 0, 0, 0);
+        sixthPin.setLayoutParams(textParams);
+        sixthPin.setEt(fifthPin);
 
         intentReceived = ((LoginActivityLollipop) context).getIntent();
         if(intentReceived!=null){
