31968a333ecb48e337112ca1498d504b9b100960
Carolina Zato <cz@mega.co.nz>
2018-08-16 12:15:28 +0200

    Bug #10279: Add "Me" to strings xml file and format it programatically

--- app/src/main/java/mega/privacy/android/app/lollipop/megachat/chatAdapters/MegaListChatLollipopAdapter.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/chatAdapters/MegaListChatLollipopAdapter.java
@@ -2024,7 +2024,7 @@ public class MegaListChatLollipopAdapter extends RecyclerView.Adapter<MegaListCh
 				if(lastMsgSender==megaChatApi.getMyUserHandle()){
 
 					log("getLastMessageSender: the last message is mine: "+lastMsgSender);
-					Spannable me = new SpannableString("Me: ");
+					Spannable me = new SpannableString(context.getString(R.string.word_me)+" ");
 					me.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.file_list_first_row)), 0, me.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
 					if(lastMessageString!=null) {
