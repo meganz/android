81e5adc0605956a8291f1e659c3a1b68a99c7476
aw <aw@mega.co.nz>
2018-09-13 16:25:16 +1200

    fix the issue: when the column number changes, cannot display correctly. fix the issue in FileBrowserFragmentLollipop.

--- app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
@@ -884,11 +884,10 @@ public class FullScreenImageViewerLollipop extends PinActivityLollipop implement
 
 		Intent intent = getIntent();
 		positionG = intent.getIntExtra("position", 0);
-		//If inserted a placehoder in CloudDriveAdapter,here the position need to be -1.
-        if (intent.getBooleanExtra("placeholder",false)) {
-            positionG -= 1;
-        }
-		orderGetChildren = intent.getIntExtra("orderGetChildren", MegaApiJava.ORDER_DEFAULT_ASC);
+		//If inserted a placehoder in CloudDriveAdapter,here the position need to be remove the placeholder.
+        int placeholder = intent.getIntExtra("placeholder",0 );
+        positionG -= placeholder;
+        orderGetChildren = intent.getIntExtra("orderGetChildren", MegaApiJava.ORDER_DEFAULT_ASC);
 		isFolderLink = intent.getBooleanExtra("isFolderLink", false);
 		isFileLink = intent.getBooleanExtra("isFileLink",false);
 
