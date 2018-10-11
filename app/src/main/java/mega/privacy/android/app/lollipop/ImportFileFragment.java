d88d0499b933b4317a0db8e0544be9418bbea654
Yenel Rodriguez <yr@mega.nz>
2018-10-11 09:20:17 +0200

    Task #10316-New UX-UI for "Import to Mega"

---
+++ app/src/main/java/mega/privacy/android/app/lollipop/ImportFileFragment.java
@@ -0,0 +1,24 @@
+package mega.privacy.android.app.lollipop;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mega.privacy.android.app.R;
+
+public class ImportFileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_importfile, container, false);
+
+        return v;
+    }
+}
