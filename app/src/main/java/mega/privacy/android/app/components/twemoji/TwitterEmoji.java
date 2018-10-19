37e1e587e87d897fcc5e1973f2c25ca51763c66e
raquelgc6 <rgc@mega.nz>
2018-10-19 11:28:59 +0200

    Task#10724-Add twemoji in chat

--- app/src/main/java/mega/privacy/android/app/components/twemoji/TwitterEmoji.java
+++ app/src/main/java/mega/privacy/android/app/components/twemoji/TwitterEmoji.java
@@ -14,16 +14,16 @@ import java.lang.ref.SoftReference;
 
 import mega.privacy.android.app.components.twemoji.emoji.CacheKey;
 import mega.privacy.android.app.components.twemoji.emoji.Emoji;
+import mega.privacy.android.app.utils.Util;
 
 public class TwitterEmoji extends Emoji {
+
   private static final Object LOCK = new Object();
   private static final int NUM_STRIPS = 51;
-  private static final SoftReference[] STRIP_REFS =
-      new SoftReference[NUM_STRIPS];
+  private static final SoftReference[] STRIP_REFS = new SoftReference[NUM_STRIPS];
   private static final int CACHE_SIZE = 100;
-  private static final LruCache<CacheKey, Bitmap> BITMAP_CACHE =
-      new LruCache<>(CACHE_SIZE);
-  private static final int SPRITE_SIZE = 64;
+  private static final LruCache<CacheKey, Bitmap> BITMAP_CACHE = new LruCache<>(CACHE_SIZE);
+  private static final int SPRITE_SIZE = 65;
   private static final int SPRITE_SIZE_INC_BORDER = 66;
 
   static {
@@ -32,51 +32,50 @@ public class TwitterEmoji extends Emoji {
     }
   }
 
-  private final int x;
-  private final int y;
+  private int x=0;
+  private int y=0;
 
-  public TwitterEmoji(@NonNull final int[] codePoints, final int x, final int y) {
+  public TwitterEmoji(@NonNull  int[] codePoints, int x, int y) {
     super(codePoints, -1);
-
     this.x = x;
     this.y = y;
   }
 
-  public TwitterEmoji(final int codePoint, final int x, final int y) {
+  public TwitterEmoji(int codePoint, int x, int y) {
     super(codePoint, -1);
-
     this.x = x;
     this.y = y;
   }
 
-  public TwitterEmoji(final int codePoint, final int x, final int y, final Emoji... variants) {
+  public TwitterEmoji( int codePoint,int x, int y, Emoji... variants) {
     super(codePoint, -1, variants);
-
     this.x = x;
     this.y = y;
   }
 
-  public TwitterEmoji(@NonNull final int[] codePoints, final int x, final int y, final Emoji... variants) {
+  public TwitterEmoji(@NonNull int[] codePoints, int x, int y, Emoji... variants) {
     super(codePoints, -1, variants);
-
     this.x = x;
     this.y = y;
   }
 
   @NonNull @Override public Drawable getDrawable(final Context context) {
+    log("getDrawable()");
+
     final CacheKey key = new CacheKey(x, y);
     final Bitmap bitmap = BITMAP_CACHE.get(key);
     if (bitmap != null) {
       return new BitmapDrawable(context.getResources(), bitmap);
     }
     final Bitmap strip = loadStrip(context);
-    final Bitmap cut = Bitmap.createBitmap(strip, 1,
-        y * SPRITE_SIZE_INC_BORDER + 1, SPRITE_SIZE, SPRITE_SIZE);
+    final Bitmap cut = Bitmap.createBitmap(strip, 1, y * SPRITE_SIZE_INC_BORDER + 1, SPRITE_SIZE, SPRITE_SIZE);
     BITMAP_CACHE.put(key, cut);
     return new BitmapDrawable(context.getResources(), cut);
   }
 
   private Bitmap loadStrip(final Context context) {
+    log("loadStrip()");
+
     Bitmap strip = (Bitmap) STRIP_REFS[x].get();
     if (strip == null) {
       synchronized (LOCK) {
@@ -94,6 +93,7 @@ public class TwitterEmoji extends Emoji {
   }
 
   @Override public void destroy() {
+    log("destroy()");
     synchronized (LOCK) {
       BITMAP_CACHE.evictAll();
       for (int i = 0; i < NUM_STRIPS; i++) {
@@ -104,5 +104,9 @@ public class TwitterEmoji extends Emoji {
         }
       }
     }
+  }
+
+  public static void log(String message) {
+    Util.log("TwitterEmoji", message);
   }
 }
