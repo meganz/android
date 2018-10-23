5a124d9d3ff398773544e18aa9e975bdf239a877
Jesús Aragón <jra@mega.co.nz>
2018-10-23 12:19:12 +0200

    v3.4.1 (212)

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -88,7 +88,7 @@ import android.support.v4.provider.FontRequest;
 public class MegaApplication extends MultiDexApplication implements MegaGlobalListenerInterface, MegaChatRequestListenerInterface, MegaChatNotificationListenerInterface, MegaChatCallListenerInterface, NetworkStateReceiver.NetworkStateReceiverListener {
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.4.0_211";
+	static final public String USER_AGENT = "MEGAAndroid/3.4.1_212";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
