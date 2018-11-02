9e883e538ea1cbb2cbbc48a2a745e0ed897a2afb
raquelgc6 <rgc@mega.nz>
2018-11-02 18:23:48 +0100

    Task#10724-Add twemoji in chat

--- app/src/main/java/mega/privacy/android/app/components/twemoji/EmojiUtils.java
+++ app/src/main/java/mega/privacy/android/app/components/twemoji/EmojiUtils.java
@@ -13,7 +13,7 @@ import mega.privacy.android.app.utils.Util;
 public final class EmojiUtils {
   private static final Pattern SPACE_REMOVAL = Pattern.compile("[\\s]");
 
-  /** returns true when the string contains only emojis. Note that whitespace will be filtered out. */
+  //returns true when the string contains only emojis. Note that whitespace will be filtered out.
   public boolean isOnlyEmojis(@Nullable final String text) {
 
     if (!TextUtils.isEmpty(text)) {
@@ -35,18 +35,17 @@ public final class EmojiUtils {
 
   /** returns the emojis that were found in the given text */
   @NonNull public static List<EmojiRange> emojis(@Nullable final String text) {
-    return EmojiManager.getInstance().findAllEmojis(text, false);
+    return EmojiManager.getInstance().findAllEmojis(text);
   }
 
-//  /** returns the number of all emojis that were found in the given text */
+//  returns the number of all emojis that were found in the given text
 //  public static int emojisCount(@Nullable final String text) {
 //    return emojis(text).size();
 //  }
 
-  /** returns a class that contains all of the emoji information that was found in the given text */
+  //returns a class that contains all of the emoji information that was found in the given text
   @NonNull public EmojiInformation emojiInformation(@Nullable final String text) {
-
-    final List<EmojiRange> emojis = EmojiManager.getInstance().findAllEmojis(text, false);
+    final List<EmojiRange> emojis = EmojiManager.getInstance().findAllEmojis(text);
 
     final boolean isOnlyEmojis = isOnlyEmojis(text);
 
