a18dff6543b041adf767bac54532fc6d2c47c5fc
aw <aw@mega.co.nz>
2018-11-23 13:35:41 +1300

    issue#incoming-call for testing

---
+++ app/src/main/java/mega/privacy/android/app/utils/TL.java
@@ -0,0 +1,56 @@
+package mega.privacy.android.app.utils;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Logger for testing.
+ */
+public class TL {
+
+    private final static boolean OUTPUT = true;
+
+    private static final String LOG_FILE = Environment.getExternalStorageDirectory() + File.separator +
+            "MEGA"+ File.separator +
+            "MEGA Logs"+ File.separator +
+            "test_log.txt";
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd HH:mm:ss.SSS");
+
+    public static void log(Object context,Object any) {
+        String msg = (any == null) ? "NULL" : any.toString();
+        String dateStr = DATE_FORMAT.format(new Date());
+        if (context != null) {
+            if (context instanceof String) {
+                msg = "[" + dateStr + "] " + context + "--->" + msg;
+            } else {
+                msg = "[" + dateStr + "] " + context.getClass().getSimpleName() + "--->" + msg;
+            }
+        }
+        if (OUTPUT) {
+            File log = new File(LOG_FILE);
+            try {
+                if (!log.exists()) {
+                    log.createNewFile();
+                }
+                FileWriter writer = new FileWriter(LOG_FILE,true);
+                writer.write(msg + "\n");
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Log.e("@#@",msg);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Date(1540165899000L));
+    }
+}
