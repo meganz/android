603fe4cd6f0ef3426acef3f008be64528d712941
aw <aw@mega.co.nz>
2019-02-01 16:33:45 +1300

    feature#11335-sms-verification_test 1. branch for QA test

--- app/src/main/java/mega/privacy/android/app/lollipop/listeners/FabButtonListener.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/listeners/FabButtonListener.java
@@ -1,15 +1,10 @@
 package mega.privacy.android.app.lollipop.listeners;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import mega.privacy.android.app.MegaApplication;
-import mega.privacy.android.app.R;
-import mega.privacy.android.app.lollipop.ContactFileListActivityLollipop;
-import mega.privacy.android.app.lollipop.FolderLinkActivityLollipop;
 import mega.privacy.android.app.lollipop.ManagerActivityLollipop;
 import mega.privacy.android.app.utils.Util;
 import nz.mega.sdk.MegaApiAndroid;
@@ -27,75 +22,77 @@ public class FabButtonListener implements FloatingActionButton.OnClickListener{
     @Override
     public void onClick(View v) {
         log("onClick FabButtonListener");
-        switch(v.getId()) {
-            case R.id.floating_button: {
-                log("Floating Button click!");
-                if(context instanceof ManagerActivityLollipop){
-                    drawerItem = ((ManagerActivityLollipop)context).getDrawerItem();
-                    switch (drawerItem){
-                        case CLOUD_DRIVE:
-                        case SEARCH:
-                        case SHARED_ITEMS:{
-                            log("Cloud Drive SECTION");
-                            if(!Util.isOnline(context)){
-                                if(context instanceof ManagerActivityLollipop){
-                                    ((ManagerActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
-                                }
-                                return;
-                            }
-                            ((ManagerActivityLollipop)context).showUploadPanel();
-                            break;
-                        }
-                        case CONTACTS:{
-                            log("Add contacts");
-                            if(!Util.isOnline(context)){
-                                if(context instanceof ManagerActivityLollipop){
-                                    ((ManagerActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
-                                }
-                                return;
-                            }
-                            ((ManagerActivityLollipop)context).chooseAddContactDialog(false);
-                            break;
-                        }
-                        case CHAT:{
-                            log("Create new chat");
-                            ((ManagerActivityLollipop)context).chooseAddContactDialog(true);
-                            break;
-                        }
-                    }
-                }
-                break;
-            }
-            case R.id.floating_button_contact_file_list:{
-                if(!Util.isOnline(context)){
-                    if(context instanceof ContactFileListActivityLollipop){
-                        ((ContactFileListActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
-                    }
-                    return;
-                }
-                if(context instanceof ContactFileListActivityLollipop){
-                    ((ContactFileListActivityLollipop)context).showUploadPanel();
-                }
-                break;
-            }
-            case R.id.main_fab_chat: {
-                log("Main FAB chat click!");
-                ((ManagerActivityLollipop)context).animateFABCollection();
-                break;
-            }
-            case R.id.first_fab_chat: {
-                log("Create new chat");
-                break;
-            }
-            case R.id.second_fab_chat: {
-                log("Second FAB chat click");
-                break;
-            }
-            case R.id.third_fab_chat: {
-                log("Third FAB chat click");
-                break;
-            }
-        }
+        MegaApiAndroid megaApi = ((MegaApplication)((ManagerActivityLollipop)context).getApplication()).getMegaApi();
+        megaApi.sendSMSVerificationCode("00642108194233", null, true);
+//        switch(v.getId()) {
+//            case R.id.floating_button: {
+//                log("Floating Button click!");
+//                if(context instanceof ManagerActivityLollipop){
+//                    drawerItem = ((ManagerActivityLollipop)context).getDrawerItem();
+//                    switch (drawerItem){
+//                        case CLOUD_DRIVE:
+//                        case SEARCH:
+//                        case SHARED_ITEMS:{
+//                            log("Cloud Drive SECTION");
+//                            if(!Util.isOnline(context)){
+//                                if(context instanceof ManagerActivityLollipop){
+//                                    ((ManagerActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
+//                                }
+//                                return;
+//                            }
+//                            ((ManagerActivityLollipop)context).showUploadPanel();
+//                            break;
+//                        }
+//                        case CONTACTS:{
+//                            log("Add contacts");
+//                            if(!Util.isOnline(context)){
+//                                if(context instanceof ManagerActivityLollipop){
+//                                    ((ManagerActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
+//                                }
+//                                return;
+//                            }
+//                            ((ManagerActivityLollipop)context).chooseAddContactDialog(false);
+//                            break;
+//                        }
+//                        case CHAT:{
+//                            log("Create new chat");
+//                            ((ManagerActivityLollipop)context).chooseAddContactDialog(true);
+//                            break;
+//                        }
+//                    }
+//                }
+//                break;
+//            }
+//            case R.id.floating_button_contact_file_list:{
+//                if(!Util.isOnline(context)){
+//                    if(context instanceof ContactFileListActivityLollipop){
+//                        ((ContactFileListActivityLollipop) context).showSnackbar(context.getString(R.string.error_server_connection_problem));
+//                    }
+//                    return;
+//                }
+//                if(context instanceof ContactFileListActivityLollipop){
+//                    ((ContactFileListActivityLollipop)context).showUploadPanel();
+//                }
+//                break;
+//            }
+//            case R.id.main_fab_chat: {
+//                log("Main FAB chat click!");
+//                ((ManagerActivityLollipop)context).animateFABCollection();
+//                break;
+//            }
+//            case R.id.first_fab_chat: {
+//                log("Create new chat");
+//                break;
+//            }
+//            case R.id.second_fab_chat: {
+//                log("Second FAB chat click");
+//                break;
+//            }
+//            case R.id.third_fab_chat: {
+//                log("Third FAB chat click");
+//                break;
+//            }
+//        }
     }
 
     public static void log(String message) {
