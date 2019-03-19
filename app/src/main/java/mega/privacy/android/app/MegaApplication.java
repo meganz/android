ea8e09baf9a6e3d0d8a855b7902157d3583683f4
Javier Gomez <jg@mega.co.nz>
2019-03-19 15:02:51 +0100

    Update version code - v3.6.1 (229)
    Upgrade SDK and MEGAchat submodules.

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -98,7 +98,7 @@ import static mega.privacy.android.app.utils.Util.toCDATA;
 public class MegaApplication extends MultiDexApplication implements MegaGlobalListenerInterface, MegaChatRequestListenerInterface, MegaChatNotificationListenerInterface, MegaChatCallListenerInterface, NetworkStateReceiver.NetworkStateReceiverListener, MegaChatListenerInterface {
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.6.1_228";
+	static final public String USER_AGENT = "MEGAAndroid/3.6.1_229";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
