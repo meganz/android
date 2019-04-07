35902573ffb804dbbae74a05b56cff9bdf7902e5
Chris Li <cl@mega.co.nz>
2019-04-08 09:53:28 +1200

    space issue

--- app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
@@ -9127,9 +9127,9 @@ public class ManagerActivityLollipop extends PinActivityLollipop implements Mega
 						});
 						break;
 					}
+                    case RUBBISH_BIN:
 		        	case CLOUD_DRIVE:
-		        	case INBOX:
-                    case RUBBISH_BIN:{
+		        	case INBOX:{
 
 		        		ascendingCheck.setOnClickListener(new OnClickListener() {
 
