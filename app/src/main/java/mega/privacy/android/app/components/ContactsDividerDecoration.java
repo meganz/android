6ba19063810d14ca5b2f53b0153d57b98f7b1065
aw <aw@mega.co.nz>
2019-07-12 09:54:24 +1200

    chat_invitation 1. add new divider decoration.

---
+++ app/src/main/java/mega/privacy/android/app/components/ContactsDividerDecoration.java
@@ -0,0 +1,32 @@
+package mega.privacy.android.app.components;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import static mega.privacy.android.app.lollipop.InvitationContactInfo.TYPE_MEGA_CONTACT_HEADER;
+import static mega.privacy.android.app.lollipop.InvitationContactInfo.TYPE_PHONE_CONTACT_HEADER;
+
+public class ContactsDividerDecoration extends SimpleDividerItemDecoration {
+
+    public ContactsDividerDecoration(Context context, DisplayMetrics outMetrics) {
+        super(context, outMetrics);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            if (child.getTag() instanceof RecyclerView.ViewHolder) {
+                int position = ((RecyclerView.ViewHolder) child.getTag()).getAdapterPosition();
+                int type = parent.getAdapter().getItemViewType(position);
+                if (type != TYPE_MEGA_CONTACT_HEADER && type != TYPE_PHONE_CONTACT_HEADER) {
+                    drawDivider(c, parent, parent.getChildAt(i));
+                }
+            }
+        }
+    }
+}
