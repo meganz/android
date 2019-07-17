c2619a860f5c4737df3cca79ddde668417305f95
Alberto Rodríguez <ar@mega.nz>
2019-07-17 14:16:07 +0200

    Adapt compilation process for 64bits

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -3,7 +3,7 @@ USE_LIBWEBSOCKETS = true
 
 USE_LIBWEBSOCKETS = true
 APP_PLATFORM = android-16
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+
 NDK_TOOLCHAIN_VERSION=clang
 APP_STL := c++_static
 APP_OPTIM := release
