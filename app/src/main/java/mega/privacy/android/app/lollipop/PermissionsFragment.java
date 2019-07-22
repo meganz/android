140cd8519ada6df22372684e325c1c54588076c9
Chris Li <cl@mega.co.nz>
2019-07-23 11:53:54 +1200

    Bug#12094: unused import statement

--- app/src/main/java/mega/privacy/android/app/lollipop/PermissionsFragment.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/PermissionsFragment.java
@@ -3,7 +3,6 @@ package mega.privacy.android.app.lollipop;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
