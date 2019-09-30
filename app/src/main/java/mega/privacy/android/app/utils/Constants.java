4933d2249536547bb81cd806157ddc4777872482
Chris Li <cl@mega.co.nz>
2019-10-01 11:31:43 +1300

    Feature13085 polish

--- app/src/main/java/mega/privacy/android/app/utils/Constants.java
+++ app/src/main/java/mega/privacy/android/app/utils/Constants.java
@@ -1,5 +1,6 @@
 package mega.privacy.android.app.utils;
 
+import java.io.File;
 import java.util.regex.Pattern;
 
 public class Constants {
@@ -379,7 +380,7 @@ public class Constants {
     public static final String CHAT_ID_TO_ANSWER = "chatHandleToAnswer";
     public static final String SECOND_CALL = "SECOND_CALL";
 
-    public static final String SEPERATOR = "/";
+    public static final String SEPERATOR = File.separator;
     public static final String[] FILE_LINK_REGEXS = {
             "^https://mega\\.co\\.nz/#!.+$",
             "^https://mega\\.nz/#!.+$",
