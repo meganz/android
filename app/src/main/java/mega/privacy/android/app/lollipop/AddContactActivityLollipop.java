650af1377c3d69a1e6447f15c461492298760a4b
aw <aw@mega.co.nz>
2019-11-13 15:26:39 +1300

    chat_invitation_chat_link 1. apply the new design for empty view in `RecentChatsFragment` 2. change `invite` button to `new chat link` button and corresponding click event.

--- app/src/main/java/mega/privacy/android/app/lollipop/AddContactActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/AddContactActivityLollipop.java
@@ -195,6 +195,9 @@ public class AddContactActivityLollipop extends PinActivityLollipop implements V
 
     private boolean comesFromChat;
 
+    private boolean comesFromRecent;
+    public static final String FROM_RECENT = "comesFromRecent";
+
     private RelativeLayout headerContacts;
     private TextView textHeader;
 
@@ -1482,6 +1485,7 @@ public class AddContactActivityLollipop extends PinActivityLollipop implements V
             contactType = getIntent().getIntExtra("contactType", CONTACT_TYPE_MEGA);
             chatId = getIntent().getLongExtra("chatId", -1);
             newGroup = getIntent().getBooleanExtra("newGroup", false);
+            comesFromRecent = getIntent().getBooleanExtra(FROM_RECENT, false);
             if (newGroup) {
                 createNewGroup = true;
                 contactsNewGroup = getIntent().getStringArrayListExtra("contactsNewGroup");
@@ -1834,6 +1838,16 @@ public class AddContactActivityLollipop extends PinActivityLollipop implements V
         }
 
         setGetChatLinkVisibility();
+        if(comesFromRecent) {
+            // wait 50ms for initialization
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    createNewChatLink = true;
+                    newGroup();
+                }
+            }, 50);
+        }
     }
 
     private void setEmptyStateVisibility (boolean visible) {
@@ -3042,8 +3056,12 @@ public class AddContactActivityLollipop extends PinActivityLollipop implements V
             if (addedContactsMEGA.contains(myContact)) {
                 addedContactsMEGA.remove(myContact);
             }
-            returnToAddContacts();
-            createMyContact();
+            if (comesFromRecent) {
+                finish();
+            } else {
+                returnToAddContacts();
+                createMyContact();
+            }
         } else if (createNewGroup && (newGroup || onlyCreateGroup)) {
             finish();
         } else if ((createNewGroup || createNewChatLink) && (!newGroup || !onlyCreateGroup)) {
