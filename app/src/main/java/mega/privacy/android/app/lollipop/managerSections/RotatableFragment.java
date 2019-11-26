25b30349c0d07ee4468db456956acfe36306eded
Yenel Rodriguez <yr@mega.nz>
2019-11-26 15:16:01 +1300

    Fix multiselection when it rotates the screen in all the possible cases except when the search result has not any folders

--- app/src/main/java/mega/privacy/android/app/lollipop/managerSections/RotatableFragment.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/managerSections/RotatableFragment.java
@@ -10,6 +10,7 @@ import mega.privacy.android.app.lollipop.ManagerActivityLollipop;
 import mega.privacy.android.app.lollipop.adapters.RotatableAdapter;
 
 import static mega.privacy.android.app.utils.LogUtil.*;
+import static mega.privacy.android.app.utils.Util.isScreenInPortrait;
 
 public abstract class RotatableFragment extends Fragment {
 
@@ -37,30 +38,27 @@ public abstract class RotatableFragment extends Fragment {
         if (selectedItems == null) return;
 
         RotatableAdapter adapter = getAdapter();
-        if (adapter != null) {
-            int folderCount = adapter.getFolderCount();
-            logDebug("Folders: " + folderCount);
-            int currentPlaceHolderCount = adapter.getPlaceholderCount();
-            logDebug("Place holder: " + currentPlaceHolderCount);
-            if (selectedItems.size() > 0) {
-                activateActionMode();
-                for (int selectedItem : selectedItems) {
-                    if (isList()) {
-                        logDebug("selectedItem:" + selectedItem);
-                        multipleItemClick(selectedItem);
-                    } else {
-//                        if (selectedItem < folderCount) {
-//                            logDebug("List folder, selectedItem: " + selectedItem);
-//                            multipleItemClick(selectedItem);
-//                        } else {
-                            logDebug("File selection, selectedItem: " + selectedItem + "lastPlaceHolderCount:" + lastPlaceHolderCount + ". currentPlaceHolderCount: " + currentPlaceHolderCount);
-                            multipleItemClick((selectedItem - lastPlaceHolderCount + currentPlaceHolderCount));
-//                        }
-                    }
+        if (adapter == null) return;
+
+        if (selectedItems.size() > 0) {
+            activateActionMode();
+
+            for (int selectedItem : selectedItems) {
+                int position;
+
+                if (isList() || adapter.getFolderCount() == 0) {
+                    position = selectedItem;
+                } else if (isScreenInPortrait(getContext())){
+                    position = selectedItem - (lastPlaceHolderCount - adapter.getPlaceholderCount());
+                } else {
+                    position = selectedItem + (adapter.getPlaceholderCount() - lastPlaceHolderCount);
                 }
+
+                multipleItemClick(position);
             }
-            updateActionModeTitle();
         }
+
+        updateActionModeTitle();
     }
 
     private boolean isList() {
