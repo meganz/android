96deb5e505b9fa0e47ece94891c33afddbe381f3
Chris Li <cl@mega.co.nz>
2020-01-09 15:24:54 +1300

    12626 UI change for taken down image

--- app/src/main/java/mega/privacy/android/app/lollipop/adapters/MegaPhotoSyncListAdapterLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/adapters/MegaPhotoSyncListAdapterLollipop.java
@@ -29,7 +29,6 @@ import mega.privacy.android.app.R;
 import mega.privacy.android.app.components.scrollBar.SectionTitleProvider;
 import mega.privacy.android.app.lollipop.managerSections.CameraUploadFragmentLollipop;
 import mega.privacy.android.app.lollipop.managerSections.CameraUploadFragmentLollipop.PhotoSyncHolder;
-import mega.privacy.android.app.utils.LogUtil;
 import mega.privacy.android.app.utils.ThumbnailUtilsLollipop;
 import nz.mega.sdk.MegaApiAndroid;
 import nz.mega.sdk.MegaNode;
@@ -45,39 +44,40 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 	private static final int ITEM_VIEW_TYPE_MONTH = 1;
 
 	private SparseBooleanArray selectedItems = new SparseBooleanArray();;
-	
+
 	private ViewHolderPhotoSyncList holder = null;
 
 	Context context;
 	private MegaApiAndroid megaApi;
 
 	private ArrayList<PhotoSyncHolder> nodesArray;
-	
+
 	private long photoSyncHandle;
-	
+
 	RecyclerView listFragment;
 	boolean multipleSelect;
-	
+
 	Object fragment;
 	private int type;
-	
+
 	/*public static view holder class*/
     public static class ViewHolderPhotoSyncList extends RecyclerView.ViewHolder {
-    	
+
     	public ViewHolderPhotoSyncList(View v){
     		super(v);
     	}
-    	
+
     	public ImageView imageView;
     	public TextView textViewFileName;
     	public TextView textViewFileSize;
+    	public ImageView takenDownIcon;
     	public RelativeLayout itemLayout;
     	public RelativeLayout monthLayout;
     	public TextView monthTextView;
     	public int currentPosition;
     	public long document;
     }
-	
+
 	public MegaPhotoSyncListAdapterLollipop(Context _context, ArrayList<PhotoSyncHolder> _nodesArray, long _photosyncHandle, RecyclerView listView, Object fragment, int type) {
 		this.context = _context;
 		this.nodesArray = _nodesArray;
@@ -89,7 +89,7 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 			megaApi = MegaApplication.getInstance().getMegaApi();
 		}
 	}
-	
+
 	public void setNodes(ArrayList<PhotoSyncHolder> nodesArray, ArrayList<MegaNode> nodes){
 		this.nodesArray = nodesArray;
 		notifyDataSetChanged();
@@ -110,7 +110,7 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 	public RecyclerView getListFragment() {
 		return this.listFragment;
 	}
-	
+
 	public void setPhotoSyncHandle(long photoSyncHandle){
 		this.photoSyncHandle = photoSyncHandle;
 		notifyDataSetChanged();
@@ -119,32 +119,32 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 	public Object getItem(int position) {
         return nodesArray.get(position);
     }
- 
+
     @Override
     public long getItemId(int position) {
         return position;
-    }    
-    
+    }
+
     @Override
 	public void onClick(View v) {
 		ViewHolderPhotoSyncList holder = (ViewHolderPhotoSyncList) v.getTag();
 		int currentPosition = holder.currentPosition;
 		PhotoSyncHolder psH = (PhotoSyncHolder) getItem(currentPosition);
-		
+
 		if (megaApi == null){
 			return;
 		}
-		
+
 		MegaNode n = megaApi.getNodeByHandle(psH.handle);
-		
+
 		if (n == null){
 			return;
 		}
-		
+
 		switch (v.getId()){
 			case R.id.photo_sync_list_item_layout:{
 				if (type == CAMERA_UPLOAD_ADAPTER){
-					ImageView imageView = (ImageView) v.findViewById(R.id.photo_sync_list_thumbnail);
+					ImageView imageView = v.findViewById(R.id.photo_sync_list_thumbnail);
 					int[] positionIV = new int[2];
 					imageView.getLocationOnScreen(positionIV);
 					int[] screenPosition = new int[4];
@@ -156,9 +156,9 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 				}
 				break;
 			}
-		}		
+		}
 	}
-		
+
 	public boolean isMultipleSelect() {
 		return multipleSelect;
 	}
@@ -169,11 +169,11 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 			notifyDataSetChanged();
 		}
 	}
-	
+
 	public long getPhotoSyncHandle(){
 		return photoSyncHandle;
 	}
-	
+
 	/*
 	 * Get document at specified position
 	 */
@@ -187,13 +187,13 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 		}
 		return null;
 	}
-	
+
 	/*
 	 * Get list of all selected nodes
 	 */
 	public List<PhotoSyncHolder> getSelectedDocuments() {
 		ArrayList<PhotoSyncHolder> nodes = new ArrayList<PhotoSyncHolder>();
-		
+
 		for (int i = 0; i < selectedItems.size(); i++) {
 			if (selectedItems.valueAt(i)) {
 				PhotoSyncHolder document = getNodeAt(selectedItems.keyAt(i));
@@ -266,17 +266,17 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 
 		startAnimation(pos, putOrDeletePosition(pos));
 	}
-	
+
 	public void toggleSelection(final int pos) {
 		logDebug("Position: " + pos);
 
 		startAnimation(pos, putOrDeletePosition(pos));
 	}
-	
+
 	private boolean isItemChecked(int position) {
         return selectedItems.get(position);
     }
-	
+
 	public void selectAll(){
 		for (int i= 0; i<this.getItemCount();i++){
 			if(!isItemChecked(i)){
@@ -284,7 +284,7 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 			}
 		}
 	}
-	
+
 	public void clearSelections() {
 		for (int i= 0; i<this.getItemCount();i++){
 			if(isItemChecked(i)){
@@ -299,111 +299,108 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 	}
 
 	@Override
-	public void onBindViewHolder(ViewHolderPhotoSyncList holder, int position) {
-		logDebug("Position: " + position);
-		
-		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
-		DisplayMetrics outMetrics = new DisplayMetrics();
-		display.getMetrics(outMetrics);
+    public void onBindViewHolder(ViewHolderPhotoSyncList holder, int position) {
+        logDebug("Position: " + position);
 
-		holder.currentPosition = position;
-		
-		if (!multipleSelect){
-			holder.itemLayout.setBackgroundColor(Color.WHITE);
-		}
-		else{
-			if(this.isItemChecked(position)){
-				holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
-			}
-			else{
-				holder.itemLayout.setBackgroundColor(Color.WHITE);
-			}
-		}
-		
-		PhotoSyncHolder psh = (PhotoSyncHolder) getItem(position);
-		if (psh.isNode){
-			MegaNode node = megaApi.getNodeByHandle(psh.handle);
-			holder.document = node.getHandle();
-			Bitmap thumb;
-			
-			holder.textViewFileName.setText(node.getName());
-			holder.monthLayout.setVisibility(View.GONE);
-			holder.itemLayout.setVisibility(View.VISIBLE);
-			
-			if (node.isFolder()){
+        Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
 
-			}
-			else{
-				long nodeSize = node.getSize();
-				holder.textViewFileSize.setText(getSizeString(nodeSize));
-				if (this.isItemChecked(position)) {
-					RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)holder.imageView.getLayoutParams();
-					params.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,48,context.getResources().getDisplayMetrics());
-					params.width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,48,context.getResources().getDisplayMetrics());
-					params.setMargins(0,0,0,0);
-					holder.imageView.setLayoutParams(params);
-					holder.imageView.setImageResource(R.drawable.ic_select_folder);
-				}
-				else {RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)holder.imageView.getLayoutParams();
-					params.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,36,context.getResources().getDisplayMetrics());
-					params.width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,36,context.getResources().getDisplayMetrics());
-					int left = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,6,context.getResources().getDisplayMetrics());
-					params.setMargins(left,0,0,0);
-					holder.imageView.setLayoutParams(params);
-					holder.imageView.setImageResource(MimeTypeList.typeForName(node.getName()).getIconResourceId());
+        holder.currentPosition = position;
 
-					if (node.hasThumbnail()) {
-						thumb = ThumbnailUtilsLollipop.getThumbnailFromCache(node);
-						if (thumb != null) {
-							holder.imageView.setImageBitmap(thumb);
-						}
-						else {
-							thumb = ThumbnailUtilsLollipop.getThumbnailFromFolder(node, context);
-							if (thumb != null) {
-								holder.imageView.setImageBitmap(thumb);
-							}
-							else {
-								try {
-									thumb = ThumbnailUtilsLollipop.getThumbnailFromMegaPhotoSyncList(node, context, holder, megaApi, this);
-								} catch (Exception e) {
-									logError("Exception happens: " + e.toString());
-								} //Too many AsyncTasks
+        if (!multipleSelect) {
+            holder.itemLayout.setBackgroundColor(Color.WHITE);
+        } else {
+            if (this.isItemChecked(position)) {
+                holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
+            } else {
+                holder.itemLayout.setBackgroundColor(Color.WHITE);
+            }
+        }
 
-								if (thumb != null) {
-									holder.imageView.setImageBitmap(thumb);
-								}
-							}
-						}
-					}
-					else {
-						thumb = ThumbnailUtilsLollipop.getThumbnailFromCache(node);
-						if (thumb != null) {
-							holder.imageView.setImageBitmap(thumb);
-						}
-						else {
-							thumb = ThumbnailUtilsLollipop.getThumbnailFromFolder(node, context);
-							if (thumb != null) {
-								holder.imageView.setImageBitmap(thumb);
-							}
-							else {
-								try {
-									ThumbnailUtilsLollipop.createThumbnailPhotoSyncList(context, node, holder, megaApi, this);
-								} catch (Exception e) {
-									logError("Exception happens: " + e.toString());
-								} //Too many AsyncTasks
-							}
-						}
-					}
-				}
-			}
-		}
-		else{
-			holder.monthTextView.setText(psh.monthYear);
-			holder.itemLayout.setVisibility(View.GONE);
-			holder.monthLayout.setVisibility(View.VISIBLE);
-			
-		}
-	}
+        PhotoSyncHolder psh = (PhotoSyncHolder) getItem(position);
+        if (psh.isNode) {
+            MegaNode node = megaApi.getNodeByHandle(psh.handle);
+            holder.document = node.getHandle();
+            Bitmap thumb;
+
+            holder.textViewFileName.setText(node.getName());
+            holder.monthLayout.setVisibility(View.GONE);
+            holder.itemLayout.setVisibility(View.VISIBLE);
+
+            long nodeSize = node.getSize();
+            holder.textViewFileSize.setText(getSizeString(nodeSize));
+            if (this.isItemChecked(position)) {
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
+                params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
+                params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
+                params.setMargins(0, 0, 0, 0);
+                holder.imageView.setLayoutParams(params);
+                holder.imageView.setImageResource(R.drawable.ic_select_folder);
+            } else {
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
+                params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 36, context.getResources().getDisplayMetrics());
+                params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 36, context.getResources().getDisplayMetrics());
+                int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6, context.getResources().getDisplayMetrics());
+                params.setMargins(left, 0, 0, 0);
+                holder.imageView.setLayoutParams(params);
+                holder.imageView.setImageResource(MimeTypeList.typeForName(node.getName()).getIconResourceId());
+
+                if (node.hasThumbnail()) {
+                    thumb = ThumbnailUtilsLollipop.getThumbnailFromCache(node);
+                    if (thumb != null) {
+                        holder.imageView.setImageBitmap(thumb);
+                    } else {
+                        thumb = ThumbnailUtilsLollipop.getThumbnailFromFolder(node, context);
+                        if (thumb != null) {
+                            holder.imageView.setImageBitmap(thumb);
+                        } else {
+                            try {
+                                thumb = ThumbnailUtilsLollipop.getThumbnailFromMegaPhotoSyncList(node, context, holder, megaApi, this);
+                            } catch (Exception e) {
+                                logError("Exception happens: " + e.toString());
+                            } //Too many AsyncTasks
+
+                            if (thumb != null) {
+                                holder.imageView.setImageBitmap(thumb);
+                            }
+                        }
+                    }
+                } else {
+                    thumb = ThumbnailUtilsLollipop.getThumbnailFromCache(node);
+                    if (thumb != null) {
+                        holder.imageView.setImageBitmap(thumb);
+                    } else {
+                        thumb = ThumbnailUtilsLollipop.getThumbnailFromFolder(node, context);
+                        if (thumb != null) {
+                            holder.imageView.setImageBitmap(thumb);
+                        } else {
+                            try {
+                                ThumbnailUtilsLollipop.createThumbnailPhotoSyncList(context, node, holder, megaApi, this);
+                            } catch (Exception e) {
+                                logError("Exception happens: " + e.toString());
+                            } //Too many AsyncTasks
+                        }
+                    }
+                }
+
+                if (node.isTakenDown()) {
+                    holder.textViewFileName.setTextColor(context.getResources().getColor(R.color.dark_primary_color));
+                    holder.takenDownIcon.setVisibility(View.VISIBLE);
+                } else {
+                    holder.textViewFileName.setTextColor(context.getResources().getColor(R.color.black));
+                    holder.takenDownIcon.setVisibility(View.GONE);
+                }
+            }
+
+        } else {
+            holder.monthTextView.setText(psh.monthYear);
+            holder.itemLayout.setVisibility(View.GONE);
+            holder.monthLayout.setVisibility(View.VISIBLE);
+
+        }
+    }
+
 	@Override
 	public int getItemViewType(int position) {
 		logDebug("Position: " + position);
@@ -428,13 +425,13 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 	@Override
 	public ViewHolderPhotoSyncList onCreateViewHolder(ViewGroup parent, int viewType) {
 		logDebug("onCreateViewHolder");
-		
+
 		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
 		DisplayMetrics outMetrics = new DisplayMetrics();
 		display.getMetrics(outMetrics);
 
 		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_photo_sync_list, parent, false);
-		
+
 		holder = new ViewHolderPhotoSyncList(v);
 		holder.itemLayout = v.findViewById(R.id.photo_sync_list_item_layout);
 		holder.monthLayout = v.findViewById(R.id.photo_sync_list_month_layout);
@@ -442,12 +439,13 @@ public class MegaPhotoSyncListAdapterLollipop extends RecyclerView.Adapter<MegaP
 		holder.imageView = v.findViewById(R.id.photo_sync_list_thumbnail);
 		holder.textViewFileName = v.findViewById(R.id.photo_sync_list_filename);
 		holder.textViewFileSize = v.findViewById(R.id.photo_sync_list_filesize);
-		
+		holder.takenDownIcon = v.findViewById(R.id.photo_sync_list_taken_down);
+
 		v.setTag(holder);
 		holder.itemLayout.setTag(holder);
 		holder.itemLayout.setOnClickListener(this);
 		holder.itemLayout.setOnLongClickListener(this);
-		
+
 		return holder;
 	}
 
