9406e69878d685b4cfeb3fd75e8999cba041bd02
raquelgc6 <rgc@mega.nz>
2020-01-14 13:26:29 +0100

    Conflicts resolved

--- app/src/main/java/mega/privacy/android/app/lollipop/ContactInfoActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ContactInfoActivityLollipop.java
@@ -103,6 +103,7 @@ import nz.mega.sdk.MegaRequestListenerInterface;
 import nz.mega.sdk.MegaShare;
 import nz.mega.sdk.MegaUser;
 import nz.mega.sdk.MegaUserAlert;
+
 import static mega.privacy.android.app.modalbottomsheet.UtilsModalBottomSheet.*;
 import static mega.privacy.android.app.utils.CacheFolderManager.*;
 import static mega.privacy.android.app.utils.ChatUtil.*;
