ef1fdeabff72953f2302d0d23b07958c9aeb909c
Javier Gomez <jg@mega.co.nz>
2020-01-29 17:02:10 +0100

    Update version code - v3.7.4 (286)
    Update SDK and MEGAchat submodules.

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -98,7 +98,7 @@ public class MegaApplication extends MultiDexApplication implements MegaChatRequ
 
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.7.3_285";
+	static final public String USER_AGENT = "MEGAAndroid/3.7.4_286";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
