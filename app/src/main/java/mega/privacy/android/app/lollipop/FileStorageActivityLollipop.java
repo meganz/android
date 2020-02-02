150c7c060eb42c0daf7af0e0650fdda68f38e284
Chris Li <cl@mega.co.nz>
2020-02-03 10:56:24 +1300

    Use the Util method

--- app/src/main/java/mega/privacy/android/app/lollipop/FileStorageActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/FileStorageActivityLollipop.java
@@ -529,11 +529,10 @@ public class FileStorageActivityLollipop extends PinActivityLollipop implements 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
-
-		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-			emptyImageView.setImageResource(R.drawable.ic_zero_landscape_empty_folder);
-		}else{
+		if (isScreenInPortrait(this)) {
 			emptyImageView.setImageResource(R.drawable.ic_zero_portrait_empty_folder);
+		} else {
+			emptyImageView.setImageResource(R.drawable.ic_zero_landscape_empty_folder);
 		}
 	}
 
