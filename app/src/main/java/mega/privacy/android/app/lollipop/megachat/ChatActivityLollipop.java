1a5e9982d10c5a4645aa4262f4434021ff9d22f2
Yenel Rodriguez <yr@mega.nz>
2020-03-24 16:47:20 +0100

    Fix crash opening Group chat info

--- app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/ChatActivityLollipop.java
@@ -2467,6 +2467,8 @@ public class ChatActivityLollipop extends DownloadableActivity implements MegaCh
      */
     public void setRecordingNow(boolean recordingNow) {
         logDebug("recordingNow: " + recordingNow);
+        if (recordView == null) return;
+
         recordView.setRecordingNow(recordingNow);
         if (recordView.isRecordingNow()) {
             recordButtonStates(RECORD_BUTTON_ACTIVATED);
