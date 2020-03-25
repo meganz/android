8f52e6221315dfbac00c388697bcf7a99662ca00
raquelgc6 <rgc@mega.nz>
2020-03-25 11:01:35 +0100

    Fixed a wrong code due to a develop merge

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -102,7 +102,7 @@ public class MegaApplication extends MultiDexApplication implements MegaChatRequ
 
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.7.4_294";
+	static final public String USER_AGENT = "MEGAAndroid/3.7.4_297";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
@@ -1619,7 +1619,6 @@ public class MegaApplication extends MultiDexApplication implements MegaChatRequ
         super.unregisterReceiver(receiver);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(logoutReceiver);
 		LocalBroadcastManager.getInstance(this).unregisterReceiver(chatCallUpdateReceiver);
-
 	}
 
     public static boolean isVerifySMSShowed() {
