2a808e2a3e68cc7d901b9e1aefc356fcf34a34f8
Yenel Rodriguez <yr@mega.nz>
2020-04-29 12:57:50 +0200

    When rejoining a chat link conversation, if some messages where received while the user was not participating in it, they are not received until they logged out and logged in (Bug #15492)

--- app/src/main/java/mega/privacy/android/app/utils/Constants.java
+++ app/src/main/java/mega/privacy/android/app/utils/Constants.java
@@ -444,6 +444,7 @@ public class Constants {
     public static final String USER_HANDLES = "USER_HANDLES";
     public static final String URL_FILE_LINK = "URL_FILE_LINK";
     public static final String OPEN_SCAN_QR = "OPEN_SCAN_QR";
+    public static final String CHAT_LINK_EXTRA = "CHAT_LINK";
 
     public static final String SHOW_MESSAGE_UPLOAD_STARTED = "SHOW_MESSAGE_UPLOAD_STARTED";
     public static final String NUMBER_UPLOADS = "NUMBER_UPLOADS";
