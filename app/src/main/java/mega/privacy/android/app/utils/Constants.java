df5f6b23fb2d6fc4150b33cf7900d1ad3828e0c5
Javier Gomez <jg@mega.co.nz>
2020-06-23 10:46:53 +0200

    Adjust `MEGA_REGEXS` values
    Bug #16126: Vulnerability report in relation to export
    "OpenLinkActivity"

--- app/src/main/java/mega/privacy/android/app/utils/Constants.java
+++ app/src/main/java/mega/privacy/android/app/utils/Constants.java
@@ -475,8 +475,8 @@ public class Constants {
     public static final String SEPARATOR = File.separator;
 
     public static final String[] MEGA_REGEXS = {
-            "^https://mega\\.co\\.nz",
-            "^https://mega\\.nz",
+            "^https://mega\\.co\\.nz.+$",
+            "^https://mega\\.nz.+$",
     };
 
     public static final String[] FILE_LINK_REGEXS = {
