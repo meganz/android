4f2350668759379a3585ee0af923ee3358013624
Javier Gomez <jg@mega.co.nz>
2020-06-23 12:08:20 +0200

    Update version code - v3.7.7 (315)

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -100,7 +100,7 @@ public class MegaApplication extends MultiDexApplication implements MegaChatRequ
 
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.7.7_313";
+	static final public String USER_AGENT = "MEGAAndroid/3.7.7_315";
 
 	DatabaseHandler dbH;
 	MegaApiAndroid megaApi;
