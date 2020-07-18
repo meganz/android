331938cfcd42b32578324de41fe7fd1562098e19
aw <aw@mega.co.nz>
2020-07-19 10:44:30 +1200

    On CU photo transfer finishes, use Glide to create preview and thumbnail.
    bug#16433-cu-crash

--- app/src/main/java/mega/privacy/android/app/utils/ImageProcessor.java
+++ app/src/main/java/mega/privacy/android/app/utils/ImageProcessor.java
@@ -11,8 +11,12 @@ import android.net.Uri;
 import android.provider.BaseColumns;
 import android.provider.MediaStore;
 
-import java.io.File;
+import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
+import nz.mega.sdk.AndroidGfxProcessor;
 import nz.mega.sdk.MegaApiJava;
 import nz.mega.sdk.MegaUtilsAndroid;
 
@@ -20,7 +24,9 @@ import static mega.privacy.android.app.utils.LogUtil.*;
 
 public class ImageProcessor {
 
-    private static int PREVIEW_SIZE = 1000;
+    private static final int PREVIEW_SIZE = 1000;
+
+    private static final int THUMBNAIL_SIZE = 200;
 
     public static void createImageThumbnail(MegaApiJava api,String localPath,File dst) {
         logDebug("Create image thumbnail for: " + localPath);
@@ -48,8 +54,57 @@ public class ImageProcessor {
         }
     }
 
-    public static boolean createImagePreview(File img,File preview) {
-        return MegaUtilsAndroid.createPreview(img,preview);
+    public static boolean createImageThumbnail(Context context, File origin ,File thumbnail) {
+        if (!origin.exists()) return false;
+        if (thumbnail.exists()) thumbnail.delete();
+
+        try {
+            Bitmap bitmap = Glide.with(context).asBitmap().load(origin).centerCrop().submit(THUMBNAIL_SIZE, THUMBNAIL_SIZE).get();
+            return AndroidGfxProcessor.saveBitmap(bitmap, thumbnail);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            logError("Create image thumbnail failed.", e);
+        }
+        return false;
+    }
+
+    public static boolean createImagePreview(Context context, File img,File preview) {
+        int [] wh = calculatePreviewWidthAndHeight(img);
+        int w = wh[0];
+        int h = wh[1];
+
+        if ((w == 0) || (h == 0)) return false;
+        try {
+            Bitmap bitmap = Glide.with(context).asBitmap().load(img).submit(w, h).get();
+            return AndroidGfxProcessor.saveBitmap(bitmap, preview);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            logError("Create image prview failed.", e);
+        }
+        return false;
+    }
+
+    private static int[] calculatePreviewWidthAndHeight(File origin) {
+        int[] wh = new int[2];
+        String path = origin.getAbsolutePath();
+        int orientation = AndroidGfxProcessor.getExifOrientation(path);
+        Rect rect = AndroidGfxProcessor.getImageDimensions(path, orientation);
+
+        int w = rect.right;
+        int h = rect.bottom;
+
+        if (w >= PREVIEW_SIZE || h >= PREVIEW_SIZE) {
+            if (h > w) {
+                w = w * PREVIEW_SIZE / h;
+                h = PREVIEW_SIZE;
+            } else {
+                h = h * PREVIEW_SIZE / w;
+                w = PREVIEW_SIZE;
+            }
+        }
+        wh[0] = w;
+        wh[1] = h;
+        return wh;
     }
 
     public static boolean createVideoPreview(Context context,File video,File preview) {
