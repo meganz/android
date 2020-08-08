e11e5023d69b47d5cf3564c3660d48debd08982d
Ke <Jokejiao@gmail.com>
2020-08-08 20:41:33 +1200

    Show no photos hint if photos is empty

--- app/src/main/java/mega/privacy/android/app/fragments/photos/PhotosFragment.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/photos/PhotosFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import dagger.hilt.android.AndroidEntryPoint
 import mega.privacy.android.app.R
 import mega.privacy.android.app.components.NewGridRecyclerView
