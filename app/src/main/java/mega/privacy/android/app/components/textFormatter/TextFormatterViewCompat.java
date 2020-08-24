2a1a7d7a1bde9bcf4485f1ebdec9f55a787675e7
raquelgc6 <rgc@mega.nz>
2020-08-24 22:29:43 +0200

    Combine monospace format with bold, italics and strikethrough

--- app/src/main/java/mega/privacy/android/app/components/textFormatter/TextFormatterViewCompat.java
+++ app/src/main/java/mega/privacy/android/app/components/textFormatter/TextFormatterViewCompat.java
@@ -1,31 +1,22 @@
 package mega.privacy.android.app.components.textFormatter;
 
 import android.graphics.Typeface;
-import android.os.Handler;
 import android.text.Editable;
 import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
-import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-
 import mega.privacy.android.app.MegaApplication;
 import mega.privacy.android.app.components.CustomTypefaceSpan;
-
 import static mega.privacy.android.app.components.textFormatter.textFormatterUtils.*;
-import static mega.privacy.android.app.utils.LogUtil.logDebug;
 import static mega.privacy.android.app.utils.TextUtil.isTextEmpty;
 
 public class TextFormatterViewCompat {
-
-    private static final long DELAY_MILLIS = 220L;
     private static final int GENERAL_FLAG = 18;
-    private static final int COLOR_SPAN = -7829368;
-    private static final Typeface font = Typeface.createFromAsset(MegaApplication.getInstance().getBaseContext().getAssets(), "font/RobotoMono-Regular.ttf");
+    private static final int NUM_CHAR_MONOSPACE = 2;
+    private static final Typeface monospaceFont = Typeface.createFromAsset(MegaApplication.getInstance().getBaseContext().getAssets(), "font/RobotoMono-Regular.ttf");
 
     public TextFormatterViewCompat() {
     }
@@ -56,6 +47,7 @@ public class TextFormatterViewCompat {
                 TextFormatterViewCompat.addTextChangedListener(textView, this);
             }
         };
+
         String text = textView.getText().toString();
         CharSequence formattedText = getFormattedText(text);
         if (formattedText != null) {
@@ -71,7 +63,6 @@ public class TextFormatterViewCompat {
                 mListeners[i].afterTextChanged(s);
             }
         }
-
     }
 
     private static void sendOnTextChanged(TextWatcher[] mListeners, CharSequence s, int start, int before, int count) {
@@ -80,7 +71,6 @@ public class TextFormatterViewCompat {
                 mListeners[i].onTextChanged(s, start, before, count);
             }
         }
-
     }
 
     private static void sendBeforeTextChanged(TextWatcher[] mListeners, CharSequence s, int start, int count, int after) {
@@ -89,7 +79,6 @@ public class TextFormatterViewCompat {
                 mListeners[i].beforeTextChanged(s, start, count, after);
             }
         }
-
     }
 
     static void removeTextChangedListener(TextView textView, TextWatcher watcher) {
@@ -105,7 +94,7 @@ public class TextFormatterViewCompat {
         ArrayList<Character> characters = new ArrayList();
         ArrayList<Flag> flags = new ArrayList();
         Flag boldFlag = new Flag(INVALID_INDEX, INVALID_INDEX, BOLD_FLAG);
-        Flag quoteFlag = new Flag(INVALID_INDEX, INVALID_INDEX, MONOSPACE_FLAG);
+        Flag monospaceFlag = new Flag(INVALID_INDEX, INVALID_INDEX, MONOSPACE_FLAG);
         Flag strikeFlag = new Flag(INVALID_INDEX, INVALID_INDEX, STRIKE_FLAG);
         Flag italicFlag = new Flag(INVALID_INDEX, INVALID_INDEX, ITALIC_FLAG);
         int i = 0;
@@ -156,30 +145,30 @@ public class TextFormatterViewCompat {
 
                 case MONOSPACE_FLAG:
                     if (textChars.length > 6) {
-                        if (textChars.length > i + 2) {
+                        if (textChars.length > i + NUM_CHAR_MONOSPACE) {
 
-                            if (quoteFlag.start != INVALID_INDEX) {
-                                if (quoteFlag.end == INVALID_INDEX && textChars[i + 1] == MONOSPACE_FLAG && textChars[i + 2] == MONOSPACE_FLAG) {
-                                    quoteFlag.end = j;
-                                    flags.add(quoteFlag);
-                                    quoteFlag = new Flag(INVALID_INDEX, INVALID_INDEX, MONOSPACE_FLAG);
-                                    i = i + 2;
-                                    j = j + 2;
+                            if (monospaceFlag.start != INVALID_INDEX) {
+                                if (monospaceFlag.end == INVALID_INDEX && textChars[i + 1] == MONOSPACE_FLAG && textChars[i + NUM_CHAR_MONOSPACE] == MONOSPACE_FLAG) {
+                                    j = j - NUM_CHAR_MONOSPACE;
+                                    monospaceFlag.end = j;
+                                    flags.add(monospaceFlag);
+                                    monospaceFlag = new Flag(INVALID_INDEX, INVALID_INDEX, MONOSPACE_FLAG);
+                                    i = i + NUM_CHAR_MONOSPACE;
                                     continue;
                                 }
                             } else {
-                                if (textChars[i + 1] == MONOSPACE_FLAG && textChars[i + 2] == MONOSPACE_FLAG) {
-                                    quoteFlag.start = j;
-                                    i = i + 2;
-                                    j = j + 2;
+                                if (textChars[i + 1] == MONOSPACE_FLAG && textChars[i + NUM_CHAR_MONOSPACE] == MONOSPACE_FLAG) {
+                                    monospaceFlag.start = j;
+                                    i = i + NUM_CHAR_MONOSPACE;
+                                    j = j + NUM_CHAR_MONOSPACE;
                                     continue;
-
                                 }
                             }
                         }
                     }
                     break;
             }
+
             characters.add(c);
             ++j;
         }
@@ -191,21 +180,32 @@ public class TextFormatterViewCompat {
             StyleSpan iss;
             switch (flag.flag) {
                 case BOLD_FLAG:
+                    if (flag.end < builder.length())
+                        break;
+
                     iss = new StyleSpan(1);
                     builder.setSpan(iss, flag.start, flag.end, GENERAL_FLAG);
                     break;
 
                 case STRIKE_FLAG:
+                    if (flag.end < builder.length())
+                        break;
+
                     builder.setSpan(new StrikethroughSpan(), flag.start, flag.end, GENERAL_FLAG);
                     break;
 
                 case ITALIC_FLAG:
+                    if (flag.end < builder.length())
+                        break;
+
                     iss = new StyleSpan(2);
                     builder.setSpan(iss, flag.start, flag.end, GENERAL_FLAG);
                     break;
 
                 case MONOSPACE_FLAG:
-                    builder.setSpan(new CustomTypefaceSpan("", font), flag.start, flag.end-3, GENERAL_FLAG);
+                    if (flag.end < builder.length())
+                        break;
+                    builder.setSpan(new CustomTypefaceSpan("", monospaceFont), flag.start, flag.end, GENERAL_FLAG);
                     break;
             }
         }
