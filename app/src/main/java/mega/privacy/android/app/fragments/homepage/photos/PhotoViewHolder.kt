01e7fd077f70f4eaa2f9ce44a6f963b161603e70
Ke <Jokejiao@gmail.com>
2020-09-11 13:44:40 +1200

    Photos: Workaround for an UI issue of Fresco placeholder

--- app/src/main/java/mega/privacy/android/app/fragments/homepage/photos/PhotoViewHolder.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/homepage/photos/PhotoViewHolder.kt
@@ -2,6 +2,7 @@ package mega.privacy.android.app.fragments.homepage.photos
 
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import mega.privacy.android.app.R
 import mega.privacy.android.app.databinding.ItemNodeListBinding
 import mega.privacy.android.app.databinding.ItemPhotoBrowseBinding
 import mega.privacy.android.app.databinding.ItemPhotosTitleBinding
@@ -33,6 +34,17 @@ class PhotoViewHolder(private val binding: ViewDataBinding) :
                     this.actionModeViewModel = actionModeViewModel
                     this.item = item
                     this.megaApi = megaApi
+
+                    // This convoluted logic is a workaround for an UI issue of Fresco placeholder :
+                    // The placeholder(If set) image shows up transiently before showing the "tick"
+                    // image when the user long press an item for the very first time
+                    if (actionModeViewModel.selectedNodes.value?.isEmpty() == true ||
+                        actionModeViewModel.longClick.value?.peekContent()?.index != item.index ||
+                            !item.selected) {
+                        thumbnail.hierarchy.setPlaceholderImage(R.drawable.ic_image_list)
+                    } else {
+                        thumbnail.hierarchy.setPlaceholderImage(null)
+                    }
                 }
                 is ItemPhotosTitleBinding -> {
                     this.item = item
