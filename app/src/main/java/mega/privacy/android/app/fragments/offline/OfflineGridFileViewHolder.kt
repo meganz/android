3402c92a19f5c0e663f4bed6420cd9a6e2fc655d
Piasy Xu <px@mega.co.nz>
2020-09-17 21:27:53 +0800

    Fix default thumbnail size of offline grid view

--- app/src/main/java/mega/privacy/android/app/fragments/offline/OfflineGridFileViewHolder.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/offline/OfflineGridFileViewHolder.kt
@@ -2,6 +2,8 @@ package mega.privacy.android.app.fragments.offline
 
 import android.net.Uri
 import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.facebook.drawee.generic.RoundingParams
@@ -23,6 +25,20 @@ class OfflineGridFileViewHolder(
             binding.thumbnail.setImageURI(Uri.fromFile(node.thumbnail))
         } else {
             binding.thumbnail.setActualImageResource(placeHolderRes)
+        }
+
+        val params = binding.thumbnail.layoutParams
+        if (params is FrameLayout.LayoutParams) {
+            val realThumbnailSize =
+                binding.root.resources.getDimensionPixelSize(R.dimen.grid_node_item_width)
+            val defaultThumbnailSize =
+                binding.root.resources.getDimensionPixelSize(R.dimen.grid_node_default_thumbnail_size)
+            val defaultThumbnailMarginTop = (realThumbnailSize - defaultThumbnailSize) / 2
+            params.width =
+                if (node.thumbnail == null) defaultThumbnailSize else ViewGroup.LayoutParams.MATCH_PARENT
+            params.height = if (node.thumbnail == null) defaultThumbnailSize else realThumbnailSize
+            params.topMargin = if (node.thumbnail == null) defaultThumbnailMarginTop else 0
+            binding.thumbnail.layoutParams = params
         }
 
         val radius =
