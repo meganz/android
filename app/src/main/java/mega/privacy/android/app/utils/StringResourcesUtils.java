973a81aeef7a3e9cbf2f30e7eca89fbc5b4aa07b
Javier Gomez <jg@mega.co.nz>
2020-09-21 14:09:24 +0200

    Add generic methods to get translated strings
    Avoid the app crashes due a bad format of a translated string and use
    the original English in that case.
    
    Enhancement AND-11506: Avoid the app crashes due a bad format of a
    translated string

--- app/src/main/java/mega/privacy/android/app/utils/StringResourcesUtils.java
+++ app/src/main/java/mega/privacy/android/app/utils/StringResourcesUtils.java
@@ -1,14 +1,127 @@
 package mega.privacy.android.app.utils;
 
+import android.content.res.Configuration;
+
+import java.util.Locale;
+
 import mega.privacy.android.app.MegaApplication;
 import mega.privacy.android.app.R;
 import nz.mega.sdk.MegaChatError;
 import nz.mega.sdk.MegaError;
 
+import static mega.privacy.android.app.utils.LogUtil.*;
 import static nz.mega.sdk.MegaChatError.*;
 import static nz.mega.sdk.MegaError.*;
 
 public class StringResourcesUtils {
+
+    /**
+     * Return the string value associated with a particular resource ID in the current app language.
+     * If something fails return the original string in English.
+     *
+     * @param resId The resource identifier of the desired string.
+     * @return The desired string.
+     */
+    public static String getString(int resId) {
+
+        MegaApplication app = MegaApplication.getInstance();
+
+        try {
+            return app.getString(resId);
+        } catch (Exception e) {
+            logWarning("Error getting a translated string.", e);
+
+            Configuration configuration = new Configuration(app.getResources().getConfiguration());
+            configuration.setLocale(new Locale("en"));
+            String originalString = app.createConfigurationContext(configuration).getResources().getString(resId);
+
+            logInfo("Using the original English string: " + originalString);
+            return originalString;
+        }
+    }
+
+    /**
+     * Return the string value associated with a particular resource ID in the current app language,
+     * substituting the format arguments as defined in Formatter and String.format(String, Object...).
+     * If something fails return the original string in English.
+     *
+     * @param resId      The resource identifier of the desired string.
+     * @param formatArgs The format arguments that will be used for substitution.
+     * @return The desired string.
+     */
+    public static String getString(int resId, Object... formatArgs) {
+
+        MegaApplication app = MegaApplication.getInstance();
+
+        try {
+            return app.getString(resId, formatArgs);
+        } catch (Exception e) {
+            logWarning("Error getting a translated and formatted string.", e);
+
+            Configuration configuration = new Configuration(app.getResources().getConfiguration());
+            configuration.setLocale(new Locale("en"));
+            String originalString = app.createConfigurationContext(configuration).getResources().getString(resId, formatArgs);
+
+            logInfo("Using the original English string: " + originalString);
+            return originalString;
+        }
+    }
+
+    /**
+     * Returns the string necessary for grammatically correct pluralization of the given
+     * resource ID for the given quantity in the current app language.
+     * If something fails return the original string in English.
+     *
+     * @param resId    The resource identifier of the desired string
+     * @param quantity The number used to get the correct string for the current language's plural rules.
+     * @return The desired string.
+     */
+    public static String getQuantityString(int resId, int quantity) {
+
+        MegaApplication app = MegaApplication.getInstance();
+
+        try {
+            return app.getResources().getQuantityString(resId, quantity);
+        } catch (Exception e) {
+            logWarning("Error getting a translated string with quantity modifier.", e);
+
+            Configuration configuration = new Configuration(app.getResources().getConfiguration());
+            configuration.setLocale(new Locale("en"));
+            String originalString = app.createConfigurationContext(configuration).getResources().getQuantityString(resId, quantity);
+
+            logInfo("Using the original English string: " + originalString);
+            return originalString;
+        }
+    }
+
+    /**
+     * Returns the string necessary for grammatically correct pluralization of the given resource ID
+     * for the given quantity in the current app language and substituting the format arguments as
+     * defined in Formatter and String.format(String, Object...).
+     * If something fails return the original string in English.
+     *
+     * @param resId      The resource identifier of the desired string.
+     * @param quantity   The number used to get the correct string for the current language's plural rules.
+     * @param formatArgs The format arguments that will be used for substitution.
+     * @return The desired string.
+     */
+    public static String getQuantityString(int resId, int quantity, Object... formatArgs) {
+
+        MegaApplication app = MegaApplication.getInstance();
+
+        try {
+            return app.getResources().getQuantityString(resId, quantity, formatArgs);
+        } catch (Exception e) {
+            logWarning("Error getting a translated and formatted string with quantity modifier.", e);
+
+            Configuration configuration = new Configuration(app.getResources().getConfiguration());
+            configuration.setLocale(new Locale("en"));
+            String originalString = app.createConfigurationContext(configuration).getResources().getQuantityString(resId, quantity, formatArgs);
+
+            logInfo("Using the original English string: " + originalString);
+            return originalString;
+        }
+    }
 
     /**
      * Gets the translated string of an error received in a request.
@@ -23,97 +136,97 @@ public class StringResourcesUtils {
         }
 
         if (error.getErrorCode() > 0) {
-            return app.getString(R.string.api_error_http);
+            return getString(R.string.api_error_http);
         }
 
         switch (error.getErrorCode()) {
             case API_OK:
-                return app.getString(R.string.api_ok);
+                return getString(R.string.api_ok);
             case API_EINTERNAL:
-                return app.getString(R.string.api_einternal);
+                return getString(R.string.api_einternal);
             case API_EARGS:
-                return app.getString(R.string.api_eargs);
+                return getString(R.string.api_eargs);
             case API_EAGAIN:
-                return app.getString(R.string.api_eagain);
+                return getString(R.string.api_eagain);
             case API_ERATELIMIT:
-                return app.getString(R.string.api_eratelimit);
+                return getString(R.string.api_eratelimit);
             case API_EFAILED:
-                return app.getString(R.string.api_efailed);
+                return getString(R.string.api_efailed);
             case API_ETOOMANY:
                 if (error.getErrorString().equals("Terms of Service breached")) {
-                    return app.getString(R.string.api_etoomany_ec_download);
+                    return getString(R.string.api_etoomany_ec_download);
                 } else if (error.getErrorString().equals("Too many concurrent connections or transfers")){
-                    return app.getString(R.string.api_etoomay);
+                    return getString(R.string.api_etoomay);
                 } else {
                     return error.getErrorString();
                 }
             case API_ERANGE:
-                return app.getString(R.string.api_erange);
+                return getString(R.string.api_erange);
             case API_EEXPIRED:
-                return app.getString(R.string.api_eexpired);
+                return getString(R.string.api_eexpired);
             case API_ENOENT:
-                return app.getString(R.string.api_enoent);
+                return getString(R.string.api_enoent);
             case API_ECIRCULAR:
                 if (error.getErrorString().equals("Upload produces recursivity")) {
-                    return app.getString(R.string.api_ecircular_ec_upload);
+                    return getString(R.string.api_ecircular_ec_upload);
                 } else if (error.getErrorString().equals("Circular linkage detected")){
-                    return app.getString(R.string.api_ecircular);
+                    return getString(R.string.api_ecircular);
                 } else {
                     return error.getErrorString();
                 }
             case API_EACCESS:
-                return app.getString(R.string.api_eaccess);
+                return getString(R.string.api_eaccess);
             case API_EEXIST:
-                return app.getString(R.string.api_eexist);
+                return getString(R.string.api_eexist);
             case API_EINCOMPLETE:
-                return app.getString(R.string.api_eincomplete);
+                return getString(R.string.api_eincomplete);
             case API_EKEY:
-                return app.getString(R.string.api_ekey);
+                return getString(R.string.api_ekey);
             case API_ESID:
-                return app.getString(R.string.api_esid);
+                return getString(R.string.api_esid);
             case API_EBLOCKED:
                 if (error.getErrorString().equals("Not accessible due to ToS/AUP violation")) {
-                    return app.getString(R.string.api_eblocked_ec_import_ec_download);
+                    return getString(R.string.api_eblocked_ec_import_ec_download);
                 } else if (error.getErrorString().equals("Blocked")) {
-                    return app.getString(R.string.api_eblocked);
+                    return getString(R.string.api_eblocked);
                 } else {
                     return error.getErrorString();
                 }
             case API_EOVERQUOTA:
-                return app.getString(R.string.api_eoverquota);
+                return getString(R.string.api_eoverquota);
             case API_ETEMPUNAVAIL:
-                return app.getString(R.string.api_etempunavail);
+                return getString(R.string.api_etempunavail);
             case API_ETOOMANYCONNECTIONS:
-                return app.getString(R.string.api_etoomanyconnections);
+                return getString(R.string.api_etoomanyconnections);
             case API_EWRITE:
-                return app.getString(R.string.api_ewrite);
+                return getString(R.string.api_ewrite);
             case API_EREAD:
-                return app.getString(R.string.api_eread);
+                return getString(R.string.api_eread);
             case API_EAPPKEY:
-                return app.getString(R.string.api_eappkey);
+                return getString(R.string.api_eappkey);
             case API_ESSL:
-                return app.getString(R.string.api_essl);
+                return getString(R.string.api_essl);
             case API_EGOINGOVERQUOTA:
-                return app.getString(R.string.api_egoingoverquota);
+                return getString(R.string.api_egoingoverquota);
             case API_EMFAREQUIRED:
-                return app.getString(R.string.api_emfarequired);
+                return getString(R.string.api_emfarequired);
             case API_EMASTERONLY:
-                return app.getString(R.string.api_emasteronly);
+                return getString(R.string.api_emasteronly);
             case API_EBUSINESSPASTDUE:
-                return app.getString(R.string.api_ebusinesspastdue);
+                return getString(R.string.api_ebusinesspastdue);
             case PAYMENT_ECARD:
-                return app.getString(R.string.payment_ecard);
+                return getString(R.string.payment_ecard);
             case PAYMENT_EBILLING:
-                return app.getString(R.string.payment_ebilling);
+                return getString(R.string.payment_ebilling);
             case PAYMENT_EFRAUD:
-                return app.getString(R.string.payment_efraud);
+                return getString(R.string.payment_efraud);
             case PAYMENT_ETOOMANY:
-                return app.getString(R.string.payment_etoomay);
+                return getString(R.string.payment_etoomay);
             case PAYMENT_EBALANCE:
-                return app.getString(R.string.payment_ebalance);
+                return getString(R.string.payment_ebalance);
             case PAYMENT_EGENERIC:
             default:
-                return app.getString(R.string.payment_egeneric_api_error_unknown);
+                return getString(R.string.payment_egeneric_api_error_unknown);
         }
     }
 
@@ -131,20 +244,20 @@ public class StringResourcesUtils {
 
         switch (error.getErrorCode()) {
             case ERROR_OK:
-                return app.getString(R.string.error_ok);
+                return getString(R.string.error_ok);
             case ERROR_ARGS:
-                return app.getString(R.string.error_args);
+                return getString(R.string.error_args);
             case ERROR_ACCESS:
-                return app.getString(R.string.error_access);
+                return getString(R.string.error_access);
             case ERROR_NOENT:
-                return app.getString(R.string.error_noent);
+                return getString(R.string.error_noent);
             case ERROR_EXIST:
-                return app.getString(R.string.error_exist);
+                return getString(R.string.error_exist);
             case ERROR_TOOMANY:
-                return app.getString(R.string.error_toomany);
+                return getString(R.string.error_toomany);
             case ERROR_UNKNOWN:
             default:
-                return app.getString(R.string.error_unknown);
+                return getString(R.string.error_unknown);
         }
     }
 }
