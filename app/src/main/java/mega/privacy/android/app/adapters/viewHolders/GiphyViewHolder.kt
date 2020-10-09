302d5a00043317c9b6318305d48c30fee4391e69
Yenel <yr@mega.nz>
2020-10-09 17:45:50 +0200

    Fix NullPointerException

--- app/src/main/java/mega/privacy/android/app/adapters/viewHolders/GiphyViewHolder.kt
+++ app/src/main/java/mega/privacy/android/app/adapters/viewHolders/GiphyViewHolder.kt
@@ -15,21 +15,23 @@ class GiphyViewHolder(val view: View, private val giphyInterface: GiphyInterface
     var gifData: GifData? = null
 
     fun bind(gif: Data?) {
-        val imageAttributes = gif?.images?.fixedHeight
-        val gifWidth = imageAttributes?.width ?: 0
-        val gifHeight = imageAttributes?.height ?: 0
+        val imageAttributes = gif?.images?.fixedHeight ?: return
+        val gifWidth = imageAttributes.width
+        val gifHeight = imageAttributes.height
 
-        gifData = GifData(imageAttributes?.mp4,
-                imageAttributes?.webp,
-                imageAttributes?.mp4Size ?: 0,
-                imageAttributes?.webpSize ?: 0,
+        gifData = GifData(imageAttributes.mp4,
+                imageAttributes.webp,
+                imageAttributes.mp4Size,
+                imageAttributes.webpSize,
                 gifWidth,
                 gifHeight,
-                gif?.title)
+                gif.title)
 
         updateLayoutParams(gifWidth, gifHeight)
-        FrescoUtils.loadGif(gifImage, Uri.parse(imageAttributes?.webp))
         view.tag = this@GiphyViewHolder
+
+        val webpUri = imageAttributes.webp ?: return
+        FrescoUtils.loadGif(gifImage, Uri.parse(webpUri))
     }
 
     /**
