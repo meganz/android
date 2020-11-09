52b95659e73d9f826f5bc9022880750970e0a7d0
Yenel <yr@mega.nz>
2020-11-09 12:43:02 +0100

    Make available in progress transfers on offline mode: - Show transfers widget - Show in progress transfers on in progress tab of Transfers section

--- app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
@@ -2596,6 +2596,17 @@ public class ManagerActivityLollipop extends SorterContentActivity implements Me
 
 		setTransfersWidgetLayout(findViewById(R.id.transfers_widget_layout), this);
 
+		transferData = megaApi.getTransferData(this);
+		if (transferData != null) {
+			for (int i = 0; i < transferData.getNumDownloads(); i++) {
+				transfersInProgress.add(transferData.getDownloadTag(i));
+			}
+
+			for (int i = 0; i < transferData.getNumUploads(); i++) {
+				transfersInProgress.add(transferData.getUploadTag(i));
+			}
+		}
+
         if (!isOnline(this)){
 			logDebug("No network -> SHOW OFFLINE MODE");
 
@@ -2858,17 +2869,6 @@ public class ManagerActivityLollipop extends SorterContentActivity implements Me
 			megaApi.shouldShowRichLinkWarning(this);
 			megaApi.isRichPreviewsEnabled(this);
 			megaApi.isGeolocationEnabled(this);
-
-			transferData = megaApi.getTransferData(this);
-			int downloadsInProgress = transferData.getNumDownloads();
-			int uploadsInProgress = transferData.getNumUploads();
-
-            for(int i=0;i<downloadsInProgress;i++){
-                transfersInProgress.add(transferData.getDownloadTag(i));
-            }
-            for(int i=0;i<uploadsInProgress;i++){
-                transfersInProgress.add(transferData.getUploadTag(i));
-            }
 
 			if(savedInstanceState==null) {
 				logDebug("Run async task to check offline files");
@@ -3344,6 +3344,7 @@ public class ManagerActivityLollipop extends SorterContentActivity implements Me
 				selectDrawerItemLollipop(drawerItem);
 			}
 		}
+
 		megaApi.shouldShowPasswordReminderDialog(false, this);
 
 		if (verify2FADialogIsShown){
