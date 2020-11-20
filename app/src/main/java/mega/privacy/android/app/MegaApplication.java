2e1e595c0cc9538f6185f2885852c6274150441b
Javier Gomez <jg@mega.co.nz>
2020-11-20 11:16:48 +0100

    Update version code - v3.8.2 (338)

--- app/src/main/java/mega/privacy/android/app/MegaApplication.java
+++ app/src/main/java/mega/privacy/android/app/MegaApplication.java
@@ -111,7 +111,7 @@ public class MegaApplication extends MultiDexApplication implements Application.
 
 	final String TAG = "MegaApplication";
 
-	static final public String USER_AGENT = "MEGAAndroid/3.8.2_337";
+	static final public String USER_AGENT = "MEGAAndroid/3.8.2_338";
 
     private static PushNotificationSettingManagement pushNotificationSettingManagement;
     DatabaseHandler dbH;
