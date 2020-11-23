1c77cc7b9737b47810f72f1a434f6f2016cca4cc
raquelgc6 <rgc@mega.nz>
2020-11-23 13:02:26 +0100

    Process the onChatRoomUpdate listener in a separate listener

--- app/src/main/java/mega/privacy/android/app/activities/ManageChatHistoryActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/ManageChatHistoryActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import mega.privacy.android.app.R
 import mega.privacy.android.app.databinding.ActivityManageChatHistoryBinding
+import mega.privacy.android.app.listeners.UpdatePreviewersListener
 import mega.privacy.android.app.lollipop.PinActivityLollipop
 import mega.privacy.android.app.utils.ChatUtil
 import mega.privacy.android.app.utils.ChatUtil.createHistoryRetentionAlertDialog
@@ -13,6 +14,7 @@ import mega.privacy.android.app.utils.Constants
 import mega.privacy.android.app.utils.LogUtil.logDebug
 import mega.privacy.android.app.utils.LogUtil.logError
 import mega.privacy.android.app.utils.TextUtil
+import nz.mega.sdk.MegaChatApiJava.MEGACHAT_INVALID_HANDLE
 import nz.mega.sdk.MegaChatRoom
 
 class ManageChatHistoryActivity : PinActivityLollipop(), View.OnClickListener {
@@ -20,6 +22,9 @@ class ManageChatHistoryActivity : PinActivityLollipop(), View.OnClickListener {
     private var screenOrientation = 0
     private lateinit var binding: ActivityManageChatHistoryBinding
     private var chat: MegaChatRoom? = null
+    private var chatId = MEGACHAT_INVALID_HANDLE
+    private var listener: UpdatePreviewersListener? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +52,10 @@ class ManageChatHistoryActivity : PinActivityLollipop(), View.OnClickListener {
             logError("Cannot init view, chat is null")
             finish()
         }
+        chatId = chat?.chatId!!
+        listener = UpdatePreviewersListener(this)
+
+        megaChatApi.openChatRoom(chatId, listener)
 
         binding = ActivityManageChatHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -64,6 +73,17 @@ class ManageChatHistoryActivity : PinActivityLollipop(), View.OnClickListener {
         screenOrientation = resources.configuration.orientation
 
         binding.clearChatHistoryLayout?.setOnClickListener(this)
+        binding.historyRetentionSwitch?.setOnClickListener(null)
+
+        binding.historyRetentionSwitch?.setOnCheckedChangeListener { switchCompat, isChecked ->
+            if (isChecked) {
+                logDebug("is Checked")
+            } else {
+                logDebug("no checked")
+
+            }
+        }
+
         binding.historyRetentionLayout?.setOnClickListener(this)
     }
 
@@ -87,4 +107,8 @@ class ManageChatHistoryActivity : PinActivityLollipop(), View.OnClickListener {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onDestroy() {
+        megaChatApi.closeChatRoom(chatId, listener)
+        super.onDestroy()
+    }
 }
---
+++ app/src/main/java/mega/privacy/android/app/listeners/ChatRoomBaseListener.java
@@ -0,0 +1,46 @@
+package mega.privacy.android.app.listeners;
+
+import android.content.Context;
+
+import nz.mega.sdk.MegaChatApiJava;
+import nz.mega.sdk.MegaChatMessage;
+import nz.mega.sdk.MegaChatRoom;
+import nz.mega.sdk.MegaChatRoomListenerInterface;
+
+public class ChatRoomBaseListener implements MegaChatRoomListenerInterface {
+
+    protected Context context;
+
+    public ChatRoomBaseListener(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onChatRoomUpdate(MegaChatApiJava api, MegaChatRoom chat) {
+    }
+
+    @Override
+    public void onMessageLoaded(MegaChatApiJava api, MegaChatMessage msg) {
+
+    }
+
+    @Override
+    public void onMessageReceived(MegaChatApiJava api, MegaChatMessage msg) {
+
+    }
+
+    @Override
+    public void onMessageUpdate(MegaChatApiJava api, MegaChatMessage msg) {
+
+    }
+
+    @Override
+    public void onHistoryReloaded(MegaChatApiJava api, MegaChatRoom chat) {
+
+    }
+
+    @Override
+    public void onReactionUpdate(MegaChatApiJava api, long msgid, String reaction, int count) {
+
+    }
+}
