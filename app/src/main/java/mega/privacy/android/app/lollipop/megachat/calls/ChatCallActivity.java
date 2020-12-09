c6d9079226c631281de70e21e5fa6e7a989567e0
raquelgc6 <rgc@mega.nz>
2020-12-09 12:00:10 +0100

    Show the video button Do not stop the incoming call sound when hanging up a call in progress
    T1184815 - AND-12507 The incoming call notification disappears when I have a call in progress and it hangs up
    T1191707 - Show/hide callKit icons by tapping anywhere on the screen while being on a video call

--- app/src/main/java/mega/privacy/android/app/lollipop/megachat/calls/ChatCallActivity.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/megachat/calls/ChatCallActivity.java
@@ -1485,6 +1485,7 @@ public class ChatCallActivity extends BaseActivity implements MegaChatRequestLis
             if (!onHoldFAB.isShown()) onHoldFAB.show();
             updateOnHoldFABStatus();
 
+            if (!videoFAB.isShown()) videoFAB.show();
             updateVideoFABStatus();
 
             if(!hangFAB.isShown()) hangFAB.show();
