8c92a60fa29e5dce4c620217ef211e3a23fa24db
Ash <aw@mega.co.nz>
2020-12-29 22:35:23 +1300

    Change naming for new added color pair
    homepage_dark

--- app/src/main/java/mega/privacy/android/app/lollipop/PinLockActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/PinLockActivityLollipop.java
@@ -323,7 +323,7 @@ public class PinLockActivityLollipop extends BaseActivity implements OnClickList
 	}
 
 	private void changeStatusbarColorToRed() {
-	    getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red_900_red_400));
+	    getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red_900_400_080));
     }
 
 	private void addAlphanumericPin(){
