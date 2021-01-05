6a55ba0fa814333efbec7ee8fa443ed1cc684ec3
Gonzalo Toledano <gonzalotldn@gmail.com>
2021-01-05 11:14:08 +0100

    Add cookie change confirmation dialog

--- app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/CookieSettingsFragment.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/CookieSettingsFragment.kt
@@ -1,9 +1,12 @@
 package mega.privacy.android.app.fragments.settingsFragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.SwitchPreferenceCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import mega.privacy.android.app.R
 import mega.privacy.android.app.constants.SettingsConstants.*
@@ -61,14 +64,42 @@ class CookieSettingsFragment : SettingsBaseFragment() {
     override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
         val enable = newValue as? Boolean ?: false
 
-        when (preference?.key) {
-            acceptCookiesPreference.key -> viewModel.toggleCookies(enable)
-            preferenceCookiesPreference.key -> viewModel.changeCookie(PREFERENCE, enable)
-            analyticsCookiesPreference.key -> viewModel.changeCookie(ANALYTICS, enable)
-            advertisingCookiesPreference.key -> viewModel.changeCookie(ADVERTISEMENT, enable)
-            thirdPartyCookiesPreference.key -> viewModel.changeCookie(THIRDPARTY, enable)
+        val action: () -> Unit = when (preference?.key) {
+            acceptCookiesPreference.key -> { { viewModel.toggleCookies(enable) } }
+            preferenceCookiesPreference.key -> { { viewModel.changeCookie(PREFERENCE, enable) } }
+            analyticsCookiesPreference.key -> { { viewModel.changeCookie(ANALYTICS, enable) } }
+            advertisingCookiesPreference.key -> { { viewModel.changeCookie(ADVERTISEMENT, enable) } }
+            thirdPartyCookiesPreference.key -> { { viewModel.changeCookie(THIRDPARTY, enable) } }
+            else -> { {} }
+        }
+
+        if (!enable) {
+            showConfirmationDialog(action)
+        } else {
+            action.invoke()
+            showConfirmationSnackbar()
         }
 
         return false
+    }
+
+    private fun showConfirmationDialog(action: () -> Unit) {
+        MaterialAlertDialogBuilder(requireContext(), R.style.MaterialAlertDialogStyle)
+            .setMessage(R.string.dialog_cookie_confirmation_message)
+            .setPositiveButton(android.R.string.yes) { _: DialogInterface, _: Int ->
+                action.invoke()
+                showConfirmationSnackbar()
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .create()
+            .show()
+    }
+
+    private fun showConfirmationSnackbar() {
+        Snackbar.make(
+            requireView(),
+            R.string.dialog_cookie_confirmation_saved,
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
