5e7a5b17eba063d0e7d77d7846aa3a384492420a
Piasy Xu <px@mega.co.nz>
2021-01-14 15:00:19 +0800

    Draw image under status bar in OverDiskQuotaPaywallActivity

--- app/src/main/java/mega/privacy/android/app/activities/OverDiskQuotaPaywallActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/OverDiskQuotaPaywallActivity.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
@@ -55,7 +56,12 @@ class OverDiskQuotaPaywallActivity : PinActivityLollipop(), View.OnClickListener
         megaApi.getUserData(GetUserDataListener(this))
 
         setContentView(R.layout.activity_over_disk_quota_paywall)
-        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.status_bar_red_alert)
+
+        // show content behind status bar
+        window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // make status bar transparent
+        window?.statusBarColor = Color.TRANSPARENT
 
         scrollContentLayout = findViewById(R.id.scroll_content_layout)
 
