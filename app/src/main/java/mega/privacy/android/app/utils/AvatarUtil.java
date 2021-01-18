1efb1a535c107c36ef6b159fe74badd1776b5268
Yenel <yr@mega.nz>
2021-01-18 15:07:19 +0100

    Fix crash at AvatarUtit, hasEmojiCompatAtFirst (AND-12637)

--- app/src/main/java/mega/privacy/android/app/utils/AvatarUtil.java
+++ app/src/main/java/mega/privacy/android/app/utils/AvatarUtil.java
@@ -34,6 +34,7 @@ import nz.mega.sdk.MegaUser;
 import static mega.privacy.android.app.utils.CacheFolderManager.*;
 import static mega.privacy.android.app.utils.Constants.*;
 import static mega.privacy.android.app.utils.FileUtil.*;
+import static mega.privacy.android.app.utils.TextUtil.isTextEmpty;
 import static mega.privacy.android.app.utils.ThumbnailUtilsLollipop.*;
 import static mega.privacy.android.app.utils.Util.*;
 import static nz.mega.sdk.MegaApiJava.INVALID_HANDLE;
@@ -89,14 +90,21 @@ public class AvatarUtil {
     }
 
     private static String hasEmojiCompatAtFirst(String text) {
+        if (isTextEmpty(text)) {
+            return null;
+        }
+
         List<String> listEmojis = EmojiParser.extractEmojis(text);
+
         if (listEmojis != null && !listEmojis.isEmpty()) {
             String substring = text.substring(0, listEmojis.get(0).length());
             List<String> sublistEmojis = EmojiParser.extractEmojis(substring);
+
             if (sublistEmojis != null && !sublistEmojis.isEmpty()) {
                 return substring;
             }
         }
+
         return null;
     }
 
