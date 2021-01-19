bbb2c3d805dcf0e5b3eee2458b0af072d5e0efd2
Yenel <yr@mega.nz>
2021-01-19 09:42:10 +0100

    Fix disable select mode when there are no transfers in progress T1212991 The select transfers state remains in the top bar even when there are no transfers to select.

--- app/src/main/java/mega/privacy/android/app/lollipop/managerSections/TransfersFragmentLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/managerSections/TransfersFragmentLollipop.java
@@ -260,6 +260,7 @@ public class TransfersFragmentLollipop extends TransfersBaseFragment implements 
 		}
 
 		if (tL.isEmpty()) {
+			destroyActionMode();
 			setEmptyView(tL.size());
 			managerActivity.supportInvalidateOptionsMenu();
 		}
