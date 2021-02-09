0c894d36773cc2a23d5355a33095d9c311ec3844
Yenel <yr@mega.nz>
2021-02-09 16:06:20 +0100

    Fix AND-12616 Missing option to move in inshares+multiselect

--- app/src/main/java/mega/privacy/android/app/lollipop/managerSections/IncomingSharesFragmentLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/managerSections/IncomingSharesFragmentLollipop.java
@@ -24,6 +24,7 @@ import mega.privacy.android.app.utils.MegaNodeUtil;
 import nz.mega.sdk.MegaApiJava;
 import nz.mega.sdk.MegaNode;
 
+import static mega.privacy.android.app.utils.MegaNodeUtil.allHaveFullAccess;
 import static mega.privacy.android.app.utils.MegaNodeUtil.areAllFileNodes;
 import static mega.privacy.android.app.utils.SortUtil.*;
 import static mega.privacy.android.app.utils.Constants.*;
@@ -77,6 +78,17 @@ public class IncomingSharesFragmentLollipop extends MegaNodeBaseFragment {
 				}
 			}
 
+			if (managerActivity.getDeepBrowserTreeIncoming() > 0 && selected.size() > 0
+					&& allHaveFullAccess(selected)) {
+				control.move().setVisible(true);
+
+				if (control.alwaysActionCount() < CloudStorageOptionControlUtil.MAX_ACTION_COUNT) {
+					control.move().setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+				} else {
+					control.move().setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+				}
+			}
+
 			control.copy().setVisible(true);
 			if (control.alwaysActionCount() < CloudStorageOptionControlUtil.MAX_ACTION_COUNT) {
 				control.copy().setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -86,7 +98,7 @@ public class IncomingSharesFragmentLollipop extends MegaNodeBaseFragment {
 
 			control.selectAll().setVisible(notAllNodesSelected());
 			control.trash().setVisible(managerActivity.getDeepBrowserTreeIncoming() > 0
-					&& MegaNodeUtil.allHaveFullAccess(selected));
+					&& allHaveFullAccess(selected));
 
 			CloudStorageOptionControlUtil.applyControl(menu, control);
 
