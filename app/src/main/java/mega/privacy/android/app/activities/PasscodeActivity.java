5b96838c108ad0801cefd4d2021acd7bdfa4db56
Yenel <yr@mega.nz>
2021-02-15 12:46:09 +0100

    Fix AND-12697 Passcode prompt every time orientation is changed

--- app/src/main/java/mega/privacy/android/app/activities/PasscodeActivity.java
+++ app/src/main/java/mega/privacy/android/app/activities/PasscodeActivity.java
@@ -1,5 +1,6 @@
 package mega.privacy.android.app.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import mega.privacy.android.app.BaseActivity;
@@ -12,36 +13,51 @@ import static mega.privacy.android.app.utils.Util.*;
 
 public class PasscodeActivity extends BaseActivity {
 
-	private PasscodeUtil passcodeUtil;
+    private PasscodeUtil passcodeUtil;
     private static long lastStart;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		passcodeUtil = new PasscodeUtil(this, dbH);
-	}
 
-	@Override
-	protected void onPause() {
-		passcodeUtil.pause();
-		lastStart = System.currentTimeMillis();
-		super.onPause();
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
+    /**
+     * Used to control when onResume comes from a screen orientation change.
+     * Since onConfigurationChanged is not implemented in all activities,
+     * it cannot be used for that purpose.
+     *
+     * @see android.app.Activity#onConfigurationChanged(Configuration)
+     */
+    private boolean isScreenRotation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        passcodeUtil = new PasscodeUtil(this, dbH);
+
+        if (savedInstanceState != null) {
+            isScreenRotation = true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        passcodeUtil.pause();
+        lastStart = System.currentTimeMillis();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         setAppFontSize(this);
 
-		if(MegaApplication.getPasscodeManagement().getShowPasscodeScreen()){
-			passcodeUtil.resume();
-		}
-
-		//if leave the APP then get back, should trigger camera upload.
-        if(System.currentTimeMillis() - lastStart > 1000) {
-			if (megaApi.getRootNode() != null && !MegaApplication.isLoggingIn()){
-				startCameraUploadServiceIgnoreAttr(PasscodeActivity.this);
-			}
+        if (isScreenRotation) {
+            isScreenRotation = false;
+        } else if (MegaApplication.getPasscodeManagement().getShowPasscodeScreen()) {
+            passcodeUtil.resume();
         }
-	}
+
+        //if leave the APP then get back, should trigger camera upload.
+        if (System.currentTimeMillis() - lastStart > 1000) {
+            if (megaApi.getRootNode() != null && !MegaApplication.isLoggingIn()) {
+                startCameraUploadServiceIgnoreAttr(PasscodeActivity.this);
+            }
+        }
+    }
 }
