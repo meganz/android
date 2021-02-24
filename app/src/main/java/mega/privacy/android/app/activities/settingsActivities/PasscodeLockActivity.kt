c65786008b955758f34aa36bb8e1419831eee84d
Yenel <yr@mega.nz>
2021-02-24 11:06:05 +0100

    Remove CustomTextWatcher and replace it by doAfterTextChanged at PasscodeLockActivity

--- app/src/main/java/mega/privacy/android/app/activities/settingsActivities/PasscodeLockActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/settingsActivities/PasscodeLockActivity.kt
@@ -3,7 +3,6 @@ package mega.privacy.android.app.activities.settingsActivities
 import android.content.res.Configuration
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.os.Bundle
-import android.text.Editable
 import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
@@ -11,10 +10,10 @@ import android.view.inputmethod.EditorInfo.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import mega.privacy.android.app.BaseActivity
 import mega.privacy.android.app.MegaApplication
 import mega.privacy.android.app.R
-import mega.privacy.android.app.components.CustomTextWatcher
 import mega.privacy.android.app.components.ListenScrollChangesHelper
 import mega.privacy.android.app.databinding.ActivityPasscodeBinding
 import mega.privacy.android.app.lollipop.controllers.AccountController
@@ -164,51 +163,47 @@ class PasscodeLockActivity : BaseActivity() {
             binding.passFirstInput.apply {
                 visibility = VISIBLE
                 requestFocus()
-                addTextChangedListener(object : CustomTextWatcher() {
-                    override fun afterTextChanged(editable: Editable?) {
-                        if (editable.toString().isNotEmpty()) {
-                            binding.passSecondInput.requestFocus()
-                        }
+
+                doAfterTextChanged {
+                    if (this.text.toString().isNotEmpty()) {
+                        binding.passSecondInput.requestFocus()
                     }
-                })
+                }
             }
 
             binding.passSecondInput.apply {
                 visibility = VISIBLE
-                addTextChangedListener(object : CustomTextWatcher() {
-                    override fun afterTextChanged(editable: Editable?) {
-                        if (editable.toString().isNotEmpty()) {
-                            binding.passThirdInput.requestFocus()
-                        }
+
+                doAfterTextChanged {
+                    if (this.text.toString().isNotEmpty()) {
+                        binding.passThirdInput.requestFocus()
                     }
-                })
+                }
             }
 
 
             binding.passThirdInput.apply {
                 visibility = VISIBLE
-                addTextChangedListener(object : CustomTextWatcher() {
-                    override fun afterTextChanged(editable: Editable?) {
-                        if (editable.toString().isNotEmpty()) {
-                            binding.passFourthInput.requestFocus()
-                        }
+
+                doAfterTextChanged {
+                    if (this.text.toString().isNotEmpty()) {
+                        binding.passFourthInput.requestFocus()
                     }
-                })
+                }
             }
 
             binding.passFourthInput.apply {
                 visibility = VISIBLE
-                addTextChangedListener(object : CustomTextWatcher() {
-                    override fun afterTextChanged(editable: Editable?) {
-                        if (editable.toString().isNotEmpty()) {
-                            if (passcodeType == PIN_4) {
-                                checkPasscode()
-                            } else {
-                                binding.passFifthInput.requestFocus()
-                            }
+
+                doAfterTextChanged {
+                    if (this.text.toString().isNotEmpty()) {
+                        if (passcodeType == PIN_4) {
+                            checkPasscode()
+                        } else {
+                            binding.passFifthInput.requestFocus()
                         }
                     }
-                })
+                }
             }
 
             val params = binding.passFourthInput.layoutParams as ConstraintLayout.LayoutParams
@@ -227,24 +222,22 @@ class PasscodeLockActivity : BaseActivity() {
 
                 binding.passFifthInput.apply {
                     visibility = VISIBLE
-                    addTextChangedListener(object : CustomTextWatcher() {
-                        override fun afterTextChanged(editable: Editable?) {
-                            if (editable.toString().isNotEmpty()) {
-                                binding.passSixthInput.requestFocus()
-                            }
+
+                    doAfterTextChanged {
+                        if (this.text.toString().isNotEmpty()) {
+                            binding.passSixthInput.requestFocus()
                         }
-                    })
+                    }
                 }
 
                 binding.passSixthInput.apply {
                     visibility = VISIBLE
-                    addTextChangedListener(object : CustomTextWatcher() {
-                        override fun afterTextChanged(editable: Editable?) {
-                            if (editable.toString().isNotEmpty()) {
-                                checkPasscode()
-                            }
+
+                    doAfterTextChanged {
+                        if (this.text.toString().isNotEmpty()) {
+                            checkPasscode()
                         }
-                    })
+                    }
                 }
             }
 
