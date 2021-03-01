5fd1244fe4d712dd67d49d9ba15f9c746ae9728e
Yenel <yr@mega.nz>
2021-03-01 17:43:13 +0100

    Fix folders view in MegaProviderLollipopAdapter AND-12749 Add the right rounded corners to all node lists

--- app/src/main/java/mega/privacy/android/app/lollipop/providers/MegaProviderLollipopAdapter.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/providers/MegaProviderLollipopAdapter.java
@@ -167,27 +167,24 @@ public class MegaProviderLollipopAdapter extends RecyclerView.Adapter<MegaProvid
 		setViewAlpha(holder.imageView, 1);
 		holder.textViewFileName.setTextColor(ContextCompat.getColor(context, android.R.color.black));
 
-		if (node.isFolder()){
+		RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
 
-			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
-			params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-			params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-			params.setMargins(0, 0, 0, 0);
-			holder.imageView.setLayoutParams(params);
+		if (node.isFolder()) {
+			params.height = params.width = dp2px(ICON_SIZE_DP);
+			params.setMargins(dp2px(ICON_MARGIN_DP), 0, 0, 0);
 
 			holder.imageView.setImageResource(R.drawable.ic_folder_list);
 			holder.textViewFileSize.setText(getMegaNodeFolderInfo(node));
 
-			if(node.isInShare()){
+			if (node.isInShare()) {
 				ArrayList<MegaShare> sharesIncoming = megaApi.getInSharesList();
-				for(int j=0; j<sharesIncoming.size(); j++){
+				for (int j = 0; j < sharesIncoming.size(); j++) {
 					MegaShare mS = sharesIncoming.get(j);
-					if(mS.getNodeHandle() == node.getHandle()){
+					if (mS.getNodeHandle() == node.getHandle()) {
 						MegaUser user = megaApi.getContact(mS.getUser());
-						if(user != null){
+						if (user != null) {
 							holder.textViewFileSize.setText(getMegaUserNameDB(user));
-						}
-						else{
+						} else {
 							holder.textViewFileSize.setText(mS.getUser());
 						}
 					}
@@ -196,62 +193,26 @@ public class MegaProviderLollipopAdapter extends RecyclerView.Adapter<MegaProvid
 				holder.permissionsIcon.setVisibility(View.VISIBLE);
 				int accessLevel = megaApi.getAccess(node);
 
-				if(accessLevel == MegaShare.ACCESS_FULL){
+				if (accessLevel == MegaShare.ACCESS_FULL) {
 					holder.permissionsIcon.setImageResource(R.drawable.ic_shared_fullaccess);
-				}
-				else if(accessLevel == MegaShare.ACCESS_READ){
+				} else if (accessLevel == MegaShare.ACCESS_READ) {
 					holder.permissionsIcon.setImageResource(R.drawable.ic_shared_read);
-				}
-				else{
+				} else {
 					holder.permissionsIcon.setImageResource(R.drawable.ic_shared_read_write);
 				}
-
-				if (!multipleSelect) {
-					holder.itemLayout.setBackgroundColor(Color.WHITE);
-					holder.imageView.setImageResource(R.drawable.ic_folder_list);
-				}
-				else {
-					if(this.isItemChecked(position)){
-						RelativeLayout.LayoutParams paramsMultiselect = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
-						paramsMultiselect.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-						paramsMultiselect.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-						paramsMultiselect.setMargins(0, 0, 0, 0);
-						holder.imageView.setLayoutParams(paramsMultiselect);
-
-						holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
-						holder.imageView.setImageResource(R.drawable.ic_select_folder);
-					}
-					else{
-						holder.itemLayout.setBackgroundColor(Color.WHITE);
-						holder.imageView.setImageResource(R.drawable.ic_folder_incoming_list);
-					}
-				}
-
-			}
-			else{
+			} else {
 				holder.permissionsIcon.setVisibility(View.GONE);
 				holder.textViewFileSize.setText(getMegaNodeFolderInfo(node));
+			}
 
-				if (!multipleSelect) {
-					holder.itemLayout.setBackgroundColor(Color.WHITE);
-					holder.imageView.setImageResource(R.drawable.ic_folder_list);
-				}
-				else {
-					if(this.isItemChecked(position)){
-						RelativeLayout.LayoutParams paramsMultiselect = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
-						paramsMultiselect.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-						paramsMultiselect.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, context.getResources().getDisplayMetrics());
-						paramsMultiselect.setMargins(0, 0, 0, 0);
-						holder.imageView.setLayoutParams(paramsMultiselect);
-
-						holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
-						holder.imageView.setImageResource(R.drawable.ic_select_folder);
-					}
-					else{
-						holder.itemLayout.setBackgroundColor(Color.WHITE);
-						holder.imageView.setImageResource(R.drawable.ic_folder_list);
-					}
-				}
+			if (isMultipleSelect() && isItemChecked(position)) {
+				holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
+				holder.imageView.setImageResource(R.drawable.ic_select_folder);
+			} else {
+				holder.itemLayout.setBackgroundColor(Color.WHITE);
+				holder.imageView.setImageResource(node.isInShare()
+						? R.drawable.ic_folder_incoming_list
+						: R.drawable.ic_folder_list);
 			}
 		} else {
 			holder.permissionsIcon.setVisibility(View.GONE);
@@ -259,9 +220,7 @@ public class MegaProviderLollipopAdapter extends RecyclerView.Adapter<MegaProvid
 			long nodeSize = node.getSize();
 			holder.textViewFileSize.setText(getSizeString(nodeSize));
 
-			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.imageView.getLayoutParams();
-
-			if (multipleSelect && isItemChecked(position)) {
+			if (isMultipleSelect() && isItemChecked(position)) {
 				params.height = params.width = dp2px(ICON_SIZE_DP);
 				params.setMargins(dp2px(ICON_MARGIN_DP), 0, 0, 0);
 				holder.itemLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.new_multiselect_color));
@@ -293,9 +252,9 @@ public class MegaProviderLollipopAdapter extends RecyclerView.Adapter<MegaProvid
 					holder.imageView.setImageResource(MimeTypeList.typeForName(node.getName()).getIconResourceId());
 				}
 			}
-
-			holder.imageView.setLayoutParams(params);
 		}
+
+		holder.imageView.setLayoutParams(params);
 	}
 
 	@Override
