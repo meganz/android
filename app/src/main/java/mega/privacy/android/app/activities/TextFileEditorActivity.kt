20c36c4d7fb1e61606ded94d51401baaac98af99
Yenel <yr@mega.nz>
2021-03-24 11:24:54 +0100

    AND-12732 Text editing: - Add toolbar menu options and control edit mode - Allow to edit/view text - Update UI after tap on save/edit buttons

--- app/src/main/java/mega/privacy/android/app/activities/TextFileEditorActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/TextFileEditorActivity.kt
@@ -1,13 +1,15 @@
 package mega.privacy.android.app.activities
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.core.view.isVisible
+import mega.privacy.android.app.R
 import mega.privacy.android.app.databinding.ActivityTextFileEditorBinding
 import mega.privacy.android.app.lollipop.PinActivityLollipop
 import mega.privacy.android.app.utils.Constants
 import mega.privacy.android.app.utils.FileUtil.TXT_EXTENSION
-import mega.privacy.android.app.utils.StringResourcesUtils
+import mega.privacy.android.app.utils.Util.showKeyboardDelayed
 import nz.mega.sdk.MegaApiJava.INVALID_HANDLE
 import nz.mega.sdk.MegaNode
 
@@ -19,31 +21,63 @@ class TextFileEditorActivity : PinActivityLollipop() {
         const val EDITING_MODE = "EDITING_MODE"
     }
 
-    private lateinit var bindings: ActivityTextFileEditorBinding
+    private lateinit var binding: ActivityTextFileEditorBinding
 
+    private var fileName: String? = null
     private var node: MegaNode? = null
     private var mode = VIEW_AND_EDIT_MODE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindings = ActivityTextFileEditorBinding.inflate(layoutInflater)
-        setContentView(bindings.root)
+        binding = ActivityTextFileEditorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setSupportActionBar(bindings.fileEditorToolbar)
+        setSupportActionBar(binding.fileEditorToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = null
 
         setUpMode()
+
+        val receivedName = intent.getStringExtra(Constants.INTENT_EXTRA_KEY_FILE_NAME)
+
+        fileName = if (receivedName != null) receivedName + TXT_EXTENSION else node?.name
+
         setUpTextFileName()
+        setUpTextView()
         setUpEditFAB()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> onBackPressed()
+            R.id.action_save -> saveFile()
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_text_file_editor, menu)
+
+        if (mode == VIEW_AND_EDIT_MODE) {
+            val downloadMenuItem = menu?.findItem(R.id.action_download)
+            val infoMenuItem = menu?.findItem(R.id.action_properties)
+            val shareMenuItem = menu?.findItem(R.id.action_share)
+            val sendToChatMenuItem = menu?.findItem(R.id.action_send_to_chat)
+            val getLinkMenuItem = menu?.findItem(R.id.action_get_link)
+            val removeLinkMenuItem = menu?.findItem(R.id.action_remove_link)
+            val renameMenuItem = menu?.findItem(R.id.action_rename)
+            val moveMenuItem = menu?.findItem(R.id.action_move)
+            val copyMenuItem = menu?.findItem(R.id.action_copy)
+            val moveToTrashMenuItem = menu?.findItem(R.id.action_move_to_trash)
+            val removeMenuItem = menu?.findItem(R.id.action_remove)
+            val chatImportMenuItem = menu?.findItem(R.id.chat_action_import)
+            val chatOfflineMenuItem = menu?.findItem(R.id.chat_action_save_for_offline)
+            val chatRemoveMenuItem = menu?.findItem(R.id.chat_action_remove)
+        } else {
+            menu?.findItem(R.id.action_save)?.isVisible = true
+        }
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun setUpMode() {
@@ -58,22 +92,50 @@ class TextFileEditorActivity : PinActivityLollipop() {
     }
 
     private fun setUpTextFileName() {
-        val receivedName = intent.getStringExtra(Constants.INTENT_EXTRA_KEY_FILE_NAME)
+        if (mode == VIEW_AND_EDIT_MODE) {
+            supportActionBar?.title = null
 
-        bindings.nameText.text =
-            if (mode == CREATE_MODE && receivedName != null) receivedName + TXT_EXTENSION
-            else node?.name
+            binding.nameText.apply {
+                isVisible = true
+                text = fileName
+            }
+        } else {
+            supportActionBar?.title = fileName
+            binding.nameText.isVisible = false
+        }
+    }
+
+    private fun setUpTextView() {
+        if (mode == VIEW_AND_EDIT_MODE) {
+            binding.editText.isEnabled = false
+        } else {
+            binding.editText.isEnabled = true
+            binding.editText.requestFocus()
+            showKeyboardDelayed(binding.editText)
+        }
     }
 
     private fun setUpEditFAB() {
-        bindings.editFab.apply {
+        binding.editFab.apply {
             isVisible = mode == VIEW_AND_EDIT_MODE
 
             setOnClickListener {
                 mode = EDITING_MODE
                 this.hide()
-                invalidateOptionsMenu()
+                updateUIAfterChangeMode()
             }
         }
+    }
+
+    private fun saveFile() {
+        mode = VIEW_AND_EDIT_MODE
+        binding.editFab.show()
+        updateUIAfterChangeMode()
+    }
+
+    private fun updateUIAfterChangeMode() {
+        setUpTextFileName()
+        setUpTextView()
+        invalidateOptionsMenu()
     }
 }
