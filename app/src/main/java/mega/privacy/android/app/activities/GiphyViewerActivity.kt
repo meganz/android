f0b253f01d64727bfb6f91183144ed7978f710e2
Javier Gomez <jg@mega.co.nz>
2021-05-04 12:21:21 +0200

    Fix build warnings at `GiphyViewerActivity.kt` and `ColorUtils.kt` files
    w: 'setter for systemUiVisibility: Int' is deprecated. Deprecated in
    Java
    w: 'SYSTEM_UI_FLAG_LAYOUT_STABLE: Int' is deprecated. Deprecated in Java
    w: 'SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR: Int' is deprecated. Deprecated
    in Java
    w: 'SYSTEM_UI_FLAG_LIGHT_STATUS_BAR: Int' is deprecated. Deprecated in
    Java
    
    AND-12038: Android 11 support

--- app/src/main/java/mega/privacy/android/app/activities/GiphyViewerActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/GiphyViewerActivity.kt
@@ -1,8 +1,12 @@
 package mega.privacy.android.app.activities
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
 import androidx.core.content.ContextCompat
 import mega.privacy.android.app.R
 import mega.privacy.android.app.activities.GiphyPickerActivity.Companion.GIF_DATA
@@ -11,6 +15,7 @@ import mega.privacy.android.app.objects.GifData
 import mega.privacy.android.app.utils.Constants.ACTION_PREVIEW_GIPHY
 import mega.privacy.android.app.utils.FrescoUtils.loadGif
 import mega.privacy.android.app.utils.GiphyUtil.Companion.getOriginalGiphySrc
+import mega.privacy.android.app.utils.Util
 import mega.privacy.android.app.utils.Util.isScreenInPortrait
 
 class GiphyViewerActivity : PasscodeActivity() {
@@ -25,7 +30,32 @@ class GiphyViewerActivity : PasscodeActivity() {
         super.onCreate(savedInstanceState)
 
         window?.statusBarColor = ContextCompat.getColor(this,R.color.black)
-        window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or 0x00000010
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window?.setDecorFitsSystemWindows(true)
+            val wic: WindowInsetsController? = window?.decorView?.windowInsetsController
+
+            if (!Util.isDarkMode(applicationContext)) {
+                wic?.setSystemBarsAppearance(
+                    0,
+                    APPEARANCE_LIGHT_STATUS_BARS
+                )
+            }
+
+            wic?.setSystemBarsAppearance(
+                APPEARANCE_LIGHT_NAVIGATION_BARS,
+                APPEARANCE_LIGHT_NAVIGATION_BARS
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                window?.decorView?.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            } else {
+                window?.decorView?.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or 0x00000010
+            }
+        }
 
         binding = ActivityGiphyViewerBinding.inflate(layoutInflater)
         setContentView(binding.root)
