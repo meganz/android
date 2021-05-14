6dd305dca56409fd94e7e249b039b911cb37274a
Yenel <yr@mega.nz>
2021-05-14 15:17:56 +0200

    Fix scroll elevation and add upgrade account info AND-12882 Convert fragments Choose and Upgrade account to activities and migrate them to Kotlin

--- app/src/main/java/mega/privacy/android/app/activities/upgradeAccount/UpgradeAccountActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/upgradeAccount/UpgradeAccountActivity.kt
@@ -1,21 +1,33 @@
 package mega.privacy.android.app.activities.upgradeAccount
 
 import android.os.Bundle
+import android.text.Spanned
 import android.view.MenuItem
 import android.view.View
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import mega.privacy.android.app.R
 import mega.privacy.android.app.activities.PasscodeActivity
+import mega.privacy.android.app.activities.upgradeAccount.UpgradeAccountViewModel.Companion.TYPE_STORAGE_LABEL
+import mega.privacy.android.app.activities.upgradeAccount.UpgradeAccountViewModel.Companion.TYPE_TRANSFER_LABEL
 import mega.privacy.android.app.components.ListenScrollChangesHelper
 import mega.privacy.android.app.databinding.ActivityUpgradeAccountBinding
 import mega.privacy.android.app.fragments.homepage.Scrollable
 import mega.privacy.android.app.utils.ColorUtils.changeStatusBarColorForElevation
 import mega.privacy.android.app.utils.ColorUtils.getColorForElevation
-import mega.privacy.android.app.utils.Constants.SCROLLING_UP_DIRECTION
+import mega.privacy.android.app.utils.Constants.*
+import mega.privacy.android.app.utils.LogUtil.logDebug
+import mega.privacy.android.app.utils.LogUtil.logWarning
 import mega.privacy.android.app.utils.Util.isDarkMode
 
 open class UpgradeAccountActivity : PasscodeActivity(), Scrollable {
 
     private lateinit var binding: ActivityUpgradeAccountBinding
+    private val viewModel by viewModels<UpgradeAccountViewModel>()
+
+    private var displayedAccountType = INVALID_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,20 +54,79 @@ open class UpgradeAccountActivity : PasscodeActivity(), Scrollable {
         }
 
         ListenScrollChangesHelper().addViewToListen(
-            binding.scrollViewUpgrade
+            binding.scrollView
         ) { _: View?, _: Int, _: Int, _: Int, _: Int ->
             checkScroll()
         }
 
+        binding.upgradeProliteLayout.setOnClickListener {
+            if (binding.prolite.selectPaymentsLayout.isVisible) {
+                binding.prolite.selectPaymentsLayout.isVisible = false
+                binding.semitransparentLayer.isVisible = false
+            } else {
+                binding.proI.selectPaymentsLayout.isVisible = false
+                binding.proIi.selectPaymentsLayout.isVisible = false
+                binding.proIii.selectPaymentsLayout.isVisible = false
+                onUpgradeClick(PRO_LITE)
+            }
+        }
+
+        binding.upgradeProILayout.setOnClickListener {
+            if (binding.proI.selectPaymentsLayout.isVisible) {
+                binding.proI.selectPaymentsLayout.isVisible = false
+            } else {
+                binding.prolite.selectPaymentsLayout.isVisible = false
+                binding.proIi.selectPaymentsLayout.isVisible = false
+                binding.proIii.selectPaymentsLayout.isVisible = false
+                binding.semitransparentLayer.isVisible = false
+                onUpgradeClick(PRO_I)
+            }
+        }
+
+        binding.upgradeProIiLayout.setOnClickListener {
+            if (binding.proIi.selectPaymentsLayout.isVisible) {
+                binding.proIi.selectPaymentsLayout.isVisible = false
+            } else {
+                binding.prolite.selectPaymentsLayout.isVisible = false
+                binding.proI.selectPaymentsLayout.isVisible = false
+                binding.proIii.selectPaymentsLayout.isVisible = false
+                binding.semitransparentLayer.isVisible = false
+                onUpgradeClick(PRO_II)
+            }
+        }
+
+        binding.upgradeProIiiLayout.setOnClickListener {
+            if (binding.proIii.selectPaymentsLayout.isVisible) {
+                binding.proIii.selectPaymentsLayout.isVisible = false
+            } else {
+                binding.prolite.selectPaymentsLayout.isVisible = false
+                binding.proI.selectPaymentsLayout.isVisible = false
+                binding.proIi.selectPaymentsLayout.isVisible = false
+                binding.semitransparentLayer.isVisible = false
+                onUpgradeClick(PRO_III)
+            }
+        }
+
+        binding.lblCustomPlan.setOnClickListener {
+
+        }
+
         checkScroll()
+        setAccountDetails()
+        refreshAccountInfo()
+        setPricingInfo()
+        showAvailableAccount()
     }
 
     override fun checkScroll() {
         if (!this::binding.isInitialized)
             return
 
-        val withElevation = binding.scrollViewUpgrade.canScrollVertically(SCROLLING_UP_DIRECTION)
+        val withElevation = binding.scrollView.canScrollVertically(SCROLLING_UP_DIRECTION)
         val elevation = resources.getDimension(R.dimen.toolbar_elevation)
+
+        binding.appBarLayout.elevation = if (withElevation) elevation else 0f
+        changeStatusBarColorForElevation(this, withElevation)
 
         binding.toolbar.setBackgroundColor(
             if (isDarkMode(this@UpgradeAccountActivity) && withElevation) getColorForElevation(
@@ -63,9 +134,128 @@ open class UpgradeAccountActivity : PasscodeActivity(), Scrollable {
                 elevation
             ) else android.R.color.transparent
         )
+    }
 
-        supportActionBar?.elevation = if (withElevation) elevation else 0f
-        val el = supportActionBar?.elevation
-        changeStatusBarColorForElevation(this, withElevation)
+    private fun setAccountDetails() {
+        if (viewModel.isGettingInfo()) {
+            binding.textOfMyAccount.text = getString(R.string.recovering_info)
+            binding.textOfMyAccount.setTextColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.grey_054_white_054
+                )
+            )
+
+            return
+        }
+
+        var text: String
+        val color: String
+
+        when (viewModel.getAccountType()) {
+            FREE -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.free_account))
+                color = ContextCompat.getColor(this, R.color.green_500_green_400).toString()
+            }
+            PRO_I -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.pro1_account))
+                color = ContextCompat.getColor(this, R.color.red_600_red_300).toString()
+            }
+            PRO_II -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.pro2_account))
+                color = ContextCompat.getColor(this, R.color.red_600_red_300).toString()
+            }
+            PRO_III -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.pro3_account))
+                color = ContextCompat.getColor(this, R.color.red_600_red_300).toString()
+            }
+            PRO_LITE -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.prolite_account))
+                color = ContextCompat.getColor(this, R.color.lite_account).toString()
+            }
+            else -> {
+                text = getString(R.string.type_of_my_account, getString(R.string.free_account))
+                color = ContextCompat.getColor(this, R.color.green_500_green_400).toString()
+            }
+        }
+
+        try {
+            text = text.replace("[A]", "<font color='$color'>")
+            text = text.replace("[/A]", "</font>")
+        } catch (e: Exception) {
+            logWarning("Exception formatting string", e)
+        }
+
+        binding.textOfMyAccount.text =
+            HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+
+    private fun setPricingInfo() {
+        val productAccounts = viewModel.getProductAccounts()
+
+        if (productAccounts == null) {
+            logDebug("productAccounts == null")
+            app.askForPricing()
+            return
+        }
+
+        for (i in productAccounts.indices) {
+            val account = productAccounts[i]
+
+            if (account.months == 1) {
+                val textToShow: Spanned = viewModel.getPriceString(this, account, true)
+                val textStorage: Spanned = viewModel.generateByteString(
+                    this,
+                    account.storage.toLong(),
+                    TYPE_STORAGE_LABEL
+                )
+
+                val textTransfer: Spanned = viewModel.generateByteString(
+                    this,
+                    account.transfer.toLong(),
+                    TYPE_TRANSFER_LABEL
+                )
+
+                when (account.level) {
+                    PRO_I -> {
+                        binding.monthProI.text = textToShow
+                        binding.storageProI.text = textStorage
+                        binding.bandwidthProI.text = textTransfer
+                    }
+                    PRO_II -> {
+                        binding.monthProIi.text = textToShow
+                        binding.storageProIi.text = textStorage
+                        binding.bandwidthProIi.text = textTransfer
+                    }
+                    PRO_III -> {
+                        binding.monthProIii.text = textToShow
+                        binding.storageProIii.text = textStorage
+                        binding.bandwidthProIii.text = textTransfer
+                    }
+                    PRO_LITE -> {
+                        binding.monthLite.text = textToShow
+                        binding.storageLite.text = textStorage
+                        binding.bandwidthLite.text = textTransfer
+                    }
+                }
+            }
+        }
+
+        if (displayedAccountType != INVALID_VALUE) {
+            onUpgradeClick(displayedAccountType)
+        }
+    }
+
+    private fun showAvailableAccount() {
+        when (viewModel.getAccountType()) {
+            PRO_I -> binding.upgradeProILayoutTransparent.isVisible = true
+            PRO_II -> binding.upgradeProIiLayoutTransparent.isVisible = true
+            PRO_III -> binding.upgradeProIiiLayoutTransparent.isVisible = true
+            PRO_LITE -> binding.upgradeProliteLayoutTransparent.isVisible = true
+        }
+    }
+
+    private fun onUpgradeClick(upgradeType: Int) {
+        refreshAccountInfo()
     }
 }
--- app/src/main/java/mega/privacy/android/app/activities/upgradeAccount/UpgradeAccountViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/activities/upgradeAccount/UpgradeAccountViewModel.kt
@@ -1,11 +1,133 @@
 package mega.privacy.android.app.activities.upgradeAccount
 
+import android.content.Context
+import android.text.Spanned
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.hilt.lifecycle.ViewModelInject
+import mega.privacy.android.app.MegaApplication
+import mega.privacy.android.app.Product
+import mega.privacy.android.app.R
 import mega.privacy.android.app.arch.BaseRxViewModel
 import mega.privacy.android.app.globalmanagement.MyAccountInfo
+import mega.privacy.android.app.utils.ColorUtils.getColorHexString
+import mega.privacy.android.app.utils.Constants
+import mega.privacy.android.app.utils.Constants.FREE
+import mega.privacy.android.app.utils.Constants.PRO_LITE
+import mega.privacy.android.app.utils.DBUtil
+import mega.privacy.android.app.utils.LogUtil.logDebug
+import mega.privacy.android.app.utils.LogUtil.logError
+import mega.privacy.android.app.utils.StringResourcesUtils.getString
+import mega.privacy.android.app.utils.Util
+import mega.privacy.android.app.utils.billing.PaymentUtils
+import java.text.NumberFormat
+import java.util.*
 
 class UpgradeAccountViewModel @ViewModelInject constructor(
     private val myAccountInfo: MyAccountInfo
 ) : BaseRxViewModel() {
 
+    companion object {
+        const val TYPE_STORAGE_LABEL = 0
+        const val TYPE_TRANSFER_LABEL = 1
+    }
+
+    fun isGettingInfo(): Boolean =
+        myAccountInfo.accountType < FREE || myAccountInfo.accountType > PRO_LITE
+
+    fun getAccountType(): Int = myAccountInfo.accountType
+
+    fun getProductAccounts(): ArrayList<Product>? = myAccountInfo.productAccounts
+
+    fun refreshAccountInfo() {
+        logDebug("Check the last call to callToPricing")
+        if (DBUtil.callToPricing()) {
+            logDebug("megaApi.getPricing SEND")
+            MegaApplication.getInstance().askForPricing()
+        }
+
+        logDebug("Check the last call to callToPaymentMethods")
+        if (DBUtil.callToPaymentMethods()) {
+            logDebug("megaApi.getPaymentMethods SEND")
+            MegaApplication.getInstance().askForPaymentMethods()
+        }
+    }
+
+    fun getPriceString(
+        context: Context,
+        product: Product,
+        monthlyBasePrice: Boolean
+    ): Spanned {
+        // First get the "default" pricing details from the MEGA server
+        var price = product.amount / 100.00
+        var currency = product.currency
+
+        // Try get the local pricing details from the store if available
+        val details =
+            PaymentUtils.getSkuDetails(myAccountInfo.availableSkus, PaymentUtils.getSku(product))
+
+        if (details != null) {
+            price = details.priceAmountMicros / 1000000.00
+            currency = details.priceCurrencyCode
+        }
+
+        val format = NumberFormat.getCurrencyInstance()
+        format.currency = Currency.getInstance(currency)
+
+        var stringPrice = format.format(price)
+        var color = getColorHexString(context, R.color.grey_900_grey_100)
+
+        if (monthlyBasePrice) {
+            if (product.months != 1) {
+                return HtmlCompat.fromHtml("", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+
+            when (product.level) {
+                Constants.PRO_I, Constants.PRO_II, Constants.PRO_III -> color =
+                    ContextCompat.getColor(context, R.color.red_600_red_300).toString()
+                PRO_LITE -> color =
+                    ContextCompat.getColor(context, R.color.orange_400_orange_300).toString()
+            }
+
+            stringPrice = getString(R.string.type_month, stringPrice)
+        } else {
+            stringPrice = getString(
+                if (product.months == 12) R.string.billed_yearly_text else R.string.billed_monthly_text,
+                stringPrice
+            )
+        }
+
+        try {
+            stringPrice = stringPrice.replace("[A]", "<font color='$color'>")
+            stringPrice = stringPrice.replace("[/A]", "</font>")
+        } catch (e: Exception) {
+            logError("Exception formatting string", e)
+        }
+
+        return HtmlCompat.fromHtml(stringPrice, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+
+    fun generateByteString(context: Context, gb: Long, labelType: Int): Spanned {
+        var textToShow =
+            "[A] " + Util.getSizeStringGBBased(gb) + " [/A] " + storageOrTransferLabel(labelType)
+        try {
+            textToShow = textToShow.replace(
+                "[A]", "<font color='"
+                        + getColorHexString(context, R.color.grey_900_grey_100)
+                        + "'>"
+            )
+            textToShow = textToShow.replace("[/A]", "</font>")
+        } catch (e: java.lang.Exception) {
+            logError("Exception formatting string", e)
+        }
+        return HtmlCompat.fromHtml(textToShow, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+
+    private fun storageOrTransferLabel(labelType: Int): String? {
+        return when (labelType) {
+            TYPE_STORAGE_LABEL -> getString(R.string.label_storage_upgrade_account)
+            TYPE_TRANSFER_LABEL -> getString(R.string.label_transfer_quota_upgrade_account)
+            else -> ""
+        }
+    }
 }
