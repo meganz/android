8001da366f6a38ed8dff3475a79f02b7bc23a5ae
Yenel <yr@mega.nz>
2021-05-17 14:44:12 +0200

    Move rich links initial behaviour to MegaApplication AND-12903 Rich URL previews setting initial value is always disabled by default

--- app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
@@ -2429,9 +2429,6 @@ public class ManagerActivityLollipop extends TransfersManagementActivity
 			initGooglePlayPayments();
 
 			megaApi.addGlobalListener(this);
-
-			megaApi.shouldShowRichLinkWarning(this);
-			megaApi.isRichPreviewsEnabled(this);
 			megaApi.isGeolocationEnabled(this);
 
 			if(savedInstanceState==null) {
