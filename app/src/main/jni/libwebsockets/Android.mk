787f4d6264f2cdbfbd2d508027ed14ae23c410cf
Alfonso Abella <aag@mega.co.nz>
2021-06-08 21:50:48 +0200

    Fixed libwebsockets paths
    Added some missing log outputs

--- app/src/main/jni/libwebsockets/Android.mk
+++ app/src/main/jni/libwebsockets/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libwebsockets
 LOCAL_CFLAGS    := 
-LOCAL_SRC_FILES := $(LOCAL_PATH)/libwebsockets-android-$(TARGET_ARCH_ABI)/lib/libwebsockets.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libwebsockets-android-$(TARGET_ARCH_ABI)/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/libwebsockets/libwebsockets-android-$(TARGET_ARCH_ABI)/lib/libwebsockets.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libwebsockets/libwebsockets-android-$(TARGET_ARCH_ABI)/include
 
 ifeq ($(DISABLE_WEBRTC),true)
 LOCAL_STATIC_LIBRARIES := libuv ssl crypto
