dd9fa105479100e27a4c356dd5f4fe725cf3ce9d
Ash <aw@mega.co.nz>
2021-07-13 19:37:47 +1200

    Fix a layout issue when there're >= 6 participants on portrait mode
    feature/meetings

--- app/src/main/java/mega/privacy/android/app/meeting/adapter/VideoMeetingViewHolder.kt
+++ app/src/main/java/mega/privacy/android/app/meeting/adapter/VideoMeetingViewHolder.kt
@@ -599,15 +599,9 @@ class VideoMeetingViewHolder(
                     when (adapterPosition) {
                         POSITION_0, POSITION_1 -> {
                             marginTop = ((screenHeight - screenWidth / 2 * 3) / 2)
-                            marginBottom = borderWidth
                         }
-                        POSITION_2, POSITION_3 -> {
-                            marginTop = 0
-                            marginBottom = 0
-                        }
-                        POSITION_4, POSITION_5 -> {
+                        POSITION_2, POSITION_3, POSITION_4, POSITION_5 -> {
                             marginTop = borderWidth
-                            marginBottom = 0
                         }
                     }
 
@@ -622,14 +616,13 @@ class VideoMeetingViewHolder(
 
             when (adapterPosition) {
                 POSITION_0, POSITION_1 -> marginTop = ((screenHeight - screenWidth / 2 * 3) / 2)
+                POSITION_2, POSITION_3, POSITION_4, POSITION_5 -> {
+                    marginTop = borderWidth
+                }
             }
 
             when (adapterPosition) {
-                POSITION_0, POSITION_1, POSITION_2, POSITION_3 -> marginBottom = borderWidth
-            }
-
-            when (adapterPosition) {
-                POSITION_0, POSITION_2, POSITION_4 -> marginRight = borderWidth
+                POSITION_1, POSITION_3, POSITION_5 -> marginLeft = borderWidth
             }
         }
 
@@ -693,6 +686,6 @@ class VideoMeetingViewHolder(
         const val POSITION_2 = 2
         const val POSITION_3 = 3
         const val POSITION_4 = 4
-        const val POSITION_5 = 6
+        const val POSITION_5 = 5
     }
 }
