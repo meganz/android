7d94a3ad26ce0287978a8119dc09e06cf3f8156e
Yenel <yr@mega.nz>
2021-10-28 15:59:17 +0200

    Add StartScreenPreferencesActivity subtitle AND-12623 Add the ability to config app initial start screen

--- app/src/main/java/mega/privacy/android/app/activities/settingsActivities/StartScreenPreferencesActivity.kt
+++ app/src/main/java/mega/privacy/android/app/activities/settingsActivities/StartScreenPreferencesActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.fragment.app.commit
 import mega.privacy.android.app.R
 import mega.privacy.android.app.fragments.settingsFragments.startSceen.StartScreenSettingsFragment
+import mega.privacy.android.app.utils.StringResourcesUtils
 
 class StartScreenPreferencesActivity : PreferencesBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.subtitle = StringResourcesUtils.getString(R.string.configure_start_screen)
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
