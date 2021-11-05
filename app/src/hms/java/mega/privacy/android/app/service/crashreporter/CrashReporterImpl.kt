090d4948e6276cd3fecdda6d151e89c4d1092fb4
Ash <aw@mega.co.nz>
2021-11-05 16:31:54 +1300

    Integrate crash analytics feature for HMS
    task/AND-13214-hms-crash-service

--- app/src/hms/java/mega/privacy/android/app/service/crashreporter/CrashReporterImpl.kt
+++ app/src/hms/java/mega/privacy/android/app/service/crashreporter/CrashReporterImpl.kt
@@ -1,10 +1,11 @@
 package mega.privacy.android.app.service.crashreporter
 
+import com.huawei.agconnect.crash.AGConnectCrash
 import mega.privacy.android.app.middlelayer.crashreporter.CrashReporter
 
 class CrashReporterImpl : CrashReporter {
 
     override fun report(e: Throwable) {
-        // Haven't implemented for HMS.
+        AGConnectCrash.getInstance().recordException(e)
     }
 }
