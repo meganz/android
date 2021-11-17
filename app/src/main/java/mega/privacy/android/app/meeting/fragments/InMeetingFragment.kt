e5d59fd0f8de4b853a29d4117d8780f95942a039
raquelgc6 <rgc@mega.nz>
2021-11-17 10:34:11 +0100

    AND-13232 - NPE InMeetingFragment.finishActivityAsGuest

--- app/src/main/java/mega/privacy/android/app/meeting/fragments/InMeetingFragment.kt
+++ app/src/main/java/mega/privacy/android/app/meeting/fragments/InMeetingFragment.kt
@@ -2646,11 +2646,15 @@ class InMeetingFragment : MeetingBaseFragment(), BottomFloatingPanelListener, Sn
      */
     private fun finishActivityAsGuest() {
         val chatId = inMeetingViewModel.getChatId()
-        val callId = inMeetingViewModel.getCall()?.callId
-        logDebug("Finishing the activity as guest: chatId $chatId, callId $callId")
-        if (chatId != MEGACHAT_INVALID_HANDLE && callId != MEGACHAT_INVALID_HANDLE) {
-            MegaApplication.getChatManagement().controlCallFinished(callId!!, chatId)
+
+        inMeetingViewModel.getCall()?.let {
+            val callId = it.callId
+            logDebug("Finishing the activity as guest: chatId $chatId, callId $callId")
+            if (chatId != MEGACHAT_INVALID_HANDLE && callId != MEGACHAT_INVALID_HANDLE) {
+                MegaApplication.getChatManagement().controlCallFinished(callId, chatId)
+            }
         }
+
         AccountController.logout(
             meetingActivity,
             MegaApplication.getInstance().megaApi
@@ -2873,6 +2877,7 @@ class InMeetingFragment : MeetingBaseFragment(), BottomFloatingPanelListener, Sn
     }
 
     override fun onJoinedChat(chatId: Long, userHandle: Long) {
+        logDebug("Joined to the chat")
         controlWhenJoinedAChat(chatId)
     }
 
