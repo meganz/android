4770412868937959210e8ad04f1fb9aecd90bc6d
Javier Gomez <jg@mega.co.nz>
2021-11-18 14:06:39 +0100

    Remove MANAGE_EXTERNAL_STORAGE permission
    AND-13229: Remove MANAGE_EXTERNAL_STORAGE permission

--- app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/FullScreenImageViewerLollipop.java
@@ -1538,15 +1538,7 @@ public class FullScreenImageViewerLollipop extends PasscodeActivity
 				}
 			}
 		} else if (requestCode == REQUEST_WRITE_STORAGE) {
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-				if (Environment.isExternalStorageManager()) {
-					nodeSaver.handleRequestPermissionsResult(requestCode);
-				} else {
-					super.onActivityResult(requestCode, resultCode, intent);
-				}
-			} else {
-				super.onActivityResult(requestCode, resultCode, intent);
-			}
+			super.onActivityResult(requestCode, resultCode, intent);
 		}
 	}
 
--- app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
+++ app/src/main/java/mega/privacy/android/app/lollipop/ManagerActivityLollipop.java
@@ -8568,47 +8568,29 @@ public class ManagerActivityLollipop extends TransfersManagementActivity
                 fileIntent.setType(intent.getType());
                 startActivity(fileIntent);
             }
-        } else if (requestCode == PERMISSIONS_FRAGMENT) {
+        } else if (requestCode == REQUEST_WRITE_STORAGE || requestCode == REQUEST_READ_WRITE_STORAGE) {
 			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-				if (!Environment.isExternalStorageManager()) {
-					Toast.makeText(this,
-							StringResourcesUtils.getString(R.string.snackbar_storage_permission_denied_android_11),
-							Toast.LENGTH_SHORT).show();
-				}
-
-				if (getPermissionsFragment() != null) {
-					pF.setNextPermission();
-				}
-			}
-		} else if (requestCode == REQUEST_WRITE_STORAGE || requestCode == REQUEST_READ_WRITE_STORAGE) {
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-				if (!Environment.isExternalStorageManager()) {
-					Toast.makeText(this,
-							StringResourcesUtils.getString(R.string.snackbar_storage_permission_denied_android_11),
-							Toast.LENGTH_SHORT).show();
-				} else {
-					switch (requestCode) {
-						case REQUEST_WRITE_STORAGE:
-							// Take picture scenarios
-							if (typesCameraPermission == TAKE_PICTURE_OPTION) {
-								if (!hasPermissions(this, Manifest.permission.CAMERA)) {
-									requestPermission(this, REQUEST_CAMERA, Manifest.permission.CAMERA);
-								} else {
-									checkTakePicture(this, TAKE_PHOTO_CODE);
-									typesCameraPermission = INVALID_TYPE_PERMISSIONS;
-								}
-								break;
+				switch (requestCode) {
+					case REQUEST_WRITE_STORAGE:
+						// Take picture scenarios
+						if (typesCameraPermission == TAKE_PICTURE_OPTION) {
+							if (!hasPermissions(this, Manifest.permission.CAMERA)) {
+								requestPermission(this, REQUEST_CAMERA, Manifest.permission.CAMERA);
+							} else {
+								checkTakePicture(this, TAKE_PHOTO_CODE);
+								typesCameraPermission = INVALID_TYPE_PERMISSIONS;
 							}
-
-							// General download scenario
-							nodeSaver.handleRequestPermissionsResult(requestCode);
 							break;
+						}
 
-						case REQUEST_READ_WRITE_STORAGE:
-							// Upload scenario
-							new Handler(Looper.getMainLooper()).post(this::showUploadPanel);
-							break;
-					}
+						// General download scenario
+						nodeSaver.handleRequestPermissionsResult(requestCode);
+						break;
+
+					case REQUEST_READ_WRITE_STORAGE:
+						// Upload scenario
+						new Handler(Looper.getMainLooper()).post(this::showUploadPanel);
+						break;
 				}
 			}
 		} else {
