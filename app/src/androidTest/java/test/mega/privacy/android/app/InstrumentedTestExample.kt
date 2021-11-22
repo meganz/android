6d093dd2ac7a8e7cfc94e4e2cdbb23f2188dfc26
Robin Shi <rsh@mega.co.nz>
2021-11-22 14:00:41 +1300

    Example unit test cases - Mockito, Roboletric, InstrumentedTest

---
+++ app/src/androidTest/java/test/mega/privacy/android/app/InstrumentedTestExample.kt
@@ -0,0 +1,17 @@
+package test.mega.privacy.android.app
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class InstrumentedTestExample {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("mega.privacy.android.app", appContext.packageName)
+    }
+}
