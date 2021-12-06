4c02eb398f59c29179ece310a47717539f31ca68
joeji <jj@mega.co.nz>
2021-12-06 19:19:51 +1300

    Photos sprint 3  - fix bug: cannot read local media file when delete cu folder. CC/photos_sprint_3

--- app/src/main/java/mega/privacy/android/app/fragments/managerFragments/cu/CuViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/managerFragments/cu/CuViewModel.kt
@@ -3,53 +3,28 @@ package mega.privacy.android.app.fragments.managerFragments.cu
 import android.content.Context
 import android.os.Environment
 import android.util.Pair
-import androidx.collection.LongSparseArray
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.PublishSubject
-import io.reactivex.rxjava3.subjects.Subject
 import mega.privacy.android.app.DatabaseHandler
-import mega.privacy.android.app.MegaApplication
 import mega.privacy.android.app.MegaPreferences
-import mega.privacy.android.app.arch.BaseRxViewModel
 import mega.privacy.android.app.constants.SettingsConstants
 import mega.privacy.android.app.di.MegaApi
-import mega.privacy.android.app.fragments.homepage.photos.CardClickHandler.getClickedCard
-import mega.privacy.android.app.fragments.homepage.photos.CardClickHandler.monthClicked
-import mega.privacy.android.app.fragments.homepage.photos.CardClickHandler.yearClicked
-import mega.privacy.android.app.fragments.homepage.photos.DateCardsProvider
-import mega.privacy.android.app.gallery.data.GalleryCard
-import mega.privacy.android.app.gallery.data.GalleryItem
 import mega.privacy.android.app.gallery.ui.GalleryViewModel
 import mega.privacy.android.app.globalmanagement.SortOrderManagement
-import mega.privacy.android.app.listeners.OptionalMegaRequestListenerInterface
 import mega.privacy.android.app.repo.MegaNodeRepo
-import mega.privacy.android.app.utils.Constants.GET_THUMBNAIL_THROTTLE_MS
-import mega.privacy.android.app.utils.Constants.INVALID_POSITION
 import mega.privacy.android.app.utils.FileUtil
 import mega.privacy.android.app.utils.LogUtil.logDebug
-import mega.privacy.android.app.utils.PreviewUtils
 import mega.privacy.android.app.utils.RxUtil.IGNORE
 import mega.privacy.android.app.utils.RxUtil.logErr
-import mega.privacy.android.app.utils.ThumbnailUtilsLollipop
-import mega.privacy.android.app.utils.Util.fromEpoch
-import mega.privacy.android.app.utils.ZoomUtil
-import nz.mega.sdk.*
-import java.io.File
-import java.time.LocalDate
-import java.time.Year
-import java.time.YearMonth
-import java.time.format.DateTimeFormatter
-import java.util.ArrayList
-import java.util.HashSet
-import java.util.concurrent.TimeUnit
+import nz.mega.sdk.MegaApiAndroid
+import nz.mega.sdk.MegaNode
+import java.util.*
 
 class CuViewModel @ViewModelInject constructor(
     @MegaApi private val mMegaApi: MegaApiAndroid,
@@ -112,8 +87,9 @@ class CuViewModel @ViewModelInject constructor(
 
     fun enableCu(enableCellularSync: Boolean, syncVideo: Boolean) {
         add(Completable.fromCallable {
-            val localFile =
-                MegaApplication.getInstance().getExternalFilesDir(Environment.DIRECTORY_DCIM)
+            val localFile = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM
+            )
             mDbHandler.setCamSyncLocalPath(localFile?.absolutePath)
             mDbHandler.setCameraFolderExternalSDCard(false)
             mDbHandler.setCamSyncWifi(!enableCellularSync)
