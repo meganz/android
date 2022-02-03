cd75744e00dca7598423a56c178bbd45f9ab4461
Gonzalo Toledano <gonzalotldn@gmail.com>
2022-02-03 11:05:43 +0100

    Ensuring Firebase is initialised before getting Crashlytics instance

--- app/src/gms/java/mega/privacy/android/app/service/di/AnalyticsModule.kt
+++ app/src/gms/java/mega/privacy/android/app/service/di/AnalyticsModule.kt
@@ -1,15 +1,19 @@
 package mega.privacy.android.app.service.di
 
+import android.content.Context
+import com.google.firebase.FirebaseApp
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.perf.FirebasePerformance
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import mega.privacy.android.app.middlelayer.reporter.CrashReporter
 import mega.privacy.android.app.middlelayer.reporter.PerformanceReporter
 import mega.privacy.android.app.service.reporter.FirebaseCrashReporter
 import mega.privacy.android.app.service.reporter.FirebasePerformanceReporter
+import mega.privacy.android.app.utils.LogUtil.logWarning
 import javax.inject.Singleton
 
 @Module
@@ -17,8 +21,10 @@ import javax.inject.Singleton
 class AnalyticsModule {
 
     @Provides
-    fun provideFirebaseCrashlytics(): FirebaseCrashlytics =
-        FirebaseCrashlytics.getInstance()
+    fun provideFirebaseCrashlytics(@ApplicationContext context: Context): FirebaseCrashlytics {
+        initializeFirebaseIfNeeded(context)
+        return FirebaseCrashlytics.getInstance()
+    }
 
     @Provides
     fun provideFirebasePerformance(): FirebasePerformance =
@@ -33,4 +39,13 @@ class AnalyticsModule {
     @Provides
     fun providePerformanceReporter(firebasePerformance: FirebasePerformance): PerformanceReporter =
         FirebasePerformanceReporter(firebasePerformance)
+
+    private fun initializeFirebaseIfNeeded(context: Context) {
+        try {
+            FirebaseApp.getInstance()
+        } catch (ignored: Exception) {
+            logWarning(ignored.stackTraceToString())
+            FirebaseApp.initializeApp(context)
+        }
+    }
 }
