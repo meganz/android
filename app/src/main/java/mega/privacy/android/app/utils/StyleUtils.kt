0092fade679987d49ed749fcb19429b739f83df3
Gregg Meyrick Jover <gmj@mega.co.nz>
2022-02-11 14:56:52 +0800

    Remove Version Checking >= Marshmallow on StyleUtils.kt
    AND-13201: Drop Android 5.x support
    
    Remove the checker on setTextStyle() since Android Marshmallow is now the Minimum OS the MEGA app supports.
    
    Additional Changes:
    - Remove unneeded parameters on setTextStyle() due to the checker being removed.
    - Use Kotlin named parameters on classes that use setTextStyle() to improve readability.

--- app/src/main/java/mega/privacy/android/app/utils/StyleUtils.kt
+++ app/src/main/java/mega/privacy/android/app/utils/StyleUtils.kt
@@ -1,10 +1,6 @@
 package mega.privacy.android.app.utils
 
-import android.content.Context
-import android.os.Build
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import mega.privacy.android.app.utils.ColorUtils.getThemeColor
 
 /**
  * Util class to add common functionalities related to styles.
@@ -12,55 +8,24 @@ import mega.privacy.android.app.utils.ColorUtils.getThemeColor
 object StyleUtils {
 
     /**
-     * Sets a text style if the current version supports textAppearance,
-     * changes the text color if not.
+     * Sets a text style
      *
-     * @param context         Current context.
      * @param textView        View in which the styled text has to be set.
      * @param textAppearance  The style to set as textAppearance
-     * @param textColor       Color to set to the text in case the current version does not support textAppearance.
-     * @param isTextAttrColor True if the color to set is an attr color, false otherwise.
      */
+    @JvmName("setTextStyleJava")
     @JvmStatic
     fun setTextStyle(
-        context: Context,
         textView: TextView,
         textAppearance: Int,
-        textColor: Int,
-        isTextAttrColor: Boolean
-    ) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            textView.setTextAppearance(textAppearance)
-        } else {
-            textView.setTextColor(
-                if (isTextAttrColor) getThemeColor(context, textColor)
-                else ContextCompat.getColor(context, textColor)
-            )
-        }
-    }
+    ) = textView.setTextAppearance(textAppearance)
 
     /**
-     * Sets a text style if the current version supports textAppearance,
-     * changes the text color if not.
+     * Sets a text style
      *
-     * @param context         Current context.
      * @param textAppearance  The style to set as textAppearance
-     * @param textColor       Color to set to the text in case the current version does not support textAppearance.
-     * @param isTextAttrColor True if the color to set is an attr color, false otherwise.
      */
     fun TextView.setTextStyle(
-        context: Context,
         textAppearance: Int,
-        textColor: Int,
-        isTextAttrColor: Boolean
-    ) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            setTextAppearance(textAppearance)
-        } else {
-            setTextColor(
-                if (isTextAttrColor) getThemeColor(context, textColor)
-                else ContextCompat.getColor(context, textColor)
-            )
-        }
-    }
+    ) = setTextAppearance(textAppearance)
 }
