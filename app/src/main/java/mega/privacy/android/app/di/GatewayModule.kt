192b3fac40757d71dae3814a29ea33746817bc7c
Daniel Oosthuizen <do@mega.co.nz>
2022-03-18 08:10:36 +1300

    AND-13556 - Get user account null pointer

---
+++ app/src/main/java/mega/privacy/android/app/di/GatewayModule.kt
@@ -0,0 +1,31 @@
+package mega.privacy.android.app.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import mega.privacy.android.app.data.facade.AccountInfoFacade
+import mega.privacy.android.app.data.facade.AccountInfoWrapper
+import mega.privacy.android.app.data.facade.MegaApiFacade
+import mega.privacy.android.app.data.gateway.api.MegaApiGateway
+
+/**
+ * Gateway module
+ *
+ * Registers bindings for gateway dependencies used by the repository classes.
+ *
+ * Facades and wrappers used by the repositories will also be provided here until they are replaced
+ * with repository code or gateways.
+ *
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class GatewayModule {
+
+    @Binds
+    abstract fun bindAccountInfoWrapper(implementation: AccountInfoFacade): AccountInfoWrapper
+
+    @Binds
+    abstract fun bindMegaApiWrapper(implementation: MegaApiFacade): MegaApiGateway
+
+}
