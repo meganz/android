8afe7eae5c2986c8ae3fc05230766a5e0e7ffc0d
Yenel <yr@mega.nz>
2022-04-01 14:02:11 +0200

    Fix crash opening a chat with containing a file link without key AND-13598 App is crashing when user opens a chat with a file link message (the link should not contain the key).

---
+++ app/src/main/java/mega/privacy/android/app/usecase/GetPublicNodeUseCase.kt
@@ -0,0 +1,42 @@
+package mega.privacy.android.app.usecase
+
+import io.reactivex.rxjava3.core.Single
+import mega.privacy.android.app.di.MegaApi
+import mega.privacy.android.app.listeners.OptionalMegaRequestListenerInterface
+import mega.privacy.android.app.main.megachat.AndroidMegaRichLinkMessage
+import nz.mega.sdk.MegaApiAndroid
+import nz.mega.sdk.MegaError.API_OK
+import javax.inject.Inject
+
+/**
+ * Use case for getting the public node from a file link.
+ */
+class GetPublicNodeUseCase @Inject constructor(
+    @MegaApi private val megaApi: MegaApiAndroid
+) {
+
+    /**
+     * Gets file link as [AndroidMegaRichLinkMessage].
+     *
+     * @param link The file link.
+     * @return [AndroidMegaRichLinkMessage] containing the file link and its public node.
+     */
+    fun get(link: String): Single<AndroidMegaRichLinkMessage> =
+        Single.create { emitter ->
+            megaApi.getPublicNode(
+                link,
+                OptionalMegaRequestListenerInterface(onRequestFinish = { request, error ->
+                    val publicNode = request.publicMegaNode
+
+                    when {
+                        error.errorCode == API_OK && publicNode != null -> {
+                            emitter.onSuccess(AndroidMegaRichLinkMessage(link, publicNode))
+                        }
+                        else -> {
+                            emitter.onError(MegaException(error.errorCode, error.errorString))
+                        }
+                    }
+                })
+            )
+        }
+}
