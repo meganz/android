94d1eae863b96eba4ced0634f075d26f9f15e8cf
Gonzalo Toledano <gonzalotldn@gmail.com>
2022-04-05 10:34:37 +0200

    Replace passcode check inheritance with PassCodeFacade

--- app/src/main/java/mega/privacy/android/app/imageviewer/ImageViewerActivity.kt
+++ app/src/main/java/mega/privacy/android/app/imageviewer/ImageViewerActivity.kt
@@ -16,8 +16,8 @@ import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OFFSCREEN_PAGE_LIMIT_DEFAULT
 import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.AndroidEntryPoint
+import mega.privacy.android.app.BaseActivity
 import mega.privacy.android.app.R
-import mega.privacy.android.app.activities.PasscodeActivity
 import mega.privacy.android.app.components.attacher.MegaAttacher
 import mega.privacy.android.app.components.dragger.DragToExitSupport
 import mega.privacy.android.app.components.saver.NodeSaver
@@ -37,16 +37,18 @@ import mega.privacy.android.app.utils.LogUtil.logError
 import mega.privacy.android.app.utils.LogUtil.logWarning
 import mega.privacy.android.app.utils.MegaNodeDialogUtil.showRenameNodeDialog
 import mega.privacy.android.app.utils.ViewUtils.waitForLayout
+import mega.privacy.presentation.security.PasscodeCheck
 import nz.mega.documentscanner.utils.IntentUtils.extra
 import nz.mega.sdk.MegaApiJava.INVALID_HANDLE
 import nz.mega.sdk.MegaApiJava.ORDER_PHOTO_ASC
 import nz.mega.sdk.MegaNode
+import javax.inject.Inject
 
 /**
  * Entry point to show an image based on an existing Node.
  */
 @AndroidEntryPoint
-class ImageViewerActivity : PasscodeActivity(), PermissionRequester, SnackbarShower {
+class ImageViewerActivity : BaseActivity(), PermissionRequester, SnackbarShower {
 
     companion object {
         private const val IMAGE_OFFSCREEN_PAGE_LIMIT = 2
@@ -203,6 +205,9 @@ class ImageViewerActivity : PasscodeActivity(), PermissionRequester, SnackbarSho
                 putExtra(INTENT_EXTRA_KEY_SHOW_NEARBY_FILES, showNearbyFiles)
             }
     }
+
+    @Inject
+    lateinit var passCodeFacade: PasscodeCheck
 
     private val nodeHandle: Long? by extra(INTENT_EXTRA_KEY_HANDLE, INVALID_HANDLE)
     private val nodeOfflineHandle: Long? by extra(INTENT_EXTRA_KEY_OFFLINE_HANDLE, INVALID_HANDLE)
