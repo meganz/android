6b65a450c0a7550cc9cb8457498742d6be6965c5
Daniel Oosthuizen <do@mega.co.nz>
2022-04-26 10:20:22 +1200

    Remove ExperimentalApi build warnings Replace Listener interface with optional listener in DefaultChatRepository

--- app/src/main/java/mega/privacy/android/app/data/repository/DefaultChatRepository.kt
+++ app/src/main/java/mega/privacy/android/app/data/repository/DefaultChatRepository.kt
@@ -4,40 +4,34 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import mega.privacy.android.app.domain.repository.ChatRepository
+import mega.privacy.android.app.listeners.OptionalMegaChatRequestListenerInterface
 import nz.mega.sdk.*
 import javax.inject.Inject
 
+
+/**
+ * Default implementation of [ChatRepository]
+ *
+ * @property chatApi
+ */
 class DefaultChatRepository @Inject constructor(
     private val chatApi: MegaChatApiAndroid
 ) : ChatRepository {
     override fun notifyChatLogout(): Flow<Boolean> {
         return callbackFlow {
-            val listener = object : MegaChatRequestListenerInterface{
-                override fun onRequestStart(api: MegaChatApiJava?, request: MegaChatRequest?) {}
-
-                override fun onRequestUpdate(api: MegaChatApiJava?, request: MegaChatRequest?) {}
-
-                override fun onRequestFinish(
-                    api: MegaChatApiJava?,
-                    request: MegaChatRequest?,
-                    e: MegaChatError?
-                ) {
-                    if (request?.type == MegaChatRequest.TYPE_LOGOUT) {
-                        if (e?.errorCode == MegaError.API_OK) {
+            val listener = OptionalMegaChatRequestListenerInterface(
+                onRequestFinish = { request, e ->
+                    if (request.type == MegaChatRequest.TYPE_LOGOUT) {
+                        if (e.errorCode == MegaError.API_OK) {
                             trySend(true)
                         }
                     }
                 }
+            )
 
-                override fun onRequestTemporaryError(
-                    api: MegaChatApiJava?,
-                    request: MegaChatRequest?,
-                    e: MegaChatError?
-                ) {}
-            }
             chatApi.addChatRequestListener(listener)
 
-            awaitClose{ chatApi.removeChatRequestListener(listener) }
+            awaitClose { chatApi.removeChatRequestListener(listener) }
         }
     }
 }
