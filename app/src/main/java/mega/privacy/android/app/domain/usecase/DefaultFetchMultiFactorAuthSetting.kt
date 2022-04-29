06f6245eeed1d1a0cd8cd66ab0fbdeb4cb22c0c4
Kevin Ham <kh@mega.co.nz>
2022-04-29 13:11:48 +0800

    Catch exception in DefaultFetchMultiFactorAuthSettings when api call error occurs

--- app/src/main/java/mega/privacy/android/app/domain/usecase/DefaultFetchMultiFactorAuthSetting.kt
+++ app/src/main/java/mega/privacy/android/app/domain/usecase/DefaultFetchMultiFactorAuthSetting.kt
@@ -1,9 +1,11 @@
 package mega.privacy.android.app.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import mega.privacy.android.app.domain.repository.AccountRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -17,6 +19,9 @@ class DefaultFetchMultiFactorAuthSetting @Inject constructor(private val account
         return flow {
             emit(accountRepository.isMultiFactorAuthEnabled())
             emitAll(accountRepository.monitorMultiFactorAuthChanges())
+        }.catch { e ->
+            Timber.w(e)
+            emit(false)
         }
     }
 }
