7122c644e217f6585f9b0e448acf11c92fb839f6
JoeJi <jj@mega.co.nz>
2022-05-11 14:10:52 +1200

    Inject CacheFolderGateway to DefaultAlbumsRepository.kt task/CC-512-multiple-album-arc-refactor

--- app/src/main/java/mega/privacy/android/app/data/repository/DefaultAlbumsRepository.kt
+++ app/src/main/java/mega/privacy/android/app/data/repository/DefaultAlbumsRepository.kt
@@ -1,10 +1,9 @@
 package mega.privacy.android.app.data.repository
 
-import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import mega.privacy.android.app.data.extensions.failWithError
+import mega.privacy.android.app.data.gateway.CacheFolderGateway
 import mega.privacy.android.app.data.gateway.api.MegaApiGateway
 import mega.privacy.android.app.data.gateway.api.MegaLocalStorageGateway
 import mega.privacy.android.app.di.IoDispatcher
@@ -18,11 +17,19 @@ import java.io.File
 import javax.inject.Inject
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ * Default implementation of [AlbumsRepository]
+ *
+ * @property apiFacade MegaApiGateway
+ * @property ioDispatcher CoroutineDispatcher
+ * @property megaLocalStorageFacade MegaLocalStorageGateway
+ * @property cacheFolderFacade CacheFolderGateway
+ */
 class DefaultAlbumsRepository @Inject constructor(
-    private val apiFacade: MegaApiGateway,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
-    val megaLocalStorageFacade: MegaLocalStorageGateway,
-    @ApplicationContext val context: Context
+        private val apiFacade: MegaApiGateway,
+        @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+        private val megaLocalStorageFacade: MegaLocalStorageGateway,
+        private val cacheFolderFacade: CacheFolderGateway,
 ) : AlbumsRepository {
 
     override fun getCameraUploadFolderId(): Long? = megaLocalStorageFacade.camSyncHandle
@@ -30,32 +37,34 @@ class DefaultAlbumsRepository @Inject constructor(
     override fun getMediaUploadFolderId(): Long? = megaLocalStorageFacade.megaHandleSecondaryFolder
 
     override fun getThumbnailFromLocal(nodeId: Long): File? {
-        return getThumbnailFile(apiFacade.getMegaNodeByHandle(nodeId)).takeIf{ it.exists() }
+        return getThumbnailFile(apiFacade.getMegaNodeByHandle(nodeId)).takeIf {
+            it?.exists() ?: false
+        }
     }
 
-    private fun getThumbnailFile(node: MegaNode): File = File(
-        File(context.cacheDir, CacheFolderManager.THUMBNAIL_FOLDER),
-        "${node.base64Handle}${FileUtil.JPG_EXTENSION}"
-    )
+    private fun getThumbnailFile(node: MegaNode): File? =
+            cacheFolderFacade.getCacheFile(CacheFolderManager.THUMBNAIL_FOLDER, "${node.base64Handle}${FileUtil.JPG_EXTENSION}")
 
     override suspend fun getThumbnailFromServer(nodeId: Long): File =
-        withContext(ioDispatcher) {
-            suspendCoroutine { continuation ->
-                val node = apiFacade.getMegaNodeByHandle(nodeId)
-                val thumbnail = getThumbnailFile(node)
+            withContext(ioDispatcher) {
+                suspendCoroutine { continuation ->
+                    val node = apiFacade.getMegaNodeByHandle(nodeId)
+                    val thumbnail = getThumbnailFile(node)
 
-                apiFacade.getThumbnail(node, thumbnail.absolutePath,
-                    OptionalMegaRequestListenerInterface(
-                        onRequestFinish = { _, error ->
-                            if (error.errorCode == MegaError.API_OK) {
-                                continuation.resumeWith(Result.success(thumbnail))
-                            } else {
-                                continuation.failWithError(error)
-                            }
-                        }
-                    )
-                )
+                    thumbnail?.let {
+                        apiFacade.getThumbnail(node, it.absolutePath,
+                                OptionalMegaRequestListenerInterface(
+                                        onRequestFinish = { _, error ->
+                                            if (error.errorCode == MegaError.API_OK) {
+                                                continuation.resumeWith(Result.success(thumbnail))
+                                            } else {
+                                                continuation.failWithError(error)
+                                            }
+                                        }
+                                )
+                        )
+                    }
+                }
             }
-        }
 
 }
--- app/src/test/java/test/mega/privacy/android/app/data/repository/DefaultAlbumsRepositoryTest.kt
+++ app/src/test/java/test/mega/privacy/android/app/data/repository/DefaultAlbumsRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
+import mega.privacy.android.app.data.gateway.CacheFolderGateway
 import mega.privacy.android.app.data.gateway.api.MegaApiGateway
 import mega.privacy.android.app.data.gateway.api.MegaLocalStorageGateway
 import mega.privacy.android.app.data.repository.DefaultAlbumsRepository
@@ -12,6 +13,7 @@ import mega.privacy.android.app.domain.repository.AlbumsRepository
 import mega.privacy.android.app.utils.CacheFolderManager
 import nz.mega.sdk.*
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
@@ -24,61 +26,62 @@ class DefaultAlbumsRepositoryTest {
 
     private val megaApiGateway = mock<MegaApiGateway>()
     private val megaDBHandlerGateway = mock<MegaLocalStorageGateway>()
+    private val cacheFolderGateway = mock<CacheFolderGateway>()
 
     private val cacheDir = File("cache")
 
     @Before
     fun setUp() {
         underTest = DefaultAlbumsRepository(
-            apiFacade = megaApiGateway,
-            ioDispatcher = UnconfinedTestDispatcher(),
-            megaLocalStorageFacade = megaDBHandlerGateway,
-            context = mock { on { cacheDir }.thenReturn(cacheDir) } // Should be using CacheFolderGateway to retrieve cache dir once DK's MR is merged
+                apiFacade = megaApiGateway,
+                ioDispatcher = UnconfinedTestDispatcher(),
+                megaLocalStorageFacade = megaDBHandlerGateway,
+                cacheFolderFacade = cacheFolderGateway
         )
     }
 
     @Test
     fun `test that get thumbnail from server returns successfully if no error is thrown`() =
-        runTest {
-            val node = mock<MegaNode>()
-            val thumbnailName = "Test"
-            whenever(node.base64Handle).thenReturn(thumbnailName)
-            whenever(megaApiGateway.getMegaNodeByHandle(any())).thenReturn(node)
+            runTest {
+                val node = mock<MegaNode>()
+                val thumbnailName = "Test"
+                whenever(node.base64Handle).thenReturn(thumbnailName)
+                whenever(megaApiGateway.getMegaNodeByHandle(any())).thenReturn(node)
 
-            val api = mock<MegaApiJava>()
-            val request = mock<MegaRequest>()
-            val error = mock<MegaError> { on { errorCode }.thenReturn(MegaError.API_OK) }
-            whenever(megaApiGateway.getThumbnail(any(), any(), any())).thenAnswer {
-                (it.arguments[2] as MegaRequestListenerInterface).onRequestFinish(
-                    api,
-                    request,
-                    error
-                )
+                val api = mock<MegaApiJava>()
+                val request = mock<MegaRequest>()
+                val error = mock<MegaError> { on { errorCode }.thenReturn(MegaError.API_OK) }
+                whenever(megaApiGateway.getThumbnail(any(), any(), any())).thenAnswer {
+                    (it.arguments[2] as MegaRequestListenerInterface).onRequestFinish(
+                            api,
+                            request,
+                            error
+                    )
+                }
+
+                val actual = underTest.getThumbnailFromServer(1L)
+                assertThat(actual.path).isEqualTo("${cacheDir.path}/${CacheFolderManager.THUMBNAIL_FOLDER}/$thumbnailName")
             }
-
-            val actual = underTest.getThumbnailFromServer(1L)
-            assertThat(actual.path).isEqualTo("${cacheDir.path}/${CacheFolderManager.THUMBNAIL_FOLDER}/$thumbnailName")
-        }
 
     @Test(expected = MegaException::class)
     fun `test that error is thrown if fetch thumbnail from server does not return successfully`() =
-        runTest {
-            val node = mock<MegaNode>()
-            val thumbnailName = "Test"
-            whenever(node.base64Handle).thenReturn(thumbnailName)
-            whenever(megaApiGateway.getMegaNodeByHandle(any())).thenReturn(node)
+            runTest {
+                val node = mock<MegaNode>()
+                val thumbnailName = "Test"
+                whenever(node.base64Handle).thenReturn(thumbnailName)
+                whenever(megaApiGateway.getMegaNodeByHandle(any())).thenReturn(node)
 
-            val api = mock<MegaApiJava>()
-            val request = mock<MegaRequest>()
-            val error = mock<MegaError> { on { errorCode }.thenReturn(MegaError.API_OK + 1) }
-            whenever(megaApiGateway.getThumbnail(any(), any(), any())).thenAnswer {
-                (it.arguments[2] as MegaRequestListenerInterface).onRequestFinish(
-                    api,
-                    request,
-                    error
-                )
+                val api = mock<MegaApiJava>()
+                val request = mock<MegaRequest>()
+                val error = mock<MegaError> { on { errorCode }.thenReturn(MegaError.API_OK + 1) }
+                whenever(megaApiGateway.getThumbnail(any(), any(), any())).thenAnswer {
+                    (it.arguments[2] as MegaRequestListenerInterface).onRequestFinish(
+                            api,
+                            request,
+                            error
+                    )
+                }
+
+                underTest.getThumbnailFromServer(1L)
             }
-
-            underTest.getThumbnailFromServer(1L)
-        }
 }
