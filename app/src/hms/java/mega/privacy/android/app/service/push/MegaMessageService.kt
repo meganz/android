5e291349bbccbe7cfd9d1864c3118eaf45b707fe
Yenel <yr@mega.nz>
2022-05-23 13:02:53 +0200

    Migrate to Kotlin MegaMessageService for HMS AND-13737 Non-fatal Exception: ForegroundServiceStartNotAllowedException in `awakeCpu` (PushMessageHanlder.java)

--- app/src/hms/java/mega/privacy/android/app/service/push/MegaMessageService.kt
+++ app/src/hms/java/mega/privacy/android/app/service/push/MegaMessageService.kt
@@ -1,30 +1,46 @@
-package mega.privacy.android.app.service.push;
+package mega.privacy.android.app.service.push
 
-import android.content.Context;
+import android.content.Context
+import com.huawei.agconnect.AGConnectOptionsBuilder
+import com.huawei.hms.push.HmsMessageService
+import mega.privacy.android.app.middlelayer.push.PushMessageHandler
+import com.huawei.hms.push.RemoteMessage
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.common.ApiException
+import dagger.hilt.android.AndroidEntryPoint
+import mega.privacy.android.app.DatabaseHandler
+import mega.privacy.android.app.di.MegaApi
+import mega.privacy.android.app.di.MegaApiFolder
+import mega.privacy.android.app.utils.Constants
+import nz.mega.sdk.MegaApiAndroid
+import nz.mega.sdk.MegaChatApiAndroid
+import timber.log.Timber
+import java.util.concurrent.Executors
+import javax.inject.Inject
 
-import com.huawei.agconnect.config.AGConnectServicesConfig;
-import com.huawei.hms.aaid.HmsInstanceId;
-import com.huawei.hms.common.ApiException;
-import com.huawei.hms.push.HmsMessageService;
-import com.huawei.hms.push.RemoteMessage;
+@AndroidEntryPoint
+class MegaMessageService : HmsMessageService() {
 
-import java.util.concurrent.Executors;
+    @MegaApi
+    @Inject
+    lateinit var megaApi: MegaApiAndroid
 
-import mega.privacy.android.app.middlelayer.push.PushMessageHandler;
+    @MegaApiFolder
+    @Inject
+    lateinit var megaApiFolder: MegaApiAndroid
 
-import static mega.privacy.android.app.utils.Constants.DEVICE_HUAWEI;
-import static mega.privacy.android.app.utils.LogUtil.logDebug;
-import static mega.privacy.android.app.utils.LogUtil.logError;
+    @Inject
+    lateinit var megaChatApi: MegaChatApiAndroid
 
-public class MegaMessageService extends HmsMessageService {
+    @Inject
+    lateinit var dbH: DatabaseHandler
 
-    private PushMessageHanlder messageHanlder;
+    private var messageHandler: PushMessageHandler? = null
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        logDebug("HMS message service created");
-        messageHanlder = new PushMessageHanlder();
+    override fun onCreate() {
+        super.onCreate()
+        Timber.d("HMS message service created")
+        messageHandler = PushMessageHandler(this, megaApi, megaApiFolder, megaChatApi, dbH)
     }
 
     /**
@@ -33,44 +49,54 @@ public class MegaMessageService extends HmsMessageService {
      * @param remoteMessage Huawei RemoteMessage.
      * @return Generic Message object.
      */
-    private PushMessageHanlder.Message convert(RemoteMessage remoteMessage) {
-        return new PushMessageHanlder.Message(
-                remoteMessage.getFrom(),
-                remoteMessage.getOriginalUrgency(),
-                remoteMessage.getUrgency(),
-                remoteMessage.getDataOfMap());
+    private fun convert(remoteMessage: RemoteMessage): PushMessageHandler.Message {
+        return PushMessageHandler.Message(
+            remoteMessage.from,
+            remoteMessage.originalUrgency,
+            remoteMessage.urgency,
+            remoteMessage.dataOfMap
+        )
     }
 
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        PushMessageHanlder.Message message = convert(remoteMessage);
-        logDebug("Receive remote msg: " + message);
-        messageHanlder.handleMessage(message);
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val message = convert(remoteMessage)
+        Timber.d("Receive remote msg: $message")
+        messageHandler?.handleMessage(message)
     }
 
-    @Override
-    public void onNewToken(String s) {
-        logDebug("New token is: " + s);
-        messageHanlder.sendRegistrationToServer(s, DEVICE_HUAWEI);
+    override fun onNewToken(s: String) {
+        Timber.d("New token is: $s")
+        messageHandler?.sendRegistrationToServer(s, Constants.DEVICE_HUAWEI)
     }
 
-    /**
-     * Request push service token by sending appId to HMS, then register it in API as an identifier of the device.
-     *
-     * @param context Context.
-     */
-    public static void getToken(Context context) {
-        Executors.newFixedThreadPool(1).submit(() -> {
-            String appId = AGConnectServicesConfig.fromContext(context).getString("client/app_id");
-            try {
-                // Wait for the callback
-                String token = HmsInstanceId.getInstance(context).getToken(appId, "HCM");
-                logDebug("Get token: " + token);
-                new PushMessageHanlder().sendRegistrationToServer(token, DEVICE_HUAWEI);
-            } catch (ApiException e) {
-                logError(e.getMessage(), e);
-                e.printStackTrace();
+    companion object {
+        /**
+         * Request push service token by sending appId to HMS, then register it in API as an identifier of the device.
+         *
+         * @param context       Context.
+         * @param megaApi       MegaApi instance.
+         * @param megaApiFolder MegaApi instance for folder links.
+         * @param megaChatApi   MegaChatApi instance.
+         * @param dbH           DatabaseHandler.
+         */
+        @JvmStatic
+        fun getToken(
+            context: Context, megaApi: MegaApiAndroid, megaApiFolder: MegaApiAndroid,
+            megaChatApi: MegaChatApiAndroid, dbH: DatabaseHandler
+        ) {
+            Executors.newFixedThreadPool(1).submit {
+                val appId = AGConnectOptionsBuilder().build(context).getString("client/app_id")
+                try {
+                    // Wait for the callback
+                    val token = HmsInstanceId.getInstance(context).getToken(appId, "HCM")
+                    Timber.d("Get token: $token")
+                    PushMessageHandler(context, megaApi, megaApiFolder, megaChatApi, dbH)
+                        .sendRegistrationToServer(token, Constants.DEVICE_HUAWEI)
+                } catch (e: ApiException) {
+                    Timber.e(e.message, e)
+                    e.printStackTrace()
+                }
             }
-        });
+        }
     }
 }
