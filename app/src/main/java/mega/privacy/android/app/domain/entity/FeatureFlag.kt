7c526b7fd0416b50f43606d2da83515ae33bc7a8
Dhananjay Kulkarni <dk@mega.co.nz>
2022-06-28 18:16:13 +0530

    Added documentation for FeatureFlag data class

--- app/src/main/java/mega/privacy/android/app/domain/entity/FeatureFlag.kt
+++ app/src/main/java/mega/privacy/android/app/domain/entity/FeatureFlag.kt
@@ -1,3 +1,9 @@
 package mega.privacy.android.app.domain.entity
 
+/**
+ * Data class to hold feature flag information
+ *
+ * @property featureName
+ * @property isEnabled
+ */
 data class FeatureFlag(var featureName: String, var isEnabled: Boolean)
