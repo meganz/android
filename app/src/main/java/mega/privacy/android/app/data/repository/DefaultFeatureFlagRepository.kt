8be1790083ae373a7191830954f4cf7aa36a295c
Dhananjay Kulkarni <dk@mega.co.nz>
2022-06-28 09:02:24 +0530

    Moved mapper to the corresponding package Used ioDispatcher in Repository

--- app/src/main/java/mega/privacy/android/app/data/repository/DefaultFeatureFlagRepository.kt
+++ app/src/main/java/mega/privacy/android/app/data/repository/DefaultFeatureFlagRepository.kt
@@ -1,20 +1,28 @@
 package mega.privacy.android.app.data.repository
 
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.withContext
 import mega.privacy.android.app.data.gateway.preferences.FeatureFlagPreferencesGateway
+import mega.privacy.android.app.data.mapper.FeatureFlagMapper
+import mega.privacy.android.app.di.IoDispatcher
 import mega.privacy.android.app.domain.entity.FeatureFlag
 import mega.privacy.android.app.domain.repository.FeatureFlagRepository
 import javax.inject.Inject
 
 /**
  * Implementation of @FeatureFlagRepository
- * @param preferencesGateway: Data Store Preferences gateway
+ * @property preferencesGateway
+ * @property featureFlagMapper
+ * @property ioDispatcher
  */
 class DefaultFeatureFlagRepository @Inject constructor(
     private val preferencesGateway: FeatureFlagPreferencesGateway,
+    private val featureFlagMapper: FeatureFlagMapper,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : FeatureFlagRepository {
 
     /**
@@ -24,7 +32,10 @@ class DefaultFeatureFlagRepository @Inject constructor(
      * @param isEnabled: Boolean value
      */
     override suspend fun setFeature(featureName: String, isEnabled: Boolean) =
-        preferencesGateway.setFeature(featureName, isEnabled)
+        withContext(ioDispatcher) {
+            preferencesGateway.setFeature(featureName, isEnabled)
+        }
+
 
     /**
      * Gets a map of Preferences from gateway and returns flow
@@ -36,22 +47,8 @@ class DefaultFeatureFlagRepository @Inject constructor(
         preferencesGateway.getAllFeatures().mapLatest { map ->
             map.asMap().mapNotNull { entry: Map.Entry<Preferences.Key<*>, Any> ->
                 entry.takeIf { it.value is Boolean }?.let {
-                    toFeatureFlag(it.key, it.value as Boolean)
+                    featureFlagMapper(it.key, it.value as Boolean)
                 }
             }
         }
 }
-
-/**
- * Type alias to make map output readable
- */
-typealias FeatureFlagMapper = (
-    @JvmSuppressWildcards Preferences.Key<*>,
-    @JvmSuppressWildcards Boolean,
-) ->
-@JvmSuppressWildcards FeatureFlag
-
-internal fun toFeatureFlag(
-    key: Preferences.Key<*>,
-    value: Boolean,
-) = FeatureFlag(key.toString(), value)
