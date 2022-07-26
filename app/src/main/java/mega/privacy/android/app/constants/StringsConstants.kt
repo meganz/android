b32680bd570f572dd40f24f1dfb7514cdef685d7
Javier Gomez <jg@mega.co.nz>
2022-07-26 11:02:59 +0200

    AND-14037 Hard code the list of characters not allowed

---
+++ app/src/main/java/mega/privacy/android/app/constants/StringsConstants.kt
@@ -0,0 +1,12 @@
+package mega.privacy.android.app.constants
+
+/**
+ * For storing all the constants required for Strings.
+ */
+object StringsConstants {
+
+    /**
+     * Invalid characters for naming files and folders
+     */
+    const val INVALID_CHARACTERS = "\" * / : < > ? \\ |"
+}
