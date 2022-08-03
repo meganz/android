b95178f8059fb982b1cfcc0a8f18582b56c6e997
Javier Gomez <jg@mega.co.nz>
2022-08-03 13:03:24 +0200

    Fix Lint Error Appcompat Custom Widgets at `EditTextPIN`
    AND-14154: Lint Errors - Appcompat Custom Widgets
    
    - Fix Lint Error Appcompat Custom Widgets at `EditTextPIN`.
    - Minor code clean.

--- app/src/main/java/mega/privacy/android/app/components/EditTextPIN.java
+++ app/src/main/java/mega/privacy/android/app/components/EditTextPIN.java
@@ -2,70 +2,68 @@ package mega.privacy.android.app.components;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputConnectionWrapper;
-import android.widget.EditText;
 
-public class EditTextPIN extends EditText {
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatEditText;
 
-	EditText et = null;
+import timber.log.Timber;
+
+public class EditTextPIN extends AppCompatEditText {
+
+    AppCompatEditText et = null;
 
     public EditTextPIN(Context context, AttributeSet attrs,
-					   int defStyle) {
+                       int defStyle) {
         super(context, attrs, defStyle);
-
     }
 
     public EditTextPIN(Context context, AttributeSet attrs) {
         super(context, attrs);
-
     }
-
-	public void setEt(EditText et){
-		this.et = et;
-	}
 
     public EditTextPIN(Context context) {
         super(context);
     }
 
-	private class PinInputConnection extends InputConnectionWrapper{
+    public void setEt(AppCompatEditText et) {
+        this.et = et;
+    }
 
-		public PinInputConnection(InputConnection target, boolean mutable) {
-			super(target, mutable);
-		}
+    @Override
+    public InputConnection onCreateInputConnection(@NonNull EditorInfo outAttrs) {
+        Timber.tag("EditTextPIN").d("onCreateInputConnection()");
+        return new PinInputConnection(super.onCreateInputConnection(outAttrs), true);
+    }
 
-		@Override
-		public boolean sendKeyEvent(KeyEvent event) {
-			if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
-				Log.d("EditTextPIN", "KEYCODE_DEL");
-				if (getText() != null){
-					if (getText().toString().compareTo("") != 0){
-						setText("");
-					}
-					else{
-						if (et != null){
-							et.requestFocus();
-							et.setCursorVisible(true);
-							et.setText("");
-						}
-					}
-				}
-				return true;
-			}
+    private class PinInputConnection extends InputConnectionWrapper {
 
-			return super.sendKeyEvent(event);
-		}
-	}
+        public PinInputConnection(InputConnection target, boolean mutable) {
+            super(target, mutable);
+        }
 
-	@Override
-	public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
-		Log.d("EditTextPIN", "onCreateInputConnection()");
-		return new PinInputConnection(super.onCreateInputConnection(outAttrs), true);
-	}
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+                Timber.tag("EditTextPIN").d("KEYCODE_DEL");
+                if (getText() != null) {
+                    if (getText().toString().compareTo("") != 0) {
+                        setText("");
+                    } else {
+                        if (et != null) {
+                            et.requestFocus();
+                            et.setCursorVisible(true);
+                            et.setText("");
+                        }
+                    }
+                }
+                return true;
+            }
 
-
+            return super.sendKeyEvent(event);
+        }
+    }
 }
