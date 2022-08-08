cd4739af8047c6db352e3b133ffd73c55c17395c
Yenel <yr@mega.nz>
2022-08-08 11:48:23 +0200

    Fix ExportRecoveryKeyActivity.onActivityResult

--- app/src/main/java/mega/privacy/android/app/exportRK/ExportRecoveryKeyActivity.kt
+++ app/src/main/java/mega/privacy/android/app/exportRK/ExportRecoveryKeyActivity.kt
@@ -88,8 +88,8 @@ class ExportRecoveryKeyActivity : PasscodeActivity() {
      */
     @Suppress("deprecation") // TODO Migrate to registerForActivityResult()
     @Deprecated("Use registerForActivityResult()")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
 
         viewModel.manageActivityResult(this, requestCode, resultCode, intent)
     }
