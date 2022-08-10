6048ae7ec4324011ac20103fc5a4984024f16e1f
Raquel García Chico <rgc@mega.nz>
2022-08-10 16:29:57 +0200

    Update documentation in SettingsCallsState

--- app/src/main/java/mega/privacy/android/app/presentation/settings/calls/model/SettingsCallsState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/settings/calls/model/SettingsCallsState.kt
@@ -3,8 +3,8 @@ package mega.privacy.android.app.presentation.settings.calls.model
 import mega.privacy.android.domain.entity.CallsSoundNotifications
 
 /**
- * Data class representing the state of the chat settings.
+ * Data class representing the state of the calls settings.
  *
- * @property soundNotifications   Current chat image quality.
+ * @property soundNotifications  Current sound notifications status.
  */
 data class SettingsCallsState(val soundNotifications: CallsSoundNotifications? = null)
