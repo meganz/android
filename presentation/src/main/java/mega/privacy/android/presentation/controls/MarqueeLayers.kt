522a8b1350f3aea88fc9bcb68e94bc865c3cc54e
Yenel <yr@mega.nz>
2022-09-12 16:46:25 +0200

    AND-14459 New MarqueeText Composable view

---
+++ presentation/src/main/java/mega/privacy/android/presentation/controls/MarqueeLayers.kt
@@ -0,0 +1,21 @@
+package mega.privacy.android.presentation.controls
+
+/**
+ * Marquee layers.
+ */
+enum class MarqueeLayers {
+    /**
+     * Main text.
+     */
+    MainText,
+
+    /**
+     * Secondary text.
+     */
+    SecondaryText,
+
+    /**
+     * Edges gradient.
+     */
+    EdgesGradient
+}
