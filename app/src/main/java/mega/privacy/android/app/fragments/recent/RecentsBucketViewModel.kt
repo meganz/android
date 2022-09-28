0de15a63c865f6a61c60758de5da946c9b24667d
Kevin Ham <kh@mega.co.nz>
2022-09-28 18:33:57 +0800

    use UpdateRecentAction and GetRecentActionNodes in RecentsBucketViewModel

--- app/src/main/java/mega/privacy/android/app/fragments/recent/RecentsBucketViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/recent/RecentsBucketViewModel.kt
@@ -5,25 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import mega.privacy.android.app.di.MegaApi
+import mega.privacy.android.app.domain.usecase.GetParentMegaNode
+import mega.privacy.android.app.domain.usecase.GetRecentActionNodes
 import mega.privacy.android.app.domain.usecase.MonitorNodeUpdates
+import mega.privacy.android.app.domain.usecase.UpdateRecentAction
 import mega.privacy.android.app.fragments.homepage.NodeItem
-import mega.privacy.android.app.utils.MegaNodeUtil.getRootParentNode
-import mega.privacy.android.app.utils.MegaNodeUtil.isVideo
-import mega.privacy.android.domain.qualifier.IoDispatcher
-import mega.privacy.android.domain.usecase.GetThumbnail
 import nz.mega.sdk.MegaApiAndroid
 import nz.mega.sdk.MegaNode
 import nz.mega.sdk.MegaRecentActionBucket
@@ -36,8 +30,9 @@ import javax.inject.Inject
 @HiltViewModel
 class RecentsBucketViewModel @Inject constructor(
     @MegaApi private val megaApi: MegaApiAndroid,
-    private val getThumbnail: GetThumbnail,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val getParentMegaNode: GetParentMegaNode,
+    private val updateRecentAction: UpdateRecentAction,
+    private val getRecentActionNodes: GetRecentActionNodes,
     monitorNodeUpdates: MonitorNodeUpdates,
 ) : ViewModel() {
     private val _actionMode = MutableLiveData<Boolean>()
@@ -79,7 +74,12 @@ class RecentsBucketViewModel @Inject constructor(
      *  List of node items in the current bucket
      */
     val items = bucket
-        .map { it?.let { getNodes(it) } ?: emptyList() }
+        .map { it?.let { getRecentActionNodes(it.nodes) } ?: emptyList() }
+        .onEach {
+            isInShare = it.firstOrNull()?.node?.let { node ->
+                getParentMegaNode(node)?.isInShare
+            } ?: false
+        }
         .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
 
     init {
@@ -89,12 +89,6 @@ class RecentsBucketViewModel @Inject constructor(
                 updateCurrentBucket()
                 clearSelection()
             }
-
-            items.collectLatest {
-                isInShare = it.firstOrNull()?.node?.let { node ->
-                    megaApi.getRootParentNode(node).isInShare
-                } ?: false
-            }
         }
     }
 
@@ -103,10 +97,8 @@ class RecentsBucketViewModel @Inject constructor(
      *
      * @param selectedBucket
      */
-    fun setBucket(selectedBucket: MegaRecentActionBucket?) {
-        viewModelScope.launch {
-            bucket.emit(selectedBucket)
-        }
+    fun setBucket(selectedBucket: MegaRecentActionBucket?) = viewModelScope.launch {
+        bucket.emit(selectedBucket)
     }
 
     /**
@@ -228,96 +220,14 @@ class RecentsBucketViewModel @Inject constructor(
     /**
      * Update the current bucket
      */
-    private suspend fun updateCurrentBucket() = withContext(ioDispatcher) {
-        val recentActions = megaApi.recentActions
-
-        // Update the current bucket
-        bucket.value?.let { currentBucket ->
-            recentActions.firstOrNull { isSameBucket(it, currentBucket) }?.let {
-                bucket.emit(it)
-                return@withContext
+    private suspend fun updateCurrentBucket() {
+        bucket.value
+            ?.let { updateRecentAction(it, cachedActionList) }
+            ?.let { bucket.emit(it) }
+            ?: run {
+                // No nodes contained in the bucket or the action bucket is no loner exists.
+                _shouldCloseFragment.postValue(true)
             }
-        }
-
-        // Compare the list of recentActions with the new list of recent actions
-        // and only keep the actions that differs from each other
-        cachedActionList?.forEach { b ->
-            val iterator = recentActions.iterator()
-            while (iterator.hasNext()) {
-                if (isSameBucket(iterator.next(), b)) {
-                    iterator.remove()
-                }
-            }
-        }
-
-        // The last one is the changed one
-        if (recentActions.size == 1) {
-            bucket.emit(recentActions[0])
-            return@withContext
-        }
-
-        // No nodes contained in the bucket or the action bucket is no loner exists.
-        _shouldCloseFragment.postValue(true)
     }
-
-    /**
-     * Compare two MegaRecentActionBucket
-     *
-     * @param selected the first MegaRecentActionBucket to compare
-     * @param other the second MegaRecentActionBucket to compare
-     * @return true if the two MegaRecentActionBucket are the same
-     */
-    private fun isSameBucket(
-        selected: MegaRecentActionBucket,
-        other: MegaRecentActionBucket,
-    ): Boolean {
-        return selected.isMedia == other.isMedia &&
-                selected.isUpdate == other.isUpdate &&
-                selected.timestamp == other.timestamp &&
-                selected.parentHandle == other.parentHandle &&
-                selected.userEmail == other.userEmail
-    }
-
-    /**
-     * Get the node from the bucket
-     *
-     * @return a list of NodeItem
-     */
-    private suspend fun getNodes(bucket: MegaRecentActionBucket): List<NodeItem> =
-        withContext(ioDispatcher) {
-            val size = bucket.nodes.size()
-            val coroutineScope = CoroutineScope(SupervisorJob())
-            val deferredNodeItems = mutableListOf<Deferred<NodeItem>>().apply {
-                for (i in 0 until size) {
-                    bucket.nodes[i]?.let { node ->
-                        add(
-                            coroutineScope.async {
-                                NodeItem(
-                                    node = node,
-                                    thumbnail = getThumbnail(node.handle),
-                                    index = -1,
-                                    isVideo = node.isVideo(),
-                                    modifiedDate = node.modificationTime.toString(),
-                                )
-                            }
-                        )
-                    }
-                }
-            }
-
-            val nodesList = ArrayList<NodeItem>().apply {
-                deferredNodeItems.forEach {
-                    try {
-                        add(it.await())
-                    } catch (e: Exception) {
-                        Timber.e(e)
-                    }
-                }
-
-            }
-
-            return@withContext nodesList
-        }
-
 }
 
