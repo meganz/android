697e1160a08a9ebbc82905ddf38428b61f900cba
Atiqur Rahman <atr@mega.co.nz>
2022-10-03 20:04:26 +0600

    AND-14628: Fix DefaultDisableCameraUploadSettingsTest

--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/DefaultDisableCameraUploadSettingsTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/DefaultDisableCameraUploadSettingsTest.kt
@@ -2,7 +2,6 @@ package mega.privacy.android.domain.usecase
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import mega.privacy.android.domain.entity.SyncRecordType
 import mega.privacy.android.domain.repository.CameraUploadRepository
 import mega.privacy.android.domain.repository.SettingsRepository
 import org.junit.Before
@@ -19,7 +18,6 @@ class DefaultDisableCameraUploadSettingsTest {
     private val cameraUploadRepository = mock<CameraUploadRepository>()
     private val settingsRepository = mock<SettingsRepository>()
 
-
     @Before
     fun setUp() {
         underTest = DefaultDisableCameraUploadSettings(
@@ -33,7 +31,6 @@ class DefaultDisableCameraUploadSettingsTest {
         runTest {
             whenever(cameraUploadRepository.shouldClearSyncRecords()).thenReturn(true)
             underTest(true)
-            verify(cameraUploadRepository, times(1)).shouldClearSyncRecords()
             verify(settingsRepository,
                 times(1)).setEnableCameraUpload(false)
             verify(cameraUploadRepository, times(1)).setSecondaryEnabled(false)
@@ -47,45 +44,5 @@ class DefaultDisableCameraUploadSettingsTest {
             verify(settingsRepository,
                 times(1)).setEnableCameraUpload(false)
             verify(cameraUploadRepository, times(1)).setSecondaryEnabled(false)
-        }
-
-    @Test
-    fun `test that invoke with true completes the operation successfully when sync records should be cleared`() =
-        runTest {
-            whenever(cameraUploadRepository.shouldClearSyncRecords()).thenReturn(true)
-            underTest(true)
-            verify(cameraUploadRepository,
-                times(1)).deleteAllSyncRecords(SyncRecordType.TYPE_ANY.value)
-            verify(cameraUploadRepository, times(1)).saveShouldClearCamSyncRecords(false)
-        }
-
-    @Test
-    fun `test that invoke with true completes the operation successfully when sync records should not be cleared`() =
-        runTest {
-            whenever(cameraUploadRepository.shouldClearSyncRecords()).thenReturn(false)
-            underTest(true)
-            verify(cameraUploadRepository,
-                times(0)).deleteAllSyncRecords(SyncRecordType.TYPE_ANY.value)
-            verify(cameraUploadRepository, times(0)).saveShouldClearCamSyncRecords(false)
-        }
-
-    @Test
-    fun `test that invoke with false completes the operation successfully when sync records should be cleared`() =
-        runTest {
-            whenever(cameraUploadRepository.shouldClearSyncRecords()).thenReturn(true)
-            underTest(false)
-            verify(cameraUploadRepository,
-                times(1)).deleteAllSyncRecords(SyncRecordType.TYPE_ANY.value)
-            verify(cameraUploadRepository, times(1)).saveShouldClearCamSyncRecords(false)
-        }
-
-    @Test
-    fun `test that invoke with false completes the operation successfully when sync records should not be cleared`() =
-        runTest {
-            whenever(cameraUploadRepository.shouldClearSyncRecords()).thenReturn(false)
-            underTest(false)
-            verify(cameraUploadRepository,
-                times(0)).deleteAllSyncRecords(SyncRecordType.TYPE_ANY.value)
-            verify(cameraUploadRepository, times(0)).saveShouldClearCamSyncRecords(false)
         }
 }
