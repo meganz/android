76209d18749ecbcd438f300dec6b265b374f7075
Daniel Oosthuizen <do@mega.co.nz>
2022-10-10 17:39:05 +1300

    AND-14778 - Create get user attribute gateway function

--- app/src/main/java/mega/privacy/android/app/data/facade/MegaApiFacade.kt
+++ app/src/main/java/mega/privacy/android/app/data/facade/MegaApiFacade.kt
@@ -464,4 +464,11 @@ class MegaApiFacade @Inject constructor(
     override fun creditCardQuerySubscriptions(listener: MegaRequestListenerInterface?) {
         megaApi.creditCardQuerySubscriptions(listener)
     }
+
+    override fun getUserAttribute(
+        attributeIdentifier: Int,
+        listener: MegaRequestListenerInterface
+    ) {
+        megaApi.getUserAttribute(attributeIdentifier, listener)
+    }
 }
