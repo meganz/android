d6f9195db3c8bc9dc2dc05365531a289a8bccb72
Atiqur Rahman <atr@mega.co.nz>
2022-10-11 10:18:42 +0600

    AND-14278: Update documentation of CheckCameraUpload

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/CheckCameraUpload.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/CheckCameraUpload.kt
@@ -8,6 +8,10 @@ import mega.privacy.android.domain.entity.CameraUploadState
 fun interface CheckCameraUpload {
     /**
      * Invoke
+     * @param shouldDisable whether to disable camera upload job or not
+     * @param primaryHandle Primary Folder Handle
+     * @param secondaryHandle Secondary Folder Handle
+     * @return [CameraUploadState]
      */
     suspend operator fun invoke(
         shouldDisable: Boolean,
