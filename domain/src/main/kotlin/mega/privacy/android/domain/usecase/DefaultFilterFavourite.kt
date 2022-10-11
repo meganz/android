32186461edf847342cded9e88481c186e48771a4
Joe Ji <jj@mega.co.nz>
2022-10-11 14:10:51 +1300

    AND-14787:Implement DefaultFilterFavourite and unit test and DI

---
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/DefaultFilterFavourite.kt
@@ -0,0 +1,24 @@
+package mega.privacy.android.domain.usecase
+
+import mega.privacy.android.domain.entity.photos.Photo
+import mega.privacy.android.domain.repository.PhotosRepository
+import javax.inject.Inject
+
+/**
+ * Filter Favourite photos
+ */
+class DefaultFilterFavourite @Inject constructor(
+    val photosRepository: PhotosRepository,
+) : FilterFavourite {
+
+    override fun invoke(): suspend (photo: Photo) -> Boolean =
+        {
+            it.isFavourite
+                    && (it is Photo.Video && inSyncFolder(it.parentId)
+                    || it is Photo.Image)
+        }
+
+    private suspend fun inSyncFolder(parentId: Long): Boolean =
+        parentId == photosRepository.getCameraUploadFolderId()
+                || parentId == photosRepository.getMediaUploadFolderId()
+}
