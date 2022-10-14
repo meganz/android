9c19918ce099a552f8678a03e129dd02737afd0e
Joe Ji <jj@mega.co.nz>
2022-10-14 15:26:49 +1300

    CC-2594:Add dynamic album content compose view

--- app/src/main/java/mega/privacy/android/app/presentation/photos/albums/model/AlbumsViewState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/photos/albums/model/AlbumsViewState.kt
@@ -2,6 +2,10 @@ package mega.privacy.android.app.presentation.photos.albums.model
 
 import mega.privacy.android.domain.entity.photos.Album
 
+/**
+ * @property albums
+ * @property currentAlbum
+ */
 data class AlbumsViewState(
     val albums: List<UIAlbum> = emptyList(),
     val currentAlbum: Album? = null
