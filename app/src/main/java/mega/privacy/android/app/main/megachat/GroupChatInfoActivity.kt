a8d811aaf1a5e803376e95a1daac39637626f632
Raquel García Chico <rgc@mega.nz>
2022-10-18 14:51:00 +0200

    Use StartChatCall in GroupChatInfo

--- app/src/main/java/mega/privacy/android/app/main/megachat/GroupChatInfoActivity.kt
+++ app/src/main/java/mega/privacy/android/app/main/megachat/GroupChatInfoActivity.kt
@@ -1,5 +1,6 @@
 package mega.privacy.android.app.main.megachat
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.ClipData
 import android.content.ClipboardManager
@@ -62,8 +63,6 @@ import mega.privacy.android.app.presentation.chat.groupInfo.GroupChatInfoViewMod
 import mega.privacy.android.app.presentation.chat.dialog.AddParticipantsNoContactsDialogFragment
 import mega.privacy.android.app.usecase.call.EndCallUseCase
 import mega.privacy.android.app.usecase.call.GetCallUseCase
-import mega.privacy.android.app.usecase.call.StartCallUseCase
-import mega.privacy.android.app.usecase.call.StartCallUseCase.StartCallResult
 import mega.privacy.android.app.usecase.chat.GetChatChangesUseCase
 import mega.privacy.android.app.utils.AlertDialogUtil.dismissAlertDialogIfExists
 import mega.privacy.android.app.utils.AlertDialogUtil.isAlertDialogShown
@@ -83,10 +82,12 @@ import mega.privacy.android.app.utils.StringResourcesUtils
 import mega.privacy.android.app.utils.TextUtil
 import mega.privacy.android.app.utils.TimeUtils
 import mega.privacy.android.app.utils.Util
+import mega.privacy.android.app.utils.permission.PermissionUtils
 import nz.mega.sdk.MegaApiAndroid
 import nz.mega.sdk.MegaApiJava
 import nz.mega.sdk.MegaChatApi
 import nz.mega.sdk.MegaChatApiJava
+import nz.mega.sdk.MegaChatApiJava.MEGACHAT_INVALID_HANDLE
 import nz.mega.sdk.MegaChatError
 import nz.mega.sdk.MegaChatListItem
 import nz.mega.sdk.MegaChatPeerList
@@ -106,7 +107,6 @@ import javax.inject.Inject
  * Activity which shows group chat room information.
  *
  * @property getChatChangesUseCase [GetChatChangesUseCase]
- * @property startCallUseCase   [StartCallUseCase]
  * @property endCallUseCase [EndCallUseCase]
  * @property getCallUseCase [GetCallUseCase]
  * @property binding [ActivityGroupChatPropertiesBinding]
@@ -125,9 +125,6 @@ class GroupChatInfoActivity : PasscodeActivity(), MegaChatRequestListenerInterfa
 
     @Inject
     lateinit var getChatChangesUseCase: GetChatChangesUseCase
-
-    @Inject
-    lateinit var startCallUseCase: StartCallUseCase
 
     @Inject
     lateinit var endCallUseCase: EndCallUseCase
@@ -229,8 +226,8 @@ class GroupChatInfoActivity : PasscodeActivity(), MegaChatRequestListenerInterfa
 
 
         intent.extras?.let { extras ->
-            chatHandle = extras.getLong(Constants.HANDLE, MegaChatApiJava.MEGACHAT_INVALID_HANDLE)
-            if (chatHandle == MegaChatApiJava.MEGACHAT_INVALID_HANDLE) {
+            chatHandle = extras.getLong(Constants.HANDLE, MEGACHAT_INVALID_HANDLE)
+            if (chatHandle == MEGACHAT_INVALID_HANDLE) {
                 finish()
                 return
             }
@@ -1379,20 +1376,12 @@ class GroupChatInfoActivity : PasscodeActivity(), MegaChatRequestListenerInterfa
      * Start a call
      */
     fun startCall() {
-        startCallUseCase.startCallFromUserHandle(userHandle = userWaitingForCall,
-            enableVideo = false,
-            enableAudio = true)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { (id, videoEnable, audioEnable): StartCallResult, error: Throwable? ->
-                if (error == null) {
-                    CallUtil.openMeetingWithAudioOrVideo(this,
-                        id,
-                        audioEnable,
-                        videoEnable,
-                        passcodeManagement)
-                }
+        viewModel.getChatRoomId(userWaitingForCall).observe(this) { chatId ->
+            if (chatId != MEGACHAT_INVALID_HANDLE) {
+                val audio = PermissionUtils.hasPermissions(this, Manifest.permission.RECORD_AUDIO)
+                viewModel.onCallTap(chatId = chatId, video = false, audio = audio)
             }
+        }
     }
 
     private fun onChatPresenceLastGreen(userHandle: Long, lastGreen: Int) {
--- app/src/main/java/mega/privacy/android/app/presentation/chat/groupInfo/GroupChatInfoViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/chat/groupInfo/GroupChatInfoViewModel.kt
@@ -1,21 +1,36 @@
 package mega.privacy.android.app.presentation.chat.groupInfo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jeremyliao.liveeventbus.LiveEventBus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import mega.privacy.android.app.MegaApplication
 import mega.privacy.android.app.R
+import mega.privacy.android.app.arch.BaseRxViewModel
 import mega.privacy.android.app.constants.EventConstants
+import mega.privacy.android.app.contacts.usecase.GetChatRoomUseCase
+import mega.privacy.android.app.objects.PasscodeManagement
 import mega.privacy.android.app.presentation.chat.groupInfo.model.GroupInfoState
+import mega.privacy.android.app.utils.CallUtil
+import mega.privacy.android.app.utils.CallUtil.openMeetingWithAudioOrVideo
+import mega.privacy.android.data.gateway.api.MegaChatApiGateway
+import mega.privacy.android.domain.entity.ChatRequestParamType
 import mega.privacy.android.domain.usecase.MonitorConnectivity
 import mega.privacy.android.domain.usecase.SetOpenInvite
+import mega.privacy.android.domain.usecase.StartChatCall
 import nz.mega.sdk.MegaChatRoom
 import timber.log.Timber
 import javax.inject.Inject
@@ -24,13 +39,19 @@ import javax.inject.Inject
  * GroupChatInfoActivity view model.
  *
  * @property setOpenInvite               [SetOpenInvite]
+ * @property startChatCall               [StartChatCall]
+ * @property getChatRoomUseCase          [GetChatRoomUseCase]
  * @property state                       Current view state as [GroupInfoState]
  */
 @HiltViewModel
 class GroupChatInfoViewModel @Inject constructor(
     private val setOpenInvite: SetOpenInvite,
     monitorConnectivity: MonitorConnectivity,
-) : ViewModel() {
+    private val startChatCall: StartChatCall,
+    private val getChatRoomUseCase: GetChatRoomUseCase,
+    private val passcodeManagement: PasscodeManagement,
+    private val chatApiGateway: MegaChatApiGateway
+) : BaseRxViewModel() {
 
     /**
      * private UI state
@@ -82,6 +103,66 @@ class GroupChatInfoViewModel @Inject constructor(
             }
         } else {
             _state.update { it.copy(error = R.string.check_internet_connection_error) }
+        }
+    }
+
+    /**
+     * Get chat id
+     *
+     * @param userHandle User handle
+     */
+    fun getChatRoomId(userHandle: Long): LiveData<Long> {
+        val result = MutableLiveData<Long>()
+        getChatRoomUseCase.get(userHandle)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { chatId ->
+                    result.value = chatId
+                },
+                onError = Timber::e
+            )
+            .addTo(composite)
+        return result
+    }
+
+    /**
+     * Starts a call
+     *
+     * @param chatId Chat id
+     * @param video Start call with video on or off
+     * @param audio Start call with audio on or off
+     */
+    fun onCallTap(chatId: Long, video: Boolean, audio: Boolean) {
+        MegaApplication.isWaitingForCall = false
+
+        viewModelScope.launch {
+            runCatching {
+                startChatCall(chatId, video, audio)
+            }.onFailure { exception ->
+                Timber.e(exception)
+            }.onSuccess { resultStartCall ->
+                val resultChatId = resultStartCall.chatHandle
+                if (resultChatId != null) {
+                    val videoEnable = resultStartCall.flag
+                    val paramType = resultStartCall.paramType
+                    val audioEnable: Boolean = paramType == ChatRequestParamType.Video
+
+                    CallUtil.addChecksForACall(resultChatId, videoEnable)
+                    chatApiGateway.getChatCall(resultChatId)?.let { call ->
+                        if (call.isOutgoing) {
+                            MegaApplication.getChatManagement()
+                                .setRequestSentCall(call.callId, true)
+                        }
+                    }
+
+                    openMeetingWithAudioOrVideo(MegaApplication.getInstance().applicationContext,
+                        resultChatId,
+                        audioEnable,
+                        videoEnable,
+                        passcodeManagement)
+                }
+            }
         }
     }
 }
