c25a6c0795e63408a2d2553935b8584d22c37c6b
Joe Ji <jj@mega.co.nz>
2022-10-19 22:28:57 +1300

    CC-2667:Add menu for new content page

--- app/src/main/java/mega/privacy/android/app/presentation/photos/albums/model/AlbumsViewState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/photos/albums/model/AlbumsViewState.kt
@@ -1,14 +1,17 @@
 package mega.privacy.android.app.presentation.photos.albums.model
 
+import mega.privacy.android.app.presentation.photos.model.Sort
 import mega.privacy.android.domain.entity.photos.Album
 
 /**
  * @property albums
  * @property currentAlbum
  * @property selectedPhotoIds
+ * @property currentSort
  */
 data class AlbumsViewState(
     val albums: List<UIAlbum> = emptyList(),
     val currentAlbum: Album? = null,
     val selectedPhotoIds: MutableSet<Long> = mutableSetOf(),
+    val currentSort: Sort = Sort.NEWEST,
 )
