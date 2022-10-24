f7dc2f7353c2afea36dd59fef5a1b479c521fd49
Daniel Oosthuizen <do@mega.co.nz>
2022-10-25 12:25:35 +1300

    AND-14900 Remove unneeded MegaNode type definitions

--- app/src/main/java/mega/privacy/android/app/presentation/inbox/InboxViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/inbox/InboxViewModel.kt
@@ -1,6 +1,5 @@
 package mega.privacy.android.app.presentation.inbox
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -13,7 +12,6 @@ import mega.privacy.android.app.domain.usecase.MonitorNodeUpdates
 import mega.privacy.android.app.fragments.homepage.Event
 import mega.privacy.android.data.mapper.SortOrderIntMapper
 import mega.privacy.android.domain.usecase.GetCloudSortOrder
-import nz.mega.sdk.MegaNode
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -34,7 +32,7 @@ class InboxViewModel @Inject constructor(
     /**
      * Monitor global node updates
      */
-    val updateNodes: LiveData<Event<List<MegaNode>>> =
+    val updateNodes =
         monitorNodeUpdates()
             .also { Timber.d("onNodesUpdate()") }
             .shareIn(viewModelScope, SharingStarted.WhileSubscribed())
