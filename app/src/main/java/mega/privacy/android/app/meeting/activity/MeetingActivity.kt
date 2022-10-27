1f233c5457e65d0f25d932d8579d2ef55078f1ab
Raquel García Chico <rgc@mega.nz>
2022-10-27 16:10:57 +0200

    Fix bug: open call ui when meeting activity is in background

--- app/src/main/java/mega/privacy/android/app/meeting/activity/MeetingActivity.kt
+++ app/src/main/java/mega/privacy/android/app/meeting/activity/MeetingActivity.kt
@@ -198,10 +198,7 @@ class MeetingActivity : BaseActivity() {
 
     override fun onDestroy() {
         navController?.removeOnDestinationChangedListener(destinationChangedListener)
-        val currentFragment = getCurrentFragment()
-        if (currentFragment is InMeetingFragment) {
-            meetingViewModel.sendEnterCallEvent(false)
-        }
+
         super.onDestroy()
     }
 
@@ -385,9 +382,16 @@ class MeetingActivity : BaseActivity() {
         showSnackbar(binding.navHostFragment, content)
     }
 
+    override fun onStop() {
+        super.onStop()
+        val currentFragment = getCurrentFragment()
+        if (currentFragment is InMeetingFragment) {
+            meetingViewModel.sendEnterCallEvent(false)
+        }
+    }
+
     override fun onPause() {
         super.onPause()
-
         val timeRequired = passcodeUtil.timeRequiredForPasscode()
         if (timeRequired != REQUIRE_PASSCODE_INVALID) {
             if (isLockingEnabled) {
@@ -400,7 +404,6 @@ class MeetingActivity : BaseActivity() {
 
     override fun onResume() {
         super.onResume()
-
         isLockingEnabled = passcodeUtil.shouldLock()
 
         @Suppress("DEPRECATION")
