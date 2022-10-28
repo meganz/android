f1536e3b311ebec571fa562c47ce8c3b82aa0bf8
Atiqur Rahman <atr@mega.co.nz>
2022-10-28 14:03:30 +0600

    AND-14910: Refactor to return actual object instead of Mock

--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/DefaultUpdateRecentActionTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/DefaultUpdateRecentActionTest.kt
@@ -30,13 +30,14 @@ class DefaultUpdateRecentActionTest {
         parentHandle: Long,
         userEmail: String,
     ): RecentActionBucket =
-        mock {
-            on { this.isMedia }.thenReturn(isMedia)
-            on { this.isUpdate }.thenReturn(isUpdate)
-            on { this.timestamp }.thenReturn(timestamp)
-            on { this.parentHandle }.thenReturn(parentHandle)
-            on { this.userEmail }.thenReturn(userEmail)
-        }
+        RecentActionBucket(
+            isMedia = isMedia,
+            isUpdate = isUpdate,
+            timestamp = timestamp,
+            parentHandle = parentHandle,
+            userEmail = userEmail,
+            nodes = emptyList(),
+        )
 
     @Test
     fun `test that is same bucket return true if two buckets have same properties`() = runTest {
@@ -60,7 +61,7 @@ class DefaultUpdateRecentActionTest {
     fun `test that when updated action list contains the current action, then return this action`() =
         runTest {
             val expected = createBucket(isMedia = true, isUpdate = true, 0L, 1L, "1")
-            val list = listOf(expected, mock(), mock())
+            val list = listOf(expected, expected, expected)
             whenever(getRecentActions()).thenReturn(list)
 
             assertThat(underTest.invoke(expected, null)).isEqualTo(expected)
