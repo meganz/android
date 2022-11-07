8777ff4d843e51a2a0da06d6894f3eb0ef47a072
Daniel Oosthuizen <do@mega.co.nz>
2022-11-08 12:23:56 +1300

    AND-14991 Add favourite sort order domain logic

---
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/DefaultMapFavouriteSortOrder.kt
@@ -0,0 +1,20 @@
+package mega.privacy.android.domain.usecase
+
+import mega.privacy.android.domain.entity.SortOrder
+import mega.privacy.android.domain.entity.favourite.FavouriteSortOrder
+
+/**
+ * Default map favourite sort order
+ */
+class DefaultMapFavouriteSortOrder : MapFavouriteSortOrder {
+    override fun invoke(sortOrder: SortOrder) = when (sortOrder) {
+        SortOrder.ORDER_DEFAULT_ASC -> FavouriteSortOrder.Name(false)
+        SortOrder.ORDER_DEFAULT_DESC -> FavouriteSortOrder.Name(true)
+        SortOrder.ORDER_SIZE_ASC -> FavouriteSortOrder.Size(false)
+        SortOrder.ORDER_SIZE_DESC -> FavouriteSortOrder.Size(true)
+        SortOrder.ORDER_MODIFICATION_ASC -> FavouriteSortOrder.ModifiedDate(false)
+        SortOrder.ORDER_MODIFICATION_DESC -> FavouriteSortOrder.ModifiedDate(true)
+        else -> FavouriteSortOrder.Label
+    }
+
+}
