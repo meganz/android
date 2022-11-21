af4dc3bc0ac4db806cc01d983e43f83fdf118550
Javier Gómez Martín <jg@mega.co.nz>
2022-11-21 22:46:49 +1300

    MEET-111: Mute icon for local video feed/avatar

--- app/src/main/java/mega/privacy/android/app/meeting/listeners/IndividualCallVideoListener.kt
+++ app/src/main/java/mega/privacy/android/app/meeting/listeners/IndividualCallVideoListener.kt
@@ -1,13 +1,10 @@
 package mega.privacy.android.app.meeting.listeners
 
 import android.graphics.Bitmap
-import android.graphics.PixelFormat
 import android.util.DisplayMetrics
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import com.github.barteksc.pdfviewer.util.MathUtils.min
+import android.view.TextureView
 import mega.privacy.android.app.meeting.MegaSurfaceRenderer
-import mega.privacy.android.app.utils.Constants.INVALID_DIMENSION
+import mega.privacy.android.app.utils.Constants
 import mega.privacy.android.app.utils.VideoCaptureUtils
 import nz.mega.sdk.MegaChatApiJava
 import nz.mega.sdk.MegaChatApiJava.MEGACHAT_INVALID_HANDLE
@@ -18,22 +15,18 @@ import java.nio.ByteBuffer
  * A listener for metadata corresponding to video being rendered.
  */
 class IndividualCallVideoListener(
-    private val surfaceView: SurfaceView,
+    private val textureView: TextureView,
     outMetrics: DisplayMetrics?,
     clientId: Long,
-    isFloatingWindow: Boolean = true,
-    isOneToOneCall: Boolean = true
+    isFloatingWindow: Boolean = true
 ) : MegaChatVideoListenerInterface {
 
     var width = 0
     var height = 0
-    var isFloatingWindow = false
+    private var isFloatingWindow = false
     private var isLocal = true
     val renderer: MegaSurfaceRenderer
-    private val surfaceHolder: SurfaceHolder
     private var bitmap: Bitmap? = null
-    private var viewWidth = 0
-    private var viewHeight = 0
 
     fun setAlpha(alpha: Int) {
         renderer.setAlpha(alpha)
@@ -46,50 +39,26 @@ class IndividualCallVideoListener(
         height: Int,
         byteBuffer: ByteArray
     ) {
-
         if (width == 0 || height == 0) {
             return
         }
 
-        // viewWidth != surfaceView.width || viewHeight != surfaceView.height
-        // Re-calculate the camera preview ratio when surface view size changed
-        if (this.width != width || this.height != height
-            || viewWidth != surfaceView.width || viewHeight != surfaceView.height) {
+        if (this.width != width || this.height != height) {
             this.width = width
             this.height = height
-            val holder = surfaceView.holder
-
-            if(holder != null){
-                val viewWidth = surfaceView.width
-                val viewHeight = surfaceView.height
-
-                if (viewWidth != 0 && viewHeight != 0) {
-                    var holderWidth = min(viewWidth, width)
-                    var holderHeight = holderWidth * viewHeight / viewWidth
-
-                    if (holderHeight > viewHeight) {
-                        holderHeight = viewHeight
-                        holderWidth = holderHeight * viewWidth / viewHeight
-                    }
-
-                    bitmap = renderer.createBitmap(width, height)
-
-                    if(!isLocal){
-                        holder.setFixedSize(holderWidth, holderHeight)
-                    }
-
-                } else {
-                    this.width = INVALID_DIMENSION
-                    this.height = INVALID_DIMENSION
-                }
+            val viewWidth = textureView.width
+            val viewHeight = textureView.height
+            if (viewWidth != 0 && viewHeight != 0) {
+                bitmap = renderer.createBitmap(width, height)
+            } else {
+                this.width = Constants.INVALID_DIMENSION
+                this.height = Constants.INVALID_DIMENSION
             }
         }
 
-        if (bitmap == null) return
-
-        bitmap!!.copyPixelsFromBuffer(ByteBuffer.wrap(byteBuffer))
+        (bitmap ?: return).copyPixelsFromBuffer(ByteBuffer.wrap(byteBuffer))
         if (VideoCaptureUtils.isVideoAllowed()) {
-            renderer.drawBitmap(isLocal, false)
+            renderer.drawBitmap(isLocal)
         }
     }
 
@@ -98,16 +67,8 @@ class IndividualCallVideoListener(
 
         this.isFloatingWindow = isFloatingWindow
 
-        if ((isFloatingWindow && isLocal) || !isOneToOneCall) {
-            this.surfaceView.setZOrderOnTop(true)
-        } else if (!isFloatingWindow && !isLocal) {
-            this.surfaceView.setZOrderOnTop(false)
-        }
-
-        surfaceHolder = surfaceView.holder
-        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT)
         renderer = MegaSurfaceRenderer(
-            surfaceView,
+            textureView,
             isFloatingWindow,
             outMetrics
         )
