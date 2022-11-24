07a3880833c2bf92cd84179d104eb24b2f506db9
Yenel <yr@mega.nz>
2022-11-24 15:01:13 +0100

    AND-15064 NullPointerException
    (GetFolderContentUseCase.getContentToUpload)
    Add new FolderNameNullException

---
+++ domain/src/main/kotlin/mega/privacy/android/domain/exception/FolderNameNullException.kt
@@ -0,0 +1,6 @@
+package mega.privacy.android.domain.exception
+
+/**
+ * Exception thrown when a folder name is required but the provided one is null.
+ */
+class FolderNameNullException : RuntimeException("FolderNameNullException")
