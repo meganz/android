d0c74445a8df8ed668303f7c4c01ed48af424ece
Raquel García Chico <rgc@mega.nz>
2022-11-25 17:45:21 +0100

    MEET-1604 DEV - Implement meeting title

--- domain/src/main/kotlin/mega/privacy/android/domain/entity/chat/ScheduledMeetingItem.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/entity/chat/ScheduledMeetingItem.kt
@@ -12,7 +12,7 @@ package mega.privacy.android.domain.entity.chat
 data class ScheduledMeetingItem constructor(
     val chatId: Long,
     val scheduledMeetingId: Long,
-    val title: String = "",
-    val description: String?,
-    val date: String,
+    val title: String? = "",
+    val description: String? = "",
+    val date: String? = "",
 )
