4830df30dad674cce6185ef2d64e008c4e3ae32c
Raquel García Chico <rgc@mega.nz>
2022-11-26 18:17:13 +0100

    MEET-1607 DEV - Implement Allow non-hosts to add participants option

--- app/src/main/java/mega/privacy/android/app/presentation/meeting/ScheduledMeetingInfoActivity.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/meeting/ScheduledMeetingInfoActivity.kt
@@ -69,8 +69,10 @@ class ScheduledMeetingInfoActivity : PasscodeActivity() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.RESUMED) {
                 viewModel.state.collect { (chatId, _, _, finish, _, _, inviteParticipantAction) ->
-                    if (finish)
+                    if (finish){
+                        Timber.d("Finish activity")
                         finish()
+                    }
 
                     if (chatRoomId != chatId)
                         chatRoomId = chatId
@@ -79,6 +81,7 @@ class ScheduledMeetingInfoActivity : PasscodeActivity() {
                         viewModel.removeInviteParticipantsAction()
                         when (action) {
                             InviteParticipantsAction.ADD_CONTACTS -> {
+                                Timber.d("Open Invite participants screen")
                                 resultLauncher.launch(Intent(this@ScheduledMeetingInfoActivity,
                                     AddContactActivity::class.java)
                                     .putExtra(INTENT_EXTRA_KEY_CONTACT_TYPE,
@@ -90,10 +93,13 @@ class ScheduledMeetingInfoActivity : PasscodeActivity() {
                                 )
                             }
                             InviteParticipantsAction.NO_CONTACTS_DIALOG -> {
+                                Timber.d("Finish activity")
+                                Timber.d("Show dialog when all contacts are chat participants.")
                                 val dialog = AddParticipantsNoContactsDialogFragment.newInstance()
                                 dialog.show(supportFragmentManager, dialog.tag)
                             }
                             InviteParticipantsAction.NO_MORE_CONTACTS_DIALOG -> {
+                                Timber.d("Show dialog when there are no contacts added.")
                                 val dialog =
                                     AddParticipantsNoContactsLeftToAddDialogFragment.newInstance()
                                 dialog.show(supportFragmentManager, dialog.tag)
@@ -130,7 +136,7 @@ class ScheduledMeetingInfoActivity : PasscodeActivity() {
         val uiState by viewModel.state.collectAsState()
         AndroidTheme(isDark = isDark) {
             ScheduledMeetingInfoView(state = uiState,
-                onButtonClicked = { viewModel::onActionTap },
+                onButtonClicked = viewModel::onActionTap ,
                 onEditClicked = { viewModel::onEditTap },
                 onAddParticipantsClicked = { viewModel.onInviteParticipantsTap() },
                 onSeeMoreClicked = { viewModel::onSeeMoreTap },
