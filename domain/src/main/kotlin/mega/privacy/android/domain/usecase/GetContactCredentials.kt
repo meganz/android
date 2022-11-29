b4d842e1c6ed99c86c96ac2bd3f6479425bf2798
Yenel <yr@mega.nz>
2022-11-29 11:37:22 +0100

    AND-15108 Add new GetContactCredentials use case

---
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/GetContactCredentials.kt
@@ -0,0 +1,17 @@
+package mega.privacy.android.domain.usecase
+
+import mega.privacy.android.domain.entity.contacts.AccountCredentials
+
+/**
+ * Gets contact's credentials.
+ */
+fun interface GetContactCredentials {
+
+    /**
+     * Invoke.
+     *
+     * @param userEmail User's email.
+     * @return [AccountCredentials.ContactCredentials]
+     */
+    suspend fun invoke(userEmail: String): AccountCredentials.ContactCredentials?
+}
