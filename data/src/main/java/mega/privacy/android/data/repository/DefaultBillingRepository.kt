d2f76f617be9dd48f405daa580f2404963547aeb
Luong Hai <lh@mega.co.nz>
2022-12-07 21:08:25 +1300

    AND-15206: Create GetPricing UseCase

--- data/src/main/java/mega/privacy/android/data/repository/DefaultBillingRepository.kt
+++ data/src/main/java/mega/privacy/android/data/repository/DefaultBillingRepository.kt
@@ -5,11 +5,14 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import mega.privacy.android.data.cache.Cache
 import mega.privacy.android.data.extensions.failWithError
+import mega.privacy.android.data.extensions.getRequestListener
 import mega.privacy.android.data.facade.AccountInfoWrapper
 import mega.privacy.android.data.gateway.api.MegaApiGateway
 import mega.privacy.android.data.listener.OptionalMegaRequestListenerInterface
 import mega.privacy.android.data.mapper.LocalPricingMapper
+import mega.privacy.android.data.mapper.PricingMapper
 import mega.privacy.android.domain.entity.billing.PaymentMethodFlags
+import mega.privacy.android.domain.entity.billing.Pricing
 import mega.privacy.android.domain.qualifier.IoDispatcher
 import mega.privacy.android.domain.repository.BillingRepository
 import nz.mega.sdk.MegaError
@@ -31,6 +34,8 @@ internal class DefaultBillingRepository @Inject constructor(
     private val megaApiGateway: MegaApiGateway,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
     private val paymentMethodFlagsCache: Cache<PaymentMethodFlags>,
+    private val pricingCache: Cache<Pricing>,
+    private val pricingMapper: PricingMapper,
     private val localPricingMapper: LocalPricingMapper,
 ) : BillingRepository {
 
@@ -38,6 +43,10 @@ internal class DefaultBillingRepository @Inject constructor(
         accountInfoWrapper.availableSkus.firstOrNull { megaSku ->
             megaSku.sku == sku
         }?.let { localPricingMapper(it) }
+
+    override suspend fun getPricing(clearCache: Boolean): Pricing =
+        pricingCache.get()?.takeUnless { clearCache }
+            ?: fetchPricing().also { pricingCache.set(it) }
 
     override suspend fun getPaymentMethod(clearCache: Boolean): PaymentMethodFlags =
         paymentMethodFlagsCache.get()?.takeUnless { clearCache }
@@ -56,6 +65,18 @@ internal class DefaultBillingRepository @Inject constructor(
                 },
             )
             megaApiGateway.getPaymentMethods(listener)
+            continuation.invokeOnCancellation {
+                megaApiGateway.removeRequestListener(listener)
+            }
+        }
+    }
+
+    private suspend fun fetchPricing(): Pricing = withContext(ioDispatcher) {
+        suspendCancellableCoroutine { continuation ->
+            val listener = continuation.getRequestListener {
+                pricingMapper(it.pricing, it.currency)
+            }
+            megaApiGateway.getPricing(listener)
             continuation.invokeOnCancellation {
                 megaApiGateway.removeRequestListener(listener)
             }
--- data/src/test/java/mega/privacy/android/data/repository/DefaultBillingRepositoryTest.kt
+++ data/src/test/java/mega/privacy/android/data/repository/DefaultBillingRepositoryTest.kt
@@ -8,20 +8,27 @@ import mega.privacy.android.data.facade.AccountInfoWrapper
 import mega.privacy.android.data.gateway.api.MegaApiGateway
 import mega.privacy.android.data.listener.OptionalMegaRequestListenerInterface
 import mega.privacy.android.data.mapper.LocalPricingMapper
+import mega.privacy.android.data.mapper.PricingMapper
 import mega.privacy.android.domain.entity.Currency
 import mega.privacy.android.domain.entity.LocalPricing
 import mega.privacy.android.domain.entity.account.CurrencyPoint
 import mega.privacy.android.domain.entity.account.MegaSku
 import mega.privacy.android.domain.entity.billing.PaymentMethodFlags
+import mega.privacy.android.domain.entity.billing.Pricing
 import mega.privacy.android.domain.exception.MegaException
 import mega.privacy.android.domain.repository.BillingRepository
 import nz.mega.sdk.MegaChatError
+import nz.mega.sdk.MegaCurrency
 import nz.mega.sdk.MegaError
+import nz.mega.sdk.MegaPricing
 import nz.mega.sdk.MegaRequest
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
 import org.mockito.kotlin.whenever
 import kotlin.contracts.ExperimentalContracts
 
@@ -33,6 +40,7 @@ class DefaultBillingRepositoryTest {
     private val accountInfoWrapper = mock<AccountInfoWrapper>()
     private val megaApiGateway = mock<MegaApiGateway>()
     private val paymentMethodFlagsCache = mock<Cache<PaymentMethodFlags>>()
+    private val pricingCache = mock<Cache<Pricing>>()
     private val megaSkuObject1 = MegaSku("mega.android.pro1.onemonth", 9990000, "EUR")
     private val megaSkuObject2 = MegaSku("mega.android.pro2.onemonth", 9990000, "EUR")
     private val skuString = "mega.android.pro1.onemonth"
@@ -40,6 +48,7 @@ class DefaultBillingRepositoryTest {
     private val localPricing =
         LocalPricing(CurrencyPoint.LocalCurrencyPoint(9990000), Currency("EUR"), skuString)
     private val localPricingMapper = mock<LocalPricingMapper>()
+    private val pricingMapper = mock<PricingMapper>()
 
     @Before
     fun setUp() {
@@ -48,7 +57,9 @@ class DefaultBillingRepositoryTest {
             megaApiGateway = megaApiGateway,
             ioDispatcher = UnconfinedTestDispatcher(),
             paymentMethodFlagsCache = paymentMethodFlagsCache,
+            pricingCache = pricingCache,
             localPricingMapper = localPricingMapper,
+            pricingMapper = pricingMapper
         )
     }
 
@@ -83,6 +94,79 @@ class DefaultBillingRepositoryTest {
             }
 
             underTest.getPaymentMethod(true)
+        }
+
+    @Test
+    fun `test that getPricing returns success when we clear cache and MegaApi returns ERROR_OK`() =
+        runTest {
+            val megaError = mock<MegaError> {
+                on { errorCode }.thenReturn(MegaChatError.ERROR_OK)
+            }
+
+            val expectedMegaPricing = mock<MegaPricing>()
+            val expectedMegaCurrency = mock<MegaCurrency>()
+            val expectedPricing = Pricing(emptyList())
+
+            val megaRequest = mock<MegaRequest> {
+                on { type }.thenReturn(MegaRequest.TYPE_GET_PRICING)
+                on { pricing }.thenReturn(expectedMegaPricing)
+                on { currency }.thenReturn(expectedMegaCurrency)
+            }
+
+            whenever(megaApiGateway.getPricing(listener = any())).thenAnswer {
+                ((it.arguments[0]) as OptionalMegaRequestListenerInterface).onRequestFinish(
+                    mock(),
+                    megaRequest,
+                    megaError,
+                )
+            }
+
+            whenever(pricingMapper(expectedMegaPricing, expectedMegaCurrency)).thenReturn(
+                expectedPricing)
+
+            underTest.getPricing(true)
+            verify(pricingCache, times(1)).set(expectedPricing)
+        }
+
+    @Test
+    fun `when clear cache false and cache return null then getPricing api call`() =
+        runTest {
+            val megaError = mock<MegaError> {
+                on { errorCode }.thenReturn(MegaChatError.ERROR_OK)
+            }
+
+            val expectedMegaPricing = mock<MegaPricing>()
+            val expectedMegaCurrency = mock<MegaCurrency>()
+            val expectedPricing = Pricing(emptyList())
+
+            val megaRequest = mock<MegaRequest> {
+                on { type }.thenReturn(MegaRequest.TYPE_GET_PRICING)
+                on { pricing }.thenReturn(expectedMegaPricing)
+                on { currency }.thenReturn(expectedMegaCurrency)
+            }
+
+            whenever(megaApiGateway.getPricing(listener = any())).thenAnswer {
+                ((it.arguments[0]) as OptionalMegaRequestListenerInterface).onRequestFinish(
+                    mock(),
+                    megaRequest,
+                    megaError,
+                )
+            }
+            whenever(pricingCache.get()).thenReturn(null)
+            whenever(pricingMapper(expectedMegaPricing, expectedMegaCurrency)).thenReturn(
+                expectedPricing)
+
+            underTest.getPricing(false)
+            verify(pricingCache, times(1)).set(expectedPricing)
+        }
+
+    @Test
+    fun `when clear cache false and cache return value then no getPricing api call`() =
+        runTest {
+            whenever(pricingCache.get()).thenReturn(Pricing(emptyList()))
+
+            underTest.getPricing(false)
+            verifyNoMoreInteractions(megaApiGateway)
         }
 
     @Test(expected = MegaException::class)
