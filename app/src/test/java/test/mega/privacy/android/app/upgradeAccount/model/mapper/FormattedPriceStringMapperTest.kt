85fb5d8f7704c9fd5be30933e5c2f16e2bb71f05
Veronika Koreiba <vko@mega.co.nz>
2022-12-14 10:13:44 +1300

    AND-14229: Create a compose view that consumes the new ui state model

---
+++ app/src/test/java/test/mega/privacy/android/app/upgradeAccount/model/mapper/FormattedPriceStringMapperTest.kt
@@ -0,0 +1,17 @@
+package test.mega.privacy.android.app.upgradeAccount.model.mapper
+
+import mega.privacy.android.app.upgradeAccount.model.mapper.toFormattedPriceString
+import mega.privacy.android.domain.entity.Currency
+import mega.privacy.android.domain.entity.account.CurrencyAmount
+import org.junit.Assert.*
+import org.junit.Test
+
+class FormattedPriceStringMapperTest {
+    @Test
+    fun `test that mapper returns correctly formatted price string`() {
+        val expectedResult = "€4.99"
+        val currencyAmount = CurrencyAmount(4.99.toFloat(), Currency("EUR"))
+
+        assertEquals(expectedResult, toFormattedPriceString(currencyAmount))
+    }
+}
