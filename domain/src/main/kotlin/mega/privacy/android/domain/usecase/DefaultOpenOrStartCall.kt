6c54d94346b7c8dbb3e856982e115c6a05fbc54d
Raquel García Chico <rgc@mega.nz>
2022-12-14 10:24:37 +0100

    Use chatRepository in DefaultCallRepository.kt and DefaultOpenOrStartCall.kt

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/DefaultOpenOrStartCall.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/DefaultOpenOrStartCall.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.withContext
 import mega.privacy.android.domain.entity.chat.ChatCall
 import mega.privacy.android.domain.qualifier.DefaultDispatcher
 import mega.privacy.android.domain.repository.CallRepository
+import mega.privacy.android.domain.repository.ChatRepository
 import javax.inject.Inject
 
 /**
@@ -12,24 +13,28 @@ import javax.inject.Inject
  */
 class DefaultOpenOrStartCall @Inject constructor(
     private val callRepository: CallRepository,
-    private val getChatCall: GetChatCall,
+    private val chatRepository: ChatRepository,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
 ) : OpenOrStartCall {
 
     override suspend fun invoke(chatId: Long, video: Boolean, audio: Boolean): ChatCall? {
         withContext(defaultDispatcher) {
             runCatching {
-                getChatCall(chatId)
-            }.onFailure {
-                //Call does not yet exist
-                return@withContext callRepository.startCall(chatId, video, audio)
-            }.onSuccess { call ->
-                call?.let {
-                    //Call already in progress
-                    return@withContext it
+                chatRepository.getChatCall(chatId = chatId)
+            }.fold(
+                onSuccess = { call ->
+                    call?.let {
+                        //Call already in progress
+                        return@withContext it
+                    }
+                },
+                onFailure = {
+                    //Call does not yet exist
+                    return@withContext callRepository.startCall(chatId, video, audio)
                 }
-            }
+            )
         }
+
         return null
     }
 
