2c2862ea8a9542ba005099aaa22403d3a0246dd5
Dhananjay Kulkarni <dk@mega.co.nz>
2023-01-09 22:15:18 +0530

    Code formatted

--- data/src/main/java/mega/privacy/android/data/facade/MegaApiFacade.kt
+++ data/src/main/java/mega/privacy/android/data/facade/MegaApiFacade.kt
@@ -740,7 +740,6 @@ internal class MegaApiFacade @Inject constructor(
     override fun upgradeSecurity(listener: MegaRequestListenerInterface) =
         megaApi.upgradeSecurity(listener)
 
-    override fun setSecureFlag(enable: Boolean) {
-        megaApi.setSecureFlag(enable)
-    }
+    override fun setSecureFlag(enable: Boolean) = megaApi.setSecureFlag(enable)
+
 }
