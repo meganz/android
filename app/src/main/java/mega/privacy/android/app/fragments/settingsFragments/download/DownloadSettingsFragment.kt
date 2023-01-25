9e8534217572961e0bb65b824a3fdbcb7559dc7e
Kevin Gozali <kg@mega.co.nz>
2023-01-25 21:15:42 +1300

    AND-15484: Change DownloadSettingsFragment Inheritance to PreferenceFragmentCompat

--- app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/download/DownloadSettingsFragment.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/download/DownloadSettingsFragment.kt
@@ -1,9 +1,11 @@
-package mega.privacy.android.app.fragments.settingsFragments
+package mega.privacy.android.app.fragments.settingsFragments.download
 
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import mega.privacy.android.app.MegaApplication
 import mega.privacy.android.app.R
 import mega.privacy.android.app.components.TwoLineCheckPreference
 import mega.privacy.android.app.constants.SettingsConstants.KEY_STORAGE_ASK_ME_ALWAYS
@@ -12,16 +14,22 @@ import mega.privacy.android.app.main.FileStorageActivity
 import mega.privacy.android.app.utils.Constants
 import mega.privacy.android.app.utils.FileUtil
 import mega.privacy.android.app.utils.TextUtil
+import mega.privacy.android.data.database.DatabaseHandler
+import mega.privacy.android.data.model.MegaPreferences
 import timber.log.Timber
 
 /**
  * The fragment for download settings
  */
-class DownloadSettingsFragment : SettingsBaseFragment() {
+class DownloadSettingsFragment : PreferenceFragmentCompat(),
+    Preference.OnPreferenceClickListener,
+    Preference.OnPreferenceChangeListener {
     private var downloadLocation: Preference? = null
     private var storageAskMeAlways: TwoLineCheckPreference? = null
     private var askMe = false
     private var downloadLocationPath: String? = null
+    private val dbH: DatabaseHandler = MegaApplication.getInstance().dbH
+    private val prefs: MegaPreferences? = dbH.preferences
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences_download)
@@ -30,7 +38,7 @@ class DownloadSettingsFragment : SettingsBaseFragment() {
 
         downloadLocation?.onPreferenceClickListener = this
         storageAskMeAlways?.onPreferenceClickListener = this
-        if (prefs.storageAskAlways == null) {
+        if (prefs?.storageAskAlways == null) {
             askMe = true
             dbH.setStorageAskAlways(true)
         } else {
@@ -42,17 +50,12 @@ class DownloadSettingsFragment : SettingsBaseFragment() {
                 preferenceScreen.removePreference(it)
             }
         }
-        if (TextUtil.isTextEmpty(prefs.storageDownloadLocation)) {
+        if (TextUtil.isTextEmpty(prefs?.storageDownloadLocation)) {
             resetDefaultDownloadLocation()
         } else {
-            downloadLocationPath = prefs.storageDownloadLocation
+            downloadLocationPath = prefs?.storageDownloadLocation
         }
         setDownloadLocation()
-    }
-
-    private fun resetDefaultDownloadLocation() {
-        val defaultDownloadLocation = FileUtil.buildDefaultDownloadDir(context)
-        downloadLocationPath = defaultDownloadLocation.absolutePath
     }
 
     override fun onPreferenceClick(preference: Preference): Boolean {
@@ -76,14 +79,26 @@ class DownloadSettingsFragment : SettingsBaseFragment() {
         return true
     }
 
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean = false
+
+    private fun resetDefaultDownloadLocation() {
+        try {
+            val defaultDownloadLocation = FileUtil.buildDefaultDownloadDir(context)
+            downloadLocationPath = defaultDownloadLocation.absolutePath
+        } catch (e: RuntimeException) {
+            Timber.e(e)
+        }
+    }
+
     private fun setDownloadLocation() {
         dbH.setStorageDownloadLocation(downloadLocationPath)
-        prefs.storageDownloadLocation = downloadLocationPath
+        prefs?.storageDownloadLocation = downloadLocationPath
         downloadLocation?.run {
             summary = downloadLocationPath
         }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         if (requestCode == Constants.REQUEST_DOWNLOAD_FOLDER && intent != null) {
             if (resultCode == Activity.RESULT_OK) {
