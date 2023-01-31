8ea264a1bd87ae6e076d9215c1be57ae7b64f567
Kevin Gozali <kg@mega.co.nz>
2023-01-31 21:57:21 +1300

    AND-15506 Refactor Fragment and Integrate DownloadSettingViewModel to Fragment

--- app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/download/DownloadSettingsFragment.kt
+++ app/src/main/java/mega/privacy/android/app/fragments/settingsFragments/download/DownloadSettingsFragment.kt
@@ -3,120 +3,94 @@ package mega.privacy.android.app.fragments.settingsFragments.download
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import mega.privacy.android.app.MegaApplication
+import dagger.hilt.android.AndroidEntryPoint
 import mega.privacy.android.app.R
+import mega.privacy.android.app.arch.extensions.collectFlow
 import mega.privacy.android.app.components.TwoLineCheckPreference
 import mega.privacy.android.app.constants.SettingsConstants.KEY_STORAGE_ASK_ME_ALWAYS
 import mega.privacy.android.app.constants.SettingsConstants.KEY_STORAGE_DOWNLOAD_LOCATION
 import mega.privacy.android.app.main.FileStorageActivity
-import mega.privacy.android.app.utils.Constants
-import mega.privacy.android.app.utils.FileUtil
-import mega.privacy.android.app.utils.TextUtil
-import mega.privacy.android.data.database.DatabaseHandler
-import mega.privacy.android.data.model.MegaPreferences
+import mega.privacy.android.app.main.FileStorageActivity.PickFolderType
 import timber.log.Timber
 
 /**
  * The fragment for download settings
  */
-class DownloadSettingsFragment : PreferenceFragmentCompat(),
-    Preference.OnPreferenceClickListener,
-    Preference.OnPreferenceChangeListener {
+@AndroidEntryPoint
+class DownloadSettingsFragment : PreferenceFragmentCompat() {
+    private val viewModel by viewModels<DownloadSettingsViewModel>()
+
     private var downloadLocation: Preference? = null
     private var storageAskMeAlways: TwoLineCheckPreference? = null
-    private var askMe = false
-    private var downloadLocationPath: String? = null
-    private val dbH: DatabaseHandler = MegaApplication.getInstance().dbH
-    private val prefs: MegaPreferences? = dbH.preferences
+
+    private val downloadFolderActivityResult =
+        registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.data != null) {
+                if (result.resultCode == Activity.RESULT_OK) {
+                    viewModel.setDownloadLocation(result.data?.getStringExtra(FileStorageActivity.EXTRA_PATH))
+                } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                    Timber.w("REQUEST_DOWNLOAD_FOLDER - canceled")
+                }
+            }
+        }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences_download)
         downloadLocation = findPreference(KEY_STORAGE_DOWNLOAD_LOCATION)
         storageAskMeAlways = findPreference(KEY_STORAGE_ASK_ME_ALWAYS)
-
-        downloadLocation?.onPreferenceClickListener = this
-        storageAskMeAlways?.onPreferenceClickListener = this
-        if (prefs?.storageAskAlways == null) {
-            askMe = true
-            dbH.setStorageAskAlways(true)
-        } else {
-            askMe = java.lang.Boolean.parseBoolean(prefs.storageAskAlways)
-        }
-        storageAskMeAlways?.isChecked = askMe
-        if (askMe) {
-            downloadLocation?.let {
-                preferenceScreen.removePreference(it)
-            }
-        }
-        if (TextUtil.isTextEmpty(prefs?.storageDownloadLocation)) {
-            resetDefaultDownloadLocation()
-        } else {
-            downloadLocationPath = prefs?.storageDownloadLocation
-        }
-        setDownloadLocation()
+        setPreferencesClickListener()
     }
 
-    override fun onPreferenceClick(preference: Preference): Boolean {
-        when (preference.key) {
-            KEY_STORAGE_ASK_ME_ALWAYS -> {
-                storageAskMeAlways?.let {
-                    dbH.setStorageAskAlways(it.isChecked.also { isChecked -> askMe = isChecked })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectUIState()
+    }
+
+    /**
+     * Collect UI State from the View Model to update the fragment view
+     */
+    private fun collectUIState() {
+        viewLifecycleOwner.collectFlow(viewModel.uiState) { (downloadPath, askAlwaysChecked) ->
+            downloadLocation?.summary = downloadPath
+            storageAskMeAlways?.isChecked = askAlwaysChecked
+
+            downloadLocation?.let { dl ->
+                if (askAlwaysChecked) {
+                    preferenceScreen.removePreference(dl)
+                } else {
+                    preferenceScreen.addPreference(dl)
                 }
-                downloadLocation?.let {
-                    if (askMe) {
-                        preferenceScreen.removePreference(it)
-                    } else {
-                        resetDefaultDownloadLocation()
-                        preferenceScreen.addPreference(it)
-                    }
-                }
-                setDownloadLocation()
-            }
-            KEY_STORAGE_DOWNLOAD_LOCATION -> toSelectFolder()
-        }
-        return true
-    }
-
-    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean = false
-
-    private fun resetDefaultDownloadLocation() {
-        try {
-            val defaultDownloadLocation = FileUtil.buildDefaultDownloadDir(context)
-            downloadLocationPath = defaultDownloadLocation.absolutePath
-        } catch (e: RuntimeException) {
-            Timber.e(e)
-        }
-    }
-
-    private fun setDownloadLocation() {
-        dbH.setStorageDownloadLocation(downloadLocationPath)
-        prefs?.storageDownloadLocation = downloadLocationPath
-        downloadLocation?.run {
-            summary = downloadLocationPath
-        }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        if (requestCode == Constants.REQUEST_DOWNLOAD_FOLDER && intent != null) {
-            if (resultCode == Activity.RESULT_OK) {
-                downloadLocationPath = intent.getStringExtra(FileStorageActivity.EXTRA_PATH)
-                setDownloadLocation()
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                Timber.d("REQUEST_DOWNLOAD_FOLDER - canceled")
             }
         }
     }
 
-    private fun toSelectFolder() {
-        val intent = Intent(context, FileStorageActivity::class.java)
-        intent.action = FileStorageActivity.Mode.PICK_FOLDER.action
-        intent.putExtra(
-            FileStorageActivity.PICK_FOLDER_TYPE,
-            FileStorageActivity.PickFolderType.DOWNLOAD_FOLDER.folderType
-        )
-        startActivityForResult(intent, Constants.REQUEST_DOWNLOAD_FOLDER)
+    private fun setPreferencesClickListener() {
+        downloadLocation?.onPreferenceClickListener = launchFolderSelectionScreen()
+        storageAskMeAlways?.onPreferenceClickListener = onStorageAskMeAlwaysCheckChanged()
+    }
+
+    private fun launchFolderSelectionScreen() = Preference.OnPreferenceClickListener {
+        val intent = Intent(context, FileStorageActivity::class.java).apply {
+            action = FileStorageActivity.Mode.PICK_FOLDER.action
+            putExtra(
+                FileStorageActivity.PICK_FOLDER_TYPE,
+                PickFolderType.DOWNLOAD_FOLDER.folderType
+            )
+        }
+
+        downloadFolderActivityResult.launch(intent)
+        true
+    }
+
+    private fun onStorageAskMeAlwaysCheckChanged() = Preference.OnPreferenceClickListener {
+        storageAskMeAlways?.let { pref ->
+            viewModel.onStorageAskAlwaysChanged(pref.isChecked)
+        }
+        true
     }
 }
