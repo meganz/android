05ac93d0f957bd309ff567bade500c879df97542
Dhananjay Kulkarni <dk@mega.co.nz>
2023-02-15 02:14:57 +1300

    AND-15672 NodeOptionsBottomSheetViewModel created specifically for NodeOptionsBottomSheetDialogFragment

--- app/src/main/java/mega/privacy/android/app/presentation/shares/outgoing/OutgoingSharesViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/shares/outgoing/OutgoingSharesViewModel.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.launch
 import mega.privacy.android.app.domain.usecase.GetNodeByHandle
 import mega.privacy.android.app.domain.usecase.GetOutgoingSharesChildrenNode
 import mega.privacy.android.app.domain.usecase.MonitorNodeUpdates
-import mega.privacy.android.app.domain.usecase.OpenShareDialog
 import mega.privacy.android.app.featuretoggle.AppFeatures
 import mega.privacy.android.app.presentation.shares.outgoing.model.OutgoingSharesState
 import mega.privacy.android.domain.usecase.GetCloudSortOrder
@@ -37,7 +36,6 @@ class OutgoingSharesViewModel @Inject constructor(
     monitorNodeUpdates: MonitorNodeUpdates,
     private val getFeatureFlagValue: GetFeatureFlagValue,
     private val getUnverifiedOutgoingShares: GetUnverifiedOutgoingShares,
-    private val openShareDialog: OpenShareDialog,
 ) : ViewModel() {
 
     /** private UI state */
@@ -201,31 +199,6 @@ class OutgoingSharesViewModel @Inject constructor(
     private suspend fun isMandatoryFingerprintRequired() {
         _state.update {
             it.copy(isMandatoryFingerprintVerificationNeeded = getFeatureFlagValue(AppFeatures.MandatoryFingerprintVerification))
-        }
-    }
-
-    /**
-     * Calls OpenShareDialog use case to create crypto key for sharing
-     *
-     * @param nodeHandle: [MegaNode] handle
-     */
-    fun callOpenShareDialog(nodeHandle: Long) {
-        kotlin.runCatching {
-            viewModelScope.launch {
-                if (!isInvalidHandle(nodeHandle)) {
-                    getNodeByHandle(nodeHandle)?.let { megaNode ->
-                        openShareDialog(megaNode)
-                    }
-                }
-            }
-        }.onSuccess {
-            _state.update {
-                it.copy(isOpenShareDialogSuccess = true)
-            }
-        }.onFailure {
-            _state.update {
-                it.copy(isOpenShareDialogSuccess = false)
-            }
         }
     }
 }
