da71dca28eb2dfb1161da79f76d1f3dbaf2293a6
Dhananjay Kulkarni <dk@mega.co.nz>
2023-02-14 13:36:44 +0530

    Removed repetitive collection of flow

--- app/src/main/java/mega/privacy/android/app/main/ManagerActivity.java
+++ app/src/main/java/mega/privacy/android/app/main/ManagerActivity.java
@@ -2463,6 +2463,11 @@ public class ManagerActivity extends TransfersManagementActivity
         });
 
         ViewExtensionsKt.collectFlow(this, viewModel.getState(), Lifecycle.State.STARTED, managerState -> {
+
+            if (viewModel.getState().getValue().getShouldAlertUserAboutSecurityUpgrade()) {
+                replaceFragment(SecurityUpgradeDialogFragment.Companion.newInstance(), SecurityUpgradeDialogFragment.TAG);
+            }
+
             updateInboxSectionVisibility(managerState.getHasInboxChildren());
             stopUploadProcessAndSendBroadcast(managerState.getShouldStopCameraUpload(), managerState.getShouldSendCameraBroadcastEvent());
             if (managerState.getShowSyncSection()) {
@@ -2522,13 +2527,6 @@ public class ManagerActivity extends TransfersManagementActivity
         ViewExtensionsKt.collectFlow(this, outgoingSharesViewModel.getState(), Lifecycle.State.STARTED, outgoingSharesState -> {
             if (outgoingSharesState.isMandatoryFingerprintVerificationNeeded()) {
                 addUnverifiedOutgoingCountBadge(outgoingSharesState.getUnverifiedOutgoingShares().size());
-            }
-            return Unit.INSTANCE;
-        });
-
-        ViewExtensionsKt.collectFlow(this, viewModel.getState(), Lifecycle.State.STARTED, state -> {
-            if (viewModel.getState().getValue().getShouldAlertUserAboutSecurityUpgrade()) {
-                replaceFragment(SecurityUpgradeDialogFragment.Companion.newInstance(), SecurityUpgradeDialogFragment.TAG);
             }
             return Unit.INSTANCE;
         });
