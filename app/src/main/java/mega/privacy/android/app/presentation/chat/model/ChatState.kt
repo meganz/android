e6958b5c2eec8429843eb367c7ec6390c186dc4e
Raquel Garcia Chico <rgc@mega.nz>
2023-03-01 02:33:22 +1300

    T4575148 The user should be able to send a private message

--- app/src/main/java/mega/privacy/android/app/presentation/chat/model/ChatState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/chat/model/ChatState.kt
@@ -5,8 +5,10 @@ package mega.privacy.android.app.presentation.chat.model
  *
  * @property error              String resource id for showing an error.
  * @property isCallAnswered     Handle when a call is answered.
+ * @property isChatInitialised  True, if the chat is initialised. False, if not.
  */
 data class ChatState(
     val error: Int? = null,
     val isCallAnswered: Boolean = false,
+    val isChatInitialised: Boolean = false,
 )
