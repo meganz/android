2d916f2dce32a1c20f71f46cccfbdb2800a04119
Raquel Garcia Chico <rgc@mega.nz>
2023-03-06 21:15:59 +1300

    MEET-2070 DEV - Add HandleListMapperImplTest

--- data/src/main/java/mega/privacy/android/data/mapper/HandleListMapperImpl.kt
+++ data/src/main/java/mega/privacy/android/data/mapper/HandleListMapperImpl.kt
@@ -1,5 +1,6 @@
 package mega.privacy.android.data.mapper
 
+import nz.mega.sdk.MegaChatApiJava.MEGACHAT_INVALID_HANDLE
 import nz.mega.sdk.MegaHandleList
 import javax.inject.Inject
 
@@ -9,8 +10,12 @@ import javax.inject.Inject
 internal class HandleListMapperImpl @Inject constructor() : HandleListMapper {
     override fun invoke(handleList: MegaHandleList) =
         mutableListOf<Long>().apply {
-            for (i in 0..handleList.size()) {
-                add(handleList[i])
+            if (handleList.size() > 0) {
+                for (i in 0..handleList.size()) {
+                    if (handleList[i] != MEGACHAT_INVALID_HANDLE) {
+                        add(handleList[i])
+                    }
+                }
             }
         }
 }
