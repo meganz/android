fd323bb181025de6b32fcbca49752716621597c7
Gregg Meyrick Jover <gmj@mega.co.nz>
2023-03-31 14:04:28 +0800

    CU-325: Remove Unused VideoCompressor
    Since the Video Compression functionality was refactored, this class is unused in the app and can safely be removed. Furthermore, majority of the functions in VideoCompressionCallback have been removed.

--- app/src/main/java/mega/privacy/android/app/utils/conversion/VideoCompressionCallback.java
+++ app/src/main/java/mega/privacy/android/app/utils/conversion/VideoCompressionCallback.java
@@ -1,18 +1,5 @@
 package mega.privacy.android.app.utils.conversion;
 
-import mega.privacy.android.domain.entity.SyncRecord;
-
 public interface VideoCompressionCallback {
-
     void onCompressUpdateProgress(int progress);
-
-    void onCompressSuccessful(SyncRecord record);
-
-    void onCompressFailed(SyncRecord record);
-
-    void onCompressFinished(String currentIndexString);
-
-    void onCompressNotSupported(SyncRecord record);
-
-    void onInsufficientSpace();
 }
