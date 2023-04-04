c8d2dc9dd4e62b6c05248b26267c638160d8942f
Atiqur Rahman <atr@mega.co.nz>
2023-04-04 15:19:20 +0600

    AND-16165: Filter if filetypeInfo is VideoFileTypeInfo

--- data/src/main/java/mega/privacy/android/data/repository/DefaultPhotosRepository.kt
+++ data/src/main/java/mega/privacy/android/data/repository/DefaultPhotosRepository.kt
@@ -322,7 +322,7 @@ internal class DefaultPhotosRepository @Inject constructor(
      */
     private suspend fun mapPhotoNodesToVideos(megaNodes: List<MegaNode>): List<Photo> {
         return megaNodes.filter {
-            it.isValidPhotoNode()
+            it.isValidPhotoNode() && fileTypeInfoMapper(it) is VideoFileTypeInfo
         }.map { megaNode ->
             mapMegaNodeToVideo(megaNode)
         }
