32898c309e22402049bc11d7df0026d7efc1ce94
Robin Shi <rsh@mega.co.nz>
2023-04-28 21:14:16 +1200

    AND-16350 replace hardcoded ICU version in Android.mk

--- sdk/src/main/jni/icu/Android.mk
+++ sdk/src/main/jni/icu/Android.mk
@@ -2,9 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ICU_VERSION:= 71_1
 LOCAL_MODULE := icu
-LOCAL_SRC_FILES := $(LOCAL_PATH)/icuSource-71_1/icu/$(TARGET_ARCH_ABI)/lib/libicuuc.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/icuSource-71_1/icu/source/common
+LOCAL_SRC_FILES := $(LOCAL_PATH)/icuSource-${ICU_VERSION}/icu/$(TARGET_ARCH_ABI)/lib/libicuuc.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/icuSource-${ICU_VERSION}/icu/source/common
 LOCAL_STATIC_LIBRARIES := icu
 
 include $(PREBUILT_STATIC_LIBRARY)
