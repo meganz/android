5bf5f70760c163a279f7141925ecae530761810b
Ilya Konnov <iko@mega.co.nz>
2023-05-27 00:16:08 +1200

    FM-231 Create design for Sync empty screen

---
+++ feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/SyncEmptyScreen.kt
@@ -0,0 +1,84 @@
+package mega.privacy.android.feature.sync.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mega.privacy.android.core.ui.controls.buttons.RaisedDefaultMegaButton
+import mega.privacy.android.core.ui.theme.AndroidTheme
+import mega.privacy.android.core.ui.theme.extensions.textColorPrimary
+import mega.privacy.android.core.ui.theme.extensions.grey_alpha_054_white_alpha_054
+import mega.privacy.android.feature.sync.R
+
+@Composable
+internal fun SyncEmptyScreen(getStartedClicked: () -> Unit) {
+    Column {
+        Image(
+            /**
+             * Temporary image placeholder, the design for production image is not ready yet
+             */
+            painterResource(R.drawable.sync_image_placeholder),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(
+                    top = 48.dp, bottom = 32.dp
+                )
+                .testTag(TAG_SYNC_EMPTY_SCREEN_ILLUSTRATION)
+                .fillMaxWidth()
+        )
+
+        Text(
+            text = "Sync",
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6.copy(color = MaterialTheme.colors.textColorPrimary)
+        )
+        Text(
+            text = "You will need to set up a local folder on your \n" + "device that would pair with a chosen folder on \n" + "your Cloud Drive.",
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.subtitle2.copy(color = MaterialTheme.colors.grey_alpha_054_white_alpha_054)
+        )
+        Box(
+            Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
+            RaisedDefaultMegaButton(
+                /**
+                 *  The string for this screen is not available on transifex yet,
+                 *  So I'm reusing a similar string from :app module
+                 *
+                 */
+                textId = R.string.start_screen_setting,
+                onClick = getStartedClicked,
+            )
+        }
+    }
+}
+
+internal const val TAG_SYNC_EMPTY_SCREEN_ILLUSTRATION = "sync_empty_screen_illustration"
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun SyncEmptyScreenPreview() {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        SyncEmptyScreen({})
+    }
+}
---
+++ feature/sync/src/test/java/mega/privacy/android/feature/sync/ui/SyncEmptyScreenTest.kt
@@ -0,0 +1,35 @@
+package mega.privacy.android.feature.sync.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import mega.privacy.android.feature.sync.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class SyncEmptyScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun `test that all sync empty screen components are visible`() {
+        composeTestRule.setContent {
+            SyncEmptyScreen({})
+        }
+
+        composeTestRule.onNodeWithTag(TAG_SYNC_EMPTY_SCREEN_ILLUSTRATION)
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Sync")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("You will need to set up a local folder on your \n" + "device that would pair with a chosen folder on \n" + "your Cloud Drive.")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.start_screen_setting))
+            .assertIsDisplayed()
+    }
+}
