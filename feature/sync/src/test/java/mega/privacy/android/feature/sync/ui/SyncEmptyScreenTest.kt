efd1854b315afa164864fa7fedccb85fcc83ef54
Ilya Konnov <iko@mega.co.nz>
2023-06-01 03:11:53 +1200

    FM-425 Fix Jetpack Compose Unit Test - SyncEmptyScreenTest

--- feature/sync/src/test/java/mega/privacy/android/feature/sync/ui/SyncEmptyScreenTest.kt
+++ feature/sync/src/test/java/mega/privacy/android/feature/sync/ui/SyncEmptyScreenTest.kt
@@ -7,11 +7,12 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mega.privacy.android.feature.sync.R
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
+@Config(qualifiers = "fr-rFr-w1080dp-h1920dp")
 @RunWith(AndroidJUnit4::class)
 internal class SyncEmptyScreenTest {
 
@@ -19,7 +20,6 @@ internal class SyncEmptyScreenTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
-    @Ignore
     fun `test that all sync empty screen components are visible`() {
         composeTestRule.setContent {
             SyncEmptyScreen({})
