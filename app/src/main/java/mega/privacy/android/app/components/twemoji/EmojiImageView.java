385f8baaddf5c7d3a3d0fa92c11c60f4af2ffdcc
Javier Gomez <jg@mega.co.nz>
2023-06-09 12:09:39 +0200

    AND-16759: Lint error - Suspicious indentation

--- app/src/main/java/mega/privacy/android/app/components/twemoji/EmojiImageView.java
+++ app/src/main/java/mega/privacy/android/app/components/twemoji/EmojiImageView.java
@@ -97,19 +97,20 @@ public final class EmojiImageView extends AppCompatImageView {
   }
 
   public void setEmoji(@NonNull final Emoji emoji, boolean isInfoReaction) {
-    if(emoji.equals(currentEmoji))
+    if (emoji.equals(currentEmoji)) {
       return;
+    }
 
-      setImageDrawable(null);
-      currentEmoji = emoji;
-      hasVariants = !isInfoReaction;
+    setImageDrawable(null);
+    currentEmoji = emoji;
+    hasVariants = !isInfoReaction;
 
-      if (imageLoadingTask != null) {
-        imageLoadingTask.cancel(true);
-      }
+    if (imageLoadingTask != null) {
+      imageLoadingTask.cancel(true);
+    }
 
-      imageLoadingTask = new ImageLoadingTask(this);
-      imageLoadingTask.execute(emoji);
+    imageLoadingTask = new ImageLoadingTask(this);
+    imageLoadingTask.execute(emoji);
   }
 
   public void setEmoji(@NonNull final Emoji emoji) {
