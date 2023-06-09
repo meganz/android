c83850c718b0361c7e31684985fede1b458803f7
Jonathan Andy <jta@mega.co.nz>
2023-06-09 18:07:07 +1200

    CC-4597: Fresh Account Remember Timeline PReferences

--- data/src/main/java/mega/privacy/android/data/mapper/photos/ContentConsumptionMegaStringMapMapper.kt
+++ data/src/main/java/mega/privacy/android/data/mapper/photos/ContentConsumptionMegaStringMapMapper.kt
@@ -5,6 +5,7 @@ import mega.privacy.android.data.extensions.encodeBase64
 import mega.privacy.android.data.extensions.getValueFor
 import mega.privacy.android.domain.entity.photos.TimelinePreferencesJSON
 import nz.mega.sdk.MegaStringMap
+import org.json.JSONException
 import org.json.JSONObject
 import javax.inject.Inject
 
@@ -20,8 +21,11 @@ internal class ContentConsumptionMegaStringMapMapper @Inject constructor() {
         } ?: JSONObject()
 
         val asJSON = JSONObject(newPreferences)
-        val androidPreferences =
+        val androidPreferences = try {
             latestPreferences.getJSONObject(TimelinePreferencesJSON.JSON_KEY_ANDROID.value)
+        } catch (exception: JSONException) {
+            JSONObject()
+        }
 
         androidPreferences.put(TimelinePreferencesJSON.JSON_KEY_TIMELINE.value, asJSON)
         latestPreferences.put(TimelinePreferencesJSON.JSON_KEY_ANDROID.value, androidPreferences)
