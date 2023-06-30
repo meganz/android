5f1e81e585f571c161b29dcdc7a59ca09a01432b
Joe Ji <jj@mega.co.nz>
2023-06-30 19:13:19 +1200

    T10619179 Clear photos cache when logout

--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/login/LocalLogoutAppUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/login/LocalLogoutAppUseCaseTest.kt
@@ -3,8 +3,10 @@ package mega.privacy.android.domain.usecase.login
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import mega.privacy.android.domain.repository.AccountRepository
+import mega.privacy.android.domain.repository.AlbumRepository
 import mega.privacy.android.domain.repository.BillingRepository
 import mega.privacy.android.domain.repository.LoginRepository
+import mega.privacy.android.domain.repository.PhotosRepository
 import mega.privacy.android.domain.repository.PushesRepository
 import mega.privacy.android.domain.repository.TransferRepository
 import mega.privacy.android.domain.usecase.ClearPsa
@@ -31,6 +33,8 @@ class LocalLogoutAppUseCaseTest {
     private val billingRepository = mock<BillingRepository>()
     private val stopCameraUploadsUseCase = mock<StopCameraUploadsUseCase>()
     private val stopAudioService = mock<StopAudioService>()
+    private val photosRepository = mock<PhotosRepository>()
+    private val albumRepository = mock<AlbumRepository>()
 
     @BeforeAll
     fun setUp() {
@@ -40,6 +44,8 @@ class LocalLogoutAppUseCaseTest {
             transferRepository = transferRepository,
             pushesRepository = pushesRepository,
             billingRepository = billingRepository,
+            photosRepository = photosRepository,
+            albumRepository = albumRepository,
             stopCameraUploadsUseCase = stopCameraUploadsUseCase,
             stopAudioService = stopAudioService
         )
