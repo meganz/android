6893450b7363974d516ab994a7c3ba01a2ec04b5
Gregg Meyrick Jover <gmj@mega.co.nz>
2023-07-07 23:49:44 +1200

    CU-294: Remove CameraUploadUtil.getSecondaryFolderHandle()

--- app/src/main/java/mega/privacy/android/app/utils/CameraUploadUtil.java
+++ app/src/main/java/mega/privacy/android/app/utils/CameraUploadUtil.java
@@ -15,14 +15,6 @@ public class CameraUploadUtil {
     private static final DatabaseHandler dbH = app.getDbH();
 
     /**
-     * @see GetUploadFolderHandleUseCase
-     * @deprecated Replace all calls with use case
-     */
-    public static long getSecondaryFolderHandle() {
-        return getUploadFolderHandle(false);
-    }
-
-    /**
      * @see mega.privacy.android.domain.usecase.camerauploads.IsCameraUploadsEnabledUseCase
      * @deprecated Replace all calls with use case
      */
@@ -38,16 +30,5 @@ public class CameraUploadUtil {
     public static boolean isSecondaryEnabled() {
         MegaPreferences prefs = dbH.getPreferences();
         return prefs != null && Boolean.parseBoolean(prefs.getSecondaryMediaFolderEnabled());
-    }
-
-    private static long getUploadFolderHandle(boolean isPrimary) {
-        MegaPreferences prefs = dbH.getPreferences();
-        if (prefs == null) {
-            return INVALID_HANDLE;
-        }
-
-        String handle = isPrimary ? prefs.getCamSyncHandle() : prefs.getMegaHandleSecondaryFolder();
-
-        return isTextEmpty(handle) ? INVALID_HANDLE : Long.parseLong(handle);
     }
 }
