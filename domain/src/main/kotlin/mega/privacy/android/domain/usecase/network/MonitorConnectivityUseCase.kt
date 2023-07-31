2e673ff64d94940e7491562f00cac62cb6c09c39
Kevin Ham <kh@mega.co.nz>
2023-08-01 00:47:38 +1200

    CU-502: Refactor MonitorConnectivityUseCase to Singleton (Revert)

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/network/MonitorConnectivityUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/network/MonitorConnectivityUseCase.kt
@@ -7,30 +7,25 @@ import kotlinx.coroutines.flow.stateIn
 import mega.privacy.android.domain.qualifier.ApplicationScope
 import mega.privacy.android.domain.repository.NetworkRepository
 import javax.inject.Inject
-import javax.inject.Singleton
 
 
 /**
  * Use case for monitoring connectivity.
  *
  */
-@Singleton
 class MonitorConnectivityUseCase @Inject constructor(
-    networkRepository: NetworkRepository,
-    @ApplicationScope appScope: CoroutineScope,
+    private val networkRepository: NetworkRepository,
+    @ApplicationScope private val appScope: CoroutineScope,
 ) {
-
-    private val flow = networkRepository.monitorConnectivityChanges().map { it.connected }
-        .stateIn(
-            appScope,
-            SharingStarted.Eagerly,
-            networkRepository.getCurrentConnectivityState().connected,
-        )
-
     /**
      * Invoke.
      *
      * @return Flow of Boolean.
      */
-    operator fun invoke() = flow
+    operator fun invoke() = networkRepository.monitorConnectivityChanges().map { it.connected }
+        .stateIn(
+            appScope,
+            SharingStarted.Eagerly,
+            networkRepository.getCurrentConnectivityState().connected,
+        )
 }
