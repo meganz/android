7acef28a3120976009b2a11bd99d343756ba45a9
Robin Shi <rsh@mega.co.nz>
2023-08-01 09:40:00 +1200

    Pre release v8.7 - Upgrade SWIG

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ extra["targetSdkVersion"] = 33
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20230726.001126-rel"
+extra["megaSdkVersion"] = "20230731.044945-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
