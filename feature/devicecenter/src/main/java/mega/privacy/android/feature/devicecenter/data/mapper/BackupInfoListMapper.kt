b7b40673fb936edd3fe8c64a5f4ee430e5da3a8a
Gregg Meyrick Jover <gmj@mega.co.nz>
2023-08-03 19:08:05 +1200

    BAC-536: Create a Mapper to Convert MegaBackupInfoList into BackupInfo Objects
    Changelog:
    - Create BackupInfoListMapper that maps the raw MegaBackupInfoList data from the SDK into a list of BackupInfo objects.
    - Create BackupInfoListMapperTest to verify the Mapper implementation.

---
+++ feature/devicecenter/src/main/java/mega/privacy/android/feature/devicecenter/data/mapper/BackupInfoListMapper.kt
@@ -0,0 +1,30 @@
+package mega.privacy.android.feature.devicecenter.data.mapper
+
+import mega.privacy.android.feature.devicecenter.data.entity.BackupInfo
+import nz.mega.sdk.MegaBackupInfoList
+import javax.inject.Inject
+
+/**
+ * Mapper that converts a [MegaBackupInfoList] into a list of [BackupInfo] objects
+ *
+ * @property backupInfoMapper [BackupInfoMapper]
+ */
+internal class BackupInfoListMapper @Inject constructor(
+    private val backupInfoMapper: BackupInfoMapper,
+) {
+    /**
+     * Invocation function
+     *
+     * @param sdkBackupInfoList A potentially nullable [MegaBackupInfoList]
+     * @return A list of [BackupInfo] objects
+     */
+    operator fun invoke(sdkBackupInfoList: MegaBackupInfoList?): List<BackupInfo> {
+        sdkBackupInfoList?.let { backupInfoList ->
+            val backupSize = backupInfoList.size()
+            return if (backupSize <= 0) emptyList()
+            else (0 until backupSize).mapNotNull { index ->
+                backupInfoMapper(backupInfoList.get(index))
+            }
+        } ?: return emptyList()
+    }
+}
---
+++ feature/devicecenter/src/test/java/mega/privacy/android/feature/devicecenter/data/mapper/BackupInfoListMapperTest.kt
@@ -0,0 +1,64 @@
+package mega.privacy.android.feature.devicecenter.data.mapper
+
+import com.google.common.truth.Truth.assertThat
+import mega.privacy.android.feature.devicecenter.data.entity.BackupInfo
+import nz.mega.sdk.MegaBackupInfo
+import nz.mega.sdk.MegaBackupInfoList
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.whenever
+
+/**
+ * Test class for [BackupInfoListMapper]
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class BackupInfoListMapperTest {
+
+    private lateinit var underTest: BackupInfoListMapper
+
+    private val backupInfoMapper = mock<BackupInfoMapper>()
+
+    @BeforeAll
+    fun setUp() {
+        underTest = BackupInfoListMapper(backupInfoMapper)
+    }
+
+    @BeforeEach
+    fun resetMocks() {
+        reset(backupInfoMapper)
+    }
+
+    @Test
+    fun `test that the mapping is correct`() {
+        val backupSize = 2L
+        val megaBackupInfoList = mock<MegaBackupInfoList> { on { size() }.thenReturn(backupSize) }
+        val backupInfoList = mutableListOf<BackupInfo>()
+
+        (0 until backupSize).forEach { index ->
+            val megaBackupInfo = mock<MegaBackupInfo> { on { id() }.thenReturn(index + 1000L) }
+            val backupInfo = mock<BackupInfo> { on { id }.thenReturn(index + 1000L) }
+            whenever(megaBackupInfoList.get(index)).thenReturn(megaBackupInfo)
+            whenever(backupInfoMapper(megaBackupInfo)).thenReturn(backupInfo)
+            backupInfoList.add(backupInfo)
+        }
+        assertThat(underTest(megaBackupInfoList)).isEqualTo(backupInfoList)
+    }
+
+    @ParameterizedTest(name = "backup size: {0}")
+    @ValueSource(longs = [0L, -1L])
+    fun `test that an empty backup info list is returned`(backupSize: Long) {
+        val megaBackupInfoList = mock<MegaBackupInfoList> { on { size() }.thenReturn(backupSize) }
+        assertThat(underTest(megaBackupInfoList)).isEmpty()
+    }
+
+    @Test
+    fun `test that an empty backup info list is returned if mega backup info list is null`() {
+        assertThat(underTest(null)).isEmpty()
+    }
+}
