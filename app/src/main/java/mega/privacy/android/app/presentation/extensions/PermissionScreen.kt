8dd94120cf689dc8ea55c4e87e11f64ba188eef8
Bhavna Thacker <bt@mega.co.nz>
2023-08-09 12:00:55 +0530

    AND-17152: Update Permission Message for Android 14+ users in Initial Permission Flow

--- app/src/main/java/mega/privacy/android/app/presentation/extensions/PermissionScreen.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/extensions/PermissionScreen.kt
@@ -1,12 +1,18 @@
 package mega.privacy.android.app.presentation.extensions
 
+import android.os.Build
 import mega.privacy.android.app.R
 import mega.privacy.android.app.presentation.permissions.model.PermissionScreen
 
 internal val PermissionScreen.title: Int
     get() = when (this) {
         PermissionScreen.Notifications -> R.string.permissions_notifications_title
-        PermissionScreen.Media -> R.string.allow_acces_media_title
+        PermissionScreen.Media -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            R.string.allow_access_audio_title
+        } else {
+            R.string.allow_acces_media_title
+        }
+
         PermissionScreen.Camera -> R.string.allow_acces_camera_title
         PermissionScreen.Calls -> R.string.allow_acces_calls_title
         PermissionScreen.Contacts -> R.string.allow_acces_contact_title
