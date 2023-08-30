70c9601410d5e4638f24b980adc3ce30384f3509
Erick Sumargo <es@mega.co.nz>
2023-08-30 12:16:56 +1200

    Pre Release v9.1 - upgrade SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20230823.122117-rel"
+extra["megaSdkVersion"] = "20230829.135641-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
