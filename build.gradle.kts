652fe1e94d99e85db882f1d026b747d6c52d3e0d
Ilya Konnov <iko@mega.co.nz>
2023-09-01 19:47:29 +0800

    FM-243 Update SDK to the latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20230828.142913-dev"
+extra["megaSdkVersion"] = "20230901.113309-dev"
 
 //JDK and Java Version
 extra["jdk"] = "17"
