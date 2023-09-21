41346bbe503817023890031778c0006b5afca249
Kevin Ham <kh@mega.co.nz>
2023-09-21 22:29:44 +1200

    CU-593: Integrate CameraUploadsRecordMapper inside RetrieveMediaFromMediaStoreUseCase

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/camerauploads/RetrieveMediaFromMediaStoreUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/camerauploads/RetrieveMediaFromMediaStoreUseCase.kt
@@ -1,8 +1,16 @@
 package mega.privacy.android.domain.usecase.camerauploads
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.yield
 import mega.privacy.android.domain.entity.MediaStoreFileType
+import mega.privacy.android.domain.entity.SyncRecordType
+import mega.privacy.android.domain.entity.camerauploads.CameraUploadFolderType
 import mega.privacy.android.domain.entity.camerauploads.CameraUploadsMedia
+import mega.privacy.android.domain.entity.camerauploads.CameraUploadsRecord
 import mega.privacy.android.domain.repository.CameraUploadRepository
+import mega.privacy.android.domain.usecase.camerauploads.mapper.CameraUploadsRecordMapper
 import javax.inject.Inject
 
 /**
@@ -10,6 +18,7 @@ import javax.inject.Inject
  */
 class RetrieveMediaFromMediaStoreUseCase @Inject constructor(
     private val cameraUploadRepository: CameraUploadRepository,
+    private val cameraUploadsRecordMapper: CameraUploadsRecordMapper,
 ) {
 
     /**
@@ -18,18 +27,33 @@ class RetrieveMediaFromMediaStoreUseCase @Inject constructor(
      * @param parentPath used for filtering the media contained in the parent path
      * @param types types of files that we want to retrieve. This types will be converted to proper Uri
      *
-     * @return a list of [CameraUploadsMedia]
+     * @return a list of [CameraUploadsRecord]
      */
     suspend operator fun invoke(
         parentPath: String,
         types: List<MediaStoreFileType>,
-    ): List<CameraUploadsMedia> {
+        folderType: CameraUploadFolderType,
+        fileType: SyncRecordType,
+        tempRoot: String,
+    ): List<CameraUploadsRecord> = coroutineScope {
         val selectionQuery = cameraUploadRepository.getMediaSelectionQuery(parentPath)
-        return types.flatMap {
+        return@coroutineScope types.flatMap {
             cameraUploadRepository.getMediaList(
                 mediaStoreFileType = it,
                 selectionQuery = selectionQuery,
-            )
+            ).map {
+                async {
+                    yield()
+                    runCatching {
+                        cameraUploadsRecordMapper(
+                            media = it,
+                            folderType = folderType,
+                            fileType = fileType,
+                            tempRoot = tempRoot,
+                        )
+                    }.getOrNull()
+                }
+            }.awaitAll().filterNotNull()
         }
     }
 }
--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/camerauploads/RetrieveMediaFromMediaStoreUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/camerauploads/RetrieveMediaFromMediaStoreUseCaseTest.kt
@@ -4,8 +4,12 @@ import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import mega.privacy.android.domain.entity.MediaStoreFileType
+import mega.privacy.android.domain.entity.SyncRecordType
+import mega.privacy.android.domain.entity.camerauploads.CameraUploadFolderType
 import mega.privacy.android.domain.entity.camerauploads.CameraUploadsMedia
+import mega.privacy.android.domain.entity.camerauploads.CameraUploadsRecord
 import mega.privacy.android.domain.repository.CameraUploadRepository
+import mega.privacy.android.domain.usecase.camerauploads.mapper.CameraUploadsRecordMapper
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -24,17 +28,22 @@ class RetrieveMediaFromMediaStoreUseCaseTest {
     private lateinit var underTest: RetrieveMediaFromMediaStoreUseCase
 
     private val cameraUploadRepository = mock<CameraUploadRepository>()
+    private val cameraUploadsRecordMapper = mock<CameraUploadsRecordMapper>()
 
     @BeforeAll
     fun setUp() {
         underTest = RetrieveMediaFromMediaStoreUseCase(
             cameraUploadRepository = cameraUploadRepository,
+            cameraUploadsRecordMapper = cameraUploadsRecordMapper,
         )
     }
 
     @BeforeEach
     fun resetMocks() {
-        reset(cameraUploadRepository)
+        reset(
+            cameraUploadRepository,
+            cameraUploadsRecordMapper,
+        )
     }
 
     @Test
@@ -44,11 +53,13 @@ class RetrieveMediaFromMediaStoreUseCaseTest {
             val mediaStoreFileType1 = mock<MediaStoreFileType>()
             val mediaStoreFileType2 = mock<MediaStoreFileType>()
             val types = listOf(mediaStoreFileType1, mediaStoreFileType2)
+            val folderType = mock<CameraUploadFolderType>()
+            val fileType = mock<SyncRecordType>()
+            val tempRoot = "tempRoot"
             val selectionQuery = "selectionQuery"
 
             val cameraUploadsMediaList1 = listOf<CameraUploadsMedia>(mock(), mock())
             val cameraUploadsMediaList2 = listOf<CameraUploadsMedia>(mock())
-            val expected = cameraUploadsMediaList1 + cameraUploadsMediaList2
             whenever(cameraUploadRepository.getMediaSelectionQuery(parentPath))
                 .thenReturn(selectionQuery)
             whenever(
@@ -63,6 +74,98 @@ class RetrieveMediaFromMediaStoreUseCaseTest {
                     selectionQuery
                 )
             ).thenReturn(cameraUploadsMediaList2)
-            assertThat(underTest(parentPath, types)).isEqualTo(expected)
+
+            val cameraUploadsRecordList1 = listOf<CameraUploadsRecord>(mock(), mock())
+            val cameraUploadsRecordList2 = listOf<CameraUploadsRecord>(mock())
+            cameraUploadsMediaList1.forEachIndexed { index, media ->
+                whenever(
+                    cameraUploadsRecordMapper(
+                        media,
+                        folderType,
+                        fileType,
+                        tempRoot
+                    )
+                ).thenReturn(
+                    cameraUploadsRecordList1[index]
+                )
+            }
+
+            cameraUploadsMediaList2.forEachIndexed { index, media ->
+                whenever(
+                    cameraUploadsRecordMapper(
+                        media,
+                        folderType,
+                        fileType,
+                        tempRoot
+                    )
+                ).thenReturn(
+                    cameraUploadsRecordList2[index]
+                )
+            }
+
+            val expected = cameraUploadsRecordList1 + cameraUploadsRecordList2
+
+            assertThat(underTest(parentPath, types, folderType, fileType, tempRoot))
+                .isEqualTo(expected)
+        }
+
+    @Test
+    fun `test that if an error is thrown when mapping, the record is omitted in the result returned`() =
+        runTest {
+            val parentPath = ""
+            val mediaStoreFileType1 = mock<MediaStoreFileType>()
+            val mediaStoreFileType2 = mock<MediaStoreFileType>()
+            val types = listOf(mediaStoreFileType1, mediaStoreFileType2)
+            val folderType = mock<CameraUploadFolderType>()
+            val fileType = mock<SyncRecordType>()
+            val tempRoot = "tempRoot"
+            val selectionQuery = "selectionQuery"
+
+            val cameraUploadsMediaList1 = listOf<CameraUploadsMedia>(mock(), mock())
+            val cameraUploadsMediaList2 = listOf<CameraUploadsMedia>(mock())
+            whenever(cameraUploadRepository.getMediaSelectionQuery(parentPath))
+                .thenReturn(selectionQuery)
+            whenever(
+                cameraUploadRepository.getMediaList(
+                    mediaStoreFileType1,
+                    selectionQuery
+                )
+            ).thenReturn(cameraUploadsMediaList1)
+            whenever(
+                cameraUploadRepository.getMediaList(
+                    mediaStoreFileType2,
+                    selectionQuery
+                )
+            ).thenReturn(cameraUploadsMediaList2)
+
+            val cameraUploadsRecordList1 = listOf<CameraUploadsRecord>(mock(), mock())
+            cameraUploadsMediaList1.forEachIndexed { index, media ->
+                whenever(
+                    cameraUploadsRecordMapper(
+                        media,
+                        folderType,
+                        fileType,
+                        tempRoot
+                    )
+                ).thenReturn(
+                    cameraUploadsRecordList1[index]
+                )
+            }
+
+            cameraUploadsMediaList2.forEachIndexed { _, media ->
+                whenever(
+                    cameraUploadsRecordMapper(
+                        media,
+                        folderType,
+                        fileType,
+                        tempRoot
+                    )
+                ).thenThrow(
+                    RuntimeException("error")
+                )
+            }
+
+            assertThat(underTest(parentPath, types, folderType, fileType, tempRoot))
+                .isEqualTo(cameraUploadsRecordList1)
         }
 }
