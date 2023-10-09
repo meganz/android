bce91bd604d7eaeea7e4bb9b398814f6882895b7
Robin Shi <rsh@mega.co.nz>
2023-10-09 18:02:28 +1300

    T12137121 Infinite loading when opening file link URL

--- app/src/main/java/mega/privacy/android/app/presentation/openlink/OpenLinkState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/openlink/OpenLinkState.kt
@@ -1,14 +1,10 @@
 package mega.privacy.android.app.presentation.openlink
 
-import mega.privacy.android.domain.entity.Feature
-
 /**
  * Open link state
  *
  * @property isLoggedOut checks if app is logged in or not
- * @property enabledFeatureFlags set of enabled feature flags
  */
 data class OpenLinkState(
     val isLoggedOut: Boolean = false,
-    val enabledFeatureFlags: Set<Feature>? = null
 )
