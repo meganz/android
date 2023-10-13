3a7917221f3a126408dfec11516425531321bc1f
Hai Luong <lh@mega.co.nz>
2023-10-13 13:36:45 +0700

    MEET-3050: Refactor CreateGroupChatUseCase to remove usages of RxJava

--- app/src/main/java/mega/privacy/android/app/contacts/group/ContactGroupsViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/contacts/group/ContactGroupsViewModel.kt
@@ -3,19 +3,20 @@ package mega.privacy.android.app.contacts.group
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.launch
 import mega.privacy.android.app.arch.BaseRxViewModel
 import mega.privacy.android.app.contacts.group.data.ContactGroupItem
-import mega.privacy.android.app.contacts.usecase.CreateGroupChatUseCase
 import mega.privacy.android.app.contacts.usecase.GetContactGroupsUseCase
 import mega.privacy.android.app.utils.notifyObserver
+import mega.privacy.android.domain.usecase.chat.CreateGroupChatRoomUseCase
 import nz.mega.sdk.MegaChatApiJava.MEGACHAT_INVALID_HANDLE
 import timber.log.Timber
-import java.util.*
 import javax.inject.Inject
 
 /**
@@ -26,7 +27,7 @@ import javax.inject.Inject
 @HiltViewModel
 class ContactGroupsViewModel @Inject constructor(
     getContactGroupsUseCase: GetContactGroupsUseCase,
-    private val getGroupChatRoomUseCase: CreateGroupChatUseCase
+    private val createGroupChatRoomUseCase: CreateGroupChatRoomUseCase,
 ) : BaseRxViewModel() {
 
     private val groups: MutableLiveData<List<ContactGroupItem>> = MutableLiveData()
@@ -69,22 +70,25 @@ class ContactGroupsViewModel @Inject constructor(
     fun getGroupChatRoom(
         contactsData: ArrayList<String>,
         chatTitle: String?,
-        allowAddParticipants: Boolean
+        allowAddParticipants: Boolean,
     ): LiveData<Long> {
         val result = MutableLiveData<Long>()
-        getGroupChatRoomUseCase.getGroupChatRoomCreated(contactsData, chatTitle, allowAddParticipants)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy(
-                onSuccess = { chatId ->
-                    result.value = chatId
-                },
-                onError = { error ->
-                    Timber.e(error)
-                    result.value = MEGACHAT_INVALID_HANDLE
-                }
-            )
-            .addTo(composite)
+        viewModelScope.launch {
+            runCatching {
+                createGroupChatRoomUseCase(
+                    contactsData,
+                    chatTitle,
+                    false,
+                    allowAddParticipants,
+                    false
+                )
+            }.onSuccess { chatId ->
+                result.value = chatId
+            }.onFailure {
+                Timber.e(it)
+                result.value = MEGACHAT_INVALID_HANDLE
+            }
+        }
 
         return result
     }
