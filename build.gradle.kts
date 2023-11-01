04b21378c4273b52a36c8d46d778252f9052d98a
Atiqur Rahman <atr@mega.co.nz>
2023-11-01 10:05:06 +0600

    update megaSDKVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20231024.112234-dev"
+extra["megaSdkVersion"] = "20231101.035944-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
