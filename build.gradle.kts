c487858347b3d33e8df2c442ebfe5b9e6965c31a
Nikhil Nagori <nna@mega.co.nz>
2023-11-10 00:24:18 +1300

    Release/10.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "10.0.1"
+extra["appVersion"] = "10.0.2"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
