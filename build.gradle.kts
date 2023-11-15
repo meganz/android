40f9a58932c4c8bba6be779131d88c65ae82b771
Kevin Gozali <kg@mega.co.nz>
2023-11-15 14:25:44 +0700

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "10.1"
+extra["appVersion"] = "11.0"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
