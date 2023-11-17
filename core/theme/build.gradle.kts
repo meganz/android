e0f343f1032eea37096a9272f44d2640bf3c2c45
Pau Dominkovics <pau@mega.co.nz>
2023-11-17 18:01:56 +0100

    AND-17741: Inject color tokens to components library

--- core/theme/build.gradle.kts
+++ core/theme/build.gradle.kts
@@ -5,21 +5,51 @@ plugins {
 }
 
 android {
-    namespace = "mega.privacy.android.core.theme"
+    namespace = "mega.privacy.android.core.ui.theme"
     val compileSdkVersion: Int by rootProject.extra
     compileSdk = compileSdkVersion
     val buildTools: String by rootProject.extra
     buildToolsVersion = buildTools
 
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = androidx.versions.compose.compiler.get()
+    }
+
     defaultConfig {
         val minSdkVersion: Int by rootProject.extra
         minSdk = minSdkVersion
+
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     compileOptions {
         val javaVersion: JavaVersion by rootProject.extra
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    kotlin {
+        val jdk: String by rootProject.extra
+        jvmToolchain(jdk.toInt())
+    }
+
+    kotlinOptions {
+        val jdk: String by rootProject.extra
+        jvmTarget = jdk
+        val shouldSuppressWarnings: Boolean by rootProject.extra
+        suppressWarnings = shouldSuppressWarnings
+        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
     }
 
     lint {
@@ -33,4 +63,8 @@ android {
         implementation(platform(androidx.compose.bom))
         implementation(androidx.bundles.compose.bom)
     }
+}
+
+tasks.withType<Test> {
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
 }
