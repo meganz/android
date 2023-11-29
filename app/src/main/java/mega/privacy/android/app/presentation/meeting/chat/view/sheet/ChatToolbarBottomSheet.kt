3eec9647f79162ad3c0d42417e7b1d770e773ea3
Yenel <yr@mega.nz>
2023-11-29 14:25:53 +0100

    AND-17430 Add "Gallery" action inside attach modal

--- app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/view/sheet/ChatToolbarBottomSheet.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/view/sheet/ChatToolbarBottomSheet.kt
@@ -1,8 +1,12 @@
 package mega.privacy.android.app.presentation.meeting.chat.view.sheet
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,11 +18,12 @@ import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import mega.privacy.android.app.R
 import mega.privacy.android.core.theme.tokens.MegaAppTheme
+import mega.privacy.android.core.ui.controls.chat.attachpanel.AttachItem
 import mega.privacy.android.core.ui.preview.CombinedThemePreviews
-
-const val TEST_TAG_GALLERY_LIST = "chat_gallery_list"
 
 /**
  * Chat toolbar bottom sheet
@@ -31,6 +36,11 @@ fun ChatToolbarBottomSheet(
     modifier: Modifier = Modifier,
     sheetState: ModalBottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden),
 ) {
+    val galleryPicker =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickMultipleVisualMedia()) {
+            //Manage gallery picked files here
+        }
+
     Column(modifier = modifier) {
         ChatGallery(
             modifier = Modifier
@@ -38,6 +48,18 @@ fun ChatToolbarBottomSheet(
                 .padding(4.dp),
             sheetState = sheetState
         )
+
+        Row(
+            modifier = Modifier.padding(24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            AttachItem(
+                iconId = R.drawable.ic_attach_from_gallery,
+                itemName = stringResource(id = R.string.chat_attach_panel_gallery),
+                onItemClick = { galleryPicker.launch(PickVisualMediaRequest()) },
+                modifier = Modifier.testTag(TEST_TAG_ATTACH_FROM_GALLERY)
+            )
+        }
     }
 }
 
@@ -51,14 +73,12 @@ fun ChatToolbarBottomSheet(
 fun ChatGallery(
     sheetState: ModalBottomSheetState,
     modifier: Modifier = Modifier,
+) = LazyRow(
+    modifier = modifier.testTag(TEST_TAG_GALLERY_LIST),
+    horizontalArrangement = Arrangement.spacedBy(4.dp)
 ) {
-    LazyRow(
-        modifier = modifier.testTag(TEST_TAG_GALLERY_LIST),
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        item("camera_button") {
-            ChatCameraButton(modifier = Modifier.size(88.dp), sheetState = sheetState)
-        }
+    item("camera_button") {
+        ChatCameraButton(modifier = Modifier.size(88.dp), sheetState = sheetState)
     }
 }
 
@@ -70,3 +90,6 @@ private fun ChatToolbarBottomSheetPreview() {
         ChatToolbarBottomSheet(sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded))
     }
 }
+
+internal const val TEST_TAG_GALLERY_LIST = "chat_gallery_list"
+internal const val TEST_TAG_ATTACH_FROM_GALLERY = "chat_view:attach_panel:attach_from_gallery"
--- app/src/test/java/test/mega/privacy/android/app/presentation/meeting/chat/view/sheet/ChatToolbarBottomSheetTest.kt
+++ app/src/test/java/test/mega/privacy/android/app/presentation/meeting/chat/view/sheet/ChatToolbarBottomSheetTest.kt
@@ -3,10 +3,12 @@ package test.mega.privacy.android.app.presentation.meeting.chat.view.sheet
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mega.privacy.android.app.presentation.meeting.chat.view.sheet.ChatToolbarBottomSheet
+import mega.privacy.android.app.presentation.meeting.chat.view.sheet.TEST_TAG_ATTACH_FROM_GALLERY
 import mega.privacy.android.app.presentation.meeting.chat.view.sheet.TEST_TAG_GALLERY_LIST
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +24,12 @@ class ChatToolbarBottomSheetTest {
     fun `test that gallery list shows`() {
         initComposeRuleContent()
         composeTestRule.onNodeWithTag(TEST_TAG_GALLERY_LIST).assertExists()
+    }
+
+    @Test
+    fun `test that gallery list is shown`() {
+        initComposeRuleContent()
+        composeTestRule.onNodeWithTag(TEST_TAG_ATTACH_FROM_GALLERY).assertIsDisplayed()
     }
 
     private fun initComposeRuleContent() {
