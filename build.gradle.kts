bebb410f7b9af7c0dc392990999b5f93f68dd27e
Sougandh Mp <smp@mega.co.nz>
2023-11-30 08:38:06 +0530

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "11.0"
+extra["appVersion"] = "11.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
