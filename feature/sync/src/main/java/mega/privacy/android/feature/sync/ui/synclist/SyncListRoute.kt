be0192d33530f9c5e1c8df7619b90c5811187e1d
Ilya Konnov <iko@mega.co.nz>
2023-12-06 17:00:32 +0800

    FM-1555 Preserve Sync Options Dialog on orientation changes

--- feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/synclist/SyncListRoute.kt
+++ feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/synclist/SyncListRoute.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -23,7 +24,7 @@ internal fun SyncListRoute(
 
     val snackBarHostState = remember { SnackbarHostState() }
 
-    var showSyncOptionsDialog by remember { mutableStateOf(false) }
+    var showSyncOptionsDialog by rememberSaveable { mutableStateOf(false) }
 
     val message = state.snackbarMessage?.let {
         stringResource(id = it)
