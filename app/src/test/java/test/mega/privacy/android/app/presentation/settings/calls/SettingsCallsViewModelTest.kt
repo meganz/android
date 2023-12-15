0d103cebce32970d46562f95be87a1634bb11133
Raquel Garcia Chico <rgc@mega.nz>
2023-12-15 21:14:20 +1300

    AND-17860 Fix unit tests in SettingsCallsViewModelTest

--- app/src/test/java/test/mega/privacy/android/app/presentation/settings/calls/SettingsCallsViewModelTest.kt
+++ app/src/test/java/test/mega/privacy/android/app/presentation/settings/calls/SettingsCallsViewModelTest.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import mega.privacy.android.app.featuretoggle.AppFeatures
 import mega.privacy.android.app.presentation.settings.calls.SettingsCallsViewModel
 import mega.privacy.android.domain.entity.CallsMeetingInvitations
 import mega.privacy.android.domain.entity.CallsMeetingReminders
@@ -28,11 +29,14 @@ import mega.privacy.android.domain.usecase.meeting.SetCallsSoundNotifications
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.TestInstance
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SettingsCallsViewModelTest {
 
     private lateinit var underTest: SettingsCallsViewModel
@@ -58,6 +62,39 @@ class SettingsCallsViewModelTest {
     @Before
     fun setUp() {
         Dispatchers.setMain(standardDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    private suspend fun stubCommon() {
+        reset(
+            getCallsSoundNotifications,
+            setCallsSoundNotifications,
+            getCallsMeetingInvitations,
+            setCallsMeetingInvitations,
+            getCallsMeetingReminders,
+            setCallsMeetingReminders,
+            sendStatisticsMeetings,
+            getFeatureFlagValue,
+        )
+
+        whenever(getFeatureFlagValue(AppFeatures.MeetingNotificationSettings)).thenReturn(
+            true
+        )
+        whenever(getCallsSoundNotifications()).thenReturn(emptyFlow())
+        whenever(getCallsMeetingInvitations()).thenReturn(emptyFlow())
+        whenever(getCallsMeetingReminders()).thenReturn(emptyFlow())
+
+    }
+
+    /**
+     * Initializes [SettingsCallsViewModel] for testing
+     */
+    private suspend fun setupUnderTest() {
+        stubCommon()
         underTest = SettingsCallsViewModel(
             getCallsSoundNotifications = getCallsSoundNotifications,
             setCallsSoundNotifications = setCallsSoundNotifications,
@@ -70,13 +107,9 @@ class SettingsCallsViewModelTest {
         )
     }
 
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
-
     @Test
     fun `test that initial state is returned`() = runTest {
+        setupUnderTest()
         underTest.uiState.test {
             val initial = awaitItem()
             Truth.assertThat(initial.soundNotifications).isNull()
@@ -88,10 +121,12 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that the option returned by getCallsSoundNotifications is set the status of call notification sounds`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsSoundNotifications()).thenReturn(flowOf(CallsSoundNotifications.Enabled))
 
             underTest.uiState.map { it.soundNotifications }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
                 Truth.assertThat(awaitItem()).isEqualTo(CallsSoundNotifications.Enabled)
             }
         }
@@ -99,16 +134,19 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that status of call notification sounds is updated when a new value is emitted`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsSoundNotifications()).thenReturn(
                 flowOf(
-                    CallsSoundNotifications.Enabled,
-                    CallsSoundNotifications.Disabled
+                    CallsSoundNotifications.Enabled, CallsSoundNotifications.Disabled
                 )
             )
 
             underTest.uiState.map { it.soundNotifications }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
+                underTest.setNewCallsSoundNotifications(true)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsSoundNotifications.Enabled)
+                underTest.setNewCallsSoundNotifications(false)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsSoundNotifications.Disabled)
             }
         }
@@ -116,6 +154,7 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that setNewCallsSoundNotifications calls the set use case with the correct value`() =
         runTest {
+            setupUnderTest()
             underTest.setNewCallsSoundNotifications(false)
             scheduler.advanceUntilIdle()
             verify(setCallsSoundNotifications).invoke(CallsSoundNotifications.Disabled)
@@ -124,10 +163,12 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that the option returned by getCallsMeetingInvitations is set as the status of calls meeting invitations`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsMeetingInvitations()).thenReturn(flowOf(CallsMeetingInvitations.Enabled))
 
             underTest.uiState.map { it.callsMeetingInvitations }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingInvitations.Enabled)
             }
         }
@@ -135,16 +176,19 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that status of calls meeting invitations is updated when a new value is emitted`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsMeetingInvitations()).thenReturn(
                 flowOf(
-                    CallsMeetingInvitations.Enabled,
-                    CallsMeetingInvitations.Disabled
+                    CallsMeetingInvitations.Enabled, CallsMeetingInvitations.Disabled
                 )
             )
 
             underTest.uiState.map { it.callsMeetingInvitations }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
+                underTest.setNewCallsMeetingInvitations(true)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingInvitations.Enabled)
+                underTest.setNewCallsMeetingInvitations(false)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingInvitations.Disabled)
             }
         }
@@ -152,6 +196,7 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that setNewCallsMeetingInvitations calls the set use case with the correct value`() =
         runTest {
+            setupUnderTest()
             underTest.setNewCallsMeetingInvitations(false)
             scheduler.advanceUntilIdle()
             verify(setCallsMeetingInvitations).invoke(CallsMeetingInvitations.Disabled)
@@ -160,10 +205,12 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that the option returned by getCallsMeetingReminders is set as the status of calls meeting reminders`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsMeetingReminders()).thenReturn(flowOf(CallsMeetingReminders.Enabled))
 
             underTest.uiState.map { it.callsMeetingReminders }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingReminders.Enabled)
             }
         }
@@ -171,16 +218,19 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that status of calls meeting reminders is updated when a new value is emitted`() =
         runTest {
+            setupUnderTest()
             whenever(getCallsMeetingReminders()).thenReturn(
                 flowOf(
-                    CallsMeetingReminders.Enabled,
-                    CallsMeetingReminders.Disabled
+                    CallsMeetingReminders.Enabled, CallsMeetingReminders.Disabled
                 )
             )
 
             underTest.uiState.map { it.callsMeetingReminders }.distinctUntilChanged().test {
                 Truth.assertThat(awaitItem()).isNull()
+                scheduler.advanceUntilIdle()
+                underTest.setNewCallsMeetingReminders(true)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingReminders.Enabled)
+                underTest.setNewCallsMeetingReminders(false)
                 Truth.assertThat(awaitItem()).isEqualTo(CallsMeetingReminders.Disabled)
             }
         }
@@ -188,6 +238,7 @@ class SettingsCallsViewModelTest {
     @Test
     fun `test that setNewCallsMeetingReminders calls the set use case with the correct value`() =
         runTest {
+            setupUnderTest()
             underTest.setNewCallsMeetingReminders(false)
             scheduler.advanceUntilIdle()
             verify(setCallsMeetingReminders).invoke(CallsMeetingReminders.Disabled)
