94d6fba5dd4238a0561709281b2b448e7c074a18
Robin Shi <rsh@mega.co.nz>
2023-12-20 20:32:52 +1300

    AND-17674 Create voice clip message compose view

---
+++ core-ui/src/main/java/mega/privacy/android/core/ui/controls/chat/messages/VoiceClipMessageView.kt
@@ -0,0 +1,278 @@
+package mega.privacy.android.core.ui.controls.chat.messages
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import mega.privacy.android.core.R
+import mega.privacy.android.core.ui.controls.progressindicator.MegaLinearProgressIndicator
+import mega.privacy.android.core.ui.controls.text.MegaText
+import mega.privacy.android.core.ui.preview.BooleanProvider
+import mega.privacy.android.core.ui.preview.CombinedThemePreviews
+import mega.privacy.android.core.ui.theme.AndroidTheme
+import mega.privacy.android.core.ui.theme.MegaTheme
+import mega.privacy.android.core.ui.theme.extensions.body4
+import mega.privacy.android.core.ui.theme.tokens.TextColor
+
+internal const val VOICE_CLIP_MESSAGE_VIEW_PLAY_BUTTON_TEST_TAG =
+    "chat_voice_clip_message_view:play_button"
+internal const val VOICE_CLIP_MESSAGE_VIEW_LOAD_PROGRESS_INDICATOR_TEST_TAG =
+    "chat_voice_clip_message_view:load_progress_indicator"
+
+
+/**
+ * Compose view for voice clip message
+ *
+ * @param isMe whether message is sent from me
+ * @param timestamp timestamp of the voice clip. Show total duration of the voice clip when pause;
+ *                  show elapsed time when playing.
+ * @param modifier modifier
+ * @param loadProgress loading progress of the message. null if already loaded. The value can be 0-100.
+ * @param playProgress playing progress of the audio. null if not playing. The value can be 0-100.
+ * @param isPlaying Whether voice clip is playing. Default is false.
+ * @param onPlayClicked Callback when play button is clicked.
+ */
+@Composable
+fun VoiceClipMessageView(
+    isMe: Boolean,
+    timestamp: String,
+    modifier: Modifier = Modifier,
+    loadProgress: Int? = null,
+    playProgress: Int? = null,
+    isPlaying: Boolean = false,
+    onPlayClicked: () -> Unit = {},
+) {
+    ChatBubble(isMe = isMe, modifier = modifier.size(width = 209.dp, height = 56.dp)) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(12.dp)),
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                PlayButton(isPlaying = isPlaying, onPlayClicked = onPlayClicked)
+                PlayProgress(
+                    isMe = isMe,
+                    progress = playProgress,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                TimestampText(timestamp = timestamp, isMe = isMe)
+            }
+            LoadOverlay(loadProgress)
+            LoadProgress(loadProgress = loadProgress)
+        }
+    }
+}
+
+@Composable
+private fun LoadProgress(loadProgress: Int?) {
+    loadProgress?.let {
+        MegaLinearProgressIndicator(
+            progress = loadProgress / 100f,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .border(width = 0.dp, color = Color.Transparent)
+                .testTag(VOICE_CLIP_MESSAGE_VIEW_LOAD_PROGRESS_INDICATOR_TEST_TAG),
+        )
+    }
+}
+
+/**
+ * Show the play progress
+ *
+ * @param isMe whether message is sent from me
+ * @param progress loading progress of the audio. null if already loaded. The value can be 0-100.
+ */
+@Composable
+private fun PlayProgress(
+    isMe: Boolean,
+    progress: Int?,
+    modifier: Modifier = Modifier,
+) {
+    // @formatter:off
+    val heightList = listOf(12, 12, 18, 14, 8, 12, 16, 20, 20, 24, 8, 32, 14, 14, 14,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12)
+    // @formatter:on
+
+    Row(
+        modifier = modifier.size(width = 99.dp, height = 40.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        val dividerPos = progress?.let { (it.toFloat() / 100f * heightList.size).toInt() } ?: 0
+        val color =
+            if (isMe) MegaTheme.colors.icon.inverse else MegaTheme.colors.text.onColorDisabled
+
+        heightList.forEachIndexed { index, height ->
+            val alpha = if (index < dividerPos) 1f else 0.5f
+            Box(
+                modifier = Modifier
+                    .size(width = 2.dp, height = height.dp)
+                    .background(
+                        color = color.copy(alpha = alpha),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            )
+        }
+    }
+}
+
+@Composable
+private fun TimestampText(isMe: Boolean, timestamp: String) {
+    MegaText(
+        modifier = Modifier.padding(start = 8.dp),
+        text = timestamp,
+        textColor = if (isMe) TextColor.Inverse else TextColor.Secondary,
+        style = MaterialTheme.typography.body4,
+    )
+}
+
+@Composable
+private fun PlayButton(
+    isPlaying: Boolean,
+    modifier: Modifier = Modifier,
+    onPlayClicked: () -> Unit = {},
+) {
+    Box(
+        modifier = modifier
+            .size(40.dp)
+            .background(
+                color = MegaTheme.colors.background.blur,
+                shape = CircleShape
+            )
+            .clickable { onPlayClicked() }
+            .testTag(VOICE_CLIP_MESSAGE_VIEW_PLAY_BUTTON_TEST_TAG),
+        contentAlignment = Alignment.Center,
+    ) {
+        val iconId =
+            if (isPlaying) R.drawable.ic_pause_voice_clip else R.drawable.ic_play_voice_clip
+        Image(
+            imageVector = ImageVector.vectorResource(iconId),
+            contentDescription = "Play voice clip"
+        )
+    }
+}
+
+/**
+ * Show an overlay when voice clip is loading.
+ *
+ * @param loadProgress loading progress of the message. null if already loaded. The value can be 0-100.
+ */
+@Composable
+private fun LoadOverlay(loadProgress: Int?) {
+    loadProgress?.let {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = if (isSystemInDarkTheme()) Color.Black.copy(alpha = 0.5f)
+                    else Color.White.copy(alpha = 0.5f),
+                )
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun VoiceClipMessageViewPreview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        VoiceClipMessageView(
+            isMe = isMe,
+            timestamp = "00:49",
+            loadProgress = null,
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun LoadingVoiceClipMessageViewPreview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        VoiceClipMessageView(
+            isMe = isMe,
+            timestamp = "00:49",
+            loadProgress = 50,
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun VoiceClipMessageViewPlaying20Preview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        VoiceClipMessageView(
+            isMe = isMe,
+            timestamp = "00:49",
+            loadProgress = null,
+            isPlaying = true,
+            playProgress = 20,
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun VoiceClipMessageViewPlaying50Preview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        VoiceClipMessageView(
+            isMe = isMe,
+            timestamp = "00:49",
+            loadProgress = null,
+            isPlaying = true,
+            playProgress = 50,
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun VoiceClipMessageViewPlaying80Preview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        VoiceClipMessageView(
+            isMe = isMe,
+            timestamp = "00:49",
+            loadProgress = null,
+            isPlaying = true,
+            playProgress = 80,
+        )
+    }
+}
+
---
+++ core-ui/src/test/java/mega/privacy/android/core/ui/controls/chat/messages/VoiceClipMessageViewTest.kt
@@ -0,0 +1,83 @@
+package mega.privacy.android.core.ui.controls.chat.messages
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
+
+@RunWith(AndroidJUnit4::class)
+class VoiceClipMessageViewTest {
+    @get:Rule
+    var composeRule = createComposeRule()
+
+    @Test
+    fun `test that duration shows correctly`() {
+        val duration = "00:49"
+        initComposeRuleContent(
+            progress = null,
+            isPlaying = false,
+            onPlayClicked = {},
+        )
+        composeRule.onNodeWithText(duration).assertExists()
+    }
+
+    @Test
+    fun `test that progress indicator is visible when it is loading`() {
+        initComposeRuleContent(
+            progress = 50,
+            isPlaying = false,
+            onPlayClicked = {},
+        )
+        composeRule.onNodeWithTag(VOICE_CLIP_MESSAGE_VIEW_LOAD_PROGRESS_INDICATOR_TEST_TAG)
+            .assertExists()
+    }
+
+    @Test
+    fun `test that loading progress indicator does not exist when it is not loading`() {
+        initComposeRuleContent(
+            progress = null,
+            isPlaying = false,
+            onPlayClicked = {},
+        )
+        composeRule.onNodeWithTag(VOICE_CLIP_MESSAGE_VIEW_LOAD_PROGRESS_INDICATOR_TEST_TAG)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun `test that play button click is handled correct`() {
+        val onClickMock = mock<() -> Unit>()
+        initComposeRuleContent(
+            progress = null,
+            isPlaying = false,
+            onPlayClicked = onClickMock,
+        )
+        composeRule.onNodeWithTag(VOICE_CLIP_MESSAGE_VIEW_PLAY_BUTTON_TEST_TAG).performClick()
+        verify(onClickMock).invoke()
+    }
+
+
+    private fun initComposeRuleContent(
+        progress: Int?,
+        isPlaying: Boolean,
+        onPlayClicked: () -> Unit,
+    ) {
+        composeRule.setContent {
+            VoiceClipMessageView(
+                isMe = true,
+                timestamp = "00:49",
+                modifier = Modifier,
+                loadProgress = progress,
+                isPlaying = isPlaying,
+                onPlayClicked = onPlayClicked,
+            )
+        }
+    }
+}
