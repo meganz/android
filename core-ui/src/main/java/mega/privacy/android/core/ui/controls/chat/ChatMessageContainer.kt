551c1c710c885091de823cc15142e034feacaf41
Hai Luong <lh@mega.co.nz>
2023-12-26 07:15:50 +0700

    AND-17796: Create "Error" message compose view

--- core-ui/src/main/java/mega/privacy/android/core/ui/controls/chat/ChatMessageContainer.kt
+++ core-ui/src/main/java/mega/privacy/android/core/ui/controls/chat/ChatMessageContainer.kt
@@ -1,5 +1,6 @@
 package mega.privacy.android.core.ui.controls.chat
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,17 +15,21 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import mega.privacy.android.core.R
 import mega.privacy.android.core.ui.controls.chat.messages.ChatBubble
+import mega.privacy.android.core.ui.controls.text.MegaText
 import mega.privacy.android.core.ui.preview.BooleanProvider
 import mega.privacy.android.core.ui.preview.CombinedThemePreviews
 import mega.privacy.android.core.ui.theme.AndroidTheme
 import mega.privacy.android.core.ui.theme.MegaTheme
 import mega.privacy.android.core.ui.theme.extensions.body4
+import mega.privacy.android.core.ui.theme.tokens.TextColor
 
 internal const val TEST_TAG_FORWARD_ICON = "chat_message_container:forward_icon"
 
@@ -45,6 +50,8 @@ fun ChatMessageContainer(
     modifier: Modifier = Modifier,
     time: String? = null,
     date: String? = null,
+    isSendError: Boolean = false,
+    onSendErrorClick: () -> Unit = {},
     avatarOrIcon: @Composable (RowScope.() -> Unit)? = null,
     content: @Composable RowScope.() -> Unit,
 ) {
@@ -73,11 +80,12 @@ fun ChatMessageContainer(
         }
         Row(
             modifier = Modifier
-                .padding(if (isMine) 48.dp else 16.dp, end = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                .padding(if (isMine) 48.dp else 16.dp, end = 16.dp)
+                .alpha(if (isSendError) 0.5f else 1f),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             if (isMine) {
-                if (showForwardIcon) {
+                if (showForwardIcon && !isSendError) {
                     ForwardIcon()
                 }
                 content()
@@ -88,6 +96,16 @@ fun ChatMessageContainer(
                     ForwardIcon()
                 }
             }
+        }
+        if (isSendError) {
+            MegaText(
+                modifier = Modifier.padding(top = 2.dp)
+                    .padding(start = 48.dp, end = 16.dp)
+                    .clickable(enabled = true, onClick = onSendErrorClick),
+                text = stringResource(id = R.string.manual_retry_alert),
+                style = MaterialTheme.typography.body4,
+                textColor = TextColor.Error
+            )
         }
     }
 }
@@ -126,9 +144,44 @@ private fun TextMessageContainerPreview(
             },
             time = "12:00",
             date = "Today",
-            showForwardIcon = isMe,
+            showForwardIcon = true,
             content = {
-                ChatBubble(isMe = isMe) {
+                ChatBubble(isMe = isMe, modifier = Modifier.weight(1f)) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
+                        text = "is a caldera in the Sunda Strait between the islands of Java and Sumatra in the Indonesian province of..."
+                    )
+                }
+            },
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun TextMessageContainerSendErrorPreview(
+    @PreviewParameter(BooleanProvider::class) isMe: Boolean,
+) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        ChatMessageContainer(
+            modifier = Modifier,
+            isMine = isMe,
+            avatarOrIcon = {
+                Icon(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .align(Alignment.Bottom),
+                    painter = painterResource(id = R.drawable.ic_emoji_smile),
+                    contentDescription = "Avatar",
+                    tint = MegaTheme.colors.icon.secondary
+                )
+            },
+            time = "12:00",
+            date = "Today",
+            showForwardIcon = true,
+            isSendError = isMe,
+            content = {
+                ChatBubble(isMe = isMe, modifier = Modifier.weight(1f)) {
                     Text(
                         modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                         text = "is a caldera in the Sunda Strait between the islands of Java and Sumatra in the Indonesian province of..."
--- core-ui/src/test/java/mega/privacy/android/core/ui/controls/chat/ChatMessageContainerTest.kt
+++ core-ui/src/test/java/mega/privacy/android/core/ui/controls/chat/ChatMessageContainerTest.kt
@@ -1,9 +1,11 @@
 package mega.privacy.android.core.ui.controls.chat
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import mega.privacy.android.core.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,7 +13,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ChatMessageContainerTest {
     @get:Rule
-    var composeRule = createComposeRule()
+    var composeRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
     fun `test that forward icon show correctly`() {
@@ -44,16 +46,44 @@ class ChatMessageContainerTest {
         composeRule.onNodeWithText(time).assertExists()
     }
 
+    @Test
+    fun `test that error text shows correctly when send error`() {
+        initComposeRuleContent(
+            isMine = false,
+            showForwardIcon = false,
+            time = null,
+            isSendError = true,
+        )
+        composeRule.onNodeWithText(
+            composeRule.activity.getString(R.string.manual_retry_alert)
+        ).assertExists()
+    }
+
+    @Test
+    fun `test that error text does not show when send successfully`() {
+        initComposeRuleContent(
+            isMine = false,
+            showForwardIcon = false,
+            time = null,
+            isSendError = false,
+        )
+        composeRule.onNodeWithText(
+            composeRule.activity.getString(R.string.manual_retry_alert)
+        ).assertDoesNotExist()
+    }
+
     private fun initComposeRuleContent(
         isMine: Boolean,
         showForwardIcon: Boolean,
         time: String?,
+        isSendError: Boolean = false,
     ) {
         composeRule.setContent {
             ChatMessageContainer(
                 isMine = isMine,
                 showForwardIcon = showForwardIcon,
                 time = time,
+                isSendError = isSendError,
                 avatarOrIcon = {},
                 content = {},
             )
