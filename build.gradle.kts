d89767fd5e449bdea3ad06c65d838ee3742764fd
Kevin Sun <ks@mega.co.nz>
2024-01-11 06:31:57 +0800

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "11.3"
+extra["appVersion"] = "<11.4>"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
