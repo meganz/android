18b9aa4d97952a50e1c85ef66ea5cbc25ad562e3
Kevin Sun <ks@mega.co.nz>
2024-01-11 07:20:26 +0800

    Update the megaSdkVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20240105.141212-dev"
+extra["megaSdkVersion"] = "20240110.231623-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
