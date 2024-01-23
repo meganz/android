9b465b1aab5cf7f67105f73e241cc85ca1bfb8aa
Amr Mohsen <amoh@mega.co.nz>
2024-01-23 15:49:43 +1300

    AP-1012: Add GetCookieSettingsUseCase into FetchAdDetailUseCase to to update Ads banner url

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/advertisements/FetchAdDetailUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/advertisements/FetchAdDetailUseCase.kt
@@ -2,7 +2,9 @@ package mega.privacy.android.domain.usecase.advertisements
 
 import mega.privacy.android.domain.entity.advertisements.AdDetails
 import mega.privacy.android.domain.entity.advertisements.FetchAdDetailRequest
+import mega.privacy.android.domain.entity.settings.cookie.CookieType
 import mega.privacy.android.domain.repository.AdsRepository
+import mega.privacy.android.domain.usecase.setting.GetCookieSettingsUseCase
 import javax.inject.Inject
 
 /**
@@ -10,20 +12,40 @@ import javax.inject.Inject
  */
 class FetchAdDetailUseCase @Inject constructor(
     private val adsRepository: AdsRepository,
+    private val getCookieSettingsUseCase: GetCookieSettingsUseCase,
 ) {
     /**
      * Invoke
      * return first AdDetail object from the list (should be the only one in the list)
      * @param fetchAdDetailRequest [FetchAdDetailRequest]
-     * @return [AdDetail]
+     * @return [AdDetails]
      */
     suspend operator fun invoke(
         fetchAdDetailRequest: FetchAdDetailRequest,
-    ): AdDetails? =
-        adsRepository.fetchAdDetails(
+    ): AdDetails? {
+
+        val adDetails = adsRepository.fetchAdDetails(
             listOf(fetchAdDetailRequest.slotId),
             fetchAdDetailRequest.linkHandle
-        ).firstOrNull { adDetail ->
-            adDetail.slotId == fetchAdDetailRequest.slotId
+        ).firstOrNull { adDetails ->
+            adDetails.slotId == fetchAdDetailRequest.slotId
         }
+
+        if (adDetails == null) {
+            return null
+        } else {
+            val isAdsCookieEnabled =
+                getCookieSettingsUseCase().containsAll(
+                    listOf(
+                        CookieType.ADS_CHECK,
+                        CookieType.ADVERTISEMENT
+                    )
+                )
+            return if (isAdsCookieEnabled) {
+                adDetails.copy(slotId = adDetails.slotId, url = "${adDetails.url}&ads=1")
+            } else {
+                adDetails
+            }
+        }
+    }
 }
--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/advertisements/FetchAdDetailsUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/advertisements/FetchAdDetailsUseCaseTest.kt
@@ -5,40 +5,84 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import mega.privacy.android.domain.entity.advertisements.AdDetails
 import mega.privacy.android.domain.entity.advertisements.FetchAdDetailRequest
+import mega.privacy.android.domain.entity.settings.cookie.CookieType
 import mega.privacy.android.domain.repository.AdsRepository
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
+import mega.privacy.android.domain.usecase.setting.GetCookieSettingsUseCase
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
 import org.mockito.kotlin.whenever
+import java.util.stream.Stream
 
 @ExperimentalCoroutinesApi
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class FetchAdDetailsUseCaseTest {
+class FetchAdDetailUseCaseTest {
     private lateinit var underTest: FetchAdDetailUseCase
     private val adsRepository = mock<AdsRepository>()
+    private val getCookieSettingsUseCase = mock<GetCookieSettingsUseCase>()
 
-    @BeforeAll
-    fun setUp() {
+    private val slotId = "ANDFB"
+    private val adBaseUrl = "https://megaad.nz/#z_xyz"
+    private val adsCookieEnabledPostfix = "&ads=1"
+    private val fetchDetailsRequest = FetchAdDetailRequest(slotId, null)
+    private fun initTestClass() {
         underTest = FetchAdDetailUseCase(
             adsRepository = adsRepository,
+            getCookieSettingsUseCase = getCookieSettingsUseCase
         )
     }
 
-    private val slotId = "ANDFB"
-    private val url = "https://megaad.nz/#z_xyz"
-    private val adDetailsList = listOf(AdDetails(slotId, url))
-    private val fetchAdDetailRequest = FetchAdDetailRequest(slotId, null)
-    private val expectedResult = AdDetails(slotId, url)
+    @AfterEach
+    fun resetMocks() {
+        reset(
+            getCookieSettingsUseCase,
+        )
+    }
 
-    @Test
-    fun `test that ad details are fetched correctly`() {
-        runTest {
-            whenever(adsRepository.fetchAdDetails(listOf(slotId), null)).thenReturn(
-                adDetailsList
+    private fun provideFetchAdDetailUseCaseParameters() = Stream.of(
+        Arguments.of(
+            AdDetails(slotId, adBaseUrl),
+            setOf(CookieType.ADS_CHECK, CookieType.ADVERTISEMENT),
+            AdDetails(slotId, "$adBaseUrl$adsCookieEnabledPostfix"),
+        ),
+        Arguments.of(
+            AdDetails(slotId, adBaseUrl),
+            emptySet<CookieType>(),
+            AdDetails(slotId, adBaseUrl),
+        ),
+        Arguments.of(
+            AdDetails(slotId, adBaseUrl),
+            setOf(CookieType.ADS_CHECK),
+            AdDetails(slotId, adBaseUrl),
+        ),
+        Arguments.of(
+            AdDetails(slotId, adBaseUrl),
+            setOf(CookieType.ADVERTISEMENT),
+            AdDetails(slotId, adBaseUrl),
+        )
+    )
+
+    @ParameterizedTest(name = "when ads details is: {0} cookie settings are: {1} and received adDetails is: {2} then expectedAdDetails is: {3}")
+    @MethodSource("provideFetchAdDetailUseCaseParameters")
+    fun `test that when ads details is fetched successfully and cookie settings are returned then ads details is updated accordingly`(
+        receivedAdDetails: AdDetails,
+        cookieSettings: Set<CookieType>,
+        expectedAdDetails: AdDetails,
+    ) = runTest {
+        whenever(getCookieSettingsUseCase()).thenReturn(cookieSettings)
+        whenever(
+            adsRepository.fetchAdDetails(
+                listOf(fetchDetailsRequest.slotId),
+                fetchDetailsRequest.linkHandle
             )
-            val actual = underTest.invoke(fetchAdDetailRequest)
-            assertThat(actual).isEqualTo(expectedResult)
-        }
+        ).thenReturn(listOf(receivedAdDetails))
+        initTestClass()
+
+        val result = underTest.invoke(fetchDetailsRequest)
+        assertThat(result?.url).isEqualTo(expectedAdDetails.url)
     }
 }
