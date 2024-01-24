21a8dd8da80dfdf1dfed58ad1734a2c76fadac6f
Kevin Sun <ks@mega.co.nz>
2024-01-24 09:53:09 +0800

    Increase the version to 11.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "11.4"
+extra["appVersion"] = "11.4.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
