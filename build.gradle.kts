2f1c0f537fcd3d3c23c5a86d6639ec6db33a4b7a
Raquel Garcia Chico <rgc@mega.nz>
2024-01-25 01:21:17 +1300

    Pre-release  v11.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "11.4"
+extra["appVersion"] = "11.5"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
@@ -81,7 +81,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "34.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20240119.113835-dev"
+extra["megaSdkVersion"] = "20240124.120149-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
