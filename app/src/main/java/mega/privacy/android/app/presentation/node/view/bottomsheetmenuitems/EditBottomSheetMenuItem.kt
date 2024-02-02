3d06eb12ceaf4b861d5c830aac9908fcb1dd0278
Rohit Soni <rso@mega.co.nz>
2024-02-02 23:46:10 +1300

    FM-1838 Handle click of edit

--- app/src/main/java/mega/privacy/android/app/presentation/node/view/bottomsheetmenuitems/EditBottomSheetMenuItem.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/node/view/bottomsheetmenuitems/EditBottomSheetMenuItem.kt
@@ -1,7 +1,14 @@
 package mega.privacy.android.app.presentation.node.view.bottomsheetmenuitems
 
+import android.content.Intent
+import androidx.navigation.NavHostController
 import mega.privacy.android.app.MimeTypeList
 import mega.privacy.android.app.presentation.node.model.menuaction.EditMenuAction
+import mega.privacy.android.app.textEditor.TextEditorActivity
+import mega.privacy.android.app.textEditor.TextEditorViewModel
+import mega.privacy.android.app.utils.Constants
+import mega.privacy.android.app.utils.Constants.SEARCH_ADAPTER
+import mega.privacy.android.core.ui.model.MenuAction
 import mega.privacy.android.core.ui.model.MenuActionWithIcon
 import mega.privacy.android.domain.entity.node.FileNode
 import mega.privacy.android.domain.entity.node.TypedNode
@@ -33,4 +40,20 @@ class EditBottomSheetMenuItem @Inject constructor(
             && accessPermission in listOf(OWNER, READWRITE, FULL)
 
     override val groupId = 1
+
+    override fun getOnClickFunction(
+        node: TypedNode,
+        onDismiss: () -> Unit,
+        actionHandler: (menuAction: MenuAction, node: TypedNode) -> Unit,
+        navController: NavHostController,
+    ): () -> Unit = {
+        val textFileIntent = Intent(navController.context, TextEditorActivity::class.java)
+            .apply {
+                putExtra(Constants.INTENT_EXTRA_KEY_HANDLE, node.id.longValue)
+                putExtra(Constants.INTENT_EXTRA_KEY_ADAPTER_TYPE, SEARCH_ADAPTER)
+                putExtra(TextEditorViewModel.MODE, TextEditorViewModel.EDIT_MODE)
+            }
+        navController.context.startActivity(textFileIntent)
+        onDismiss()
+    }
 }
