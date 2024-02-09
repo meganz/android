87ba6473f2fcfd2e084caf58622ce7b213dc31fb
HM Tamim <ht@mega.co.nz>
2024-02-09 14:31:23 +0600

    Fix MapNodeToShareUseCaseTest

--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/shares/MapNodeToShareUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/shares/MapNodeToShareUseCaseTest.kt
@@ -2,24 +2,55 @@ package mega.privacy.android.domain.usecase.shares
 
 
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import mega.privacy.android.domain.entity.ShareData
 import mega.privacy.android.domain.entity.node.TypedFileNode
 import mega.privacy.android.domain.entity.node.TypedFolderNode
 import mega.privacy.android.domain.entity.node.shares.ShareFileNode
 import mega.privacy.android.domain.entity.node.shares.ShareFolderNode
+import mega.privacy.android.domain.usecase.contact.GetContactUserNameFromDatabaseUseCase
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class MapNodeToShareUseCaseTest {
     private lateinit var underTest: MapNodeToShareUseCase
+    private val getContactUserNameFromDatabaseUseCase: GetContactUserNameFromDatabaseUseCase =
+        mock()
 
     @BeforeAll
     internal fun setUp() {
-        underTest = MapNodeToShareUseCase()
+        underTest = MapNodeToShareUseCase(getContactUserNameFromDatabaseUseCase)
+    }
+
+    @BeforeEach
+    fun resetMocks() {
+        reset(getContactUserNameFromDatabaseUseCase)
+    }
+
+    @BeforeAll
+    fun initialise() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @AfterAll
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
@@ -55,4 +86,19 @@ internal class MapNodeToShareUseCaseTest {
         assertThat(actual).isInstanceOf(ShareFolderNode::class.java)
         assertThat(actual.shareData).isEqualTo(shareData)
     }
+
+    @Test
+    internal fun `test share data with count 1 and verified contact fetches user full name from database`() =
+        runTest {
+            val shareData = mock<ShareData> {
+                on { user } doReturn "john@mail.com"
+                on { count } doReturn 1
+                on { isVerified } doReturn true
+            }
+            val folderNode = mock<TypedFolderNode>()
+            whenever(getContactUserNameFromDatabaseUseCase(any())).thenReturn("John Doe")
+            val actual = underTest(folderNode, shareData)
+            assertThat(actual).isInstanceOf(ShareFolderNode::class.java)
+            verify(getContactUserNameFromDatabaseUseCase).invoke(shareData.user)
+        }
 }
