41087727d8073b2baa91e3e9f49dac3c936e4033
Daniel Oosthuizen <do@mega.co.nz>
2024-02-26 11:49:00 +1300

    AND-18284 Create checkbox control

---
+++ core-ui/src/main/java/mega/privacy/android/core/ui/controls/buttons/MegaCheckBox.kt
@@ -0,0 +1,115 @@
+package mega.privacy.android.core.ui.controls.buttons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import mega.privacy.android.core.R
+import mega.privacy.android.core.ui.preview.BooleanProvider
+import mega.privacy.android.core.ui.preview.CombinedThemePreviews
+import mega.privacy.android.core.ui.theme.AndroidTheme
+import mega.privacy.android.core.ui.theme.MegaTheme
+import mega.privacy.android.core.ui.theme.extensions.conditional
+
+/**
+ * Mega checkbox
+ *
+ * @param checked
+ * @param onCheckedChange
+ * @param modifier
+ * @param enabled
+ */
+@Composable
+fun MegaCheckbox(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    rounded: Boolean = true,
+) {
+    val imageVector = ImageVector.vectorResource(R.drawable.check)
+    val tint = if (checked) MegaTheme.colors.icon.inverse else Color.Transparent
+    val background =
+        if (checked) MegaTheme.colors.components.selectionControl else Color.Transparent
+    val borderColor =
+        if (enabled) MegaTheme.colors.icon.secondary else MegaTheme.colors.border.disabled
+    val shape = if (rounded) CircleShape else RoundedCornerShape(2.dp)
+
+    IconButton(
+        onClick = { onCheckedChange(!checked) },
+        modifier = modifier
+            .clip(shape)
+            .size(32.dp),
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = imageVector,
+            tint = tint,
+            modifier = Modifier
+                .size(20.dp)
+                .background(background, shape = shape)
+                .padding(2.dp)
+                .conditional(!checked) {
+                    border(
+                        width = 1.dp,
+                        color = borderColor,
+                        shape = shape,
+                    )
+                },
+            contentDescription = null,
+        )
+    }
+}
+
+/**
+ * Preview [MegaCheckbox]
+ */
+@CombinedThemePreviews
+@Composable
+private fun PreviewMegaCheckboxRound(
+    @PreviewParameter(BooleanProvider::class) isEnabled: Boolean,
+) {
+    Preview(checked = isEnabled, rounded = true)
+}
+
+/**
+ * Preview [MegaCheckbox]
+ */
+@CombinedThemePreviews
+@Composable
+private fun PreviewMegaCheckboxSquare(
+    @PreviewParameter(BooleanProvider::class) checked: Boolean,
+) {
+    Preview(checked = checked, rounded = false)
+}
+
+@Composable
+private fun Preview(checked: Boolean, rounded: Boolean) {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        var interactiveChecked by remember { mutableStateOf(checked) }
+        MegaCheckbox(
+            modifier = Modifier,
+            checked = interactiveChecked,
+            onCheckedChange = { interactiveChecked = !interactiveChecked },
+            rounded = rounded
+        )
+    }
+}
+
