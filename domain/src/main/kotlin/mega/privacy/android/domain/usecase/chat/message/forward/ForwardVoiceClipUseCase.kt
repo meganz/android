22e1cea9998313fe79211040931b65ea854fe7a8
Yenel Rodríguez Hernández <yr@mega.nz>
2024-03-07 01:15:49 +1300

    AND-17789 Handle "Forward" action for VoiceClipMessage

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/chat/message/forward/ForwardVoiceClipUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/chat/message/forward/ForwardVoiceClipUseCase.kt
@@ -3,7 +3,9 @@ package mega.privacy.android.domain.usecase.chat.message.forward
 import mega.privacy.android.domain.entity.chat.messages.ForwardResult
 import mega.privacy.android.domain.entity.chat.messages.TypedMessage
 import mega.privacy.android.domain.entity.chat.messages.VoiceClipMessage
+import mega.privacy.android.domain.exception.MegaException
 import mega.privacy.android.domain.usecase.chat.message.AttachVoiceClipMessageUseCase
+import mega.privacy.android.domain.usecase.node.chat.GetChatFileUseCase
 import javax.inject.Inject
 
 /**
@@ -11,24 +13,26 @@ import javax.inject.Inject
  */
 class ForwardVoiceClipUseCase @Inject constructor(
     private val attachVoiceClipMessageUseCase: AttachVoiceClipMessageUseCase,
+    private val getChatFileUseCase: GetChatFileUseCase,
 ) : ForwardMessageUseCase() {
     override suspend fun forwardMessage(targetChatId: Long, message: TypedMessage): ForwardResult? {
         val voiceClipMessage = message as? VoiceClipMessage ?: return null
-//        var result: ForwardResult = ForwardResult.Success(targetChatId)
-//        runCatching {
-//            attachVoiceClipMessageUseCase(
-//                chatId = targetChatId,
-//                fileNode = (message as VoiceClipMessage).fileNode
-//            )
-//        }.onFailure {
-//            result = if ((it as MegaException).errorCode == -9) {
-//                ForwardResult.ErrorNotAvailable
-//            } else {
-//                ForwardResult.GeneralError
-//            }
-//        }
-//        return result
-        return ForwardResult.Success(targetChatId)
+        val fileNode = getChatFileUseCase(voiceClipMessage.chatId, voiceClipMessage.msgId)
+            ?: return ForwardResult.ErrorNotAvailable
+
+        return runCatching {
+            attachVoiceClipMessageUseCase(
+                chatId = targetChatId,
+                fileNode = fileNode
+            )
+            ForwardResult.Success(targetChatId)
+        }.getOrElse {
+            if ((it as MegaException).errorCode == -9) {
+                ForwardResult.ErrorNotAvailable
+            } else {
+                ForwardResult.GeneralError
+            }
+        }
     }
 
 }
--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/chat/message/forward/ForwardVoiceClipUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/chat/message/forward/ForwardVoiceClipUseCaseTest.kt
@@ -3,13 +3,13 @@ package mega.privacy.android.domain.usecase.chat.message.forward
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
 import mega.privacy.android.domain.entity.chat.messages.ForwardResult
-import mega.privacy.android.domain.entity.chat.messages.NodeAttachmentMessage
 import mega.privacy.android.domain.entity.chat.messages.VoiceClipMessage
 import mega.privacy.android.domain.entity.chat.messages.normal.NormalMessage
-import mega.privacy.android.domain.entity.node.FileNode
+import mega.privacy.android.domain.entity.node.chat.ChatDefaultFile
 import mega.privacy.android.domain.exception.MegaException
 import mega.privacy.android.domain.usecase.chat.message.AttachVoiceClipMessageUseCase
 import mega.privacy.android.domain.usecase.chat.message.forward.ForwardNodeAttachmentUseCase.Companion.API_ENOENT
+import mega.privacy.android.domain.usecase.node.chat.GetChatFileUseCase
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.TestInstance
 import org.mockito.Mockito.reset
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -26,58 +25,66 @@ class ForwardVoiceClipUseCaseTest {
     private lateinit var underTest: ForwardVoiceClipUseCase
 
     private val attachVoiceClipMessageUseCase = mock<AttachVoiceClipMessageUseCase>()
+    private val getChatFileUseCase = mock<GetChatFileUseCase>()
 
     private val targetChatId = 789L
-    private val node = mock<FileNode>()
+    private val node = mock<ChatDefaultFile>()
     private val message = mock<VoiceClipMessage> {
-//        on { fileNode } doReturn node
+        on { chatId } doReturn 123L
+        on { msgId } doReturn 456L
     }
 
     @BeforeEach
     fun setup() {
         underTest = ForwardVoiceClipUseCase(
-            attachVoiceClipMessageUseCase = attachVoiceClipMessageUseCase
+            attachVoiceClipMessageUseCase = attachVoiceClipMessageUseCase,
+            getChatFileUseCase = getChatFileUseCase,
         )
     }
 
     @AfterEach
     fun resetMocks() {
-        reset(attachVoiceClipMessageUseCase)
+        reset(attachVoiceClipMessageUseCase, getChatFileUseCase)
     }
 
     @Test
     fun `test that empty is returned if message is not a voice clip`() = runTest {
         val message = mock<NormalMessage>()
         underTest.invoke(listOf(targetChatId), message)
+
         assertThat(underTest.invoke(listOf(targetChatId), message)).isEmpty()
     }
 
-//    @Test
-//    fun `test that general error is returned if attach request throws a general exception`() =
-//        runTest {
-//            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenAnswer {
-//                throw MegaException(errorCode = -1, null)
-//            }
-//            assertThat(underTest.invoke(listOf(targetChatId), message))
-//                .isEqualTo(listOf(ForwardResult.GeneralError))
-//        }
-//
-//    @Test
-//    fun `test that not available error is returned if attach request throws an API_ENOENT exception`() =
-//        runTest {
-//            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenAnswer {
-//                throw MegaException(errorCode = API_ENOENT, null)
-//            }
-//            assertThat(underTest.invoke(listOf(targetChatId), message))
-//                .isEqualTo(listOf(ForwardResult.ErrorNotAvailable))
-//        }
+    @Test
+    fun `test that general error is returned if attach request throws a general exception`() =
+        runTest {
+            whenever(getChatFileUseCase(message.chatId, message.msgId)).thenReturn(node)
+            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenAnswer {
+                throw MegaException(errorCode = -1, null)
+            }
+
+            assertThat(underTest.invoke(listOf(targetChatId), message))
+                .isEqualTo(listOf(ForwardResult.GeneralError))
+        }
+
+    @Test
+    fun `test that not available error is returned if attach request throws an API_ENOENT exception`() =
+        runTest {
+            whenever(getChatFileUseCase(message.chatId, message.msgId)).thenReturn(node)
+            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenAnswer {
+                throw MegaException(errorCode = API_ENOENT, null)
+            }
+
+            assertThat(underTest.invoke(listOf(targetChatId), message))
+                .isEqualTo(listOf(ForwardResult.ErrorNotAvailable))
+        }
 
     @Test
     fun `test that attach voice clip message use case is invoked and success is returned`() =
         runTest {
-//            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenReturn(Unit)
-//            underTest.invoke(listOf(targetChatId), message)
-//            verify(attachVoiceClipMessageUseCase).invoke(targetChatId, node)
+            whenever(getChatFileUseCase(message.chatId, message.msgId)).thenReturn(node)
+            whenever(attachVoiceClipMessageUseCase(targetChatId, node)).thenReturn(Unit)
+
             assertThat(underTest.invoke(listOf(targetChatId), message))
                 .isEqualTo(listOf(ForwardResult.Success(targetChatId)))
         }
