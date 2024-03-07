8904753411991f502e618f28bbcd338a5eca0ce2
Daniel Oosthuizen <do@mega.co.nz>
2024-03-08 12:53:43 +1300

    AND-18371 Unify click begaviour for chat link message view

--- app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/model/messages/normal/TextLinkUiMessage.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/model/messages/normal/TextLinkUiMessage.kt
@@ -1,10 +1,30 @@
 package mega.privacy.android.app.presentation.meeting.chat.model.messages.normal
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import mega.privacy.android.app.presentation.meeting.chat.model.messages.AvatarMessage
+import mega.privacy.android.app.presentation.meeting.chat.model.messages.onUserClick
+import mega.privacy.android.app.presentation.meeting.chat.view.message.contact.ContactMessageViewModel
 import mega.privacy.android.app.presentation.meeting.chat.view.message.link.ChatLinksMessageView
+import mega.privacy.android.app.presentation.meeting.chat.view.message.link.ChatLinksMessageViewModel
+import mega.privacy.android.app.presentation.meeting.chat.view.message.link.LinkContent
 import mega.privacy.android.core.ui.controls.chat.messages.reaction.model.UIReaction
+import mega.privacy.android.core.ui.controls.dividers.DividerSpacing
+import mega.privacy.android.core.ui.controls.dividers.MegaDivider
+import mega.privacy.android.core.ui.controls.layouts.LocalSnackBarHostState
+import mega.privacy.android.core.ui.theme.extensions.conditional
+import mega.privacy.android.domain.entity.RegexPatternType
 import mega.privacy.android.domain.entity.chat.messages.TypedMessage
 import mega.privacy.android.domain.entity.chat.messages.normal.TextLinkMessage
 
@@ -13,6 +33,7 @@ import mega.privacy.android.domain.entity.chat.messages.normal.TextLinkMessage
  * @property message Contact link message
  *
  */
+@OptIn(ExperimentalFoundationApi::class)
 data class TextLinkUiMessage(
     override val message: TextLinkMessage,
     override val reactions: List<UIReaction>,
@@ -22,12 +43,85 @@ data class TextLinkUiMessage(
         onLongClick: (TypedMessage) -> Unit,
         interactionEnabled: Boolean,
     ) {
+        val viewModel: ChatLinksMessageViewModel = hiltViewModel()
+        val contactViewModel: ContactMessageViewModel = hiltViewModel()
+        val context = LocalContext.current
+        val coroutineScope = rememberCoroutineScope()
+        val snackbarHostState = LocalSnackBarHostState.current
+        var contentLinks by remember {
+            mutableStateOf(listOf<LinkContent>())
+        }
+        LaunchedEffect(message.links) {
+            message.links.forEach {
+                when (it.type) {
+                    RegexPatternType.CONTACT_LINK -> {
+                        val linkContent =
+                            viewModel.loadContactInfo(it.link) { handle, email, name, isContact ->
+                                onUserClick(
+                                    handle,
+                                    email.orEmpty(),
+                                    name.orEmpty(),
+                                    isContact,
+                                    context,
+                                    coroutineScope,
+                                    snackbarHostState,
+                                    contactViewModel::checkUser,
+                                    contactViewModel::inviteUser,
+                                )
+                            }
+                        if (linkContent != null) {
+                            contentLinks = contentLinks + linkContent
+                        }
+                    }
+
+                    RegexPatternType.CHAT_LINK -> {
+                        val linkContent = viewModel.loadChatLinkInfo(it.link)
+                        contentLinks = contentLinks + linkContent
+                    }
+
+                    RegexPatternType.FOLDER_LINK -> {
+                        viewModel.loadFolderLinkInfo(it.link)?.let { linkContent ->
+                            contentLinks = contentLinks + linkContent
+                        }
+                    }
+
+                    RegexPatternType.FILE_LINK -> {
+                        viewModel.loadFileLinkInfo(it.link)?.let { linkContent ->
+                            contentLinks = contentLinks + linkContent
+                        }
+                    }
+                    // other link type here
+                    else -> Unit
+                }
+            }
+        }
         ChatLinksMessageView(
-            modifier = Modifier,
-            onLongClick = onLongClick,
             message = message,
-            interactionEnabled = interactionEnabled,
-        )
+            modifier = Modifier.conditional(interactionEnabled) {
+                combinedClickable(
+                    onClick = { contentLinks.firstOrNull()?.onClick(context) },
+                    onLongClick = { onLongClick(message) }
+                )
+            },
+        ) {
+            contentLinks.forEachIndexed { index, linkContent ->
+                key(linkContent.link) {
+                    linkContent.SubContentComposable(
+                        modifier = Modifier.conditional(
+                            interactionEnabled
+                        ) {
+                            combinedClickable(
+                                onClick = { linkContent.onClick(context) },
+                                onLongClick = { onLongClick(message) }
+                            )
+                        })
+                }
+
+                if (index != contentLinks.lastIndex) {
+                    MegaDivider(dividerSpacing = DividerSpacing.Full)
+                }
+            }
+        }
     }
 
     override val showAvatar = message.shouldShowAvatar
--- app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/view/message/link/ChatLinksMessageView.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/meeting/chat/view/message/link/ChatLinksMessageView.kt
@@ -1,125 +1,33 @@
 package mega.privacy.android.app.presentation.meeting.chat.view.message.link
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import mega.privacy.android.app.presentation.meeting.chat.model.messages.onUserClick
-import mega.privacy.android.app.presentation.meeting.chat.view.message.contact.ContactMessageViewModel
 import mega.privacy.android.core.ui.controls.chat.messages.ChatBubble
-import mega.privacy.android.core.ui.controls.dividers.DividerSpacing
-import mega.privacy.android.core.ui.controls.dividers.MegaDivider
-import mega.privacy.android.core.ui.controls.layouts.LocalSnackBarHostState
-import mega.privacy.android.core.ui.theme.extensions.conditional
-import mega.privacy.android.domain.entity.RegexPatternType
-import mega.privacy.android.domain.entity.chat.messages.TypedMessage
 import mega.privacy.android.domain.entity.chat.messages.normal.TextLinkMessage
 
 /**
- * Chat link message view
+ * Chat links message view
  *
- * @param message Chat link message
- * @param modifier Modifier
- * @param viewModel Chat link message view model
+ * @param message
+ * @param modifier
+ * @param linkViews
  */
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ChatLinksMessageView(
     message: TextLinkMessage,
-    onLongClick: (TypedMessage) -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ChatLinksMessageViewModel = hiltViewModel(),
-    contactViewModel: ContactMessageViewModel = hiltViewModel(),
-    interactionEnabled: Boolean = true,
+    linkViews: @Composable () -> Unit,
 ) {
     val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-    val snackbarHostState = LocalSnackBarHostState.current
-    var contentLinks by remember {
-        mutableStateOf(listOf<LinkContent>())
-    }
-    LaunchedEffect(message.links) {
-        message.links.forEach {
-            when (it.type) {
-                RegexPatternType.CONTACT_LINK -> {
-                    val linkContent =
-                        viewModel.loadContactInfo(it.link) { handle, email, name, isContact ->
-                            onUserClick(
-                                handle,
-                                email.orEmpty(),
-                                name.orEmpty(),
-                                isContact,
-                                context,
-                                coroutineScope,
-                                snackbarHostState,
-                                contactViewModel::checkUser,
-                                contactViewModel::inviteUser,
-                            )
-                        }
-                    if (linkContent != null) {
-                        contentLinks = contentLinks + linkContent
-                    }
-                }
 
-                RegexPatternType.CHAT_LINK -> {
-                    val linkContent = viewModel.loadChatLinkInfo(it.link)
-                    contentLinks = contentLinks + linkContent
-                }
-
-                RegexPatternType.FOLDER_LINK -> {
-                    viewModel.loadFolderLinkInfo(it.link)?.let { linkContent ->
-                        contentLinks = contentLinks + linkContent
-                    }
-                }
-
-                RegexPatternType.FILE_LINK -> {
-                    viewModel.loadFileLinkInfo(it.link)?.let { linkContent ->
-                        contentLinks = contentLinks + linkContent
-                    }
-                }
-                // other link type here
-                else -> Unit
-            }
-        }
-    }
     ChatBubble(
         isMe = message.isMine,
-        modifier = modifier
-            .conditional(interactionEnabled) {
-                combinedClickable(
-                    onClick = { contentLinks.firstOrNull()?.onClick(context) },
-                    onLongClick = { onLongClick(message) }
-                )
-            },
-        subContent = {
-            contentLinks.forEachIndexed { index, linkContent ->
-                key(linkContent.link) {
-                    linkContent.SubContentComposable(
-                        modifier = Modifier.conditional(interactionEnabled) {
-                            combinedClickable(
-                                onClick = { linkContent.onClick(context) },
-                                onLongClick = { onLongClick(message) }
-                            )
-                        })
-                }
-
-                if (index != contentLinks.lastIndex) {
-                    MegaDivider(dividerSpacing = DividerSpacing.Full)
-                }
-            }
-        },
+        modifier = modifier,
+        subContent = linkViews,
         content = {
             Text(
                 modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
