21e8494fd7e74e18bef1bc9777548d84f2a95bf1
Pau Dominkovics <pau@mega.co.nz>
2024-03-25 17:25:36 +0100

    T15478886: The confirmation message after a download starts is sometimes missing

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/transfers/active/MonitorActiveTransferFinishedUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/transfers/active/MonitorActiveTransferFinishedUseCase.kt
@@ -1,9 +1,9 @@
 package mega.privacy.android.domain.usecase.transfers.active
 
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.scan
+import mega.privacy.android.domain.entity.transfer.ActiveTransferTotals
 import mega.privacy.android.domain.entity.transfer.TransferType
 import mega.privacy.android.domain.repository.TransferRepository
 import javax.inject.Inject
@@ -17,15 +17,30 @@ class MonitorActiveTransferFinishedUseCase @Inject constructor(
 
     /**
      * Invoke
-     * @return flow emitting last non-zero totalFileTransfers when it goes back to 0
+     * @return flow emitting last non-zero totalFileTransfers when it goes back to 0 (unless they all were already downloaded files)
      */
     operator fun invoke(transferType: TransferType) =
         transferRepository.getActiveTransferTotalsByType(transferType)
-            .map { it.totalFileTransfers }
             .distinctUntilChanged()
-            .scan(0 to 0) { (_, prev), new ->
+            .scan(emptyTotals(transferType) to emptyTotals(transferType)) { (_, prev), new ->
                 prev to new
             }.mapNotNull { (prev, current) ->
-                prev.takeIf { prev != 0 && current == 0 }
+                prev.takeIf {
+                    prev.totalFileTransfers != 0 && current.totalFileTransfers == 0
+                            && prev.totalFinishedTransfers > prev.totalAlreadyDownloadedFiles
+                }?.totalFileTransfers
             }
+
+    private fun emptyTotals(transferType: TransferType) = ActiveTransferTotals(
+        transfersType = transferType,
+        totalTransfers = 0,
+        totalFileTransfers = 0,
+        pausedFileTransfers = 0,
+        totalFinishedTransfers = 0,
+        totalFinishedFileTransfers = 0,
+        totalCompletedFileTransfers = 0,
+        totalBytes = 0L,
+        transferredBytes = 0L,
+        totalAlreadyDownloadedFiles = 0
+    )
 }
