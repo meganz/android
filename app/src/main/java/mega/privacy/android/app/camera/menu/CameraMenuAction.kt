8aafb787456ec6ac5e9ba87263542e32e6390d2a
Hai Luong <lh@mega.co.nz>
2024-03-27 13:28:39 +0700

    AND-18505: Create flash mode menu action

---
+++ app/src/main/java/mega/privacy/android/app/camera/menu/CameraMenuAction.kt
@@ -0,0 +1,38 @@
+package mega.privacy.android.app.camera.menu
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import mega.privacy.android.app.R
+import mega.privacy.android.core.ui.model.MenuActionWithIcon
+
+internal sealed interface CameraMenuAction : MenuActionWithIcon {
+    class FlashOff : CameraMenuAction {
+        @Composable
+        override fun getDescription(): String = "Flash Off"
+
+        @Composable
+        override fun getIconPainter() = painterResource(R.drawable.ic_flash_off)
+        override val testTag = "flash_off"
+        override val orderInCategory = 1
+    }
+
+    class FlashOn : CameraMenuAction {
+        @Composable
+        override fun getDescription(): String = "Flash On"
+
+        @Composable
+        override fun getIconPainter() = painterResource(R.drawable.ic_flash)
+        override val testTag = "flash_on"
+        override val orderInCategory = 2
+    }
+
+    class FlashAuto : CameraMenuAction {
+        @Composable
+        override fun getDescription(): String = "Flash Auto"
+
+        @Composable
+        override fun getIconPainter() = painterResource(R.drawable.ic_flash_auto)
+        override val testTag = "flash_auto"
+        override val orderInCategory = 3
+    }
+}
