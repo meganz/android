96e1e8679cde0136b696bf688211748841eb6481
Kevin Sun <ks@mega.co.nz>
2024-04-08 11:08:29 +1200

    CC-6838 AND - Add the drag-drop list view

---
+++ core-ui/src/main/java/mega/privacy/android/core/ui/controls/lists/DragDropListView.kt
@@ -0,0 +1,144 @@
+package mega.privacy.android.core.ui.controls.lists
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import mega.privacy.android.core.ui.controls.text.MegaText
+import mega.privacy.android.core.ui.model.DragDropListState
+import mega.privacy.android.core.ui.model.rememberDragDropListState
+import mega.privacy.android.core.ui.preview.CombinedThemePreviews
+import mega.privacy.android.core.ui.theme.AndroidTheme
+import mega.privacy.android.core.ui.theme.MegaTheme
+import mega.privacy.android.core.ui.theme.tokens.TextColor
+
+/**
+ * A list view that supports drag and drop reordering of items.
+ *
+ * @param items The list of items to display
+ * @param lazyListState The state of the list
+ * @param onMove The callback is intended to be invoked when an item is moved, typically to update the list by swapping the dragged item with the target item.
+ * @param modifier Modifier
+ * @param elevation The elevation of the dragging item, if you don't want elevation, set it to 0.dp
+ * @param content the UI of the item, please note that don't add the view that includes the LongPress event, it will cause the event conflict
+ */
+@Composable
+fun <T : Any> DragDropListView(
+    items: List<T>,
+    lazyListState: LazyListState,
+    onMove: (from: Int, to: Int) -> Unit,
+    modifier: Modifier = Modifier,
+    elevation: Dp = 10.dp,
+    content: @Composable (index: Int, item: T) -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    val overscrollJob = remember { mutableStateOf<Job?>(null) }
+    val dragDropListState =
+        rememberDragDropListState(lazyListState = lazyListState, onMove = onMove)
+
+    LazyColumn(
+        modifier = modifier.setDragGesturesAfterLongPress(
+            dragDropListState = dragDropListState,
+            scope = scope,
+            overscrollJob = overscrollJob,
+        ),
+        state = dragDropListState.lazyListState
+    ) {
+        itemsIndexed(items) { index, item ->
+            val isDraggingItem = index == dragDropListState.draggedItemIndex
+            val offsetOrNull = dragDropListState.draggingItemOffset.takeIf { isDraggingItem }
+            Box(
+                modifier = Modifier
+                    // The zIndex is used to make the dragging item always on top of other items
+                    .zIndex(if (isDraggingItem) 1f else 0f)
+                    .graphicsLayer {
+                        translationY = offsetOrNull ?: 0f
+                    }
+                    .shadow(elevation = if (isDraggingItem) elevation else 0.dp)
+                    .background(MegaTheme.colors.background.pageBackground)
+            ) {
+                content(index, item)
+            }
+        }
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+private fun DragDropListViewPreview() {
+    AndroidTheme(isDark = isSystemInDarkTheme()) {
+        DragDropListView(
+            items = listOf("test1", "test2", "test3", "test4", "test5"),
+            lazyListState = LazyListState(),
+            onMove = { _, _ -> }
+        ) { _, item ->
+            MegaText(modifier = Modifier.fillMaxWidth(), text = item, textColor = TextColor.Primary)
+        }
+    }
+}
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+private fun Modifier.setDragGesturesAfterLongPress(
+    dragDropListState: DragDropListState,
+    scope: CoroutineScope,
+    overscrollJob: MutableState<Job?>,
+) =
+    pointerInput(Unit) {
+        detectDragGesturesAfterLongPress(
+            onDrag = { change, offset ->
+                dragDropListState.onDrag(offset)
+                handleOverscrollJob(
+                    overscrollJob = overscrollJob,
+                    scope = scope,
+                    dragDropListState = dragDropListState
+                )
+                change.consume()
+            },
+            onDragStart = { offset ->
+                dragDropListState.onDragStart(offset)
+            },
+            onDragEnd = {
+                dragDropListState.onDragInterrupted()
+            },
+            onDragCancel = {
+                dragDropListState.onDragInterrupted()
+            }
+        )
+    }
+
+private fun handleOverscrollJob(
+    overscrollJob: MutableState<Job?>,
+    scope: CoroutineScope,
+    dragDropListState: DragDropListState,
+) {
+    if (overscrollJob.value?.isActive == true) return
+    val overscrollOffset = dragDropListState.checkForOverScroll()
+    if (overscrollOffset != 0f) {
+        overscrollJob.value = scope.launch {
+            dragDropListState.lazyListState.scrollBy(overscrollOffset)
+        }
+    } else {
+        overscrollJob.value?.cancel()
+    }
+}
---
+++ core-ui/src/main/java/mega/privacy/android/core/ui/model/DragDropListState.kt
@@ -0,0 +1,127 @@
+package mega.privacy.android.core.ui.model
+
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import kotlinx.coroutines.Job
+
+@Composable
+internal fun rememberDragDropListState(
+    lazyListState: LazyListState,
+    onMove: (from: Int, to: Int) -> Unit,
+): DragDropListState =
+    remember { DragDropListState(lazyListState = lazyListState, onMove = onMove) }
+
+/**
+ * State for drag and drop reordering of items in a list.
+ *
+ * @param lazyListState The state of the list
+ * @param onMove The callback is intended to be invoked when an item is moved
+ */
+internal class DragDropListState(
+    val lazyListState: LazyListState,
+    private val onMove: (from: Int, to: Int) -> Unit,
+) {
+    private var draggedDistance by mutableFloatStateOf(0f)
+
+    private var initDraggedLayoutInfo by mutableStateOf<LazyListItemInfo?>(null)
+
+    var draggedItemIndex by mutableStateOf<Int?>(null)
+
+    private val initOffsets: Pair<Int, Int>?
+        get() = initDraggedLayoutInfo?.let { Pair(it.offset, it.offsetEnd) }
+
+    val draggingItemOffset: Float?
+        get() = draggedItemIndex
+            ?.let { lazyListState.getVisibleItemInfo(absoluteIndex = it) }
+            ?.let { item ->
+                (initDraggedLayoutInfo?.offset ?: 0f).toFloat() + draggedDistance - item.offset
+            }
+
+    private val draggedItemLayoutInfo: LazyListItemInfo?
+        get() = draggedItemIndex?.let {
+            lazyListState.getVisibleItemInfo(absoluteIndex = it)
+        }
+
+    private var overscrollJob by mutableStateOf<Job?>(null)
+
+    fun onDragStart(offset: Offset) {
+        lazyListState.layoutInfo.visibleItemsInfo
+            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
+            ?.also {
+                draggedItemIndex = it.index
+                initDraggedLayoutInfo = it
+            }
+    }
+
+    fun onDragInterrupted() {
+        draggedDistance = 0f
+        draggedItemIndex = null
+        initDraggedLayoutInfo = null
+        overscrollJob?.cancel()
+    }
+
+    fun onDrag(offset: Offset) {
+        draggedDistance += offset.y
+
+        initOffsets?.let { (topOffset, bottomOffset) ->
+            val startOffset = topOffset + draggedDistance
+            val endOffset = bottomOffset + draggedDistance
+
+            draggedItemLayoutInfo?.let { layoutInfo ->
+                lazyListState.layoutInfo.visibleItemsInfo
+                    .filterNot { item ->
+                        item.offsetEnd < startOffset || item.offset > endOffset || layoutInfo.index == item.index
+                    }
+                    .firstOrNull { item ->
+                        val draggedOffset = startOffset - layoutInfo.offset
+                        when {
+                            draggedOffset > 0 -> (endOffset > item.offsetEnd - item.size / 2)
+                            else -> (startOffset < item.offset + item.size / 2)
+                        }
+                    }
+                    ?.also { item ->
+                        draggedItemIndex?.let { current ->
+                            onMove(current, item.index)
+                        }
+                        draggedItemIndex = item.index
+                    }
+            }
+        }
+    }
+
+    fun checkForOverScroll(): Float = initDraggedLayoutInfo?.let {
+        val startOffset = it.offset + draggedDistance
+        val endOffset = it.offsetEnd + draggedDistance
+
+        when {
+            draggedDistance > 0 ->
+                (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+
+            draggedDistance < 0 ->
+                (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+
+            else -> null
+        }
+    } ?: 0f
+}
+
+/**
+ * LazyListItemInfo.index is the item's absolute index in the list
+ * Based on the item's "relative position" with the "currently top" visible item,
+ * this returns LazyListItemInfo corresponding to it
+ *
+ * @param absoluteIndex absolute index of the item
+ * @return relative LazyListItemInfo based on current visible items
+ */
+private fun LazyListState.getVisibleItemInfo(absoluteIndex: Int) =
+    layoutInfo.visibleItemsInfo.getOrNull(absoluteIndex - layoutInfo.visibleItemsInfo.first().index)
+
+private val LazyListItemInfo.offsetEnd: Int
+    get() = offset + size
