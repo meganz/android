183d4d998a479a6354e8f7160037509e89136672
Kevin Sun <ks@mega.co.nz>
2024-04-15 18:40:25 +1200

    CC-6937 AND - Create unit test for GetLocalLinkFromMegaApiUseCase

---
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/GetLocalLinkFromMegaApiUseCaseTest.kt
@@ -0,0 +1,54 @@
+package mega.privacy.android.domain.usecase
+
+import com.google.common.truth.Truth
+import kotlinx.coroutines.test.runTest
+import mega.privacy.android.domain.repository.MediaPlayerRepository
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GetLocalLinkFromMegaApiUseCaseTest {
+    private lateinit var underTest: GetLocalLinkFromMegaApiUseCase
+    private val mediaPlayerRepository = mock<MediaPlayerRepository>()
+
+    @BeforeAll
+    fun setUp() {
+        underTest = GetLocalLinkFromMegaApiUseCase(mediaPlayerRepository = mediaPlayerRepository)
+    }
+
+    @BeforeEach
+    fun resetMock() {
+        reset(mediaPlayerRepository)
+    }
+
+    @Test
+    fun `test that result is null when the local link is null`() =
+        runTest {
+            val testHandle = 123456L
+            whenever(mediaPlayerRepository.getLocalLinkFromMegaApi(testHandle)).thenReturn(null)
+            Truth.assertThat(underTest(testHandle)).isNull()
+        }
+
+    @Test
+    fun `test that the local link is returned`() =
+        runTest {
+            val testHandle = 123456L
+            val testLink = "expected link"
+            whenever(mediaPlayerRepository.getLocalLinkFromMegaApi(testHandle)).thenReturn(testLink)
+            Truth.assertThat(underTest(testHandle)).isEqualTo(testLink)
+        }
+
+    @Test
+    fun `test that the function is invoked as expected`() =
+        runTest {
+            val testHandle = 123456L
+            underTest(testHandle)
+            verify(mediaPlayerRepository).getLocalLinkFromMegaApi(testHandle)
+        }
+}
