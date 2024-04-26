06c9c7b9c7198baaf4740342321d8f444007752e
Kevin Sun <ks@mega.co.nz>
2024-04-26 18:01:22 +1200

    CC-7042 AND - Fix Flaky Test in VideoPlaylistDetailViewTest

--- app/src/test/java/test/mega/privacy/android/app/presentation/videosection/VideoPlaylistDetailViewTest.kt
+++ app/src/test/java/test/mega/privacy/android/app/presentation/videosection/VideoPlaylistDetailViewTest.kt
@@ -1,13 +1,18 @@
 package test.mega.privacy.android.app.presentation.videosection
 
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import coil.Coil
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
+import coil.test.FakeImageLoaderEngine
 import mega.privacy.android.app.presentation.videosection.model.VideoPlaylistUIEntity
 import mega.privacy.android.app.presentation.videosection.model.VideoUIEntity
 import mega.privacy.android.app.presentation.videosection.view.playlist.DETAIL_DELETE_VIDEOS_DIALOG_TEST_TAG
@@ -20,17 +25,27 @@ import mega.privacy.android.app.presentation.videosection.view.playlist.PLAYLIST
 import mega.privacy.android.app.presentation.videosection.view.playlist.VIDEO_PLAYLIST_DETAIL_EMPTY_VIEW_TEST_TAG
 import mega.privacy.android.app.presentation.videosection.view.playlist.VideoPlaylistDetailView
 import mega.privacy.android.domain.entity.node.NodeId
-import org.junit.Ignore
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
+@OptIn(ExperimentalCoilApi::class)
 @RunWith(AndroidJUnit4::class)
 class VideoPlaylistDetailViewTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Before
+    fun setUp() {
+        val engine = FakeImageLoaderEngine.Builder().build()
+        val imageLoader = ImageLoader.Builder(composeTestRule.activity)
+            .components { add(engine) }
+            .build()
+        Coil.setImageLoader(imageLoader)
+    }
 
     private val playlist = mock<VideoPlaylistUIEntity> {
         on { id }.thenReturn(NodeId(1L))
@@ -214,7 +229,6 @@ class VideoPlaylistDetailViewTest {
             .assertIsNotDisplayed()
     }
 
-    @Ignore("This Test is unstable because it fails in the Gitlab Pipeline. Check the Test implementation again")
     @Test
     fun `test that onPlayAllClicked is invoked when the play all button is clicked`() {
         val onPlayAllClicked = mock<() -> Unit>()
