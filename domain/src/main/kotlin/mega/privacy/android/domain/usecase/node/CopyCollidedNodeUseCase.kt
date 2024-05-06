b42d69aeab7520985cf5b74635ff3498a17a41f9
HM Tamim <ht@mega.co.nz>
2024-05-06 13:30:34 +1200

    SAO-1352 Implement CopyCollidedNodeUseCase

---
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/node/CopyCollidedNodeUseCase.kt
@@ -0,0 +1,64 @@
+package mega.privacy.android.domain.usecase.node
+
+import mega.privacy.android.domain.entity.node.MoveRequestResult
+import mega.privacy.android.domain.entity.node.NodeId
+import mega.privacy.android.domain.entity.node.NodeNameCollision
+import mega.privacy.android.domain.exception.extension.shouldEmitErrorForNodeMovement
+import mega.privacy.android.domain.usecase.node.chat.GetChatFilesUseCase
+import javax.inject.Inject
+
+/**
+ * Use Case to copy collided node and move it to a new Node while updating its name
+ */
+class CopyCollidedNodeUseCase @Inject constructor(
+    private val getChatFilesUseCase: GetChatFilesUseCase,
+    private val copyTypedNodeUseCase: CopyTypedNodeUseCase,
+    private val copyNodeUseCase: CopyNodeUseCase,
+) {
+
+    /**
+     * Invoke
+     *
+     * @param nameCollision Node name collision
+     * @param rename True if the node should be renamed, false otherwise
+     *
+     */
+    suspend operator fun invoke(
+        nameCollision: NodeNameCollision,
+        rename: Boolean,
+    ): MoveRequestResult {
+        try {
+            if (nameCollision is NodeNameCollision.Chat) {
+                val nodes = getChatFilesUseCase(
+                    nameCollision.chatId,
+                    nameCollision.messageId
+                )
+                val chatNode = nodes.firstOrNull { it.id.longValue == nameCollision.nodeHandle }
+
+                requireNotNull(chatNode) { "Chat file not found" }
+                copyTypedNodeUseCase(
+                    nodeToCopy = chatNode,
+                    newNodeParent = NodeId(nameCollision.parentHandle),
+                    newNodeName = if (rename) nameCollision.renameName else null
+                )
+            } else {
+                copyNodeUseCase(
+                    nodeToCopy = NodeId(nameCollision.nodeHandle),
+                    newNodeParent = NodeId(nameCollision.parentHandle),
+                    newNodeName = if (rename) nameCollision.renameName else null
+                )
+            }
+            return MoveRequestResult.Copy(
+                count = 1,
+                errorCount = 0,
+            )
+        } catch (it: Throwable) {
+            if (it.shouldEmitErrorForNodeMovement()) throw it
+            return MoveRequestResult.Copy(
+                count = 1,
+                errorCount = 1,
+            )
+        }
+    }
+
+}
---
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/node/CopyCollidedNodeUseCaseTest.kt
@@ -0,0 +1,209 @@
+package mega.privacy.android.domain.usecase.node
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
+import mega.privacy.android.domain.entity.node.NodeId
+import mega.privacy.android.domain.entity.node.NodeNameCollision
+import mega.privacy.android.domain.entity.node.chat.ChatDefaultFile
+import mega.privacy.android.domain.exception.NotEnoughQuotaMegaException
+import mega.privacy.android.domain.exception.QuotaExceededMegaException
+import mega.privacy.android.domain.exception.node.ForeignNodeException
+import mega.privacy.android.domain.usecase.node.chat.GetChatFilesUseCase
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+internal class CopyCollidedNodeUseCaseTest {
+
+    private val getChatFilesUseCase: GetChatFilesUseCase = mock()
+    private val copyTypedNodeUseCase: CopyTypedNodeUseCase = mock()
+    private val copyNodeUseCase: CopyNodeUseCase = mock()
+
+    private lateinit var underTest: CopyCollidedNodeUseCase
+
+    @BeforeEach
+    fun setUp() {
+        underTest = CopyCollidedNodeUseCase(
+            getChatFilesUseCase = getChatFilesUseCase,
+            copyTypedNodeUseCase = copyTypedNodeUseCase,
+            copyNodeUseCase = copyNodeUseCase
+        )
+    }
+
+    @AfterEach
+    fun resetMocks() {
+        reset(copyNodeUseCase, copyTypedNodeUseCase, getChatFilesUseCase)
+    }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase correctly copies a node when there is no exception`() =
+        runTest {
+            val nodeNameCollision = mock<NodeNameCollision.Default> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+                on { renameName } doReturn "new name"
+            }
+            whenever(copyNodeUseCase(NodeId(any()), NodeId(any()), anyOrNull())).thenReturn(
+                NodeId(
+                    1L
+                )
+            )
+
+            val result = underTest(nodeNameCollision, rename = true)
+
+            verify(copyNodeUseCase).invoke(NodeId(1L), NodeId(2L), "new name")
+            assertThat(result.count).isEqualTo(1)
+            assertThat(result.errorCount).isEqualTo(0)
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase correctly handles exceptions and returns the appropriate MoveRequestResult`() =
+        runTest {
+            val nodeNameCollision = mock<NodeNameCollision.Default> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+                on { renameName } doReturn "new name"
+            }
+            whenever(
+                copyNodeUseCase(
+                    NodeId(any()),
+                    NodeId(any()),
+                    anyOrNull()
+                )
+            ).thenThrow(RuntimeException::class.java)
+
+            val result = underTest(nodeNameCollision, rename = true)
+
+            verify(copyNodeUseCase).invoke(NodeId(1L), NodeId(2L), "new name")
+            assertThat(result.count).isEqualTo(1)
+            assertThat(result.errorCount).isEqualTo(1)
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase correctly copies a chat node when there is no exception`() =
+        runTest {
+            val chatFile = mock<ChatDefaultFile> {
+                on { id } doReturn NodeId(1L)
+            }
+            val nodeNameCollision = mock<NodeNameCollision.Chat> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+                on { chatId } doReturn 1L
+                on { messageId } doReturn 1L
+            }
+            whenever(getChatFilesUseCase(1L, 1L)).thenReturn(listOf(chatFile))
+            whenever(copyTypedNodeUseCase(chatFile, NodeId(2L), null)).thenReturn(NodeId(2L))
+
+            val result = underTest(nodeNameCollision, rename = false)
+
+            assertThat(result.count).isEqualTo(1)
+            assertThat(result.errorCount).isEqualTo(0)
+        }
+
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase correctly handles exceptions and returns the appropriate MoveRequestResult when chat file is not found`() =
+        runTest {
+            val chatFile = mock<ChatDefaultFile> {
+                on { id } doReturn NodeId(9L)
+            }
+            val name = "new name"
+            val nodeNameCollision = mock<NodeNameCollision.Chat> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+                on { renameName } doReturn "new name"
+                on { chatId } doReturn 1L
+                on { messageId } doReturn 1L
+            }
+            whenever(getChatFilesUseCase(1L, 1L)).thenReturn(listOf(chatFile))
+            whenever(copyTypedNodeUseCase(chatFile, NodeId(2L), name)).thenReturn(NodeId(1L))
+
+            val result = underTest(nodeNameCollision, rename = true)
+
+            assertThat(result.count).isEqualTo(1)
+            assertThat(result.errorCount).isEqualTo(1)
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase correctly handles exceptions and returns the appropriate MoveRequestResult when copying a chat node`() =
+        runTest {
+            val chatFile = mock<ChatDefaultFile>()
+            val nodeNameCollision = mock<NodeNameCollision.Chat> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+                on { renameName } doReturn "new name"
+                on { chatId } doReturn 1L
+                on { messageId } doReturn 1L
+            }
+            whenever(getChatFilesUseCase(any(), any())).thenThrow(RuntimeException::class.java)
+            whenever(
+                copyTypedNodeUseCase(
+                    chatFile,
+                    NodeId(2L),
+                    null
+                )
+            ).thenThrow(RuntimeException::class.java)
+
+            val result = underTest(nodeNameCollision, rename = false)
+
+            assert(result.count == 1)
+            assert(result.errorCount == 1)
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase throws exception when copy node throw ForeignNodeException`() =
+        runTest {
+            val nodeNameCollision = mock<NodeNameCollision.Default> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+            }
+            whenever(copyNodeUseCase(NodeId(any()), NodeId(any()), anyOrNull()))
+                .thenThrow(ForeignNodeException::class.java)
+
+            assertThrows<ForeignNodeException> {
+                underTest(nodeNameCollision, false)
+            }
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase throws exception when copy node throw NotEnoughQuotaMegaException`() =
+        runTest {
+            val nodeNameCollision = mock<NodeNameCollision.Default> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+            }
+            whenever(copyNodeUseCase(NodeId(any()), NodeId(any()), anyOrNull()))
+                .thenAnswer {
+                    throw NotEnoughQuotaMegaException(1, "")
+                }
+            assertThrows<NotEnoughQuotaMegaException> {
+                underTest(nodeNameCollision, false)
+            }
+        }
+
+    @Test
+    fun `test that CopyCollidedNodeUseCase throws exception when copy node throw QuotaExceededMegaException`() =
+        runTest {
+            val nodeNameCollision = mock<NodeNameCollision.Default> {
+                on { nodeHandle } doReturn 1L
+                on { parentHandle } doReturn 2L
+            }
+            whenever(copyNodeUseCase(NodeId(any()), NodeId(any()), anyOrNull()))
+                .thenAnswer {
+                    throw QuotaExceededMegaException(1, "")
+                }
+
+            assertThrows<QuotaExceededMegaException> {
+                underTest(nodeNameCollision, false)
+            }
+        }
+
+}
