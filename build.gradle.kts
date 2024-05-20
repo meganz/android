8f0d631dca9cbf95f06b2ec9665bc3afff8d7a4b
HM Tamim <ht@mega.co.nz>
2024-05-20 14:18:49 +0700

    Update versionName for hotfix release

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "13.1"
+extra["appVersion"] = "13.1.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 34
