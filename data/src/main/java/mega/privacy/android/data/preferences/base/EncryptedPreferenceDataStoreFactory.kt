9a12bdab21948d89fe409703c757253e504621d1
Hai Luong <lh@mega.co.nz>
2024-05-27 08:18:06 +0700

    AND-18856: Fix crash create encrypted datastore some devices

--- data/src/main/java/mega/privacy/android/data/preferences/base/EncryptedPreferenceDataStoreFactory.kt
+++ data/src/main/java/mega/privacy/android/data/preferences/base/EncryptedPreferenceDataStoreFactory.kt
@@ -2,18 +2,22 @@
 
 package mega.privacy.android.data.preferences.base
 
+import android.content.Context
 import androidx.datastore.core.DataMigration
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKey
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import timber.log.Timber
 
 /**
  * Creates Preferences DataStore instance stored in [EncryptedFile].
@@ -33,20 +37,38 @@ import kotlinx.coroutines.sync.withLock
  * @see PreferenceDataStoreFactory.create
  */
 fun PreferenceDataStoreFactory.createEncrypted(
+    context: Context,
     corruptionHandler: ReplaceFileCorruptionHandler<Preferences>? = null,
     migrations: List<DataMigration<Preferences>> = listOf(),
     scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
-    produceFile: () -> EncryptedFile,
+    masterKey: MasterKey,
+    fileName: String,
 ): DataStore<Preferences> {
-    val delegate = DataStoreFactory.createEncrypted(
-        serializer = PreferencesSerializer,
-        corruptionHandler = corruptionHandler,
-        migrations = migrations,
-        scope = scope,
-        produceFile = produceFile,
-    )
-
-    return PreferenceDataStore(delegate)
+    return try {
+        val encryptedFile = EncryptedFile.Builder(
+            context,
+            context.preferencesDataStoreFile(fileName),
+            masterKey,
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+        val delegate = DataStoreFactory.createEncrypted(
+            serializer = PreferencesSerializer,
+            corruptionHandler = corruptionHandler,
+            migrations = migrations,
+            scope = scope,
+            produceFile = { encryptedFile },
+        )
+        PreferenceDataStore(delegate)
+    } catch (e: Exception) {
+        // rollback to unencrypted DataStore if failed to create EncryptedFile
+        // https://console.firebase.google.com/u/1/project/megapoc-25443/crashlytics/app/android:mega.privacy.android.app/issues/8a1e160bcab8e85c2ecc3334d19a75ea
+        Timber.e(e, "Failed to create EncryptedFile for $fileName")
+        create(
+            corruptionHandler = corruptionHandler,
+            scope = scope,
+            produceFile = { context.preferencesDataStoreFile(fileName) }
+        )
+    }
 }
 
 internal class PreferenceDataStore(private val delegate: DataStore<Preferences>) :
