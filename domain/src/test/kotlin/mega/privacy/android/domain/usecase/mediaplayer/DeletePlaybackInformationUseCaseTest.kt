4bfad5b4da3ebc1be762366637621f00a9efe8cb
Kevin Sun <ks@mega.co.nz>
2024-05-27 17:09:43 +1200

    CC-7249 AND - Create unit test for DeletePlaybackInformationUseCase

---
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/mediaplayer/DeletePlaybackInformationUseCaseTest.kt
@@ -0,0 +1,36 @@
+package mega.privacy.android.domain.usecase.mediaplayer
+
+import kotlinx.coroutines.test.runTest
+import mega.privacy.android.domain.repository.MediaPlayerRepository
+import mega.privacy.android.domain.usecase.mediaplayer.videoplayer.DeletePlaybackInformationUseCase
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.verify
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DeletePlaybackInformationUseCaseTest {
+    private lateinit var underTest: DeletePlaybackInformationUseCase
+    private val mediaPlayerRepository = mock<MediaPlayerRepository>()
+    private val testMediaId = 1L
+
+    @BeforeAll
+    fun setUp() {
+        underTest = DeletePlaybackInformationUseCase(mediaPlayerRepository = mediaPlayerRepository)
+    }
+
+    @BeforeEach
+    fun resetMock() {
+        reset(mediaPlayerRepository)
+    }
+
+    @Test
+    fun `test that the function is invoked as expected`() =
+        runTest {
+            underTest(testMediaId)
+            verify(mediaPlayerRepository).deletePlaybackInformation(testMediaId)
+        }
+}
