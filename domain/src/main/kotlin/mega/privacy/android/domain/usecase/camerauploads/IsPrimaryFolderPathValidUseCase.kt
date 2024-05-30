84ad700fcd999098173bd36d6ab3c23893ec206a
Gregg Meyrick Jover <gmj@mega.co.nz>
2024-05-30 12:56:42 +0800

    CU-796: Update IsPrimaryFolderPathValidUseCase with Only Two Use Cases
    The entire Use Case logic has been replaced with IsFolderPathExistingUseCase and IsPrimaryFolderPathUnrelatedToSecondaryFolderUseCase for better reusability.

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/camerauploads/IsPrimaryFolderPathValidUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/camerauploads/IsPrimaryFolderPathValidUseCase.kt
@@ -1,50 +1,29 @@
 package mega.privacy.android.domain.usecase.camerauploads
 
-import mega.privacy.android.domain.repository.FileSystemRepository
-import mega.privacy.android.domain.usecase.IsSecondaryFolderEnabled
-import java.nio.file.Paths
 import javax.inject.Inject
 
 /**
  * Use Case that checks whether the Primary Folder path is valid or not
  *
- * @property getSecondaryFolderPathUseCase [GetSecondaryFolderPathUseCase]
- * @property fileSystemRepository [FileSystemRepository]
- * @property isSecondaryFolderEnabled [IsSecondaryFolderEnabled]
+ * @property isFolderPathExistingUseCase Checks if the specific Local Folder exists or not
+ * @property isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase Checks if the specific Local
+ * Primary Folder Path is not the same Folder or a parent Folder or a sub Folder from the current
+ * Local Secondary Folder
  */
 class IsPrimaryFolderPathValidUseCase @Inject constructor(
-    private val getSecondaryFolderPathUseCase: GetSecondaryFolderPathUseCase,
-    private val fileSystemRepository: FileSystemRepository,
-    private val isSecondaryFolderEnabled: IsSecondaryFolderEnabled,
+    private val isFolderPathExistingUseCase: IsFolderPathExistingUseCase,
+    private val isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase: IsPrimaryFolderPathUnrelatedToSecondaryFolderUseCase,
 ) {
 
     /**
      * Invocation function
      *
-     * @param path The Primary Folder path, which can be nullable
-     * @return true if the Primary Folder exists and is different from the Secondary Folder
-     * local path. Otherwise, return false
+     * @param path The Local Primary Folder path, which can be nullable
+     * @return true if the Local Primary Folder exists and is unrelated to the Local Secondary
+     * Folder
      */
-    suspend operator fun invoke(path: String?): Boolean =
-        if (!path.isNullOrBlank() && fileSystemRepository.doesFolderExists(path)) {
-            if (isSecondaryFolderEnabled()) {
-                val secondaryFolderPath = getSecondaryFolderPathUseCase()
-                if (secondaryFolderPath.isNotBlank()) {
-                    val primaryAbsolutePath = Paths.get(path).toAbsolutePath()
-                    val secondaryAbsolutePath =
-                        Paths.get(secondaryFolderPath).toAbsolutePath()
-
-                    !primaryAbsolutePath.startsWith(secondaryAbsolutePath) &&
-                            !secondaryAbsolutePath.startsWith(primaryAbsolutePath)
-                } else {
-                    // An empty Secondary Folder Path automatically makes the Primary Folder Path valid
-                    true
-                }
-            } else {
-                // When Secondary Folder uploads are disabled, it automatically makes the Primary
-                // Folder path valid
-                true
-            }
-        } else false
+    suspend operator fun invoke(path: String?): Boolean = path?.let {
+        isFolderPathExistingUseCase(it) && isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase(it)
+    } ?: false
 }
 
--- domain/src/test/kotlin/mega/privacy/android/domain/usecase/camerauploads/IsPrimaryFolderPathValidUseCaseTest.kt
+++ domain/src/test/kotlin/mega/privacy/android/domain/usecase/camerauploads/IsPrimaryFolderPathValidUseCaseTest.kt
@@ -2,22 +2,13 @@ package mega.privacy.android.domain.usecase.camerauploads
 
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
-import mega.privacy.android.domain.repository.FileSystemRepository
-import mega.privacy.android.domain.usecase.IsSecondaryFolderEnabled
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.NullAndEmptySource
-import org.junit.jupiter.params.provider.ValueSource
-import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.whenever
-import java.util.stream.Stream
 
 /**
  * Test class for [IsPrimaryFolderPathValidUseCase]
@@ -27,85 +18,60 @@ internal class IsPrimaryFolderPathValidUseCaseTest {
 
     private lateinit var underTest: IsPrimaryFolderPathValidUseCase
 
-    private val getSecondaryFolderPathUseCase = mock<GetSecondaryFolderPathUseCase>()
-    private val fileSystemRepository = mock<FileSystemRepository>()
-    private val isSecondaryFolderEnabled = mock<IsSecondaryFolderEnabled>()
+    private val isFolderPathExistingUseCase = mock<IsFolderPathExistingUseCase>()
+    private val isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase =
+        mock<IsPrimaryFolderPathUnrelatedToSecondaryFolderUseCase>()
 
     @BeforeAll
     fun setUp() {
         underTest = IsPrimaryFolderPathValidUseCase(
-            getSecondaryFolderPathUseCase = getSecondaryFolderPathUseCase,
-            fileSystemRepository = fileSystemRepository,
-            isSecondaryFolderEnabled = isSecondaryFolderEnabled,
+            isFolderPathExistingUseCase = isFolderPathExistingUseCase,
+            isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase = isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase,
         )
     }
 
     @BeforeEach
     fun resetMocks() {
-        reset(
-            getSecondaryFolderPathUseCase,
-            fileSystemRepository,
-            isSecondaryFolderEnabled,
-        )
+        reset(isFolderPathExistingUseCase, isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase)
     }
 
-    @ParameterizedTest(name = "path: \"{0}\"")
-    @NullAndEmptySource
-    @ValueSource(strings = [" "])
-    fun `test that the primary folder path is invalid if it is null or empty`(path: String?) =
-        runTest {
-            assertThat(underTest(path)).isFalse()
-        }
-
     @Test
-    fun `test that the primary folder path is invalid if it does not exist in the file system`() =
-        runTest {
-            whenever(fileSystemRepository.doesFolderExists(any())).thenReturn(false)
-
-            assertThat(underTest("new/path")).isFalse()
-        }
-
-    @Test
-    fun `test that the primary folder path is valid if it exists is the file system and secondary folder uploads are disabled`() =
-        runTest {
-            whenever(fileSystemRepository.doesFolderExists(any())).thenReturn(true)
-            whenever(isSecondaryFolderEnabled()).thenReturn(false)
-
-            assertThat(underTest("new/path")).isTrue()
-        }
-
-    @Test
-    fun `test that the primary folder path is valid if it exists in the file system and secondary folder uploads are enabled but the secondary folder path is empty`() =
-        runTest {
-            whenever(fileSystemRepository.doesFolderExists(any())).thenReturn(true)
-            whenever(isSecondaryFolderEnabled()).thenReturn(false)
-            whenever(getSecondaryFolderPathUseCase()).thenReturn("")
-
-            assertThat(underTest("new/path")).isTrue()
-        }
-
-    @ParameterizedTest(name = "when primary folder path is \"{0}\" and secondary folder path is \"{1}\", then is primary folder valid is {2}")
-    @MethodSource("provideFolderParameters")
-    fun `test that the primary folder path is valid or not if it exists in the file system and secondary folder uploads are enabled and the secondary folder path exists`(
-        primaryFolderPath: String,
-        secondaryFolderPath: String,
-        isPrimaryFolderValid: Boolean,
-    ) = runTest {
-        whenever(fileSystemRepository.doesFolderExists(any())).thenReturn(true)
-        whenever(isSecondaryFolderEnabled()).thenReturn(true)
-        whenever(getSecondaryFolderPathUseCase()).thenReturn(secondaryFolderPath)
-
-        assertThat(underTest(primaryFolderPath)).isEqualTo(isPrimaryFolderValid)
+    fun `test that the primary folder is invalid if its path is null`() = runTest {
+        assertThat(underTest(null)).isFalse()
     }
 
-    private fun provideFolderParameters() = Stream.of(
-        Arguments.of("A/B/C", "A/B/C", false),
-        Arguments.of("A/B/C", "A/B/CD", true),
-        Arguments.of("A/B/CD", "A/B/C", true),
-        Arguments.of("A/B", "A/B/C", false),
-        Arguments.of("A/B/C", "A/B", false),
-        Arguments.of("A", "A", false),
-        Arguments.of("A", "B", true),
-        Arguments.of("B", "A", true),
-    )
+    @Test
+    fun `test that the primary folder is invalid if the file system cannot find it`() = runTest {
+        val folderPath = "primary/folder/path"
+
+        whenever(isFolderPathExistingUseCase(folderPath)).thenReturn(false)
+
+        assertThat(underTest(folderPath)).isFalse()
+    }
+
+    @Test
+    fun `test that the primary folder is invalid if it exists in the local file system but is related to the secondary folder`() =
+        runTest {
+            val folderPath = "primary/folder/path"
+
+            whenever(isFolderPathExistingUseCase(folderPath)).thenReturn(true)
+            whenever(isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase(folderPath)).thenReturn(
+                false
+            )
+
+            assertThat(underTest(folderPath)).isFalse()
+        }
+
+    @Test
+    fun `test that the primary folder is valid if it exists in the local file system and is unrelated to the secondary folder`() =
+        runTest {
+            val folderPath = "primary/folder/path"
+
+            whenever(isFolderPathExistingUseCase(folderPath)).thenReturn(true)
+            whenever(isPrimaryFolderPathUnrelatedToSecondaryFolderUseCase(folderPath)).thenReturn(
+                true
+            )
+
+            assertThat(underTest(folderPath)).isTrue()
+        }
 }
