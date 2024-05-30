3654d92a99ad5b494968dc6658ed794c600d79e2
Gregg Meyrick Jover <gmj@mega.co.nz>
2024-05-30 17:07:32 +0800

    AND-17320: Remove the Unused RxJava GetPublicNodeUseCase
    As a result of deleting the Use Case, the unused class AndroidMegaRichLinkMessage and some code in AndroidMegaChatMessage are also removed.

--- data/src/main/java/mega/privacy/android/data/model/chat/AndroidMegaChatMessage.kt
+++ data/src/main/java/mega/privacy/android/data/model/chat/AndroidMegaChatMessage.kt
@@ -12,7 +12,6 @@ import nz.mega.sdk.MegaChatMessage
  * @property infoToShow
  * @property isShowAvatar
  * @property isUploading
- * @property richLinkMessage
  * @property contactLinkResult
  */
 data class AndroidMegaChatMessage(
@@ -21,10 +20,8 @@ data class AndroidMegaChatMessage(
     var infoToShow: Int = -1,
     var isShowAvatar: Boolean = true,
     var isUploading: Boolean = false,
-    var richLinkMessage: AndroidMegaRichLinkMessage? = null,
     var contactLinkResult: ContactLink? = null,
 ) {
-
     constructor(message: MegaChatMessage?) : this() {
         this.message = message
     }
@@ -33,10 +30,4 @@ data class AndroidMegaChatMessage(
         pendingMessage = pendingMessage,
         isUploading = uploading
     )
-
-    companion object {
-        const val CHAT_ADAPTER_SHOW_TIME = 1
-        const val CHAT_ADAPTER_SHOW_NOTHING = 0
-        const val CHAT_ADAPTER_SHOW_ALL = 2
-    }
 }
