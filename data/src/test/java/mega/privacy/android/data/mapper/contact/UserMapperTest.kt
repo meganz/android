0ef0f1ca64e7f9016c5227e2b20284b6b326fe71
Gregg Meyrick Jover <gmj@mega.co.nz>
2024-06-13 10:54:41 +0800

    AND-18982: Improve Tests for UserMapperTest
    The Test Suite now covers all MegaUser Visibility Mappings, increasing the data Code Coverage as a result.

--- data/src/test/java/mega/privacy/android/data/mapper/contact/UserMapperTest.kt
+++ data/src/test/java/mega/privacy/android/data/mapper/contact/UserMapperTest.kt
@@ -1,59 +1,84 @@
 package mega.privacy.android.data.mapper.contact
 
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
 import mega.privacy.android.domain.entity.contacts.User
 import mega.privacy.android.domain.entity.user.UserChanges
 import mega.privacy.android.domain.entity.user.UserVisibility
 import nz.mega.sdk.MegaUser
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito.spy
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.reset
-import org.mockito.kotlin.whenever
 
+/**
+ * Test class for [UserMapper]
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class UserMapperTest {
+internal class UserMapperTest {
     private lateinit var underTest: UserMapper
-    private val userChangeMapper: UserChangeMapper = mock<UserChangeMapper>() {
-        on { invoke(MegaUser.CHANGE_TYPE_AUTHRING.toLong()) }.thenReturn(listOf(UserChanges.AuthenticationInformation))
-    }
+
+    private val userChangeMapper: UserChangeMapper = spy(UserChangeMapper())
 
     @BeforeAll
     fun setup() {
         underTest = UserMapper(userChangeMapper)
     }
 
-    @BeforeEach
-    fun resetMocks() {
-        reset(
-            userChangeMapper,
-        )
+    @TestFactory
+    fun `test that a mega user is mapped to a user with the correct user visibility`() = listOf(
+        MegaUser.VISIBILITY_UNKNOWN to UserVisibility.Unknown,
+        MegaUser.VISIBILITY_HIDDEN to UserVisibility.Hidden,
+        MegaUser.VISIBILITY_VISIBLE to UserVisibility.Visible,
+        MegaUser.VISIBILITY_INACTIVE to UserVisibility.Inactive,
+        MegaUser.VISIBILITY_BLOCKED to UserVisibility.Blocked,
+    ).map { (inputVisibility, expectedVisibility) ->
+        dynamicTest("test that $inputVisibility is mapped to $expectedVisibility") {
+            val megaUser = mock<MegaUser> {
+                on { handle }.thenReturn(123L)
+                on { email }.thenReturn("email")
+                on { visibility }.thenReturn(inputVisibility)
+                on { timestamp }.thenReturn(123L)
+                on { changes }.thenReturn(MegaUser.CHANGE_TYPE_AUTHRING.toLong())
+            }
+
+            val expected = User(
+                handle = megaUser.handle,
+                email = megaUser.email,
+                visibility = expectedVisibility,
+                timestamp = megaUser.timestamp,
+                userChanges = listOf(UserChanges.AuthenticationInformation)
+            )
+            val actual = underTest(megaUser)
+
+            assertThat(actual).isEqualTo(expected)
+        }
     }
 
     @Test
-    fun `test that invoke returns User`() {
-        val megaUser = mock<MegaUser> {
-            on { handle }.thenReturn(123L)
-            on { email }.thenReturn("email")
-            on { visibility }.thenReturn(MegaUser.VISIBILITY_VISIBLE)
-            on { timestamp }.thenReturn(123L)
-            on { changes }.thenReturn(MegaUser.CHANGE_TYPE_AUTHRING.toLong())
-        }
-        whenever(userChangeMapper(MegaUser.CHANGE_TYPE_AUTHRING.toLong())).thenReturn(
-            listOf(
-                UserChanges.AuthenticationInformation
-            )
-        )
-        val expectedUser = User(
-            handle = megaUser.handle,
-            email = megaUser.email,
-            visibility = UserVisibility.Visible,
-            timestamp = megaUser.timestamp,
-            userChanges = userChangeMapper(megaUser.changes)
-        )
+    fun `test that a mega user with an unspecified user visibility is mapped to a user with an unknown user visibility`() =
+        runTest {
+            val megaUser = mock<MegaUser> {
+                on { handle }.thenReturn(123L)
+                on { email }.thenReturn("email")
+                // 100 is unspecified in the Mapper
+                on { visibility }.thenReturn(100)
+                on { timestamp }.thenReturn(123L)
+                on { changes }.thenReturn(MegaUser.CHANGE_TYPE_AUTHRING.toLong())
+            }
 
-        assertThat(underTest(megaUser)).isEqualTo(expectedUser)
-    }
+            val expected = User(
+                handle = megaUser.handle,
+                email = megaUser.email,
+                visibility = UserVisibility.Unknown,
+                timestamp = megaUser.timestamp,
+                userChanges = listOf(UserChanges.AuthenticationInformation)
+            )
+            val actual = underTest(megaUser)
+
+            assertThat(actual).isEqualTo(expected)
+        }
 }
