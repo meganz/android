afc820c1ffb1cb40a078c1271060d7e8b8ee805e
Ilya Konnov <iko@mega.co.nz>
2024-07-24 14:00:13 +0800

    Update megaSdkVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20240710.022013-rel"
+extra["megaSdkVersion"] = "20240724.055557-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
