b7f243c822a7876463b88fbb8997676d29bc1a09
Robin Shi <rsh@mega.co.nz>
2024-07-30 08:00:10 +1200

    SDK-4132 Add CSV parser in SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20240725.100505-dev"
+extra["megaSdkVersion"] = "20240729.093714-dev"
 
 //JDK and Java Version
 extra["jdk"] = "17"
