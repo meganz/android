cf9cf0b52c6e84683e4adbfd1a3522481099c047
Bhavna Thacker <bt@mega.co.nz>
2024-08-09 11:13:27 +0530

    Update App version(14.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "14.1"
+extra["appVersion"] = "14.0"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 35
