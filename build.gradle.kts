19e3dd67206f282fc0cd37d7d0777e8aa4ced571
Amr Mohsen <amoh@mega.co.nz>
2024-08-09 13:25:18 +1200

    Fix: Fix for wrong App Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "“14.1”"
+extra["appVersion"] = "14.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 35
