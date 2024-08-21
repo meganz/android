bec48b469784fc6412e2b531b57fd9087ebdb075
Nikhil <nna@mega.co.nz>
2024-08-21 14:43:43 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ extra["targetSdkVersion"] = 34
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20240808.053359-rel"
+extra["megaSdkVersion"] = "20240821.083820-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
