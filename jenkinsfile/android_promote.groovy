88a0653f10a045f7f0a0efbac46f6f18ed2afa57
HM Tamim <ht@mega.co.nz>
2024-10-02 21:38:49 +1300

    AND-19786 Add Google Play Deploy stage in promote pipeline

--- jenkinsfile/android_promote.groovy
+++ jenkinsfile/android_promote.groovy
@@ -13,7 +13,6 @@ pipeline {
     agent { label 'mac-jenkins-slave-android || mac-jenkins-slave' }
 
     options {
-        // Stop the build early in case of compile or test failures
         skipStagesAfterUnstable()
         buildDiscarder(logRotator(numToKeepStr: '30', artifactNumToKeepStr: '1'))
         timeout(time: 2, unit: 'HOURS')
@@ -21,9 +20,9 @@ pipeline {
     }
 
     parameters {
-        choice(name: 'TRACK', choices: ['Select', 'Internal', 'Alpha', 'Beta', 'Production'], description: 'Target track/channel to promote to')
+        choice(name: 'TRACK', choices: ['Select', 'Alpha', 'Beta', 'Production'], description: 'Target track/channel to promote to')
         choice(name: 'PERCENTAGE', choices: ['Select', '0', '25', '50', '100'], description: 'Rollout percentage')
-        string(name: 'VERSION', defaultValue: '', description: 'Version of the build to be promoted, e.g. 14.1(242330236)')
+        string(name: 'VERSION', defaultValue: '', description: 'Version code of the build to be promoted, e.g. 242330236')
     }
 
     stages {
@@ -32,15 +31,22 @@ pipeline {
                 script {
                     validateParams(params)
                     if (!env.BUILD_NUMBER) {
-                        error("Pipeline must be run the Jenkins dashboard")
+                        error("Pipeline must be run from the Jenkins dashboard")
                     }
                 }
             }
         }
 
-        stage('Print Provided Params') {
+        stage('Deploy to Google Play') {
             steps {
-                echo "Target Track: ${params.TRACK.toLowerCase()}, Rollout: ${params.PERCENTAGE}, Version: ${params.VERSION}"
+                script {
+                    androidApkMove  googleCredentialsId : 'GOOGLE_PLAY_SERVICE_ACCOUNT_CREDENTIAL',
+                                    applicationId       : 'mega.privacy.android.app',
+                                    trackName           : "${params.TRACK.toLowerCase()}",
+                                    rolloutPercentage   : "${params.PERCENTAGE}",
+                                    fromVersionCode     : true,
+                                    versionCodes        : "233140859, ${params.VERSION}"
+                }
             }
         }
     }
@@ -60,24 +66,14 @@ def validateParams(params) {
     if (!params.PERCENTAGE) {
         error("'PERCENTAGE' parameter is missing. Please specify the rollout percentage.")
     } else if (params.PERCENTAGE == 'Select') {
-        error("'PERCENTAGE' parameter wasn't selected. Please choose a valid rollout percentage (e.g., 25, 50, or 100)")
+        error("'PERCENTAGE' parameter was not selected. Please choose a valid rollout percentage (e.g., 25, 50, or 100).")
     } else if (!(params.PERCENTAGE.isInteger() && params.PERCENTAGE.toInteger() in 0..100)) {
         error("Invalid 'PERCENTAGE' value. Percentage must be an integer between 0 and 100.")
     }
 
     if (!params.VERSION) {
-        error("'VERSION' parameter is missing. Please provide the version of the build (e.g., 14.1(242330236)).")
-    } else if (!isValidReleaseVersion(params.VERSION)) {
-        error("Invalid 'VERSION' format. Please provide a valid release version (e.g., 14.1(242330236))")
+        error("'VERSION' parameter is missing. Please provide the version code of the build (e.g., 242330236).")
+    } else if (!params.VERSION.isInteger()) {
+        error("Invalid 'VERSION' value. Please provide a valid version code of the build (e.g., 242330236).")
     }
-}
-
-/**
- * Validate release version
- * @param version
- * @return
- */
-static def isValidReleaseVersion(String version) {
-    def pattern = ~/^\d+\.\d+\(\d+\)$/
-    return pattern.matcher(version).matches()
 }
