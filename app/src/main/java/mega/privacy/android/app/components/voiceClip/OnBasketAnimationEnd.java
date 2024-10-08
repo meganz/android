ad4443a0ac8cd810a8828f5466a183b7f55ace1e
Hai Luong <lh@mega.co.nz>
2024-10-08 14:33:50 +1300

    AND-19810: Remove unused source code

--- app/src/main/java/mega/privacy/android/app/components/voiceClip/OnBasketAnimationEnd.java
+++
@@ -1,7 +0,0 @@
-package mega.privacy.android.app.components.voiceClip;
-
-public interface OnBasketAnimationEnd {
-    void onAnimationEnd();
-
-    void deactivateRecordButton();
-}
--- app/src/main/java/mega/privacy/android/app/components/voiceClip/OnRecordClickListener.java
+++
@@ -1,7 +0,0 @@
-package mega.privacy.android.app.components.voiceClip;
-
-import android.view.View;
-
-public interface OnRecordClickListener {
-    void onClick(View v);
-}
