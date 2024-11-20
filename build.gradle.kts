bd46187c43b12e3007bc37ed0653d97f960e535b
Robin Shi <rsh@mega.co.nz>
2024-11-20 16:23:30 +1300

    AND-19986 Disable c-ares from curl for Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ extra["targetSdkVersion"] = 35
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20241113.045103-rel"
+extra["megaSdkVersion"] = "20241120.021947-dev"
 
 //JDK and Java Version
 extra["jdk"] = "17"
