749d1faf6639582aa4def462365844c099df5fd4
Daniel Oosthuizen <do@mega.co.nz>
2024-11-26 12:13:20 +1300

    AND-20011 - Create Psa container view model

---
+++ app/src/main/java/mega/privacy/android/app/presentation/psa/PsaViewModel.kt
@@ -0,0 +1,70 @@
+package mega.privacy.android.app.presentation.psa
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import mega.privacy.android.app.presentation.psa.mapper.PsaStateMapper
+import mega.privacy.android.app.presentation.psa.model.PsaState
+import mega.privacy.android.domain.usecase.psa.DismissPsaUseCase
+import mega.privacy.android.domain.usecase.psa.FetchPsaUseCase
+import mega.privacy.android.domain.usecase.psa.MonitorPsaUseCase
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * Psa view model
+ */
+@HiltViewModel
+class PsaViewModel(
+    private val monitorPsaUseCase: MonitorPsaUseCase,
+    private val fetchPsaUseCase: FetchPsaUseCase,
+    private val dismissPsaUseCase: DismissPsaUseCase,
+    private val psaStateMapper: PsaStateMapper,
+    private val currentTimeProvider: () -> Long,
+) : ViewModel() {
+
+    @Inject
+    constructor(
+        monitorPsaUseCase: MonitorPsaUseCase,
+        fetchPsaUseCase: FetchPsaUseCase,
+        dismissPsaUseCase: DismissPsaUseCase,
+        psaStateMapper: PsaStateMapper,
+    ) : this(
+        monitorPsaUseCase = monitorPsaUseCase,
+        fetchPsaUseCase = fetchPsaUseCase,
+        dismissPsaUseCase = dismissPsaUseCase,
+        psaStateMapper = psaStateMapper,
+        currentTimeProvider = System::currentTimeMillis
+    )
+
+    private val _state = MutableStateFlow<PsaState>(PsaState.NoPsa)
+
+    /**
+     * State
+     */
+    val state: StateFlow<PsaState> = _state
+
+    init {
+        viewModelScope.launch {
+            monitorPsaUseCase(currentTimeProvider)
+                .map { psaStateMapper(it) }
+                .catch { Timber.e(it, "Error in monitoring psa") }
+                .collect { _state.value = it }
+        }
+    }
+
+    /**
+     * Mark as seen
+     *
+     * @param psaId
+     */
+    fun markAsSeen(psaId: Int) = viewModelScope.launch {
+        dismissPsaUseCase(psaId)
+        _state.value = psaStateMapper(fetchPsaUseCase(currentTimeProvider()))
+    }
+}
