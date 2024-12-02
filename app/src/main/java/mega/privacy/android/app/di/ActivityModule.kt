72ceae65819d6ac6b4238f8eadfe9a4fe4897dec
Daniel Oosthuizen <do@mega.co.nz>
2024-12-02 15:09:50 +1300

    AND-20033 Create psa implementation for legacy screens

--- app/src/main/java/mega/privacy/android/app/di/ActivityModule.kt
+++ app/src/main/java/mega/privacy/android/app/di/ActivityModule.kt
@@ -4,6 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import mega.privacy.android.app.presentation.psa.legacy.LegacyPsaHandler
+import mega.privacy.android.app.presentation.psa.legacy.PsaHandler
 import mega.privacy.android.app.presentation.security.PasscodeCheck
 import mega.privacy.android.app.presentation.security.PasscodeFacade
 
@@ -16,7 +18,20 @@ import mega.privacy.android.app.presentation.security.PasscodeFacade
 @InstallIn(ActivityComponent::class)
 abstract class ActivityModule {
 
+    /**
+     * Bind passcode check
+     *
+     * @param implementation
+     */
     @Binds
     abstract fun bindPasscodeCheck(implementation: PasscodeFacade): PasscodeCheck
+
+    /**
+     * Bind psa handler
+     *
+     * @param implementation
+     */
+    @Binds
+    abstract fun bindPsaHandler(implementation: LegacyPsaHandler): PsaHandler
 
 }
