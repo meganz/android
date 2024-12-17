dce95684b053a3f964ed19d0d228dafac4e3936d
Robin Shi <rsh@mega.co.nz>
2024-12-18 12:05:00 +1300

    Pre-release - v14.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ extra["targetSdkVersion"] = 35
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20241211.225756-rel"
+extra["megaSdkVersion"] = "20241217.214437-rel"
 
 //JDK and Java Version
 extra["jdk"] = "17"
