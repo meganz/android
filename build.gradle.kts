33505f127a24b8f481314131396f934069df792d
Hai Luong <lh@mega.co.nz>
2024-12-23 14:44:53 +1300

    AND-20082 Expire date shown incorrectly in Android app

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ extra["targetSdkVersion"] = 35
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20241217.214437-rel"
+extra["megaSdkVersion"] = "20241222.085348-dev"
 
 //JDK and Java Version
 extra["jdk"] = "17"
