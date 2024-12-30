d0c382bcbf9f12d0c07644c439a96ce63c311d1f
Javier Gomez <jg@mega.co.nz>
2024-12-30 12:25:04 +0100

    SAT-1514: Missing empty folder icon when syncing empty folder

--- feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/megapicker/MegaFolderPickerView.kt
+++ feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/megapicker/MegaFolderPickerView.kt
@@ -1,15 +1,27 @@
 package mega.privacy.android.feature.sync.ui.megapicker
 
 import mega.privacy.android.icon.pack.R as iconPackR
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.unit.dp
 import mega.privacy.android.core.formatter.formatFileSize
 import mega.privacy.android.core.formatter.formatModifiedDate
 import mega.privacy.android.core.ui.mapper.FileTypeIconMapper
@@ -23,8 +35,10 @@ import mega.privacy.android.legacy.core.ui.controls.lists.HeaderViewItem
 import mega.privacy.android.legacy.core.ui.controls.lists.NodeListViewItem
 import mega.privacy.android.shared.original.core.ui.controls.dividers.DividerType
 import mega.privacy.android.shared.original.core.ui.controls.dividers.MegaDivider
+import mega.privacy.android.shared.original.core.ui.controls.text.MegaText
 import mega.privacy.android.shared.original.core.ui.preview.CombinedThemePreviews
 import mega.privacy.android.shared.original.core.ui.theme.OriginalTempTheme
+import mega.privacy.android.shared.original.core.ui.theme.values.TextColor
 
 @Composable
 internal fun MegaFolderPickerView(
@@ -40,57 +54,111 @@ internal fun MegaFolderPickerView(
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(state = listState, modifier = modifier) {
-        item(
-            key = "header"
-        ) {
-            HeaderViewItem(
-                onSortOrderClick = onSortOrderClick,
-                onChangeViewTypeClick = onChangeViewTypeClick,
-                onEnterMediaDiscoveryClick = {},
-                sortOrder = sortOrder,
-                isListView = true,
-                showSortOrder = showSortOrder,
-                showChangeViewType = showChangeViewType
-            )
-        }
-        items(count = nodesList.size,
-            key = {
-                nodesList[it].node.id.longValue
-            }) {
-            val nodeEntity = nodesList[it].node
-
-            val icon = when (nodeEntity) {
-                is FolderNode -> nodeEntity.getIcon()
-                is FileNode -> fileTypeIconMapper(nodeEntity.type.extension)
-                else -> iconPackR.drawable.ic_generic_medium_solid
+        if (nodesList.isEmpty()) {
+            item(key = "empty state") {
+                EmptyFolderPlaceHolder(
+                    modifier = Modifier
+                        .fillParentMaxHeight()
+                        .fillParentMaxWidth()
+                )
             }
-            NodeListViewItem(
-                isSelected = false,
-                folderInfo = (nodeEntity as? FolderNode)
-                    ?.folderInfo(),
-                icon = icon,
-                thumbnailData = ThumbnailRequest(nodeEntity.id),
-                fileSize = (nodeEntity as? FileNode)
-                    ?.let { node -> formatFileSize(node.size, LocalContext.current) },
-                modifiedDate = (nodeEntity as? FileNode)
-                    ?.let { node ->
-                        formatModifiedDate(
-                            java.util.Locale(
-                                Locale.current.language, Locale.current.region
-                            ),
-                            node.modificationTime
-                        )
-                    },
-                name = nodeEntity.name,
-                showMenuButton = false,
-                isTakenDown = false,
-                isFavourite = false,
-                isSharedWithPublicLink = false,
-                onClick = { onFolderClick(nodeEntity) },
-                isEnabled = nodeEntity is FolderNode && nodesList[it].isDisabled.not(),
-            )
-            MegaDivider(dividerType = DividerType.FullSize)
+        } else {
+            item(
+                key = "header"
+            ) {
+                HeaderViewItem(
+                    onSortOrderClick = onSortOrderClick,
+                    onChangeViewTypeClick = onChangeViewTypeClick,
+                    onEnterMediaDiscoveryClick = {},
+                    sortOrder = sortOrder,
+                    isListView = true,
+                    showSortOrder = showSortOrder,
+                    showChangeViewType = showChangeViewType
+                )
+            }
+            items(count = nodesList.size,
+                key = {
+                    nodesList[it].node.id.longValue
+                }) {
+                val nodeEntity = nodesList[it].node
+
+                val icon = when (nodeEntity) {
+                    is FolderNode -> nodeEntity.getIcon()
+                    is FileNode -> fileTypeIconMapper(nodeEntity.type.extension)
+                    else -> iconPackR.drawable.ic_generic_medium_solid
+                }
+                NodeListViewItem(
+                    isSelected = false,
+                    folderInfo = (nodeEntity as? FolderNode)
+                        ?.folderInfo(),
+                    icon = icon,
+                    thumbnailData = ThumbnailRequest(nodeEntity.id),
+                    fileSize = (nodeEntity as? FileNode)
+                        ?.let { node -> formatFileSize(node.size, LocalContext.current) },
+                    modifiedDate = (nodeEntity as? FileNode)
+                        ?.let { node ->
+                            formatModifiedDate(
+                                java.util.Locale(
+                                    Locale.current.language, Locale.current.region
+                                ),
+                                node.modificationTime
+                            )
+                        },
+                    name = nodeEntity.name,
+                    showMenuButton = false,
+                    isTakenDown = false,
+                    isFavourite = false,
+                    isSharedWithPublicLink = false,
+                    onClick = { onFolderClick(nodeEntity) },
+                    isEnabled = nodeEntity is FolderNode && nodesList[it].isDisabled.not(),
+                )
+                MegaDivider(dividerType = DividerType.FullSize)
+            }
         }
+    }
+}
+
+@Composable
+private fun EmptyFolderPlaceHolder(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier.verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painterResource(iconPackR.drawable.ic_empty_folder_glass),
+            contentDescription = null,
+            modifier = Modifier
+                .size(128.dp)
+                .testTag(TAG_SYNC_MEGA_FOLDER_PICKER_LIST_SCREEN_NO_ITEMS)
+        )
+        MegaText(
+            text = stringResource(R.string.sync_file_browser_empty_folder),
+            textColor = TextColor.Secondary,
+            modifier = Modifier.padding(top = 8.dp),
+            style = MaterialTheme.typography.subtitle2,
+        )
+    }
+}
+
+@Composable
+@CombinedThemePreviews
+private fun MegaFolderPickerViewEmptyPreview() {
+    OriginalTempTheme(isDark = isSystemInDarkTheme()) {
+        MegaFolderPickerView(
+            nodesList = emptyList(),
+            sortOrder = "Name",
+            onSortOrderClick = {},
+            onChangeViewTypeClick = { },
+            showSortOrder = true,
+            listState = LazyListState(),
+            modifier = Modifier,
+            showChangeViewType = true,
+            onFolderClick = {},
+            fileTypeIconMapper = FileTypeIconMapper()
+        )
     }
 }
 
@@ -137,3 +205,6 @@ private fun FolderNode.folderInfo(): String {
         )
     }
 }
+
+internal const val TAG_SYNC_MEGA_FOLDER_PICKER_LIST_SCREEN_NO_ITEMS =
+    "sync_mega_folder_picker_list_screen_no_items"
---
+++ feature/sync/src/test/java/mega/privacy/android/feature/sync/ui/megapicker/MegaPickerRouteTest.kt
@@ -0,0 +1,41 @@
+package mega.privacy.android.feature.sync.ui.megapicker
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.flow.StateFlow
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@RunWith(AndroidJUnit4::class)
+class MegaPickerRouteTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val viewModel: MegaPickerViewModel = mock()
+    private val state: StateFlow<MegaPickerState> = mock()
+
+    @Test
+    fun `test that empty state is displayed when the folder is empty`() {
+        whenever(state.value).thenReturn(MegaPickerState(nodes = emptyList()))
+        whenever(viewModel.state).thenReturn(state)
+        composeTestRule.setContent {
+            MegaPickerRoute(
+                viewModel = viewModel,
+                syncPermissionsManager = mock(),
+                folderSelected = {},
+                backClicked = {},
+                fileTypeIconMapper = mock(),
+            )
+        }
+
+        composeTestRule.onNodeWithTag(TAG_SYNC_MEGA_FOLDER_PICKER_LIST_SCREEN_NO_ITEMS)
+            .assertIsDisplayed()
+    }
+}
