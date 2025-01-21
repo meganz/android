85ce2e46555279d15081b0cdb1146ef4e6f71079
Gregg Meyrick Jover <gmj@mega.co.nz>
2025-01-21 14:14:15 +1300

    AND-20231: Remove AttachNodesListener and AttachNodeToChatListener

--- app/src/main/java/mega/privacy/android/app/interfaces/AttachNodeToChatListener.kt
+++
@@ -1,7 +0,0 @@
-package mega.privacy.android.app.interfaces
-
-import mega.privacy.android.data.model.chat.AndroidMegaChatMessage
-
-interface AttachNodeToChatListener {
-    fun onSendSuccess(message: AndroidMegaChatMessage)
-}
