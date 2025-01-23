5f072dc2861d6371026151380307b51c82593a45
Daniel Oosthuizen <do@mega.co.nz>
2025-01-23 13:52:21 +1300

    AND-20237 Return null when no psa found

--- data/src/main/java/mega/privacy/android/data/repository/psa/PsaRepositoryImpl.kt
+++ data/src/main/java/mega/privacy/android/data/repository/psa/PsaRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package mega.privacy.android.data.repository.psa
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import mega.privacy.android.data.cache.Cache
-import mega.privacy.android.data.extensions.getRequestListener
+import mega.privacy.android.data.extensions.failWithError
 import mega.privacy.android.data.gateway.api.MegaApiGateway
 import mega.privacy.android.data.gateway.psa.PsaPreferenceGateway
+import mega.privacy.android.data.listener.OptionalMegaRequestListenerInterface
 import mega.privacy.android.data.mapper.psa.PsaMapper
 import mega.privacy.android.domain.entity.psa.Psa
 import mega.privacy.android.domain.qualifier.IoDispatcher
 import mega.privacy.android.domain.repository.psa.PsaRepository
+import nz.mega.sdk.MegaError
+import timber.log.Timber
 import javax.inject.Inject
 
 internal class PsaRepositoryImpl @Inject constructor(
@@ -25,11 +29,25 @@ internal class PsaRepositoryImpl @Inject constructor(
         return@withContext psaCache.get()
     }
 
-    private suspend fun getLatestPsa() = withContext(ioDispatcher) {
+    private suspend fun getLatestPsa(): Psa? = withContext(ioDispatcher) {
         suspendCancellableCoroutine { continuation ->
-            val listener = continuation.getRequestListener("getLatestPsa") {
-                psaMapper(it)
-            }
+            val listener = OptionalMegaRequestListenerInterface(
+                onRequestFinish = { request, error ->
+                    if (isActive) {
+                        when (error.errorCode) {
+                            MegaError.API_OK -> {
+                                continuation.resumeWith(Result.success(psaMapper(request)))
+                            }
+
+                            MegaError.API_ENOENT -> continuation.resumeWith(Result.success(null))
+                            else -> {
+                                Timber.e("Calling getLatestPsa failed with error code ${error.errorCode}")
+                                continuation.failWithError(error, "getLatestPsa")
+                            }
+                        }
+                    }
+                }
+            )
             megaApiGateway.getPsa(listener)
         }
     }
