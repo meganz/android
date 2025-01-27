10db6e34c7c8e8d6015b70e832f06fc9dc220514
Atiqur Rahman <atr@mega.co.nz>
2025-01-27 12:48:27 +0600

    SAT-2043: Update SDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ extra["targetSdkVersion"] = 35
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250122.171634-dev"
+extra["megaSdkVersion"] = "20250127.063519-dev"
 
 //JDK and Java Version
 extra["jdk"] = "17"
