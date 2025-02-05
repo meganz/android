246128ee83a3ee59b1d8342d15a75257ba4b39ae
Hai Luong <lh@mega.co.nz>
2025-02-05 15:40:36 +0700

    AND-19957: Replace SimpleDraweeView in Upload folder screen

--- app/src/main/java/mega/privacy/android/app/uploadFolder/list/adapter/FolderContentListHolder.kt
+++ app/src/main/java/mega/privacy/android/app/uploadFolder/list/adapter/FolderContentListHolder.kt
@@ -5,14 +5,17 @@ import mega.privacy.android.icon.pack.R as IconPackR
 import android.net.Uri
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import com.facebook.imagepipeline.request.ImageRequest
-import com.facebook.imagepipeline.request.ImageRequestBuilder
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import coil.util.CoilUtils
 import mega.privacy.android.app.MimeTypeList
 import mega.privacy.android.app.databinding.ItemFolderContentBinding
 import mega.privacy.android.app.uploadFolder.list.data.FolderContent
+import mega.privacy.android.app.utils.Constants.THUMB_CORNER_RADIUS_DP
 import mega.privacy.android.app.utils.TextUtil
 import mega.privacy.android.app.utils.TimeUtils
 import mega.privacy.android.app.utils.Util
+import mega.privacy.android.app.utils.Util.dp2px
 
 /**
  * RecyclerView's ViewHolder to show FolderContent Data info in a list view.
@@ -28,25 +31,33 @@ class FolderContentListHolder(
             val isSelected = item.isSelected
             selectedIcon.isVisible = isSelected
 
+            CoilUtils.dispose(thumbnail)
             thumbnail.apply {
                 isVisible = !isSelected
 
                 if (!isSelected) {
                     if (item.isFolder) {
                         setImageURI(null as Uri?)
-                        hierarchy.setPlaceholderImage(IconPackR.drawable.ic_folder_medium_solid)
-                        hierarchy.setPlaceholderImage(IconPackR.drawable.ic_folder_medium_solid)
+                        setImageResource(IconPackR.drawable.ic_folder_medium_solid)
                     } else {
-                        hierarchy.setPlaceholderImage(MimeTypeList.typeForName(item.name).iconResourceId)
-
                         if (item.isSelected) {
-                            hierarchy.setPlaceholderImage(CoreUiR.drawable.ic_select_folder)
+                            setImageResource(CoreUiR.drawable.ic_select_folder)
                         } else {
-                            setImageURI(null as Uri?)
-                            setImageRequest(
-                                ImageRequestBuilder.fromRequest(ImageRequest.fromUri(item.uri))
-                                    .setLocalThumbnailPreviewsEnabled(true).build()
-                            )
+                            load(item.uri) {
+                                placeholder(MimeTypeList.typeForName(item.name).iconResourceId)
+                                transformations(
+                                    RoundedCornersTransformation(
+                                        dp2px(THUMB_CORNER_RADIUS_DP).toFloat()
+                                    )
+                                )
+                                listener(
+                                    onError = { _, _ ->
+                                        setImageResource(
+                                            MimeTypeList.typeForName(item.name).iconResourceId
+                                        )
+                                    }
+                                )
+                            }
                         }
                     }
                 }
@@ -54,7 +65,11 @@ class FolderContentListHolder(
 
             name.text = item.name
             fileInfo.text = if (item.isFolder) {
-                TextUtil.getFolderInfo(item.numberOfFolders, item.numberOfFiles, binding.root.context);
+                TextUtil.getFolderInfo(
+                    item.numberOfFolders,
+                    item.numberOfFiles,
+                    binding.root.context
+                );
             } else {
                 TextUtil.getFileInfo(
                     Util.getSizeString(item.size, binding.root.context),
