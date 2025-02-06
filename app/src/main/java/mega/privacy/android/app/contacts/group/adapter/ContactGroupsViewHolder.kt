41575365ea3af6055d6053108e12803e811315ab
Hai Luong <lh@mega.co.nz>
2025-02-06 16:26:11 +0700

    AND-19955: Replace SimpleDraweeView in Contact Screen

--- app/src/main/java/mega/privacy/android/app/contacts/group/adapter/ContactGroupsViewHolder.kt
+++ app/src/main/java/mega/privacy/android/app/contacts/group/adapter/ContactGroupsViewHolder.kt
@@ -2,9 +2,10 @@ package mega.privacy.android.app.contacts.group.adapter
 
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
 import mega.privacy.android.app.contacts.group.data.ContactGroupItem
 import mega.privacy.android.app.databinding.ItemContactGroupBinding
-import mega.privacy.android.app.utils.setImageRequestFromUri
 
 /**
  * RecyclerView's ViewHolder to show ContactGroupItem.
@@ -12,15 +13,29 @@ import mega.privacy.android.app.utils.setImageRequestFromUri
  * @property binding    Item's view binding
  */
 class ContactGroupsViewHolder(
-    private val binding: ItemContactGroupBinding
+    private val binding: ItemContactGroupBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: ContactGroupItem) {
         binding.txtTitle.text = item.title
         binding.imgPrivate.isVisible = !item.isPublic
-        binding.imgThumbnailFirst.hierarchy.setPlaceholderImage(item.firstUser.getImagePlaceholder(itemView.context))
-        binding.imgThumbnailLast.hierarchy.setPlaceholderImage(item.lastUser.getImagePlaceholder(itemView.context))
-        binding.imgThumbnailFirst.setImageRequestFromUri(item.firstUser.avatar)
-        binding.imgThumbnailLast.setImageRequestFromUri(item.lastUser.avatar)
+        binding.imgThumbnailFirst.load(item.firstUser.avatar) {
+            transformations(CircleCropTransformation())
+            placeholder(item.firstUser.getImagePlaceholder(itemView.context))
+            listener(onError = { _, _ ->
+                binding.imgThumbnailFirst.setImageDrawable(
+                    item.firstUser.getImagePlaceholder(itemView.context)
+                )
+            })
+        }
+        binding.imgThumbnailLast.load(item.lastUser.avatar) {
+            transformations(CircleCropTransformation())
+            placeholder(item.lastUser.getImagePlaceholder(itemView.context))
+            listener(onError = { _, _ ->
+                binding.imgThumbnailLast.setImageDrawable(
+                    item.lastUser.getImagePlaceholder(itemView.context)
+                )
+            })
+        }
     }
 }
