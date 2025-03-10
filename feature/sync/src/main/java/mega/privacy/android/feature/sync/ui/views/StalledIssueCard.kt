547e145c5e5d0d6c39fed55d36c24eb280e7c816
Atiqur Rahman <atr@mega.co.nz>
2025-03-10 11:08:44 +0600

    SAT-913: Update Design Token of StalledIssueCard

--- feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/views/StalledIssueCard.kt
+++ feature/sync/src/main/java/mega/privacy/android/feature/sync/ui/views/StalledIssueCard.kt
@@ -2,7 +2,6 @@ package mega.privacy.android.feature.sync.ui.views
 
 import mega.privacy.android.icon.pack.R as IconPackR
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
@@ -11,21 +10,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import mega.android.core.ui.theme.values.IconColor
+import mega.android.core.ui.theme.values.TextColor
 import mega.privacy.android.core.R
+import mega.privacy.android.shared.original.core.ui.controls.images.MegaIcon
 import mega.privacy.android.shared.original.core.ui.controls.images.ThumbnailView
+import mega.privacy.android.shared.original.core.ui.controls.text.MegaText
 import mega.privacy.android.shared.original.core.ui.preview.CombinedThemePreviews
-import mega.privacy.android.shared.original.core.ui.theme.extensions.textColorPrimary
-import mega.privacy.android.shared.original.core.ui.theme.extensions.textColorSecondary
 import mega.privacy.android.shared.original.core.ui.theme.OriginalTheme
 
 @Composable
@@ -61,20 +60,22 @@ internal fun StalledIssueCard(
                     .weight(1f)
                     .padding(start = 12.dp)
             ) {
-                Text(
+                MegaText(
                     modifier = Modifier.testTag(TEST_TAG_STALLED_ISSUE_CARD_TEXT_CONFLICT_NAME),
                     text = conflictName,
-                    style = MaterialTheme.typography.subtitle1.copy(color = MaterialTheme.colors.textColorPrimary)
+                    style = MaterialTheme.typography.subtitle1,
+                    textColor = TextColor.Primary
                 )
-                Text(
+                MegaText(
                     modifier = Modifier
                         .padding(top = 1.dp)
                         .testTag(TEST_TAG_STALLED_ISSUE_CARD_TEXT_NODE_NAME),
                     text = nodeName,
-                    style = MaterialTheme.typography.subtitle2.copy(color = MaterialTheme.colors.textColorSecondary)
+                    style = MaterialTheme.typography.subtitle2,
+                    textColor = TextColor.Secondary
                 )
             }
-            Image(
+            MegaIcon(
                 modifier = Modifier
                     .padding(end = 24.dp)
                     .size(24.dp)
@@ -82,16 +83,16 @@ internal fun StalledIssueCard(
                     .testTag(TEST_TAG_STALLED_ISSUE_CARD_BUTTON_INFO),
                 painter = painterResource(R.drawable.ic_info),
                 contentDescription = "Info",
-                colorFilter = ColorFilter.tint(MaterialTheme.colors.textColorPrimary)
+                tint = IconColor.Primary
             )
-            Image(
+            MegaIcon(
                 modifier = Modifier
                     .clickable(shouldShowMoreIcon) { moreClicked() }
                     .testTag(TEST_TAG_STALLED_ISSUE_CARD_BUTTON_MORE)
                     .alpha(if (shouldShowMoreIcon) 1f else 0f),
                 painter = painterResource(R.drawable.ic_universal_more),
                 contentDescription = "More",
-                colorFilter = ColorFilter.tint(MaterialTheme.colors.textColorSecondary)
+                tint = IconColor.Secondary
             )
         }
     }
@@ -119,4 +120,3 @@ internal const val TEST_TAG_STALLED_ISSUE_CARD_BUTTON_MORE = "stalled_issue_card
 internal const val TEST_TAG_STALLED_ISSUE_CARD_TEXT_CONFLICT_NAME =
     "stalled_issue_card:text_conflict_name"
 internal const val TEST_TAG_STALLED_ISSUE_CARD_TEXT_NODE_NAME = "stalled_issue_card:text_node_name"
-
