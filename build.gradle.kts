01c78a180cb1c1a9eb5cf4fbd85ef872693348ae
Daniel Oosthuizen <do@mega.co.nz>
2025-03-14 10:49:17 +1300

    Update sdk to rc3

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ extra["targetSdkVersion"] = 35
 extra["buildTools"] = "35.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250310.230346-rel"
+extra["megaSdkVersion"] = "20250313.213419-rel"
 
 
 //JDK and Java Version
