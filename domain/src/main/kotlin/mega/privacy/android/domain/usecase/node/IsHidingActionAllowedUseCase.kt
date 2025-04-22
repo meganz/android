f322dd1f4512e13c0d815a97e79d1bfa62e39ae9
Erick Sumargo <es@mega.co.nz>
2025-04-22 14:41:02 +1200

    CC-8698: Adjust IsHidingActionAllowedUseCase to Intercept System Folders Visibility

--- domain/src/main/kotlin/mega/privacy/android/domain/usecase/node/IsHidingActionAllowedUseCase.kt
+++ domain/src/main/kotlin/mega/privacy/android/domain/usecase/node/IsHidingActionAllowedUseCase.kt
@@ -25,10 +25,20 @@ class IsHidingActionAllowedUseCase @Inject constructor(
     /**
      * Check if the node is allowed to be hidden
      */
-    suspend operator fun invoke(nodeId: NodeId): Boolean = nodeId.longValue !in listOf(
-        getPrimarySyncHandleUseCase(),
-        getSecondarySyncHandleUseCase(),
-        getMyChatsFilesFolderIdUseCase()?.longValue ?: -1,
-        getRootNodeUseCase()?.id?.longValue ?: 0,
-    )
+    suspend operator fun invoke(nodeId: NodeId): Boolean =
+//        nodeId.longValue !in listOf(
+//            getPrimarySyncHandleUseCase(),
+//            getSecondarySyncHandleUseCase(),
+//            getMyChatsFilesFolderIdUseCase()?.longValue ?: -1,
+//            getRootNodeUseCase()?.id?.longValue ?: 0,
+//        )
+        /**
+         * This use case was introduced by previous developer and unfortunately still far from a perfect implementation.
+         * Luckily, as this use case has been integrated to every places, we can use pragmatic solution
+         * by overriding the implementation to return default true instead as the new requirement demands so.
+         *
+         * Otherwise, we need to implement the new logic back to every places which can cause huge changes and potential regression.
+         * The old implementation is commented as of now as we need to update it again in the future once we have stable requirement from business.
+         */
+        true
 }
