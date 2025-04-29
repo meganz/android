9173bf2ec57d80741ead1a4b3750178d46a8eb58
Robin Shi <rsh@mega.co.nz>
2025-04-29 19:16:05 +1200

    AND-20129 build Chat SDK by CMake

--- sdk/src/main/jni/build-cmake-chat.sh
+++ sdk/src/main/jni/build-cmake-chat.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -i
 set -e
 
-start_time=$SECONDS
-
 export ANDROID_API_LEVEL=26
 export ANDROID_NDK_HOME=${NDK_ROOT}
 
@@ -50,82 +48,6 @@ echo "-- ANDROID_NDK_HOME: ${ANDROID_NDK_HOME}"
 echo
 
 for ABI in ${BUILD_ARCHS}; do
-    export BUILD_DIR=build-dir/pdfium/${ABI}
-    echo "* Configuring PdfiumAndroid - ${ABI}"
-    cmake -B ${BUILD_DIR} \
-         -S . \
-         -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-         -DANDROID_NDK=${NDK_ROOT} \
-         -DCMAKE_ANDROID_NDK=${NDK_ROOT} \
-         -DCMAKE_SYSTEM_NAME=Android \
-         -DCMAKE_ANDROID_ARCH_ABI=${ABI} \
-         -DANDROID_ABI=${ABI} \
-         -DANDROID_PLATFORM=android-${ANDROID_API_LEVEL} \
-         -DVCPKG_ROOT=${VCPKG_ROOT} \
-         -DVCPKG_TARGET_TRIPLET=arm64-android-mega \
-         -DENABLE_CHAT=ON \
-         -DENABLE_JAVA_BINDINGS=ON \
-         -DENABLE_SDKLIB_EXAMPLES=OFF \
-         -DENABLE_SDKLIB_TESTS=OFF \
-         -DCMAKE_ANDROID_API=${ANDROID_API_LEVEL} \
-         -DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON \
-         -DUSE_PDFIUM=OFF &>> ${LOG_FILE}
-    echo "* Building PdfiumAndroid - ${ABI}"
-    cmake --build ${BUILD_DIR} -j "${JOBS}" &>> ${LOG_FILE}
-done
-
-for ABI in ${BUILD_ARCHS}; do
-
-    export BUILD_DIR=build-dir/sdk/${ABI}
-
-    case ${ABI} in
-      armeabi-v7a)
-        export VCPKG_TRIPLET='arm-android-mega'
-        ;;
-      arm64-v8a)
-        export VCPKG_TRIPLET='arm64-android-mega'
-        ;;
-      x86)
-        export VCPKG_TRIPLET='x86-android-mega'
-        ;;
-      x86_64)
-        export VCPKG_TRIPLET='x64-android-mega'
-        ;;
-      *)
-        echo "Unsupported architecture: ${ABI}" && exit 1
-        ;;
-    esac
-
-    echo "* Configuring MEGA SDK - ${ABI}"
-    cmake -B ${BUILD_DIR} \
-        -S megachat/sdk/third-party/mega \
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-        -DBUILD_SHARED_LIBS=ON \
-        -DANDROID_NDK=${NDK_ROOT} \
-        -DCMAKE_ANDROID_NDK=${NDK_ROOT} \
-        -DCMAKE_SYSTEM_NAME=Android \
-        -DCMAKE_ANDROID_ARCH_ABI=${ABI} \
-        -DANDROID_ABI=${ABI} \
-        -DANDROID_PLATFORM=android-${ANDROID_API_LEVEL} \
-        -DVCPKG_ROOT=${VCPKG_ROOT} \
-        -DVCPKG_TARGET_TRIPLET=${VCPKG_TRIPLET} \
-        -DENABLE_CHAT=ON \
-        -DENABLE_JAVA_BINDINGS=ON \
-        -DENABLE_SDKLIB_EXAMPLES=OFF \
-        -DENABLE_SDKLIB_TESTS=OFF \
-        -DCMAKE_ANDROID_API=${ANDROID_API_LEVEL} \
-        -DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON \
-        -DUSE_PDFIUM=OFF \
-        -DENABLE_CHAT=ON \
-        -DUSE_FREEIMAGE=OFF \
-        -DUSE_FFMPEG=OFF \
-        -DUSE_LIBUV=ON \
-        -DUSE_READLINE=OFF &>> ${LOG_FILE}
-    echo "* Building MEGA SDK - ${ABI}"
-    cmake --build "${BUILD_DIR}" -j "${JOBS}" &>> ${LOG_FILE}
-done
-
-for ABI in ${BUILD_ARCHS}; do
 
     export BUILD_DIR=build-dir/chat/${ABI}
 
@@ -151,7 +73,6 @@ for ABI in ${BUILD_ARCHS}; do
 
     cmake -B ${BUILD_DIR} -S megachat/sdk \
         -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-        -DBUILD_SHARED_LIBS=ON \
         -DANDROID_NDK=${NDK_ROOT} \
         -DCMAKE_ANDROID_NDK=${NDK_ROOT} \
         -DCMAKE_SYSTEM_NAME=Android \
@@ -165,6 +86,7 @@ for ABI in ${BUILD_ARCHS}; do
         -DENABLE_CHATLIB_QTAPP=OFF \
         -DENABLE_CHATLIB_MEGACLC=OFF \
         -DENABLE_QT_BINDINGS=OFF \
+        -DBUILD_SHARED_LIBS=ON \
         -DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON &>> ${LOG_FILE}
 
     echo "* Building MEGA Chat SDK - ${ABI}"
@@ -173,8 +95,3 @@ for ABI in ${BUILD_ARCHS}; do
 done
 
 echo "* Task finished OK"
-
-end_time=$SECONDS
-total_time=$((end_time - start_time))
-echo "* Task finished OK"
-echo "Total execution time: ${total_time} seconds"
