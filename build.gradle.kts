f15b63e341d574503e1263b176e4331ac6f0b5e7
Yenel <yr@mega.nz>
2025-05-27 11:15:34 +0200

    Increase version name

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.9"
+extra["appVersion"] = "15.9.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
