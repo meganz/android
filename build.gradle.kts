d7171593f0f112b23417c7cb0a9383bac2c83779
Robin Shi <rsh@mega.co.nz>
2025-06-25 23:40:45 +1200

    Pre-release - v15.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.11"
+extra["appVersion"] = "15.12"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -93,7 +93,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250615.213244-dev"
+extra["megaSdkVersion"] = "20250625.103058-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
