2cd430ccc96ad31c06fabbdceb9a6b1f31605238
Daniel Oosthuizen <do@mega.co.nz>
2025-07-09 16:55:39 +1200

    AND-20943 - Fix navigation to device center info screen

--- feature/devicecenter/src/main/java/mega/privacy/android/feature/devicecenter/navigation/DeviceCenterInfoNavGraph.kt
+++ feature/devicecenter/src/main/java/mega/privacy/android/feature/devicecenter/navigation/DeviceCenterInfoNavGraph.kt
@@ -7,12 +7,32 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import kotlinx.serialization.Serializable
 import mega.privacy.android.feature.devicecenter.ui.model.DeviceCenterUINode
+import mega.privacy.android.feature.devicecenter.ui.model.DeviceFolderUINode
+import mega.privacy.android.feature.devicecenter.ui.model.DeviceUINode
 
 @Serializable
 data object DeviceCenter
 
 @Serializable
-data class DeviceCenterInfo(val deviceCenterNode: DeviceCenterUINode)
+data class DeviceCenterInfo(
+    val name: String,
+    val icon: Int,
+    val rootHandle: Long? = null,
+    val folders: List<Long>? = null,
+) {
+    fun isDevice() = folders != null
+
+    companion object {
+        fun from(uiNode: DeviceCenterUINode): DeviceCenterInfo {
+            return DeviceCenterInfo(
+                name = uiNode.name,
+                icon = uiNode.icon.iconRes,
+                rootHandle = (uiNode as? DeviceFolderUINode)?.rootHandle,
+                folders = (uiNode as? DeviceUINode)?.folders?.map { it.rootHandle }
+            )
+        }
+    }
+}
 
 internal fun NavGraphBuilder.deviceCenterInfoNavGraph(
     navController: NavController,
@@ -20,7 +40,7 @@ internal fun NavGraphBuilder.deviceCenterInfoNavGraph(
     onBackPressHandled: () -> Unit,
 ) {
     navigation<DeviceCenter>(
-        startDestination = DeviceCenterInfo(selectedItem)
+        startDestination = DeviceCenterInfo.from(selectedItem)
     ) {
         composable<DeviceCenterInfo> {
             DeviceCenterInfoScreenRoute(
