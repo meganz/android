d8d7e015abc060f2825f6188cdcadae7b6f0f945
Kevin Sun <ks@mega.co.nz>
2025-07-28 19:28:07 +1200

    T21049424/AND-21015 AND: Support Direct Image/Video Preview via “Open in App” from Folder Link

--- app/src/main/java/mega/privacy/android/app/presentation/folderlink/FolderLinkViewModel.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/folderlink/FolderLinkViewModel.kt
@@ -450,6 +450,7 @@ class FolderLinkViewModel @Inject constructor(
             runCatching { fetchFolderNodesUseCase(folderSubHandle) }
                 .onSuccess { result ->
                     val hasMediaItem = containsMediaItemUseCase(result.childrenNodes)
+                    val title = result.parentNode?.name ?: result.rootNode?.name ?: ""
                     _state.update {
                         it.copy(
                             isNodesFetched = true,
@@ -463,7 +464,7 @@ class FolderLinkViewModel @Inject constructor(
                             hasMediaItem = hasMediaItem,
                             rootNode = result.rootNode,
                             parentNode = result.parentNode,
-                            title = result.rootNode?.name ?: ""
+                            title = title
                         )
                     }
 
