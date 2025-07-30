1313835d7b4400284335843ad4657ce7d8229b7a
Daniel Oosthuizen <do@mega.co.nz>
2025-07-31 09:36:56 +1200

    Update sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250729.214503-rel"
+extra["megaSdkVersion"] = "20250730.210343-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
