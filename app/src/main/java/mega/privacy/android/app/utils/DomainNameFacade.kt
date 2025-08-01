2abe2ac057eb4cceca11b68c486a107eda0f3c9a
Pau Dominkovics Coll <pau@mega.co.nz>
2025-08-01 11:47:51 +0200

    AND-21054 Create singleton facade that wraps the repository

---
+++ app/src/main/java/mega/privacy/android/app/utils/DomainNameFacade.kt
@@ -0,0 +1,33 @@
+package mega.privacy.android.app.utils
+
+import dagger.hilt.android.EntryPointAccessors
+import mega.privacy.android.app.MegaApplication
+import mega.privacy.android.domain.repository.DomainNameMigrationRepository
+import mega.privacy.android.domain.usecase.domainmigration.GetDomainNameUseCase
+
+/**
+ * Static wrapper to access domain name repository where dependency injection is not possible.
+ * Note: Please use [GetDomainNameUseCase] or [DomainNameMigrationRepository] when possible.
+ */
+object DomainNameFacade {
+
+    private val domainNameRepository: DomainNameMigrationRepository by lazy {
+        val context = MegaApplication.getInstance()
+        EntryPointAccessors.fromApplication(context, DomainNameMigrationRepository::class.java)
+    }
+
+    private val getDomainNameUseCase: GetDomainNameUseCase by lazy {
+        val context = MegaApplication.getInstance()
+        EntryPointAccessors.fromApplication(context, GetDomainNameUseCase::class.java)
+    }
+
+    /**
+     * Get the domain name
+     */
+    fun getDomainName(isForEmail: Boolean) = getDomainNameUseCase(isForEmail)
+
+    /**
+     * Get the domain name flag
+     */
+    fun isDomainNameMegaDotApp() = domainNameRepository.isDomainNameMegaDotAppFromCache()
+}
