0b2d853157d6e207369a9677e8e6d2bccd58069a
Hai Luong <lh@mega.co.nz>
2025-08-01 13:11:59 +0700

    AND-21064: Global snackbar component

--- app/src/main/java/mega/privacy/android/app/appstate/view/MegaApp.kt
+++ app/src/main/java/mega/privacy/android/app/appstate/view/MegaApp.kt
@@ -3,7 +3,9 @@ package mega.privacy.android.app.appstate.view
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -14,6 +16,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import mega.android.core.ui.components.LocalSnackBarHostState
 import mega.privacy.android.app.appstate.model.AppState
 import mega.privacy.android.app.appstate.transfer.AppTransferViewModel
 import mega.privacy.android.app.appstate.transfer.TransferHandlerImpl
@@ -35,6 +38,7 @@ fun MegaApp(
     val navigationHandler = remember { NavigationHandlerImpl(navController) }
     val transferHandler = remember { TransferHandlerImpl(appTransferViewModel) }
     val transferState by appTransferViewModel.state.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
 
     Box(modifier = Modifier.pointerInput(Unit) {
         awaitEachGesture {
@@ -46,35 +50,38 @@ fun MegaApp(
             } while (event.changes.any { it.pressed })
         }
     }) {
-        NavHost(
-            navController = navController,
-            startDestination = MainNavigationScaffoldDestination,
+        CompositionLocalProvider(
+            LocalSnackBarHostState provides snackbarHostState
         ) {
-            mainNavigationScaffold(
-                topLevelDestinations = appState.mainNavItems,
-                startDestination = appState.initialMainDestination,
-                builder = { navigationUiController: NavigationUiController ->
-                    appState.mainNavScreens.forEach {
-                        it(this, navigationHandler, navigationUiController, transferHandler)
+            NavHost(
+                navController = navController,
+                startDestination = MainNavigationScaffoldDestination,
+            ) {
+                mainNavigationScaffold(
+                    topLevelDestinations = appState.mainNavItems,
+                    startDestination = appState.initialMainDestination,
+                    builder = { navigationUiController: NavigationUiController ->
+                        appState.mainNavScreens.forEach {
+                            it(this, navigationHandler, navigationUiController, transferHandler)
+                        }
                     }
+                )
+                appState.featureDestinations
+                    .forEach {
+                        it.navigationGraph(this, navigationHandler, transferHandler)
+                    }
+            }
+
+            StartTransferComponent(
+                event = transferState.transferEvent,
+                onConsumeEvent = appTransferViewModel::consumedTransferEvent,
+                navigateToStorageSettings = {
+                    megaNavigator.openSettings(
+                        context,
+                        storageTargetPreference
+                    )
                 }
             )
-            appState.featureDestinations
-                .forEach {
-                    it.navigationGraph(this, navigationHandler, transferHandler)
-                }
         }
     }
-
-    StartTransferComponent(
-        event = transferState.transferEvent,
-        onConsumeEvent = appTransferViewModel::consumedTransferEvent,
-        snackBarHostState = null, // will implement global snackbar later
-        navigateToStorageSettings = {
-            megaNavigator.openSettings(
-                context,
-                storageTargetPreference
-            )
-        }
-    )
 }
