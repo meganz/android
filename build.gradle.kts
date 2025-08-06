f04fbdd947acb3784fc77ac833abb1691456cb9c
Yenel Rodríguez Hernández <yr@mega.nz>
2025-08-06 13:48:52 +0200

    Pre-release - v15.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.14"
+extra["appVersion"] = "15.5"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250723.000343-rel"
+extra["megaSdkVersion"] = "20250806.110002-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
