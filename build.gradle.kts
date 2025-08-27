f4ec4b2643ac17056f1eff247d253c1d4677617c
Atiqur Rahman <atr@mega.co.nz>
2025-08-27 11:54:43 +0600

    SAT-2226: Bump sdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250821.065530-rel"
+extra["megaSdkVersion"] = "20250826.143430-dev"
 
 //JDK and Java Version
 extra["jdk"] = "21"
