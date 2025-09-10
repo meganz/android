4bdc55769283b43d3fb2998d081c260ab6bf27d1
Pau Dominkovics <pau@mega.co.nz>
2025-09-10 11:29:44 +0200

    new MEGAchat SDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250903.104032-rel"
+extra["megaSdkVersion"] = "20250910.075525-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
