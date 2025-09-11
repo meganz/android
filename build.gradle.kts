aca7200e3681170db0df241510d599dffcd23983
Pau Dominkovics <pau@mega.co.nz>
2025-09-11 10:09:45 +0200

    new MEGA SDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250910.075525-rel"
+extra["megaSdkVersion"] = "20250911.065749-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
