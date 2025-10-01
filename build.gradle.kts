fa80ec1d4b9734cd885b20c7d9e3afc8e7c07968
Junjie He <juh@mega.co.nz>
2025-10-01 20:45:00 +1300

    Pre-release - v15.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.18"
+extra["appVersion"] = "15.19"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20250917.232128-rel"
+extra["megaSdkVersion"] = "20251001.070534-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
