e047c1f46e0546e32595efa664f91b5b8611a495
Daniel Oosthuizen <do@mega.co.nz>
2025-10-06 13:43:42 +1300

    AND-21391 Remove references to example module

--- app/app.gradle.kts
+++ app/app.gradle.kts
@@ -211,7 +211,6 @@ dependencies {
     implementation(project(":core:navigation-snowflake-components"))
     implementation(project(":core:navigation-contract"))
     implementation(project(":core:feature-flags"))
-    "qaImplementation"(project(":feature:example"))
     "qaImplementation"(project(":feature:cloud-drive:cloud-drive"))
     preBuiltSdkDependency(rootProject.extra)
     implementation(project(":core:transfers"))
