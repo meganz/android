cee48ac9b1ae63ceb876b91481e31736b05aa983
Bhavna Thacker <bt@mega.co.nz>
2025-10-08 03:27:45 +1300

    AND-21406: handle promo notification click

--- app/src/main/java/mega/privacy/android/app/presentation/notification/view/NotificationsScreenM3.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/notification/view/NotificationsScreenM3.kt
@@ -1,14 +1,18 @@
 package mega.privacy.android.app.presentation.notification.view
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import mega.privacy.android.app.presentation.notification.NotificationViewModel
 import mega.privacy.android.navigation.contract.NavigationHandler
+import timber.log.Timber
 
 @Composable
 fun NotificationsScreenM3(
@@ -16,6 +20,7 @@ fun NotificationsScreenM3(
     viewModel: NotificationViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
     NotificationViewM3(
         state = uiState,
         onNotificationClick = { notification ->
@@ -24,7 +29,13 @@ fun NotificationsScreenM3(
             }
         },
         onPromoNotificationClick = { promoNotification ->
-            //todo handle promoNotification click
+            runCatching {
+                Intent(Intent.ACTION_VIEW, promoNotification.actionURL.toUri()).apply {
+                    context.startActivity(this)
+                }
+            }.onFailure {
+                Timber.e("No Application found that can handle promo notification intent")
+            }
         },
         onNotificationsLoaded = {
             viewModel.onNotificationsLoaded()
