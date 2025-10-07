bdad1822f4e77009042bd63c32921af1d15005d6
Jeff Ruan <cjr@mega.co.nz>
2025-10-07 17:37:45 +1300

    AND-21393 Lock the FileService's cache folder to prevent others to open it while we are running

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20251003.095913-dev"
+extra["megaSdkVersion"] = "20251007.010946-dev"
 
 //JDK and Java Version
 extra["jdk"] = "21"
