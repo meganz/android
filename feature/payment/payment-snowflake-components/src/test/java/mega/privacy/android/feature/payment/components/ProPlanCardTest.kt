93b2d1be55d925b088f04729492059334ceb37a9
Hai Luong <lh@mega.co.nz>
2025-10-16 13:10:14 +0700

    AND-21482: Disable select current plan when upgrading account

--- feature/payment/payment-snowflake-components/src/test/java/mega/privacy/android/feature/payment/components/ProPlanCardTest.kt
+++ feature/payment/payment-snowflake-components/src/test/java/mega/privacy/android/feature/payment/components/ProPlanCardTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mega.privacy.android.shared.resources.R
 import org.junit.Rule
@@ -116,5 +117,146 @@ class ProPlanCardTest {
         }
         composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD_BILLING_INFO, useUnmergedTree = true)
             .assertDoesNotExist()
+    }
+
+    @Test
+    fun `test that ProPlanCard hides radio button when isCurrentPlan is true`() {
+        val planName = "Pro II"
+        val storage = "8 TB storage"
+        val transfer = "96 TB transfer"
+        val price = "€16.67"
+        val billingInfo = "€199.99 billed yearly"
+        composeRule.setContent {
+            ProPlanCard(
+                modifier = Modifier.fillMaxWidth(),
+                planName = planName,
+                isRecommended = false,
+                isSelected = false,
+                storage = storage,
+                transfer = transfer,
+                price = price,
+                billingInfo = billingInfo,
+                isCurrentPlan = true,
+                onSelected = {}
+            )
+        }
+        composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD_RADIO).assertDoesNotExist()
+    }
+
+    @Test
+    fun `test that ProPlanCard shows radio button when isCurrentPlan is false`() {
+        val planName = "Pro II"
+        val storage = "8 TB storage"
+        val transfer = "96 TB transfer"
+        val price = "€16.67"
+        val billingInfo = "€199.99 billed yearly"
+        composeRule.setContent {
+            ProPlanCard(
+                modifier = Modifier.fillMaxWidth(),
+                planName = planName,
+                isRecommended = false,
+                isSelected = false,
+                storage = storage,
+                transfer = transfer,
+                price = price,
+                billingInfo = billingInfo,
+                isCurrentPlan = false,
+                onSelected = {}
+            )
+        }
+        composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD_RADIO).assertExists()
+    }
+
+    @Test
+    fun `test that ProPlanCard is not clickable when isCurrentPlan is true`() {
+        var clickCount = 0
+        val planName = "Pro II"
+        val storage = "8 TB storage"
+        val transfer = "96 TB transfer"
+        val price = "€16.67"
+        val billingInfo = "€199.99 billed yearly"
+        
+        composeRule.setContent {
+            ProPlanCard(
+                modifier = Modifier.fillMaxWidth(),
+                planName = planName,
+                isRecommended = false,
+                isSelected = false,
+                storage = storage,
+                transfer = transfer,
+                price = price,
+                billingInfo = billingInfo,
+                isCurrentPlan = true,
+                onSelected = { clickCount++ }
+            )
+        }
+        
+        // Try to click on the card
+        composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD).performClick()
+        
+        // Verify that the click was not handled (clickCount should remain 0)
+        assert(clickCount == 0) { "Card should not be clickable when isCurrentPlan is true" }
+    }
+
+    @Test
+    fun `test that ProPlanCard is clickable when isCurrentPlan is false`() {
+        var clickCount = 0
+        val planName = "Pro II"
+        val storage = "8 TB storage"
+        val transfer = "96 TB transfer"
+        val price = "€16.67"
+        val billingInfo = "€199.99 billed yearly"
+        
+        composeRule.setContent {
+            ProPlanCard(
+                modifier = Modifier.fillMaxWidth(),
+                planName = planName,
+                isRecommended = false,
+                isSelected = false,
+                storage = storage,
+                transfer = transfer,
+                price = price,
+                billingInfo = billingInfo,
+                isCurrentPlan = false,
+                onSelected = { clickCount++ }
+            )
+        }
+        
+        // Click on the card
+        composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD).performClick()
+        
+        // Verify that the click was handled (clickCount should be 1)
+        assert(clickCount == 1) { "Card should be clickable when isCurrentPlan is false" }
+    }
+
+    @Test
+    fun `test that ProPlanCard radio button is clickable when isCurrentPlan is false`() {
+        var clickCount = 0
+        val planName = "Pro II"
+        val storage = "8 TB storage"
+        val transfer = "96 TB transfer"
+        val price = "€16.67"
+        val billingInfo = "€199.99 billed yearly"
+        
+        composeRule.setContent {
+            ProPlanCard(
+                modifier = Modifier.fillMaxWidth(),
+                planName = planName,
+                isRecommended = false,
+                isSelected = false,
+                storage = storage,
+                transfer = transfer,
+                price = price,
+                billingInfo = billingInfo,
+                isCurrentPlan = false,
+                onSelected = { clickCount++ }
+            )
+        }
+        
+        // Click on the radio button
+        composeRule.onNodeWithTag(TEST_TAG_PRO_PLAN_CARD_RADIO).performClick()
+        
+        // Verify that the click was handled (clickCount should be 1)
+        assert(clickCount == 1) { "Radio button should be clickable when isCurrentPlan is false" }
     }
 } 
