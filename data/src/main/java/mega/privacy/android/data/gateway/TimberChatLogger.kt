dac3b1c6e38cf5809ac0190dd2c69654f3346f77
Daniel Oosthuizen <do@mega.co.nz>
2025-10-17 16:04:59 +1300

    AND-21490 Add catch blocks to loggers

--- data/src/main/java/mega/privacy/android/data/gateway/TimberChatLogger.kt
+++ data/src/main/java/mega/privacy/android/data/gateway/TimberChatLogger.kt
@@ -1,5 +1,7 @@
 package mega.privacy.android.data.gateway
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.annotation.Keep
 import nz.mega.sdk.MegaChatApi
 import nz.mega.sdk.MegaChatLoggerInterface
@@ -13,16 +15,22 @@ import javax.inject.Inject
  */
 @Keep
 internal class TimberChatLogger @Inject constructor() : MegaChatLoggerInterface {
+    @SuppressLint("LogNotTimber")
     @Synchronized
     override fun log(loglevel: Int, message: String?) {
-        Timber.tag(TAG)
-        when (loglevel) {
-            MegaChatApi.LOG_LEVEL_MAX -> Timber.v(message)
-            MegaChatApi.LOG_LEVEL_DEBUG -> Timber.d(message)
-            MegaChatApi.LOG_LEVEL_INFO -> Timber.i(message)
-            MegaChatApi.LOG_LEVEL_WARNING -> Timber.w(message)
-            MegaChatApi.LOG_LEVEL_ERROR -> Timber.e(message)
-            else -> Timber.i(message)
+        try{
+            Timber.tag(TAG)
+            when (loglevel) {
+                MegaChatApi.LOG_LEVEL_MAX -> Timber.v(message)
+                MegaChatApi.LOG_LEVEL_DEBUG -> Timber.d(message)
+                MegaChatApi.LOG_LEVEL_INFO -> Timber.i(message)
+                MegaChatApi.LOG_LEVEL_WARNING -> Timber.w(message)
+                MegaChatApi.LOG_LEVEL_ERROR -> Timber.e(message)
+                else -> Timber.i(message)
+            }
+        }catch (t: Throwable) {
+            // swallow everything to protect native code
+            Log.e(TAG, "Exception in SDK logger, swallowed to prevent SIGABRT", t)
         }
     }
 
