ae1989d880412c87daa77eb56a941c132141dbc2
Robin Shi <rsh@mega.co.nz>
2025-10-17 13:30:19 +1300

    update sdk v9.15.1-rc.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20251002.165100-rel"
+extra["megaSdkVersion"] = "20251017.002455-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
