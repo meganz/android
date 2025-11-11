4ae3f8dd7358a8e42bd8d94cda9f86fba850671c
Hai Luong <lh@mega.co.nz>
2025-11-11 09:44:00 +0700

    AND-21550: Fix JNI exception handler doesn't work in release mode

--- app/src/main/java/mega/privacy/android/app/jni/JniExceptionReporter.kt
+++ app/src/main/java/mega/privacy/android/app/jni/JniExceptionReporter.kt
@@ -1,5 +1,7 @@
 package mega.privacy.android.app.jni
 
+import androidx.annotation.Keep
+
 
 /***************************************************************************************************************************
  * These classes are referenced from the SWIG code and should not be moved without updating the .i files in the SDK project!      *
@@ -10,6 +12,7 @@ package mega.privacy.android.app.jni
  * Interface for receiving exceptions that occur inside JNI code,
  * such as during callbacks from C++ into Java/Kotlin.
  */
+@Keep
 interface JniExceptionHandler {
     fun onJniException(location: String, message: String, stacktrace: String)
 }
@@ -19,6 +22,7 @@ interface JniExceptionHandler {
  * Singleton container used by native code to report JNI exceptions
  * back into the managed layer.
  */
+@Keep
 object JniExceptionReporter {
     @JvmStatic
     var handler: JniExceptionHandler? = null
