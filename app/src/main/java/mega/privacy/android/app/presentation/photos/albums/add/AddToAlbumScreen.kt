cac362bf51543491385f61c3fdab66b5a965e80c
Kevin Sun <ks@mega.co.nz>
2025-11-20 07:26:11 +0800

    CC-8852 Bottom Bar Overlaps with System Navigation Bar on AddToAlbums Screen

--- app/src/main/java/mega/privacy/android/app/presentation/photos/albums/add/AddToAlbumScreen.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/photos/albums/add/AddToAlbumScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
@@ -204,6 +205,7 @@ private fun AddToAlbumBottomBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
             .background(MaterialTheme.colors.surface)
             .padding(horizontal = 16.dp, vertical = 12.dp),
         horizontalArrangement = Arrangement.End,
