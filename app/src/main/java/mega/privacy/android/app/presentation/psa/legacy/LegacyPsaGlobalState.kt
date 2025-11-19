b5074390f917ec492b94472471a494942a0848ba
Daniel Oosthuizen <do@mega.co.nz>
2025-11-19 17:26:33 +1300

    AND-21698 Prevent psa check in login activity
    (cherry picked from commit a46e1da965dfb0a4203d5a1c25dc0b0372b6e974)
    
    d6bd2650 AND-21698 Prevent psa check in login activity
    5c12c910 Extend time
    
    Co-authored-by: Daniel Oosthuizen <do@mega.co.nz>

--- app/src/main/java/mega/privacy/android/app/presentation/psa/legacy/LegacyPsaGlobalState.kt
+++ app/src/main/java/mega/privacy/android/app/presentation/psa/legacy/LegacyPsaGlobalState.kt
@@ -1,18 +1,25 @@
 package mega.privacy.android.app.presentation.psa.legacy
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.receiveAsFlow
 import mega.privacy.android.app.presentation.psa.mapper.PsaStateMapper
 import mega.privacy.android.app.presentation.psa.model.PsaState
 import mega.privacy.android.domain.qualifier.ApplicationScope
 import mega.privacy.android.domain.usecase.psa.MonitorPsaUseCase
+import mega.privacy.android.navigation.contract.viewmodel.asUiStateFlow
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Legacy psa global state
@@ -27,23 +34,27 @@ class LegacyPsaGlobalState @Inject constructor(
     private val psaStateMapper: PsaStateMapper,
     @ApplicationScope private val coroutineScope: CoroutineScope,
 ) {
-    private val _state = MutableStateFlow<PsaState>(PsaState.NoPsa)
+    private val override = Channel<PsaState>()
 
     /**
      * State
      */
-    val state: StateFlow<PsaState> = _state
-
-    init {
-        coroutineScope.launch {
-            monitorPsaUseCase(System::currentTimeMillis)
-                .map { psaStateMapper(it) }
-                .catch { Timber.e(it, "Error in monitoring psa") }
-                .collect { _state.value = it }
-        }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val state: StateFlow<PsaState> by lazy {
+        flow {
+            delay(50.seconds)
+            emit(true)
+        }.flatMapLatest {
+            merge(
+                monitorPsaUseCase(System::currentTimeMillis)
+                    .map { psaStateMapper(it) },
+                override.receiveAsFlow()
+            ).catch { Timber.e(it, "Error in monitoring psa") }
+        }.asUiStateFlow(coroutineScope, PsaState.NoPsa)
     }
 
+
     fun clearPsa() {
-        _state.value = PsaState.NoPsa
+        override.trySend(PsaState.NoPsa)
     }
 }
