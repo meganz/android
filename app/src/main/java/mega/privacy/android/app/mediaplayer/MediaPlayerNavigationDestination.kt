cf3eb2e004d13087b2aa7b2766e247f36a1f3bb9
Hai Luong <lh@mega.co.nz>
2025-12-01 06:47:47 +0700

    AND-21888: Can't preview audio and video from new CD

--- app/src/main/java/mega/privacy/android/app/mediaplayer/MediaPlayerNavigationDestination.kt
+++ app/src/main/java/mega/privacy/android/app/mediaplayer/MediaPlayerNavigationDestination.kt
@@ -5,14 +5,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import mega.privacy.android.app.nav.MediaPlayerIntentMapper
-import mega.privacy.android.core.nodecomponents.mapper.NodeContentUriIntentMapper
-import mega.privacy.android.navigation.contract.FeatureDestination
-import mega.privacy.android.navigation.contract.NavigationHandler
-import mega.privacy.android.navigation.contract.TransferHandler
 import mega.privacy.android.navigation.contract.transparent.transparentMetadata
 import mega.privacy.android.navigation.destination.LegacyMediaPlayerNavKey
-import mega.privacy.android.navigation.destination.LegacyPdfViewerNavKey
-import javax.inject.Inject
 
 fun EntryProviderScope<NavKey>.legacyMediaPlayerScreen(
     removeDestination: () -> Unit,
@@ -32,6 +26,7 @@ fun EntryProviderScope<NavKey>.legacyMediaPlayerScreen(
                 handle = key.fileHandle,
                 parentHandle = key.parentHandle,
                 isFolderLink = key.isFolderLink,
+                viewType = key.nodeSourceType
             )
             context.startActivity(intent)
 
