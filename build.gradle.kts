8788b4121845d0420d1745ae6689afb5cf5e7fde
Hai Luong <lh@mega.co.nz>
2025-12-10 11:26:00 +0700

    Update App version(15.24)

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.23"
+extra["appVersion"] = "15.24"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
