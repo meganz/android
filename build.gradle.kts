7deadc796aafee093de8db2491dd679925bb36fd
Vincent Ardyan Putra <vap@mega.co.nz>
2026-01-22 16:18:36 +1300

    Pre-release - v15.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.25"
+extra["appVersion"] = "15.26"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260120.110306-dev"
+extra["megaSdkVersion"] = "20260122.022722-rel"
 
 
 //JDK and Java Version
