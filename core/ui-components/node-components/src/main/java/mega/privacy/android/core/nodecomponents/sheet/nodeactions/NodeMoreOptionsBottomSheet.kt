45ba6e4792531946eabece34f3afe33308d195d4
Kevin Gozali <kg@mega.co.nz>
2026-02-05 11:43:03 +0700

    SAO-3158 Fix BackHandler should dismiss the multi selection bottom sheet

--- core/ui-components/node-components/src/main/java/mega/privacy/android/core/nodecomponents/sheet/nodeactions/NodeMoreOptionsBottomSheet.kt
+++ core/ui-components/node-components/src/main/java/mega/privacy/android/core/nodecomponents/sheet/nodeactions/NodeMoreOptionsBottomSheet.kt
@@ -2,20 +2,24 @@
 
 package mega.privacy.android.core.nodecomponents.sheet.nodeactions
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import mega.android.core.ui.components.image.MegaIcon
 import mega.android.core.ui.components.list.FlexibleLineListItem
 import mega.android.core.ui.components.sheets.MegaModalBottomSheet
@@ -42,12 +46,22 @@ internal fun NodeMoreOptionsBottomSheet(
     onActionPressed: ((MenuActionWithIcon) -> Unit)? = null,
     onHelpClicked: ((MenuActionWithIcon) -> Unit)? = null,
 ) {
+    val coroutineScope = rememberCoroutineScope()
+
     MegaModalBottomSheet(
         bottomSheetBackground = MegaModalBottomSheetBackground.Surface1,
         sheetState = sheetState,
         modifier = modifier,
         onDismissRequest = onDismissRequest
     ) {
+        BackHandler(sheetState.currentValue == SheetValue.Expanded) {
+            coroutineScope.launch {
+                sheetState.hide()
+            }.invokeOnCompletion {
+                onDismissRequest()
+            }
+        }
+
         LazyColumn {
             items(
                 items = actions,
