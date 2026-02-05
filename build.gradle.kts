a119a126f77eb33b2247c2ca627541fdbf021d70
Yenel <yr@mega.nz>
2026-02-05 18:38:24 +0100

    Update SDK prebuilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260204.131756-rel"
+extra["megaSdkVersion"] = "20260205.152345-rel"
 
 
 //JDK and Java Version
