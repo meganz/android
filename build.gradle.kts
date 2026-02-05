64d7c33a501923a9114b71f54efe34a22924fa26
mega-vap <vap@mega.co.nz>
2026-02-05 09:49:28 +0700

    Bump up to hotfix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "15.26"
+extra["appVersion"] = "15.26.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
