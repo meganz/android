4fb69b9b6db01efb6ff35883c42c20570f03252a
Bhavna Thacker <bt@mega.co.nz>
2026-02-13 09:36:11 +0530

    AND-22591 Remove unused AvatarMapper

--- app/src/main/java/mega/privacy/android/app/di/avatar/AvatarMapperModule.kt
+++ app/src/main/java/mega/privacy/android/app/di/avatar/AvatarMapperModule.kt
@@ -1,16 +1,11 @@
 package mega.privacy.android.app.di.avatar
 
-import android.graphics.Bitmap
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import mega.privacy.android.app.presentation.avatar.mapper.AvatarContentMapper
 import mega.privacy.android.app.presentation.avatar.mapper.AvatarContentMapperImpl
-import mega.privacy.android.app.presentation.avatar.mapper.AvatarMapper
-import mega.privacy.android.app.utils.AvatarUtil
-import mega.privacy.android.app.utils.Constants.AVATAR_SIZE
 
 /**
  * DI to provide avatar mapper
@@ -24,19 +19,4 @@ abstract class AvatarMapperModule {
      */
     @Binds
     abstract fun bindAvatarContentMapper(implementation: AvatarContentMapperImpl): AvatarContentMapper
-
-    companion object {
-
-        /**
-         * Method to provide the avatar mapper
-         */
-        @Provides
-        fun provideAvatarMapper(): AvatarMapper = object : AvatarMapper {
-            override suspend fun getDefaultAvatar(
-                color: Int,
-                text: String,
-                isList: Boolean,
-            ): Bitmap = AvatarUtil.getDefaultAvatar(color, text, AVATAR_SIZE, isList)
-        }
-    }
 }
