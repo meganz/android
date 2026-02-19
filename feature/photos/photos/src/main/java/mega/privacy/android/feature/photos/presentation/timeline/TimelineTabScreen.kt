923180864e81fc414331dd425e0f9cf996a8cb6a
mega-vap <vap@mega.co.nz>
2026-02-19 14:32:27 +0700

    AND-21557/T21313982: Remove Unnecessary Legacy Width Requirement

--- feature/photos/photos/src/main/java/mega/privacy/android/feature/photos/presentation/timeline/TimelineTabScreen.kt
+++ feature/photos/photos/src/main/java/mega/privacy/android/feature/photos/presentation/timeline/TimelineTabScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidthIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
@@ -529,7 +528,6 @@ private fun PhotoModificationTimePeriodSelector(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp)
-                .requiredWidthIn(max = 360.dp)
                 .horizontalScroll(rememberScrollState()),
             horizontalArrangement = Arrangement.Center,
         ) {
