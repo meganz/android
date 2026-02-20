4be22270ef6d18483c5f3973aa803b318bbba4c9
Atiqur Rahman <atr@mega.co.nz>
2026-02-20 19:41:46 +0600

    T21317030,T21314340:Fix UI elements overlapping with navigation bar

--- feature/devicecenter/src/main/java/mega/privacy/android/feature/devicecenter/ui/lists/DeviceCenterListViewItem.kt
+++ feature/devicecenter/src/main/java/mega/privacy/android/feature/devicecenter/ui/lists/DeviceCenterListViewItem.kt
@@ -3,13 +3,11 @@ package mega.privacy.android.feature.devicecenter.ui.lists
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import mega.privacy.android.feature.devicecenter.R
 import mega.privacy.android.feature.devicecenter.ui.model.BackupDeviceFolderUINode
@@ -60,7 +58,6 @@ internal fun DeviceCenterListViewItem(
     ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
-            .height(72.dp)
     ) {
         val (item, divider) = createRefs()
 
