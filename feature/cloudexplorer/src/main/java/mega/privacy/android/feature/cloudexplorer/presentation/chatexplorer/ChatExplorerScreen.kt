5ddeb5211b3927db929c96ad81ce74aed51f20a1
Yenel <yr@mega.nz>
2026-03-03 14:51:54 +0100

    AND-22724 Remove MegaScaffold from explorer screens

--- feature/cloudexplorer/src/main/java/mega/privacy/android/feature/cloudexplorer/presentation/chatexplorer/ChatExplorerScreen.kt
+++ feature/cloudexplorer/src/main/java/mega/privacy/android/feature/cloudexplorer/presentation/chatexplorer/ChatExplorerScreen.kt
@@ -1,23 +1,12 @@
 package mega.privacy.android.feature.cloudexplorer.presentation.chatexplorer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import mega.android.core.ui.components.MegaScaffold
-import mega.android.core.ui.components.MegaText
 import mega.android.core.ui.preview.CombinedThemePreviews
 import mega.android.core.ui.theme.AndroidThemeForPreviews
 
 @Composable
 fun ChatExplorerScreen() {
-    MegaScaffold {
-        Box(modifier = Modifier.fillMaxSize().padding(it)){
-            MegaText(text = "Chat Explorer Screen", modifier = Modifier.align(Alignment.Center))
-        }
-    }
+
 }
 
 @Composable
