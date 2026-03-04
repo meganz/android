21ec2aa623a0078479a7722e92ac2ba8151e8f21
Daniel Oosthuizen <do@mega.co.nz>
2026-03-05 12:10:04 +1300

    Pre-release - v16.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "16.0"
+extra["appVersion"] = "16.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -99,7 +99,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260220.063414-dev"
+extra["megaSdkVersion"] = "20260304.214439-rel"
 
 
 //JDK and Java Version
