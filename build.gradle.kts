1801451b03355dab4f4e3ca973f9855d30a3cfae
Yenel <yr@mega.nz>
2026-03-11 11:23:38 +0100

    Increase app version

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "16.0"
+extra["appVersion"] = "16.0.1"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
