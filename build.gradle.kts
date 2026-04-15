c55967fb0030b5aec031c5f442e71d1f3e1dd393
Kevin Sun <ks@mega.co.nz>
2026-04-15 22:28:46 +1200

    Pre-release - v16.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "16.3"
+extra["appVersion"] = "16.4"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 36
@@ -101,7 +101,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260414.015256-dev"
+extra["megaSdkVersion"] = "20260415.095506-rel"
 
 
 //JDK and Java Version
