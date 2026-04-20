47f92571d5f4a2948d2a7428517b0092dfbf40e9
Kevin Sun <ks@mega.co.nz>
2026-04-20 10:37:59 +0800

    Update MegaChat version

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260417.015208-rel"
+extra["megaSdkVersion"] = "20260420.013535-rel"
 
 
 //JDK and Java Version
