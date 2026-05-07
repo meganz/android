b47fd43fc1d9d979eed66ad06eba12d769656731
HM Tamim <ht@mega.co.nz>
2026-05-07 11:06:36 +0600

    AND-23384 Suppress UMP consent in upgrade screen

--- feature/payment/payment/src/main/java/mega/privacy/android/feature/payment/navigation/UpgradeScreenDestination.kt
+++ feature/payment/payment/src/main/java/mega/privacy/android/feature/payment/navigation/UpgradeScreenDestination.kt
@@ -4,12 +4,18 @@ import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import mega.privacy.android.feature.payment.presentation.upgrade.UpgradeAccountRoute
 import mega.privacy.android.navigation.contract.NavigationHandler
+import mega.privacy.android.navigation.contract.metadata.buildMetadata
+import mega.privacy.android.navigation.contract.suppression.withOverlaySuppression
 import mega.privacy.android.navigation.destination.UpgradeAccountNavKey
 
 fun EntryProviderScope<NavKey>.upgradeScreens(
     navigationHandler: NavigationHandler,
 ) {
-    entry<UpgradeAccountNavKey> { key ->
+    entry<UpgradeAccountNavKey>(
+        metadata = buildMetadata {
+            withOverlaySuppression()
+        }
+    ) { key ->
         UpgradeAccountRoute(
             isNewCreationAccount = key.isNewAccount,
             isUpgradeAccount = key.isUpgrade,
