0171eef718a78197d0bd3b534dd1900b657ba63f
Kevin Gozali <kg@mega.co.nz>
2026-05-17 16:59:22 -0700

    AND-21218 / T21364328 Adjust widget default reordering

---
+++ core/navigation-contract/src/test/java/mega/privacy/android/navigation/contract/home/HomeWidgetOrderTest.kt
@@ -0,0 +1,19 @@
+package mega.privacy.android.navigation.contract.home
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class HomeWidgetOrderTest {
+
+    @Test
+    fun `test that HomeWidgetOrder entries are declared in the expected default order`() {
+        assertThat(HomeWidgetOrder.entries).containsExactly(
+            HomeWidgetOrder.Shortcuts,
+            HomeWidgetOrder.Banner,
+            HomeWidgetOrder.MyAccount,
+            HomeWidgetOrder.Recents,
+            HomeWidgetOrder.ViewedLinks,
+            HomeWidgetOrder.ContinueWhereLeftOff,
+        ).inOrder()
+    }
+}
