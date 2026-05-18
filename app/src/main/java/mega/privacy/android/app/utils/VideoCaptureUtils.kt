efdbdc68cc7d730af5b75d6f5367849bb147a83f
Daniel Oosthuizen <do@mega.co.nz>
2026-05-19 09:29:04 +1200

    AND-23605 Convert VideoCaptureUtils.java to Kotlin

---
+++ app/src/main/java/mega/privacy/android/app/utils/VideoCaptureUtils.kt
@@ -0,0 +1,253 @@
+package mega.privacy.android.app.utils
+
+import androidx.annotation.Keep
+import mega.privacy.android.app.MegaApplication
+import mega.privacy.android.app.listeners.ChatChangeVideoStreamListener
+import org.webrtc.Camera1Enumerator
+import org.webrtc.CameraEnumerator
+import org.webrtc.CapturerObserver
+import org.webrtc.SurfaceTextureHelper
+import org.webrtc.VideoCapturer
+import timber.log.Timber
+
+/**
+ * The class can call from JNI to manage the video capture devices.
+ * Don't move package name and change class name
+ */
+@Keep
+object VideoCaptureUtils {
+
+    private var videoCapturer: VideoCapturer? = null
+
+    /**
+     * Indicates if show video is allowed. The default value is TRUE, but this value will change to
+     * FALSE meanwhile swapping between front and back cameras. The value will be TRUE again once
+     * the camera swapping is completed.
+     */
+    private var isVideoAllowed = true
+
+    /**
+     * Check if show video is allowed.
+     *
+     * @return TRUE if show video is allowed or FALSE in other case.
+     * @see VideoCaptureUtils.isVideoAllowed
+     */
+    @JvmStatic
+    fun isVideoAllowed(): Boolean = isVideoAllowed
+
+    /**
+     * Set if show video is allowed.
+     *
+     * @param isVideoAllowed Value to indicate if show video is allowed.
+     * @see VideoCaptureUtils.isVideoAllowed
+     */
+    @JvmStatic
+    fun setIsVideoAllowed(isVideoAllowed: Boolean) {
+        try {
+            VideoCaptureUtils.isVideoAllowed = isVideoAllowed
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+    }
+
+    private fun createCameraCapturer(
+        enumerator: CameraEnumerator,
+        deviceName: String?,
+    ): VideoCapturer? {
+        Timber.d("createCameraCapturer: %s", deviceName)
+        try {
+            return enumerator.createCapturer(deviceName, null)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return null
+    }
+
+    /**
+     * Get the video capture devices list.
+     *
+     * @return The video capture devices list.
+     */
+    private fun deviceList(): Array<String> {
+        Timber.d("DeviceList")
+        try {
+            val enumerator: CameraEnumerator = Camera1Enumerator(true)
+            return enumerator.deviceNames
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return emptyArray()
+    }
+
+    /**
+     * Swap the current camera device to the opposite camera device.
+     *
+     * @param listener Camera swap listener.
+     */
+    @JvmStatic
+    fun swapCamera(listener: ChatChangeVideoStreamListener?) {
+        try {
+            val megaChatApi = MegaApplication.getInstance().megaChatApi
+            val currentCamera = megaChatApi.videoDeviceSelected
+            val newCamera = if (isFrontCamera(currentCamera)) {
+                getBackCamera()
+            } else {
+                getFrontCamera()
+            }
+            if (newCamera != null) {
+                isVideoAllowed = false
+                megaChatApi.setChatVideoInDevice(newCamera, listener)
+            }
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+    }
+
+    /**
+     * Get the front camera device.
+     *
+     * @return Front camera device.
+     */
+    @JvmStatic
+    fun getFrontCamera(): String? {
+        try {
+            return getCameraDevice(true)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return null
+    }
+
+    /**
+     * Get the back camera device.
+     *
+     * @return Back camera device.
+     */
+    @JvmStatic
+    fun getBackCamera(): String? {
+        try {
+            return getCameraDevice(false)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return null
+    }
+
+    /**
+     * Get a camera device (front or back).
+     *
+     * @param front Value to indicate the camera device to get (true: front / false: back).
+     * @return The camera device (front or back) requested. NULL if the requested device does not exist.
+     */
+    private fun getCameraDevice(front: Boolean): String? {
+        try {
+            val enumerator: CameraEnumerator = Camera1Enumerator(true)
+            val deviceList = deviceList()
+            for (device in deviceList) {
+                if ((front && enumerator.isFrontFacing(device)) || (!front && enumerator.isBackFacing(device))) {
+                    return device
+                }
+            }
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return null
+    }
+
+    /**
+     * Check if the camera device is the front camera.
+     *
+     * @param device Camera device to check.
+     * @return True if device is front camera or false in other case.
+     */
+    @JvmStatic
+    fun isFrontCamera(device: String?): Boolean {
+        try {
+            val enumerator: CameraEnumerator = Camera1Enumerator(true)
+            return enumerator.isFrontFacing(device)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return false
+    }
+
+    /**
+     * Check if the camera device is the back camera.
+     *
+     * @param device Camera device to check.
+     * @return True if device is back camera or false in other case.
+     */
+    @JvmStatic
+    fun isBackCamera(device: String?): Boolean {
+        try {
+            val enumerator: CameraEnumerator = Camera1Enumerator(true)
+            return enumerator.isBackFacing(device)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return false
+    }
+
+    /**
+     * Check if the front camera is the current video device in use.
+     *
+     * @return True if the front camera is in use or false in other case.
+     */
+    @JvmStatic
+    fun isFrontCameraInUse(): Boolean {
+        try {
+            val megaChatApi = MegaApplication.getInstance().megaChatApi
+            val deviceName = megaChatApi.videoDeviceSelected ?: return false
+            return isFrontCamera(deviceName)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        return false
+    }
+
+    @JvmStatic
+    fun stopVideoCapture() {
+        Timber.d("stopVideoCapture")
+        videoCapturer?.let {
+            try {
+                it.stopCapture()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+            videoCapturer = null
+        }
+    }
+
+    @JvmStatic
+    fun startVideoCapture(
+        videoWidth: Int,
+        videoHeight: Int,
+        videoFps: Int,
+        surfaceTextureHelper: SurfaceTextureHelper?,
+        nativeAndroidVideoTrackSource: CapturerObserver?,
+        deviceName: String?,
+    ) {
+        Timber.d("startVideoCapture: %s", deviceName)
+
+        stopVideoCapture()
+
+        try {
+            val context = MegaApplication.getInstance().applicationContext
+            val capturer = createCameraCapturer(Camera1Enumerator(true), deviceName)
+            videoCapturer = capturer
+
+            if (capturer == null) {
+                Timber.e("Unable to create video capturer")
+                return
+            }
+
+            capturer.initialize(surfaceTextureHelper, context, nativeAndroidVideoTrackSource)
+
+            // Start the capture!
+            capturer.startCapture(videoWidth, videoHeight, videoFps)
+            Timber.d("Start Capture")
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+    }
+}
