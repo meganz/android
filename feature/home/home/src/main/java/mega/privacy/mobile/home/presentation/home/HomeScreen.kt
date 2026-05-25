74c881ea8d6009844b2e995f7b30b89fbfd0d989
Robin Shi <rsh@mega.co.nz>
2026-05-25 12:30:14 +1200

    fix compile error

--- feature/home/home/src/main/java/mega/privacy/mobile/home/presentation/home/HomeScreen.kt
+++ feature/home/home/src/main/java/mega/privacy/mobile/home/presentation/home/HomeScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
 import mega.android.core.ui.components.LocalSnackBarHostState
