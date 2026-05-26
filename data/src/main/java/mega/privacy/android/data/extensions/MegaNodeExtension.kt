b71fdb243edfd3383fa4524ce62206ad61dee179
Kevin Gozali <kg@mega.co.nz>
2026-05-26 04:42:29 -0700

    AND-23699 [Crash] NullPointerException in MegaNodeExtensionKt.getFileName

--- data/src/main/java/mega/privacy/android/data/extensions/MegaNodeExtension.kt
+++ data/src/main/java/mega/privacy/android/data/extensions/MegaNodeExtension.kt
@@ -1,29 +1,21 @@
 package mega.privacy.android.data.extensions
 
 import mega.privacy.android.data.constant.FileConstant.JPG_EXTENSION
-import mega.privacy.android.data.model.MimeTypeList
 import nz.mega.sdk.MegaNode
 
 /**
  * Get Thumbnail Image File Name
  */
-fun MegaNode.getThumbnailFileName(): String = "$base64Handle"
+fun MegaNode?.getThumbnailFileName(): String = this?.base64Handle.orEmpty()
 
 /**
  * Get Preview Image File Name
  */
-fun MegaNode.getPreviewFileName(): String =
-    "$base64Handle${JPG_EXTENSION}"
+fun MegaNode?.getPreviewFileName(): String =
+    this?.base64Handle?.let { "$it$JPG_EXTENSION" } ?: ""
 
 /**
  * Get Full Image File Name
  */
-fun MegaNode.getFileName(): String =
-    "$base64Handle.${name.substringAfterLast(".", "")}"
-
-/**
- * Check if Node is a Video
- */
-fun MegaNode.isVideo() =
-    this.isFile && (MimeTypeList.typeForName(name).isVideoMimeType ||
-            MimeTypeList.typeForName(name).isMp4Video)
+fun MegaNode?.getFileName(): String =
+    "${this?.base64Handle}.${this?.name?.substringAfterLast(".", "")}"
