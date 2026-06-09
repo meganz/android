fa9705cdf5201bde02fb1bb2b7e2fdf224134bba
FengLu <fl@mega.co.nz>
2026-06-09 17:20:09 +0800

    Bump sdk to rc2: 20260609.082937-rel

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "36.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260529.115336-rel"
+extra["megaSdkVersion"] = "20260609.082937-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
