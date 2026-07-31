20860cb0fcce74a7ff74c2b313012a2b7f3c4eb6
HM Tamim <ht@mega.co.nz>
2026-07-31 14:36:15 +0600

    Update SDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "37.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260723.092248-rel"
+extra["megaSdkVersion"] = "20260731.083049-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
