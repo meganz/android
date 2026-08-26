f8d30f0bc6436dbf358eb0cfd011a57f71499b28
Atiqur Rahman <atr@mega.co.nz>
2026-08-26 15:27:22 +0600

    Pre-release - v16.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ tasks.register("clean", Delete::class) {
 
 // Define versions in a single place
 // App
-extra["appVersion"] = "16.12"
+extra["appVersion"] = "16.13"
 
 // Sdk and tools
 extra["compileSdkVersion"] = 37
@@ -101,7 +101,7 @@ extra["targetSdkVersion"] = 36
 extra["buildTools"] = "37.0.0"
 
 // Prebuilt MEGA SDK version
-extra["megaSdkVersion"] = "20260825.115900-dev"
+extra["megaSdkVersion"] = "20260826.085232-rel"
 
 //JDK and Java Version
 extra["jdk"] = "21"
